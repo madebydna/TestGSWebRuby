f9f23dfb889f4354817f20752cdba3b7ccd4ab6b
Samson Sprouse <samson.sprouse@gmail.com>
2013-09-23 18:50:05 -0700

    Cleanup: remove unused PageConfig model

--- app/models/page_config.rb
+++
@@ -1,2 +0,0 @@
-class PageConfig < ActiveRecord::Base
-end
--- db/schema.rb
+++ db/schema.rb
@@ -85,13 +85,6 @@ ActiveRecord::Schema.define(:version => 20130918042153) do
     t.datetime "updated_at", :null => false
   end
 
-  create_table "page_configs", :force => true do |t|
-    t.string   "key"
-    t.string   "value"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
-  end
-
   create_table "pages", :force => true do |t|
     t.string   "name"
     t.string   "parent_id"
@@ -125,6 +118,15 @@ ActiveRecord::Schema.define(:version => 20130918042153) do
     t.string   "name"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+  end
+
+  create_table "school_category_data", :force => true do |t|
+    t.string   "key"
+    t.integer  "school_id"
+    t.text     "school_data"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
+    t.string   "state"
   end
 
   create_table "school_collections", :force => true do |t|
