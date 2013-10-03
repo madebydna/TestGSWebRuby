0b9bacfac499de1aba74b9c87d8afceb4cdbfb10
Mitchell Seltzer <mseltzer@greatschools.org>
2013-10-03 13:58:25 -0700

    GS-14808 - filtered out on status (only show a,p) also if quality is "decline"

--- app/models/school_rating.rb
+++ app/models/school_rating.rb
@@ -3,22 +3,11 @@ class SchoolRating < ActiveRecord::Base
 
   self.table_name='school_rating'
 
-  #scope :recent, order("posted DESC")
-  #scope :oldest, order("posted ASC")
-  #scope :rating_top, order("quality DESC")
-  #scope :rating_bottom, order("quality ASC")
-  scope :review_selection_filter, lambda { |show_by_group| where(:who => show_by_group)  unless show_by_group == 'all' || show_by_group.empty? }
-  scope :review_limit_number, lambda { |limit_number| limit(limit_number)  unless limit_number.to_s.empty? }
-  scope :review_offset_number, lambda { |offset_start| offset(offset_start)  unless offset_start.to_s.empty? }
-
-  def self.fetch_reviews(school, group_to_fetch, order_results_by, offset_start, quantity_to_return)
-    # TODO: restrict reviews to correct statuses
-    SchoolRating.where(school_id: school.id, state: school.state)
-      .review_selection_filter(group_to_fetch)
-      .order_by_selection(order_results_by)
-      .review_limit_number(quantity_to_return)
-      .review_offset_number(offset_start)
-  end
+  scope :selection_filter, lambda { |show_by_group| where(:who => show_by_group)  unless show_by_group == 'all' || show_by_group.empty? }
+  scope :limit_number, lambda { |limit_number| limit(limit_number)  unless limit_number.to_s.empty? }
+  scope :offset_number, lambda { |offset_start| offset(offset_start)  unless offset_start.to_s.empty? }
+  scope :published, where(:status => ['a', 'p'])
+  scope :quality_decline, where("quality != 'decline'")
 
   def self.order_by_selection(order_selection)
     case order_selection
@@ -31,5 +20,15 @@ class SchoolRating < ActiveRecord::Base
       else
         order("posted DESC")
     end
+  end
+
+  def self.fetch_reviews(school, group_to_fetch, order_results_by, offset_start, quantity_to_return)
+    SchoolRating.where(school_id: school.id, state: school.state)
+      .selection_filter(group_to_fetch)
+      .order_by_selection(order_results_by)
+      .limit_number(quantity_to_return)
+      .offset_number(offset_start)
+      .published
+      .quality_decline
   end
 end
