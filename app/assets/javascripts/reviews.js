3057e7a5b027ba75c4470d1d250e44860cc2b5ca
Mitchell Seltzer <mseltzer@greatschools.org>
2013-10-09 09:38:45 -0700

    GS-14821 - a bit of a reorg - still have more to do

--- app/assets/javascripts/reviews.js
+++ app/assets/javascripts/reviews.js
@@ -1,72 +1,87 @@
-$(function () {
-    $(".js_reviewsGetNextTen").on("click", function(){
-        $(this).addClass("dn");
-        var offset = $(this).data( "offset" );
-        var limit = $(this).data( "limit" );
-        var totalCount = $(this).data( "total-count" );
-        var filter_by = $(".js_reviewFilterButton").data( "group-selected" );
-        var order_by = $(".js_reviewFilterDropDown").data( "order-selected" );
-        callAjax(offset, limit, totalCount, filter_by, order_by);
-    });
+var GS = GS || {};
+GS.reviews = GS.reviews || function($) {
+    var initializeReviewHandlers = function () {
+        // the next ten button
+        var nextTenButton = $(".js_reviewsGetNextTen");
+        var filterByGroup = $(".js_reviewFilterButton");
+        var selectOrderDropDown = $(".js_reviewFilterDropDown");
+        var selectOrderDropDownText = $(".js_reviewFilterDropDownText");
+        var reviewContentLayer = $(".js_reviewsList")
 
-    $(".js_reviewFilterButton").on("click", "button", function(){
-        if($(".js_reviewFilterButton").data( "group-selected"  ) != $(this).data( "group-name" )){
-            $(".js_reviewsGetNextTen").addClass("dn");
-            var offset = 0;
-            var limit = $(".js_reviewsGetNextTen").data( "limit" );
-            var filter_by_group = $(this).data( "group-name" );
-            var totalCount = $(".js_reviewsGetNextTen").data( "all-count" );
-            if(filter_by_group == "parent"){
-                totalCount = $(".js_reviewsGetNextTen").data( "parent-count" );
+        nextTenButton.on("click", function(){
+            $(this).addClass("dn");
+            var offset = $(this).data( "offset" );
+            var limit = $(this).data( "limit" );
+            var totalCount = $(this).data( "total-count" );
+            var filter_by = filterByGroup.data( "group-selected" );
+            var order_by = selectOrderDropDown.data( "order-selected" );
+            callReviewsAjax(offset, limit, totalCount, filter_by, order_by);
+        });
+
+        filterByGroup.on("click", "button", function(){
+            if(filterByGroup.data( "group-selected"  ) != $(this).data( "group-name" )){
+                nextTenButton.addClass("dn");
+                var offset = 0;
+                var limit = nextTenButton.data( "limit" );
+                var filter_by_group = $(this).data( "group-name" );
+                var totalCount = nextTenButton.data( "all-count" );
+                if(filter_by_group == "parent"){
+                    totalCount = nextTenButton.data( "parent-count" );
+                }
+                else{
+                   if(filter_by_group == "student"){
+                       totalCount = nextTenButton.data( "student-count" );
+                   }
+                }
+                nextTenButton.data( "total-count", totalCount );
+                reviewContentLayer.html('');
+                $(this).siblings().removeClass("active");
+                $(this).addClass("active");
+                filterByGroup.data( "group-selected", filter_by_group);
+                var order_by = selectOrderDropDown.data( "order-selected" );
+                callReviewsAjax(offset, limit, totalCount, filter_by_group, order_by);
             }
-            else{
-               if(filter_by_group == "student"){
-                   totalCount = $(".js_reviewsGetNextTen").data( "student-count" );
-               }
+        });
+        selectOrderDropDown.on("click", "a", function(){
+            nextTenButton.addClass("dn");
+            var order_by = $(this).data( "order-review" );
+
+            if(selectOrderDropDown.data( "order-selected"  ) != order_by){
+                selectOrderDropDown.data( "order-selected", order_by);
+                selectOrderDropDownText.html($(this).html()+' <b class="caret"></b>');
+                var offset = 0;
+                var limit = nextTenButton.data( "limit" );
+                var totalCount = nextTenButton.data( "total-count" );
+                var filter_by = filterByGroup.data( "group-selected" );
+                reviewContentLayer.html('');
+                callReviewsAjax(offset, limit, totalCount, filter_by, order_by);
             }
-            $(".js_reviewsGetNextTen").data( "total-count", totalCount );
-            $(".js_reviewsList").html('');
-            $(this).siblings().removeClass("active");
-            $(this).addClass("active");
-            $(".js_reviewFilterButton").data( "group-selected", filter_by_group);
-            var order_by = $(".js_reviewFilterDropDown").data( "order-selected" );
-            callAjax(offset, limit, totalCount, filter_by_group, order_by);
-        }
-    });
-    $(".js_reviewFilterDropDown").on("click", "a", function(){
-        $(".js_reviewsGetNextTen").addClass("dn");
-        var storeSelectedObj = $(".js_reviewFilterDropDown");
-        var selectedText = $(".js_reviewFilterDropDownText");
-        var order_by = $(this).data( "order-review" );
+        });
+        var callReviewsAjax = function(offset, limit, totalCount, filter_by, order_by){
+            jQuery.ajax({
+                type:'GET',
+                url:"/ajax/reviews_pagination",
+                data:{offset:offset, limit:limit, filter_by:filter_by, order_by:order_by},
+                dataType:'text',
+                async:true
+            }).done(function (html) {
+                reviewContentLayer.append(html);
+            }.gs_bind(this));
 
-        if(storeSelectedObj.data( "order-selected"  ) != order_by){
-            storeSelectedObj.data( "order-selected", order_by);
-            selectedText.html($(this).html()+' <b class="caret"></b>');
-            var offset = 0;
-            var limit = $(".js_reviewsGetNextTen").data( "limit" );
-            var totalCount = $(".js_reviewsGetNextTen").data( "total-count" );
-            var filter_by = $(".js_reviewFilterButton").data( "group-selected" );
-            $(".js_reviewsList").html('');
-            callAjax(offset, limit, totalCount, filter_by, order_by);
-        }
-    });
-    var callAjax = function(offset, limit, totalCount, filter_by, order_by){
-//        console.log(offset+":"+limit+":"+totalCount+":"+filter_by+":"+order_by);
-        jQuery.ajax({
-            type:'GET',
-            url:"/ajax/reviews_pagination",
-            data:{offset:offset, limit:limit, filter_by:filter_by, order_by:order_by},
-            dataType:'text',
-            async:true
-        }).done(function (html) {
-            $(".js_reviewsList").append(html);
-        }.gs_bind(this));
+            var new_offset = offset+limit;
+            nextTenButton.data( "offset", new_offset );
+            if(totalCount > new_offset){
+                nextTenButton.removeClass("dn");
+            }
+        };
 
-        var new_offset = offset+limit;
-        $(".js_reviewsGetNextTen").data( "offset", new_offset );
-        if(totalCount > new_offset){
-            $(".js_reviewsGetNextTen").removeClass("dn");
-        }
     };
+    return {
+        initializeReviewHandlers: initializeReviewHandlers
+    }
+}(jQuery);
 
+$(function () {
+    GS.reviews.initializeReviewHandlers();
 });
+
