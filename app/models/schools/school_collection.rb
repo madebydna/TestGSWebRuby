afec30d0ab6672689cb045e6d055728a0978bfc3
Mitchell Seltzer <mseltzer@greatschools.org>
2013-10-09 13:55:28 -0700

    GS-14821 - reorg model

--- app/models/schools/school_collection.rb
+++ app/models/schools/school_collection.rb
@@ -1,6 +1,6 @@
 require 'states'
 class SchoolCollection < ActiveRecord::Base
-  attr_accessible :collection, :school, :collection_id, :school_id, :state
+  attr_accessible :collection, :schools, :collection_id, :school_id, :state
   has_paper_trail
 
   belongs_to :school
@@ -16,7 +16,6 @@ class SchoolCollection < ActiveRecord::Base
     shard = state
     shard = 'CA' if shard.nil? || shard == ''
     shard = shard.upcase.to_sym
-    #School.using(shard).find school_id
     School.using(shard).find school_id
   end
 
