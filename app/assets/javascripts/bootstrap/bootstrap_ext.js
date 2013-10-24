0895cf33b314bb4e11b81b83eeac4c943507e573
Mitchell Seltzer <mseltzer@greatschools.org>
2013-10-23 17:24:38 -0700

    GS-14821 - global button group select - small change

--- app/assets/javascripts/bootstrap/bootstrap_ext.js
+++ app/assets/javascripts/bootstrap/bootstrap_ext.js
@@ -3,6 +3,7 @@ GS.bootstrap = GS.bootstrap || function($) {
     var initializeBootstrapExtensions = function () {
         initBootstrapButtonSelect();
     };
+    // use class js_bootstrapExtButtonSelect on any button group to allow selection to stick
     var initBootstrapButtonSelect = function () {
         $( ".js_bootstrapExtButtonSelect").on("click", "button", function( index ) {
             $(this).siblings().removeClass("active");
