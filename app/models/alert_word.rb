88643f8b1c2d561783475be85df516d76935dd73
Samson Sprouse <samson.sprouse@gmail.com>
2013-12-09 20:01:29 -0800

    OM-88 Added AlertWord model and spec

---
+++ app/models/alert_word.rb
@@ -0,0 +1,48 @@
+class AlertWord < ActiveRecord::Base
+  db_magic :connection => :community
+
+  class AlertWordSearchResult < Struct.new(:alert_words, :really_bad_words)
+    def any?
+      alert_words.any? || really_bad_words.any?
+    end
+
+    def has_alert_words?
+      alert_words.any?
+    end
+
+    def has_really_bad_words?
+      really_bad_words.any?
+    end
+  end
+
+  def really_bad
+    read_attribute(:really_bad) == "\x01" ? true : false
+  end
+
+  def really_bad?
+    really_bad
+  end
+
+  def self.all_words
+    Rails.cache.fetch('alert_word/all_words', expires_in: 1.hour) do
+      all_rows = on_db(:community).all
+      really_bad_words = all_rows.select { |word| word.really_bad? }.map(&:word)
+      alert_words = all_rows.select{ |word| word.really_bad? == false }.map(&:word)
+      [ alert_words, really_bad_words ]
+    end
+  end
+
+  def self.search(text)
+    alert_words, really_bad_words = all_words
+    string = text.squeeze(' ')
+
+    matched_alert_words = alert_words.select { |word| string =~ /.*\b(#{Regexp.quote(word)})\b.*/i }
+    matched_really_bad_words = really_bad_words.select { |word| string =~ /.*\b(#{Regexp.quote(word)})\b.*/i }
+
+    AlertWordSearchResult.new(
+      matched_alert_words,
+      matched_really_bad_words
+    )
+  end
+
+end
---
+++ spec/models/alert_word_spec.rb
@@ -0,0 +1,79 @@
+require 'spec_helper'
+
+describe AlertWord do
+
+  describe '.search' do
+    before do
+      AlertWord.create!( word: 'test_really_bad_word', really_bad: true )
+      AlertWord.create!( word: 'test_alert_word', really_bad: false )
+      AlertWord.create!( word: 'test alert phrase', really_bad: false )
+      AlertWord.create!( word: 'test-hyphen', really_bad: false )
+      AlertWord.create!( word: 'test evil apostrophe\'s', really_bad: false )
+      AlertWord.create!( word: 'test *other \\symbols', really_bad: false )
+      AlertWord.create!( word: '@', really_bad: false )
+    end
+
+    it 'should match really bad words' do
+      expect(AlertWord.search('test_really_bad_word')).to have_really_bad_words
+    end
+
+    it 'should find an alert word when there\'s an exact match' do
+      expect(AlertWord.search('test_alert_word')).to have_alert_words
+    end
+
+    it 'should treat words as case-insensitive' do
+      expect(AlertWord.search('TEST_REALLY_BAD_word')).to have_really_bad_words
+    end
+
+    it 'should be able to find full alert phrases' do
+      expect(AlertWord.search('find the test alert phrase').alert_words).to include('test alert phrase')
+    end
+
+    it 'should not mistake a word that\'s part of a phrase' do
+      expect(AlertWord.search('phrase should not be found').alert_words).to_not include('test alert phrase')
+    end
+
+    it 'should match words adjacent to punctuation' do
+      expect(AlertWord.search('test_really_bad_word?')).to have_really_bad_words
+      expect(AlertWord.search('a test_really_bad_word? b')).to have_really_bad_words
+      expect(AlertWord.search('a --test_really_bad_word? b')).to have_really_bad_words
+      expect(AlertWord.search('--test_really_bad_word? b')).to have_really_bad_words
+      expect(AlertWord.search('--test_really_bad_word?')).to have_really_bad_words
+      expect(AlertWord.search('--test_really_bad_word')).to have_really_bad_words
+    end
+
+    it 'should match a hyphenated word' do
+      expect(AlertWord.search('test-hyphen')).to have_alert_words
+      expect(AlertWord.search('a test-hyphen b')).to have_alert_words
+    end
+
+    it 'should match words with apostrophes' do
+      expect(AlertWord.search('test evil apostrophe\'s')).to have_alert_words
+      expect(AlertWord.search('apostrophe\'s')).to_not have_alert_words
+    end
+
+    it 'should match words with backslashes and asterisks' do
+      expect(AlertWord.search('test *other \\symbols')).to have_alert_words
+      expect(AlertWord.search('other')).to_not have_alert_words
+      expect(AlertWord.search('symbols')).to_not have_alert_words
+    end
+
+    it 'should compress whitespace' do
+      expect(AlertWord.search(' test_really_bad_word  ')).to have_really_bad_words
+      expect(AlertWord.search(' test  alert  phrase ')).to have_alert_words
+    end
+
+    describe 'should handle @ signs just like java code' do
+      it 'should not match an @ sign within a \'word\' of symbols' do
+        expect(AlertWord.search('*@#* you')).to_not have_alert_words
+      end
+      it 'should match an @ sign joining two words' do
+        expect(AlertWord.search('blah@example.com')).to have_alert_words
+      end
+    end
+
+  end
+
+
+
+end
