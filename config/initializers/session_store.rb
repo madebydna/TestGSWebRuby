40e2d841da804bd8e3ef85c50a3bc7748ccd85f5
Samson Sprouse <ssprouse@greatschools.org>
2013-12-11 00:05:23 -0800

    OM-117 Configure domain on session cookie differently in development/production environments

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,8 @@
 # Be sure to restart your server when you modify this file.
 
-LocalizedProfiles::Application.config.session_store :cookie_store, key: '_LocalizedProfiles_session', :domain => :all, :tld_length => 2
+# The configuration that would normally be here was moved to development and production.rb so that it can be
+# specified on a per-environment basis.
+#LocalizedProfiles::Application.config.session_store :cookie_store, key: '_LocalizedProfiles_session', :domain => :all, :tld_length => 2
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
