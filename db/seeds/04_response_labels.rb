fdd200a7b48f7d7cf582e44b162a2cba52381697
Samson Sprouse <ssprouse@greatschools.org>
2013-12-13 16:40:31 -0800

    OM-110 Add osp key-label and value-label files that Michael created, import them in a seeds file

---
+++ db/seeds/04_response_labels.rb
@@ -0,0 +1,27 @@
+key_file_name = File.join("#{Rails.root}/db/osp_key_label_map.txt")
+value_file_name = File.join("#{Rails.root}/db/osp_value_label_map.txt")
+
+[key_file_name, value_file_name].each do |file_name|
+  if File.exist?(file_name)
+    file = File.read(file_name)
+
+    file.each_line do |line|
+      fields = line.strip.split /\t/
+
+      if fields.length != 2
+        puts "Incorrect number of field values for: #{line}. Excepted 2 fields, got #{fields.length}."
+      else
+        begin
+          ResponseValue.create!(response_value: fields[0].strip, response_label: fields[1].strip)
+        rescue
+          puts "#{$!}  Could not insert new row for: #{line}"
+        end
+      end
+    end
+
+  else
+    puts "Could not find file: #{file_name}"
+  end
+end
+
+
