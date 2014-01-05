e1ad64af2202ded8a3d2c82c664c7833a7969573
Samson Sprouse <samson.sprouse@gmail.com>
2014-01-04 19:27:55 -0800

    OM-109 Monkey-patch db-charmer better, so that it's impossible to chain an association on a sharded model object, which would silently cause query to wrong db

--- app/models/concerns/state_sharding.rb
+++ app/models/concerns/state_sharding.rb
@@ -3,30 +3,50 @@ require 'active_support/concern'
 module StateSharding
   extend ActiveSupport::Concern
 
-  included do
-    db_magic :connection => :dc, slave: :dc
+=begin
+Example of sharding. This seems to work, but just using on_db to switch dbs seems to work for us right now
 
-    def self.first_level_on_slave
-      first_level = db_charmer_top_level_connection? && on_master.connection.open_transactions.zero?
-      if db_charmer_force_slave_reads? && db_charmer_slaves.any?
-        if first_level
-          current_db = db_charmer_connection_proxy.connection_name
-          # puts "current db: #{current_db}"
-          on_slave { yield }
-        else
-          current_db = db_charmer_connection_proxy.connection_name
-          # puts "current db: #{current_db}"
-          slave_name = "#{current_db}_ro"
-          # puts "slave:  #{slave_name}"
-          if ActiveRecord::Base.configurations[DbCharmer.env][slave_name]
-            on_slave(slave_name) { yield }
-          else
-            yield
-          end
-        end
+  SHARDING_MAP = {                        # This hash will map our keys to db connections
+    'ca'      => :ca,
+    'dc'      => :dc,
+    'MI'      => :mi,
+    :default  => :dc
+  }
+
+  DbCharmer::Sharding.register_connection(
+    :name   => :ca,
+    :method => :hash_map,
+    :map    => SHARDING_MAP               # Pass our map to the sharding method
+  )
+=end
+
+  included do
+    db_magic :connection => :ca, slave: :ca
+=begin
+    db_magic :connection => :dc, :sharded => {
+      :sharded_connection => :ca,
+      :key => :state
+    }
+=end
+
+    # Given a connection like gs_schooldb or ca, return the rw version
+    def self.master_version_of_connection(connection_name)
+      connection_name = connection_name.to_s
+      if connection_name.index('_ro')
+        connection_name.sub! '_ro', '_rw'
+      else
+        connection_name << '_rw'
       end
+
+      # TODO: rename xx connections to xx_rw, and rename xx_ro connections to xx in database.yml, then remove this line of code
+      connection_name.sub! '_rw', ''
+
+      connection_name.to_sym
     end
 
+    def self.prevent_chaining_without_on_db?
+      true
+    end
   end
 
   def shard
@@ -34,4 +54,12 @@ module StateSharding
     state.downcase.to_sym
   end
 
+  def retrieved_from_shard
+    @retrieved_from_shard
+  end
+  def set_retrieved_from_shard(shard)
+    @retrieved_from_shard = shard
+  end
+
 end
+
---
+++ config/initializers/extensions/db_charmer.rb
@@ -0,0 +1,74 @@
+module DbCharmer
+  module ActiveRecord
+    module MultiDbProxy
+      # Simple proxy class that switches connections and then proxies all the calls
+      # This class is used to implement chained on_db calls
+      class OnDbProxy < ActiveSupport::BasicObject
+
+        def initialize(proxy_target, slave)
+          @proxy_target = proxy_target
+          @slave = slave
+        end
+
+        def connection_name(connection_config_or_name)
+          if connection_config_or_name.respond_to? :connection_name
+            connection_config_or_name.connection_name.to_sym
+          else
+            connection_config_or_name.to_sym
+          end
+        end
+
+        def method_should_use_master?(method)
+          write_methods = [:create, :create, :update, :update!, :destroy, :delete]
+          write_methods.include? method
+        end
+
+        def db_to_use(meth)
+          default = connection_name(@slave)
+          if method_should_use_master?(meth) && @proxy_target.respond_to?(:master_version_of_connection)
+            connection_name_sym = @proxy_target.master_version_of_connection(default)
+          else
+            connection_name_sym = default
+          end
+          connection_name_sym
+        end
+
+        # Determines whether this query is chained off of a db_charmer "on_db" call.
+        def db_has_been_specified(proxy_target, method)
+          if proxy_target.respond_to? :db_charmer_connection_level
+            level = proxy_target.db_charmer_connection_level
+            return false if (method =~ /connection$/ && level <= 1)
+          end
+
+          return true
+        end
+
+        # Beware, modifications made to this method will be very brittle
+        # Even simply calling a method on the res variable will cause the db to be changed
+        def method_missing(meth, *args, &block)
+          # Switch connection and proxy the method call
+          default = db_to_use(meth)
+
+          @proxy_target.on_db(default) do |proxy_target|
+            res = proxy_target.__send__(meth, *args, &block)
+
+            # These exact conditions will determine if this sharded model hasn't had "on_db" chained
+            # If that's the case, raise an error, since this could lead to a query being ran on the wrong db
+
+            if @proxy_target.respond_to?(:prevent_chaining_without_on_db?) &&
+              @proxy_target.prevent_chaining_without_on_db? &&
+              !db_has_been_specified(proxy_target, meth)
+
+              raise "Tried to access #{proxy_target.name} without having selected a shard. Maybe you chained a method call off of an object that has an association to #{proxy_target.name}"
+            end
+
+            # If result is a scope/association, return a new proxy for it, otherwise return the result itself
+            (res.proxy?) ? OnDbProxy.new(res, default) : res
+          end
+        end
+
+      end
+
+    end
+  end
+end
