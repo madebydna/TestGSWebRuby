2dcd4ae7e6c658b401c922679c0b274e8f1bdc81
Samson Sprouse <ssprouse@greatschools.org>
2014-01-09 18:27:10 -0800

    Fix census specs

--- spec/models/census_data_set_spec.rb
+++ spec/models/census_data_set_spec.rb
@@ -7,24 +7,28 @@ describe CensusDataSet do
   it { should respond_to(:school_value) }
 
   context 'new data set' do
-    subject(:data_set) { FactoryGirl.build(:census_data_set) }
+    before do
+      @data_set = FactoryGirl.build(:census_data_set)
+    end
 
     it 'should be saveable' do
-      expect { data_set.on_db(:ca).save! }.to change { data_set.id}.from(NilClass).to(Fixnum)
+      expect { @data_set.on_db(:ca).save! }.to change { @data_set.id}.from(NilClass).to(Fixnum)
     end
   end
 
   context 'existing data set' do
-    subject(:data_set) { FactoryGirl.build(:census_data_set) }
+    before do
+      @data_set = FactoryGirl.build(:census_data_set)
+      @data_set.on_db(:ca).save!
+    end
+
 
     it 'should not be deleteable' do
-      data_set.on_db(:ca).save!
-      expect { data_set.on_db(:ca).destroy }.to raise_exception(ActiveRecord::ReadOnlyRecord)
+      expect { @data_set.on_db(:ca).destroy }.to raise_exception(ActiveRecord::ReadOnlyRecord)
     end
 
     it 'should not be saveable' do
-      data_set.on_db(:ca).save!
-      expect { data_set.on_db(:ca).save! }.to raise_exception(ActiveRecord::ReadOnlyRecord)
+      expect { @data_set.on_db(:ca).save! }.to raise_exception(ActiveRecord::ReadOnlyRecord)
     end
   end
 
