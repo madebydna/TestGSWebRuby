bb295a1204dda6842b37dc1705689e31cc5b22d0
Samson Sprouse <ssprouse@greatschools.org>
2014-01-10 02:00:24 -0800

    OM-45 Change post_login_concerns to use a cookie instead of session, change where redirect occurs

--- app/controllers/concerns/post_login_concerns.rb
+++ app/controllers/concerns/post_login_concerns.rb
@@ -2,12 +2,33 @@ module PostLoginConcerns
   extend ActiveSupport::Concern
 
   def save_post_signin_action(action, params)
-    session[:after_signin] = [action, params]
+    cookies[:after_signin] = [action, params].to_json
   end
 
   def get_post_login_action
-    if session[:after_signin]
-      return *session[:after_signin]
+    if cookies[:after_signin]
+      begin
+        data = JSON.parse(cookies[:after_signin], {:symbolize_names => true})
+        return *data
+      rescue
+        return nil
+      end
+    end
+  end
+
+  def execute_post_login_action
+    action, params = get_post_login_action
+
+    if action.present? && self.respond_to?(action)
+      begin
+        self.send action, params
+        cookies.delete :after_signin
+      rescue => error
+        Rails.logger.debug "Error when executing post login action: #{action} on #{self.class}. #{error.message}"
+      end
+    else
+      Rails.logger.debug "Action: #{action} not present on #{self.class}."
+      cookies.delete :after_signin
     end
   end
 
@@ -29,10 +50,8 @@ module PostLoginConcerns
         current_user.add_subscription!(list, school)
       end
       flash_notice message if message
-      redirect_back_or_default
     rescue => e
       flash_error e.message
-      redirect_back_or_default
     end
   end
 
--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -6,14 +6,12 @@ class SubscriptionsController < ApplicationController
 
     if logged_in?
       create_subscription subscription_params
+      redirect_back_or_default
     else
       save_post_signin_action :create_subscription, subscription_params
       flash_error 'Please log in or register your email in order to get updates on this school.'
       redirect_to signin_path
     end
   end
-
-
-
 
 end
