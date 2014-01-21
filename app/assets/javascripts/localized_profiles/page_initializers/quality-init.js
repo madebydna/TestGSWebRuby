46281799f2be8eb7f4ae46dc9d1f7b740d3a51f8
Mitchell Seltzer <mseltzer@greatschools.org>
2014-01-21 15:19:00 -0800

    OM-105 test scores - hide after load - fixes legend layout issue on mobile

--- app/assets/javascripts/localized_profiles/page_initializers/quality-init.js
+++ app/assets/javascripts/localized_profiles/page_initializers/quality-init.js
@@ -1,6 +1,13 @@
 if(gon.pagename == "Quality"){
     $(function () {
         GS.testScores.initializeHandlers();
-        $(".js_grades_div").children().first().trigger( "click" );
+        var func = function () {
+            $(".js_grades_div").children().first().trigger( "click" );
+        }
+        if (GS.visualchart.loader) {
+            GS.visualchart.loader.push(func);
+        } else {
+            google.setOnLoadCallback(func);
+        }
     });
 }
