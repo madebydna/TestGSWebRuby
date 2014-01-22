e73db1811fbd7ec22f1c1a5981014ba04130875e
Samson Sprouse <samson.sprouse@gmail.com>
2014-01-21 22:41:34 -0800

    OM-133 Make static map load on all pages

--- app/assets/javascripts/localized_profiles/page_initializers/overview-init.js
+++ app/assets/javascripts/localized_profiles/page_initializers/overview-init.js
@@ -3,6 +3,5 @@ if(gon.pagename == "Overview"){
         if($("#galleria").get(0)){
             Galleria.run('#galleria');
         }
-        GS.window.sizing.globalContactMap("js-contact_map_image");
     });
 }
---
+++ app/assets/javascripts/localized_profiles/school_static_map.js
@@ -0,0 +1,5 @@
+$(function () {
+    if ($('.js-contact_map_image').size() > 0) {
+        GS.window.sizing.globalContactMap("js-contact_map_image");
+    }
+});
