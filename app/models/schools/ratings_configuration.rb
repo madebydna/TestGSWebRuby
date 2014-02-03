4941d7173edd2a52f2639fcdfe1f7168135cfcf3
Nanditha Patury <npatury@greatschools.org>
2014-02-03 09:47:08 -0800

    bugathon : fixed copy on ratings

--- app/models/schools/ratings_configuration.rb
+++ app/models/schools/ratings_configuration.rb
@@ -3,9 +3,9 @@ class RatingsConfiguration
   def self.city_rating_configuration
     {"mi" => { "Detroit" => Hashie::Mash.new({
                                   rating_breakdowns: {
-                                      climate: {data_type_id: 200, label: "Climate"},
-                                      status: {data_type_id: 198, label: "Status"},
-                                      progress: {data_type_id: 199, label: "Progress"}
+                                      climate: {data_type_id: 200, label: "School Climate"},
+                                      status: {data_type_id: 198, label: "Academic Status"},
+                                      progress: {data_type_id: 199, label: "Academic Progress"}
                                   },
                                   overall: {data_type_id: 201, label: "overall", description_key: "mi_esd_summary"}
                               })}}
@@ -20,9 +20,9 @@ class RatingsConfiguration
   def self.gs_rating_configuration
     Hashie::Mash.new({
                          rating_breakdowns: {
-                             test_scores: {data_type_id: 164, label: "Test Scores"},
-                             progress: {data_type_id: 165, label: "Progress"},
-                             college_readiness: {data_type_id: 166, label: "College Readiness"}
+                             test_scores: {data_type_id: 164, label: "Test score rating"},
+                             progress: {data_type_id: 165, label: "Student growth rating"},
+                             college_readiness: {data_type_id: 166, label: "College readiness rating"}
                          },
                          overall: {description_key: "what_is_gs_rating_summary"}
                      })
