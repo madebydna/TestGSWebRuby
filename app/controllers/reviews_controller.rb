82c5ab4abbe10e226a5c587edea39c1e9710ff5e
Nanditha Patury <npatury@greatschools.org>
2014-02-11 09:02:51 -0800

    OM-159 added the page title

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -12,12 +12,6 @@ class ReviewsController < ApplicationController
 
   def new
     init_page
-
-    #Set the pagename before setting other omniture props.
-    gon.omniture_pagename = 'GS:SchoolProfiles:WriteAReview'
-    set_omniture_hier_for_new_profiles
-    set_omniture_data_for_school(gon.omniture_pagename)
-    set_omniture_data_for_user_request
   end
 
   def create
@@ -36,12 +30,23 @@ class ReviewsController < ApplicationController
     @user_first_name = current_user.first_name unless !logged_in?
   end
 
+  def set_omniture_data
+    #Set the pagename before setting other omniture props.
+    gon.omniture_pagename = 'GS:SchoolProfiles:WriteAReview'
+    set_omniture_hier_for_new_profiles
+    set_omniture_data_for_school(gon.omniture_pagename)
+    set_omniture_data_for_user_request
+  end
+
   def init_page
     gon.pagename = 'reviews/new'
     @google_signed_image = GoogleSignedImages.new @school, gon
     @header_metadata = @school.school_metadata
     @school_reviews_global = SchoolReviews.set_reviews_objects @school
     @cookiedough = SessionCacheCookie.new cookies[:SESSION_CACHE]
+
+    set_omniture_data
+    set_meta_tags :title =>  'Rate and review ' + @school.name + ' in ' + @school.city + ', ' + @school.state
   end
 
 end
