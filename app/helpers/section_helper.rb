d0fac27429327b29fe4f8e0f8286a149592a9600
Mitchell Seltzer <mseltzer@greatschools.org>
2014-02-18 13:23:14 -0800

    OM-188 - review button fixed on no reviews and details link issue

---
+++ app/helpers/section_helper.rb
@@ -0,0 +1,17 @@
+module SectionHelper
+  def display_section_head_link(category_placement, link_text=nil)
+    page_title = ['Reviews', 'Details', 'Quality']
+    return_str = ''
+    title = category_placement.title
+
+    if title.present? && (page_title.include? title)
+      return_str << '<div class="fr prm pt8">'
+      return_str << '<a href="'+ title
+      return_str << '">'
+      return_str << (link_text.nil? ? 'See all '+title.capitalize  : link_text)
+      return_str << '</a>'
+      return_str << '</div>'
+    end
+    return_str
+  end
+end
