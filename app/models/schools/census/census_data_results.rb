8230d80a92c83ee976af1d263153835966406f71
Samson Sprouse <ssprouse@greatschools.org>
2014-02-17 18:20:44 -0800

    OM-179 Fix logic checking for latest year

--- app/models/schools/census/census_data_results.rb
+++ app/models/schools/census/census_data_results.rb
@@ -17,13 +17,24 @@ class CensusDataResults
     self
   end
 
+  # For each data type in results, determine the maximum valid year for that data type
+  # Valid years are years that have at least one school value for this school, or
+  # year zeros where the school's modified date is later than october prior to (max data set year)
   def max_year_per_data_type
     data_types = group_by(&:data_type_id)
 
     max_years = {}
     data_types.each do |k,v|
-      max_year = v.map(&:year).max
-      max_year = 0 if v.select { |data_set| data_set.year == 0}.any?
+      # Throw out years where associated values are nil. Find max of remaining years
+      max_year = v.reject { |data_set| data_set.census_data_school_value.nil? }.map(&:year).max
+
+      # If there's any valid year zero overrides, use those
+      max_year = 0 if v.select do |data_set|
+        data_set.year == 0 &&
+        data_set.census_data_school_value &&
+        # Is the year zero override recent enough?
+        data_set.census_data_school_value.modified > Time.new(max_year - 1, 10, 1)
+      end.any?
       max_years[k] = max_year
     end
 
