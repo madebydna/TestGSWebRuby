d0a96f606bb0fad1dcef017b20bda020075daeec
Samson Sprouse <ssprouse@greatschools.org>
2014-02-18 16:25:37 -0800

    Change mocha-suite.js to include specific files, since some things break if gon is not available

--- spec/javascripts/mocha-suite.js
+++ spec/javascripts/mocha-suite.js
@@ -9,6 +9,8 @@
 //
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
-//= require application
+//= require lodash
+//= require_tree ../../app/assets/javascripts/auth
+//= require_tree ../../app/assets/javascripts/util
 //= require_tree .
 //= require sinon
