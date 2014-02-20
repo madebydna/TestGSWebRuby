912e676d9f83cdb2c7e89526703c97819f1e519f
Samson Sprouse <ssprouse@greatschools.org>
2014-02-20 15:25:08 -0800

    Fix: Change production.rb asset host to use media_server property set in /usr/local/etc/GSWebRuby-config.yml

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,7 +66,7 @@ LocalizedProfiles::Application.configure do
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server
   # config.action_controller.asset_host = "http://assets.example.com"
-  config.action_controller.asset_host = ENV_GLOBAL['media_server'] if ENV_GLOBAL['media_server']
+  config.action_controller.asset_host = ENV_GLOBAL['media_server'] if ENV_GLOBAL['media_server'].present?
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
