29582a6e56765d034ac87de6a7c0df3207d4472d
Hugo Melo <me@hugomelo.com>
2014-02-24 16:18:32 -0800

    finish rought pass of city hub port

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -2,12 +2,21 @@ class CitiesController < ApplicationController
   def show
     collection_mapping = CollectionMapping.where(city: params[:city]).first
     collection_configs = CollectionConfig.where(collection_id: collection_mapping.collection_id)
+    puts collection_configs
     @state = States::STATE_HASH.select { |k, v| v == collection_mapping.state.downcase }.keys[0]
+    @city = params[:city]
 
     # Stub data
     @collection = {
       nickname: 'Detroit'
     }
+
+    @zillow_data = {
+      'zillow_formatted_location' => @city.downcase.gsub(/ /, '-') + '-'+ States.abbreviation(@state).downcase,
+      'region_id' => ZillowRegionId.by_city_state(@city, @state)
+    }
+    @category_placement = CategoryPlacement.find(31)
+
     @breakdown_results = [
       { contents: 'Preschools', count: 10, hrefXML: 'http://google.com' },
       { contents: 'Elementary Schools', count: 20, hrefXML: 'http://google.com' },
@@ -29,6 +38,32 @@ class CitiesController < ApplicationController
     @important_event = {
       config_key_prefix_list_with_index: [{}, {}],
       max_important_event_to_display: 2
+    }
+    @reviews = [
+     {stars: 3, date: '2 days ago', comments: 'a ' * 150, school_name: 'King High School'},
+     {stars: 5, date: '3 days ago', comments: 'b ' * 120, school_name: 'MS 118'}
+    ]
+    @articles = [
+      { image: 'http://www.gscdn.org/res/img/cityHubs/1_Article_1.png', title: 'Random Access Title', content: 'foo bar baz'*5, new_window: true },
+      { image: 'http://www.gscdn.org/res/img/cityHubs/1_Article_2.png', title: 'Random Access Title', content: 'foo bar baz'*5, new_window: false },
+      { image: 'http://www.gscdn.org/res/img/cityHubs/1_Article_3.png', title: 'Random Access Title', content: 'foo bar baz'*5, new_window: false }
+    ]
+    @partner_carousel = {
+      heading: "#{@collection[:nickname]} Education Community",
+      logos: [
+        { link: 'http://google.com', image_path: 'http://www.gscdn.org/res/img/cityHubs/1_Partner_1.png', partner_name: 'Google' },
+        { link: 'http://google.com', image_path: 'http://www.gscdn.org/res/img/cityHubs/1_Partner_2.png', partner_name: 'Google' },
+        { link: 'http://google.com', image_path: 'http://www.gscdn.org/res/img/cityHubs/1_Partner_3.png', partner_name: 'Google' },
+        { link: 'http://google.com', image_path: 'http://www.gscdn.org/res/img/cityHubs/1_Partner_4.png', partner_name: 'Google' },
+        { link: 'http://google.com', image_path: 'http://www.gscdn.org/res/img/cityHubs/1_Partner_8.png', partner_name: 'Google' },
+        { link: 'http://google.com', image_path: 'http://www.gscdn.org/res/img/cityHubs/1_Partner_5.png', partner_name: 'Google' },
+        { link: 'http://google.com', image_path: 'http://www.gscdn.org/res/img/cityHubs/1_Partner_8.png', partner_name: 'Google' },
+        { link: 'http://google.com', image_path: 'http://www.gscdn.org/res/img/cityHubs/1_Partner_6.png', partner_name: 'Google' },
+        { link: 'http://google.com', image_path: 'http://www.gscdn.org/res/img/cityHubs/1_Partner_8.png', partner_name: 'Google' },
+        { link: 'http://google.com', image_path: 'http://www.gscdn.org/res/img/cityHubs/1_Partner_7.png', partner_name: 'Google' },
+        { link: 'http://google.com', image_path: 'http://www.gscdn.org/res/img/cityHubs/1_Partner_8.png', partner_name: 'Google' },
+        { link: 'http://google.com', image_path: 'http://www.gscdn.org/res/img/cityHubs/1_Partner_9.png', partner_name: 'Google' },
+      ]
     }
   end
 end
