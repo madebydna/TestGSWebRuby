d12d7b41e1b431d84b073d896d9b634554a003c5
Hugo Melo <me@hugomelo.com>
2014-02-27 13:13:20 -0800

    beef up city hub acceptance specs

--- spec/features/city_hub_spec.rb
+++ spec/features/city_hub_spec.rb
@@ -3,6 +3,13 @@ require 'spec_helper'
 describe 'City Hub Page', js: true do
   let(:city_page_url) { 'http://localhost:3000/michigan/detroit' }
 
+  describe 'entering a falty url' do
+    it 'renders the error page' do
+      visit 'http://localhost:3000/michigan/foobar'
+      expect(page).to have_content 'Page Not Found'
+    end
+  end
+
   describe 'noSchoolAlert param' do
     it 'shows an error partial' do
       error_message = "Oops! The school you were looking for may no longer exist."
@@ -16,6 +23,35 @@ describe 'City Hub Page', js: true do
   describe 'search' do
     it 'searches and redirects to java results' do
       pending("haven't gotten there yet")
+    end
+  end
+
+  describe 'choose a school section' do
+    it 'renders links for a city hub' do
+      visit city_page_url
+      expect(page).to have_content 'Finding a Great School in Detroit'
+      expect(all('#choose-a-school a').length).to eq(3)
+    end
+  end
+
+  describe 'recent reviews section' do
+    it 'show two most recent reviews for a city hub' do
+      visit city_page_url
+      expect(page).to have_css('#js-hubsRecentReviews button')
+      expect(all('.recent-review').length).to eq(2)
+    end
+  end
+
+  describe 'featured articles section' do
+    it 'display featured articles' do
+      featured_article_css = '.featured-articles>div'
+      visit city_page_url
+      expect(all(featured_article_css).length).to eq(3)
+    end
+    it 'shows nearby homes with zillow' do
+      visit city_page_url
+      expect(page).to have_content 'Nearby Homes for Sale'
+      expect(page).to have_css('iframe')
     end
   end
 end
