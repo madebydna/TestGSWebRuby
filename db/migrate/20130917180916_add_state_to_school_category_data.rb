d427aafb7951974e5bb4980db9c522b1e01f8579
Samson Sprouse <ssprouse@greatschools.org>
2014-03-03 20:41:17 -0800

    Remove unused migration

--- db/migrate/20130917180916_add_state_to_school_category_data.rb
+++
@@ -1,8 +0,0 @@
-class AddStateToSchoolCategoryData < ActiveRecord::Migration
-  db_magic connections: States.abbreviations_as_symbols
-  def change
-    unless self.table_exists? :school_category_data
-      add_column :school_category_data, :state, :string
-    end
-  end
-end
