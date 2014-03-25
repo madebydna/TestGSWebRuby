f701de0359444212a23bfb75d9379b9f16102b73
Hugo Melo <me@hugomelo.com>
2014-03-24 18:55:12 -0700

    make intermittent test pass all the time

--- spec/features/choosing_page_spec.rb
+++ spec/features/choosing_page_spec.rb
@@ -1,20 +1,24 @@
 require 'spec_helper'
 
 describe 'Choosing Page' do
-  before(:each) { CollectionConfig.destroy_all }
   before(:each) do
+    CollectionConfig.destroy_all
+    HubCityMapping.destroy_all
     FactoryGirl.create(:choosing_page_links_configs)
     FactoryGirl.create(:important_events_collection_config)
+    FactoryGirl.create(:hub_city_mapping)
   end
 
-  it 'displays basic static content' do
+  it 'displays basic static and dynamic content' do
     visit '/michigan/detroit/choosing-schools/'
 
     expect(page.title).to eq('Choosing a school in Detroit, MI')
     expect(page).to have_selector('h1', text: '5 simple steps to')
     expect(page).to have_selector('.expandable', count: 4)
-  end
 
-  it 'displays dynamic content' do
+    step3_links = CollectionConfig.choosing_page_links(1)
+    step3_links.each do |link|
+      expect(page).to have_link(link[:name], href: link[:path])
+    end
   end
 end
