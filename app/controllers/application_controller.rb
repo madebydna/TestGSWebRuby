b35bf1e700232980ee026c5934ad143b7bd0be3c
mseltzer <mseltzer@greatschools.org>
2014-03-25 17:16:35 -0700

    OM-221 - refactor to append any extra query string parameters to the end.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -51,12 +51,20 @@ class ApplicationController < ActionController::Base
 
   def redirect_tab_urls
     if params[:tab] == 'reviews'
-      redirect_to request.original_url.split('?')[0] + 'reviews/'
+      redirect_to redirect_append_query_string 'tab', 'reviews'
     elsif ['test-scores', 'ratings', 'college-readiness', 'climate'].include? params[:tab]
-      redirect_to request.original_url.split('?')[0] + 'quality/'
+      redirect_to redirect_append_query_string 'tab', 'quality'
     elsif ['demographics', 'teachers', 'programs-culture', 'programs-resources', 'extracurriculars', 'culture', 'enrollment'].include? params[:tab]
-      redirect_to request.original_url.split('?')[0] + 'details/'
+      redirect_to redirect_append_query_string 'tab', 'details'
     end
+  end
+
+  def redirect_append_query_string (do_not_append, page_name)
+    url = Addressable::URI.parse(request.original_url)
+    url.path = url.path + page_name + '/'
+    url.query_values = url.query_values.except(do_not_append)
+    url.query_values = nil unless url.query_values.present?
+    url.to_s
   end
 
   def require_state
