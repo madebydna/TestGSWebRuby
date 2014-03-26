c82ee573f31a6b56f26f6a88dba7cd263899e917
Nanditha Patury <npatury@greatschools.org>
2014-03-26 09:10:07 -0700

    OM-278 fixed merge conflicts for refactor

--- app/models/data_readers/rating_data_reader.rb
+++ app/models/data_readers/rating_data_reader.rb
@@ -1,14 +1,22 @@
 class RatingDataReader < SchoolProfileDataReader
 
   def data
+    #Get the ratings configuration from the database.
+    ratings_config = RatingsConfiguration.configuration_for_school(school.state)
+
+    #Build an array of all the data type ids so that we can query the database only once.
+    all_data_type_ids = ratings_config.city_rating_data_type_ids + ratings_config.state_rating_data_type_ids + ratings_config.gs_rating_data_type_ids + ratings_config.prek_rating_data_type_ids
+
     #Get the ratings from the database.
-    results = RatingsHelper.fetch_ratings_for_school school
+    results = TestDataSet.by_data_type_ids(school, all_data_type_ids)
+
+    ratings_helper = RatingsHelper.new(results,ratings_config)
 
     #Build a hash to hold the ratings results.
-    gs_rating_value = RatingsHelper.construct_GS_ratings results, school
-    city_rating_value =  RatingsHelper.construct_city_ratings results, school
-    state_rating_value = RatingsHelper.construct_state_ratings results, school
-    preK_ratings = RatingsHelper.construct_preK_ratings results, school
+    gs_rating_value = ratings_helper.construct_GS_ratings(school)
+    city_rating_value =  ratings_helper.construct_city_ratings
+    state_rating_value = ratings_helper.construct_state_ratings
+    preK_ratings = ratings_helper.construct_preK_ratings
 
     return_var = {}
     if gs_rating_value.present?
