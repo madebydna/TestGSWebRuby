e0f1d33edaaf9193a606d63dd31d548f63a56934
Samson Sprouse <ssprouse@greatschools.org>
2014-03-31 18:54:40 -0700

    Fix database cleaner not cleaning all test databases

--- lib/database_configuration_helper.rb
+++ lib/database_configuration_helper.rb
@@ -17,6 +17,18 @@ class DatabaseConfigurationHelper
     database_config
   end
 
+  def self.all_connections_for(rails_environment)
+    database_config_hashes = ActiveRecord::Base.configurations[rails_environment].select do |key, value|
+      value.is_a?(Hash) && value['database']
+    end
+
+    database_config_hashes.map { |key, _| key }
+  end
+
+  def self.all_rw_connections_for(rails_environment)
+    all_connections_for(rails_environment).select { |connection| connection[-3..-1] != '_ro' }
+  end
+
   def self.legacy_database_names(rails_environment)
     legacy_database_names_with_suffix = self.legacy_database_names_with_suffix(rails_environment)
     legacy_database_names_with_suffix.map { |name| name.sub "_#{rails_environment}", '' }
