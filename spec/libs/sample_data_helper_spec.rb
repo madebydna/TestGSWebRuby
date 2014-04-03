6df4c0002448c28fdc53d427242a106d989860a7
Samson Sprouse <ssprouse@greatschools.org>
2014-04-02 17:31:02 -0700

    Added clean_dbs helper method in spec_helper, added example of usage

--- spec/libs/sample_data_helper_spec.rb
+++ spec/libs/sample_data_helper_spec.rb
@@ -1,11 +1,21 @@
 require 'spec_helper'
 
 describe 'sample_data_helper.rb' do
-  pending 'pending until I get database_cleaner code from Hugo / alpha branch'
   require 'sample_data_helper'
+
+  after(:each) do
+    clean_dbs :ca, :mi
+  end
 
   describe 'load_sample_data' do
     it 'should load a single school' do
+      load_sample_data 'sample_data_helper_test'
+      schools = School.on_db(:ca).all
+      expect(schools.size).to eq 1
+      expect(schools.first.name).to eq 'Alameda High School'
+    end
+
+    it 'should load a single school again' do
       load_sample_data 'sample_data_helper_test'
       schools = School.on_db(:ca).all
       expect(schools.size).to eq 1
