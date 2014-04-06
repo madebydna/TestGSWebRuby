420b3e026f0f7b421ca9929cfe378bfa31e13e74
Samson Sprouse <ssprouse@greatschools.org>
2014-04-06 16:36:46 -0700

    Formatted file

--- spec/controllers/application_controller_spec.rb
+++ spec/controllers/application_controller_spec.rb
@@ -1,7 +1,6 @@
 require 'spec_helper'
 describe ApplicationController do
 
-
   it 'should have methods for manipulating cookies' do
     expect(controller).to respond_to(:write_cookie_value)
     expect(controller).to respond_to(:read_cookie_value)
@@ -20,26 +19,45 @@ describe ApplicationController do
 
     it 'should not overwrite cookie when asked not to' do
       subject.send :write_cookie_value, :test_cookie, 'value'
-      subject.send :write_cookie_value, :test_cookie, 'new value should not overwrite', nil, false
+      subject.send :write_cookie_value,
+                   :test_cookie,
+                   'new value should not overwrite',
+                   nil,
+                   false
       expect(cookies['test_cookie']).to eq 'value'
     end
 
     context 'when given options' do
       describe 'option "hash" is true' do
         it 'cookie key and value should be saved as json' do
-          subject.send :write_cookie_value, :test_cookie, 'value', 'key', true, hash: true
+          subject.send :write_cookie_value,
+                       :test_cookie,
+                       'value',
+                       'key',
+                       true,
+                       hash: true
           expect(cookies['test_cookie']).to eq({ key: 'value' }.to_json)
         end
 
         it 'should not save cookie as json if caller doesnt provide key' do
-          subject.send :write_cookie_value, :test_cookie, 'value', nil, true, hash: true
+          subject.send :write_cookie_value,
+                       :test_cookie,
+                       'value',
+                       nil,
+                       true,
+                       hash: true
           expect(cookies['test_cookie']).to eq 'value'.to_json
         end
       end
 
       describe 'option "hash" is false' do
         it 'should set cookie value as string and key is ignored' do
-          subject.send :write_cookie_value, :test_cookie, 'value', 'key', true, hash: false
+          subject.send :write_cookie_value,
+                       :test_cookie,
+                       'value',
+                       'key',
+                       true,
+                       hash: false
           expect(cookies['test_cookie']).to eq 'value'
         end
       end
@@ -52,7 +70,12 @@ describe ApplicationController do
 
         it 'should set duration on cookie' do
           test_duration = 1.day
-          subject.send :write_cookie_value, :test_cookie, 'value', nil, true, duration: test_duration
+          subject.send :write_cookie_value,
+                       :test_cookie,
+                       'value',
+                       nil,
+                       true,
+                       duration: test_duration
           cookie = @cookie_jar[:test_cookie]
           expect(cookie[:expires].to_s).to eq test_duration.from_now.to_s
         end
@@ -115,7 +138,7 @@ describe ApplicationController do
 
     it 'should read a simple value' do
       @cookie_jar[:test_cookie] = 'value'
-      expect(subject.send :read_cookie_value, :test_cookie ).to eq 'value'
+      expect(subject.send :read_cookie_value, :test_cookie).to eq 'value'
     end
 
     context 'when hash option is set true' do
@@ -123,12 +146,18 @@ describe ApplicationController do
         @cookie_jar[:test_cookie] = { test_key: 'test_value' }.to_json
       end
       it 'should read a json value as hash when no key provided' do
-        expect(subject.send :read_cookie_value, :test_cookie, nil, hash: true).to be_a Hash
-        expect(subject.send :read_cookie_value, :test_cookie, nil, hash: true).to eq(test_key: 'test_value')
+        expect(subject.send :read_cookie_value, :test_cookie, nil, hash: true)
+          .to be_a Hash
+        expect(subject.send :read_cookie_value, :test_cookie, nil, hash: true)
+          .to eq(test_key: 'test_value')
       end
 
       it 'should return a value from the hash if given a key' do
-        expect(subject.send :read_cookie_value, :test_cookie, :test_key, hash: true).to eq 'test_value'
+        expect(subject.send :read_cookie_value,
+                            :test_cookie,
+                            :test_key,
+                            hash: true
+                            ).to eq 'test_value'
       end
     end
 
@@ -140,19 +169,16 @@ describe ApplicationController do
 
       it 'should read a json value as hash when no key provided' do
         expect(subject.send :read_cookie_value, :test_cookie).to be_a Hash
-        expect(subject.send :read_cookie_value, :test_cookie).to eq(test_key: 'test_value')
+        expect(subject.send :read_cookie_value, :test_cookie)
+          .to eq(test_key: 'test_value')
       end
 
       it 'should return a value from the hash if given a key' do
-        expect(subject.send :read_cookie_value, :test_cookie, :test_key).to eq 'test_value'
+        expect(subject.send :read_cookie_value, :test_cookie, :test_key)
+          .to eq 'test_value'
       end
     end
 
   end
-
-
-
-
-
 
 end
