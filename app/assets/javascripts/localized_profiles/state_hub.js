890e757d3d3496750856d59b6bab2bbcabdcd425
Hugo Melo <me@hugomelo.com>
2014-04-07 16:19:26 -0700

    refactor view more/less code to be responsive

--- app/assets/javascripts/localized_profiles/state_hub.js
+++ app/assets/javascripts/localized_profiles/state_hub.js
@@ -8,15 +8,20 @@ GS.viewMoreCollapseInit = function() {
     var fullHeight = $p.css('height');
     $p.css('height', oldHeight);
     $p.data('height', fullHeight);
-    return parseInt(fullHeight) - parseInt(oldHeight);
+
+    return parseInt(fullHeight) - foldHeight;
   }
 
   function expandClick() {
     $(this).text('View Less');
     var $p = $(this).parent().find(textSelector);
     var fullHeight = $p.data('height');
-    $p.animate({ height: fullHeight });
-    $(this).unbind('click')
+    $p.animate({
+      height: fullHeight
+    }, function() {
+      $(this).css('height', 'auto');
+    });
+    $(this).unbind('click');
     $(this).click(collapseClick)
   }
 
@@ -30,13 +35,18 @@ GS.viewMoreCollapseInit = function() {
 
   function initExpandCollapse() {
     $('p.view-more').each(function() {
-      var $p = $(this).parent().find(textSelector);
-      $p.show();
-      var heightDiff = heightSwitch($p);
-      if (heightDiff > 20) {
-        $(this).click(expandClick);
+      var $this = $(this)
+      var $p = $this.parent().find(textSelector);
+      var tagText = $this.text();
+      if (tagText == 'View Less') {
+        $this.click(collapseClick);
       } else {
-        $(this).hide();
+        var heightDiff = heightSwitch($p);
+        if (heightDiff > 20) {
+          $this.click(expandClick);
+        } else {
+          $this.hide();
+        }
       }
     });
   }
