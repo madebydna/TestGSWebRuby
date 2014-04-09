033e916bced575107447dc7e7a63afebb90fecda
Hugo Melo <me@hugomelo.com>
2014-04-09 14:46:37 -0700

    refactor view more / less

--- app/assets/javascripts/localized_profiles/state_hub.js
+++ app/assets/javascripts/localized_profiles/state_hub.js
@@ -1,6 +1,8 @@
 GS.viewMoreCollapseInit = function() {
   var textSelector = '.js-module-expand';
+  var toggleSelector = 'p.view-more';
   var foldHeight = 71;
+  var lineHeight = 20;
 
   function heightSwitch($p) {
     var oldHeight = $p.css('height');
@@ -13,37 +15,47 @@ GS.viewMoreCollapseInit = function() {
   }
 
   function expandClick() {
-    $(this).text('View Less');
-    var $p = $(this).parent().find(textSelector);
+    var $this = $(this)
+    $this.text('View Less');
+    var $p = $this.parent().find(textSelector);
     var fullHeight = $p.data('height');
-    $p.animate({
+    $p.stop().animate({
       height: fullHeight
     }, 'linear', function() {
-      $(this).css('height', 'auto');
+      $this.css('height', 'auto');
     });
-    $(this).unbind('click');
-    $(this).click(collapseClick)
+    $this.unbind('click');
+    $this.click(collapseClick)
   }
 
   function collapseClick() {
-    $(this).text('View More »')
-    var $p = $(this).parent().find(textSelector);
-    $p.animate({ height: foldHeight + 'px' }, 'linear')
-    $(this).unbind('click');
-    $(this).click(expandClick);
+    var $this = $(this)
+    $this.text('View More »')
+    var $p = $this.parent().find(textSelector);
+    $p.stop().animate({ height: foldHeight + 'px' }, 'linear')
+    $this.unbind('click');
+    $this.click(expandClick);
   }
 
   function initExpandCollapse() {
-    $('p.view-more').each(function() {
+    $(toggleSelector).each(function() {
       var $this = $(this);
       var $p = $this.parent().find(textSelector);
       var tagText = $this.text();
       if (tagText == 'View Less') {
-        $this.click(collapseClick);
+        var heightDiff = heightSwitch($p);
+        if (heightDiff > lineHeight) {
+          $p.css('height', 'auto')
+          $this.unbind('click')
+               .click(collapseClick);
+        } else {
+          $this.hide();
+        }
       } else {
         var heightDiff = heightSwitch($p);
-        if (heightDiff > 20) {
-          $this.click(expandClick);
+        if (heightDiff > lineHeight) {
+          $this.show()
+               .click(expandClick);
         } else {
           $this.hide();
         }
