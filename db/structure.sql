52c6b8cc6ec2595bb6a6d312a2c3477f25f632f9
Hugo Melo <me@hugomelo.com>
2014-04-11 12:44:55 -0700

    reset structure, post-merge conficts

--- db/structure.sql
+++ db/structure.sql
@@ -1,215 +1,155 @@
 CREATE TABLE `admins` (
- `id` int(11) NOT NULL AUTO_INCREMENT,
- `email` varchar(255) COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
- `encrypted_password` varchar(255) COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
- `reset_password_token` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
- `reset_password_sent_at` datetime DEFAULT NULL,
- `remember_created_at` datetime DEFAULT NULL,
- `sign_in_count` int(11) DEFAULT '0',
- `current_sign_in_at` datetime DEFAULT NULL,
- `last_sign_in_at` datetime DEFAULT NULL,
- `current_sign_in_ip` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
- `last_sign_in_ip` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
- `created_at` datetime NOT NULL,
- `updated_at` datetime NOT NULL,
- PRIMARY KEY (`id`),
- UNIQUE KEY `index_admins_on_email` (`email`),
- UNIQUE KEY `index_admins_on_reset_password_token` (`reset_password_token`)
-) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `email` varchar(255) NOT NULL DEFAULT '',
+  `encrypted_password` varchar(255) NOT NULL DEFAULT '',
+  `reset_password_token` varchar(255) DEFAULT NULL,
+  `reset_password_sent_at` datetime DEFAULT NULL,
+  `remember_created_at` datetime DEFAULT NULL,
+  `sign_in_count` int(11) DEFAULT '0',
+  `current_sign_in_at` datetime DEFAULT NULL,
+  `last_sign_in_at` datetime DEFAULT NULL,
+  `current_sign_in_ip` varchar(255) DEFAULT NULL,
+  `last_sign_in_ip` varchar(255) DEFAULT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `index_admins_on_email` (`email`),
+  UNIQUE KEY `index_admins_on_reset_password_token` (`reset_password_token`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE `categories` (
- `id` int(11) NOT NULL AUTO_INCREMENT,
- `parent_id` int(11) DEFAULT NULL,
- `name` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
- `description` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
- `created_at` datetime NOT NULL,
- `updated_at` datetime NOT NULL,
- `source` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
- PRIMARY KEY (`id`),
- KEY `index_categories_on_parent_id` (`parent_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=33 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `parent_id` int(11) DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `description` varchar(255) DEFAULT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  `source` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `index_categories_on_parent_id` (`parent_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE `category_data` (
- `id` int(11) NOT NULL AUTO_INCREMENT,
- `category_id` int(11) DEFAULT NULL,
- `response_key` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
- `collection_id` int(11) DEFAULT NULL,
- `created_at` datetime NOT NULL,
- `updated_at` datetime NOT NULL,
- `sort_order` int(11) DEFAULT NULL,
- `source` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
- `label` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
- PRIMARY KEY (`id`),
- KEY `index_category_data_on_category_id` (`category_id`),
- KEY `index_category_data_on_response_key` (`response_key`),
- KEY `index_category_data_on_source` (`source`)
-) ENGINE=MyISAM AUTO_INCREMENT=175 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `category_id` int(11) DEFAULT NULL,
+  `response_key` varchar(255) DEFAULT NULL,
+  `collection_id` int(11) DEFAULT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  `sort_order` int(11) DEFAULT NULL,
+  `source` varchar(255) DEFAULT NULL,
+  `label` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `index_category_data_on_category_id` (`category_id`),
+  KEY `index_category_data_on_response_key` (`response_key`),
+  KEY `index_category_data_on_source` (`source`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE `category_placements` (
- `id` int(11) NOT NULL AUTO_INCREMENT,
- `category_id` int(11) DEFAULT NULL,
- `collection_id` int(11) DEFAULT NULL,
- `page_id` int(11) DEFAULT NULL,
- `position` int(11) DEFAULT NULL,
- `created_at` datetime NOT NULL,
- `updated_at` datetime NOT NULL,
- `layout` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
- `priority` int(11) DEFAULT NULL,
- `layout_config` text COLLATE utf8_unicode_ci,
- `title` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
- `ancestry` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
- PRIMARY KEY (`id`),
- KEY `index_category_placements_on_ancestry` (`ancestry`),
- KEY `index_category_placements_on_category_id` (`category_id`),
- KEY `index_category_placements_on_collection_id` (`collection_id`),
- KEY `index_category_placements_on_page_id` (`page_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=42 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
-CREATE TABLE `census_breakdowns` (
- `id` int(11) NOT NULL AUTO_INCREMENT,
- `datatype_id` int(11) DEFAULT NULL,
- `description` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
- `created_at` datetime NOT NULL,
- `updated_at` datetime NOT NULL,
- PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `category_id` int(11) DEFAULT NULL,
+  `collection_id` int(11) DEFAULT NULL,
+  `page_id` int(11) DEFAULT NULL,
+  `position` int(11) DEFAULT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  `layout` varchar(255) DEFAULT NULL,
+  `priority` int(11) DEFAULT NULL,
+  `layout_config` text,
+  `title` varchar(255) DEFAULT NULL,
+  `ancestry` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `index_category_placements_on_category_id` (`category_id`),
+  KEY `index_category_placements_on_collection_id` (`collection_id`),
+  KEY `index_category_placements_on_page_id` (`page_id`),
+  KEY `index_category_placements_on_ancestry` (`ancestry`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE `collections` (
- `id` int(11) NOT NULL AUTO_INCREMENT,
- `name` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
- `description` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
- `created_at` datetime NOT NULL,
- `updated_at` datetime NOT NULL,
- PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
-CREATE TABLE `esp_responses` (
- `id` int(11) NOT NULL AUTO_INCREMENT,
- `school_id` int(11) NOT NULL,
- `key` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
- `value` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
- `active` tinyint(1) DEFAULT NULL,
- `created_at` datetime NOT NULL,
- `updated_at` datetime NOT NULL,
- PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  `description` varchar(255) DEFAULT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE `pages` (
- `id` int(11) NOT NULL AUTO_INCREMENT,
- `name` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
- `parent_id` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
- `created_at` datetime NOT NULL,
- `updated_at` datetime NOT NULL,
- PRIMARY KEY (`id`),
- KEY `index_pages_on_parent_id` (`parent_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  `parent_id` varchar(255) DEFAULT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `index_pages_on_parent_id` (`parent_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE `rails_admin_histories` (
- `id` int(11) NOT NULL AUTO_INCREMENT,
- `message` text COLLATE utf8_unicode_ci,
- `username` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
- `item` int(11) DEFAULT NULL,
- `table` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
- `month` smallint(6) DEFAULT NULL,
- `year` bigint(20) DEFAULT NULL,
- `created_at` datetime NOT NULL,
- `updated_at` datetime NOT NULL,
- PRIMARY KEY (`id`),
- KEY `index_rails_admin_histories` (`item`,`table`,`month`,`year`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `message` text,
+  `username` varchar(255) DEFAULT NULL,
+  `item` int(11) DEFAULT NULL,
+  `table` varchar(255) DEFAULT NULL,
+  `month` smallint(6) DEFAULT NULL,
+  `year` bigint(20) DEFAULT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `index_rails_admin_histories` (`item`,`table`,`month`,`year`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE `response_values` (
- `id` int(11) NOT NULL AUTO_INCREMENT,
- `response_value` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
- `response_label` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
- `collection_id` int(11) DEFAULT NULL,
- `created_at` datetime NOT NULL,
- `updated_at` datetime NOT NULL,
- `category_id` int(11) DEFAULT NULL,
- `response_key` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
- PRIMARY KEY (`id`),
- KEY `index_response_values_on_collection_id` (`collection_id`),
- KEY `index_response_values_on_response_value` (`response_value`)
-) ENGINE=MyISAM AUTO_INCREMENT=339 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `response_value` varchar(255) DEFAULT NULL,
+  `response_label` varchar(255) DEFAULT NULL,
+  `collection_id` int(11) DEFAULT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  `category_id` int(11) DEFAULT NULL,
+  `response_key` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `index_response_values_on_response_value` (`response_value`),
+  KEY `index_response_values_on_collection_id` (`collection_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE `schema_migrations` (
- `version` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
- UNIQUE KEY `unique_schema_migrations` (`version`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+  `version` varchar(255) NOT NULL,
+  UNIQUE KEY `unique_schema_migrations` (`version`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-CREATE TABLE `school` (
- `id` int(11) NOT NULL AUTO_INCREMENT,
- `state` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
- `name` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
- `created_at` datetime NOT NULL,
- `updated_at` datetime NOT NULL,
- PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
-CREATE TABLE `school_collections` (
- `id` int(11) NOT NULL AUTO_INCREMENT,
- `school_id` int(11) DEFAULT NULL,
- `collection_id` int(11) DEFAULT NULL,
- `created_at` datetime NOT NULL,
- `updated_at` datetime NOT NULL,
- `state` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
- PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=7 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
-CREATE TABLE `users` (
- `id` int(11) NOT NULL AUTO_INCREMENT,
- `email` varchar(255) COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
- `encrypted_password` varchar(255) COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
- `reset_password_token` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
- `reset_password_sent_at` datetime DEFAULT NULL,
- `remember_created_at` datetime DEFAULT NULL,
- `sign_in_count` int(11) DEFAULT '0',
- `current_sign_in_at` datetime DEFAULT NULL,
- `last_sign_in_at` datetime DEFAULT NULL,
- `current_sign_in_ip` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
- `last_sign_in_ip` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
- `created_at` datetime NOT NULL,
- `updated_at` datetime NOT NULL,
- PRIMARY KEY (`id`),
- UNIQUE KEY `index_users_on_email` (`email`),
- UNIQUE KEY `index_users_on_reset_password_token` (`reset_password_token`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+CREATE TABLE `school_profile_configurations` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `state` varchar(255) DEFAULT NULL,
+  `configuration_key` varchar(255) DEFAULT NULL,
+  `value` varchar(255) DEFAULT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE `versions` (
- `id` int(11) NOT NULL AUTO_INCREMENT,
- `item_type` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
- `item_id` int(11) NOT NULL,
- `event` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
- `whodunnit` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
- `object` text COLLATE utf8_unicode_ci,
- `created_at` datetime DEFAULT NULL,
- PRIMARY KEY (`id`),
- KEY `index_versions_on_item_type_and_item_id` (`item_type`,`item_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=2818 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `item_type` varchar(255) NOT NULL,
+  `item_id` int(11) NOT NULL,
+  `event` varchar(255) NOT NULL,
+  `whodunnit` varchar(255) DEFAULT NULL,
+  `object` text,
+  `created_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `index_versions_on_item_type_and_item_id` (`item_type`,`item_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 INSERT INTO schema_migrations (version) VALUES ('20130820171959');
 
 INSERT INTO schema_migrations (version) VALUES ('20130820172108');
 
-INSERT INTO schema_migrations (version) VALUES ('20130820172209');
-
 INSERT INTO schema_migrations (version) VALUES ('20130820173218');
 
-INSERT INTO schema_migrations (version) VALUES ('20130820182308');
-
 INSERT INTO schema_migrations (version) VALUES ('20130820182454');
-
-INSERT INTO schema_migrations (version) VALUES ('20130820182738');
-
-INSERT INTO schema_migrations (version) VALUES ('20130820233639');
 
 INSERT INTO schema_migrations (version) VALUES ('20130820233707');
 
 INSERT INTO schema_migrations (version) VALUES ('20130820233922');
-
-INSERT INTO schema_migrations (version) VALUES ('20130823200500');
-
-INSERT INTO schema_migrations (version) VALUES ('20130823200624');
 
 INSERT INTO schema_migrations (version) VALUES ('20130823203302');
 
@@ -223,29 +163,17 @@ INSERT INTO schema_migrations (version) VALUES ('20130906014953');
 
 INSERT INTO schema_migrations (version) VALUES ('20130906022357');
 
-INSERT INTO schema_migrations (version) VALUES ('20130909232048');
-
 INSERT INTO schema_migrations (version) VALUES ('20130910011823');
 
 INSERT INTO schema_migrations (version) VALUES ('20130910163926');
 
 INSERT INTO schema_migrations (version) VALUES ('20130911003057');
 
-INSERT INTO schema_migrations (version) VALUES ('20130911193454');
-
-INSERT INTO schema_migrations (version) VALUES ('20130912023707');
-
-INSERT INTO schema_migrations (version) VALUES ('20130912044432');
-
 INSERT INTO schema_migrations (version) VALUES ('20130916205012');
-
-INSERT INTO schema_migrations (version) VALUES ('20130917180916');
 
 INSERT INTO schema_migrations (version) VALUES ('20130917181944');
 
 INSERT INTO schema_migrations (version) VALUES ('20130917224159');
-
-INSERT INTO schema_migrations (version) VALUES ('20130918042153');
 
 INSERT INTO schema_migrations (version) VALUES ('20131114184219');
 
@@ -264,3 +192,5 @@ INSERT INTO schema_migrations (version) VALUES ('20131224043314');
 INSERT INTO schema_migrations (version) VALUES ('20140114185644');
 
 INSERT INTO schema_migrations (version) VALUES ('20140114224135');
+
+INSERT INTO schema_migrations (version) VALUES ('20140324164349');
