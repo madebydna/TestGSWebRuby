97d378b8372565cb92d76d03fc74b34cffab639e
Hugo Melo <me@hugomelo.com>
2014-04-15 15:04:58 -0700

    remove unused code

--- app/models/schools/school_collection.rb
+++ app/models/schools/school_collection.rb
@@ -10,54 +10,6 @@ class SchoolCollection
     end
   end
 
-  # No longer being used
-  def self.from_hub_city_mapping(hub_city_mapping)
-    collection = Collection.from_hub_city_mapping hub_city_mapping
-    shard = hub_city_mapping.state.downcase.to_sym
-
-    # Important: The School.on_db block is required here, as opposed to a School.on_db chained call.
-    # This is because db_charmer doesn't correctly handle chaining with a pluck() method call, as far as I can tell
-    school_ids = []
-    SchoolMetadata.on_db(shard) do
-      school_ids = SchoolMetadata.collections_ids_to_school_ids.select do |pair|
-        # first element in pair is collection ID, second element is school ID
-        pair[0] == collection.id
-      end.map(&:last)
-    end
-
-    school_collections = school_ids.map do |school_id|
-      SchoolCollection.new(
-        school_id: school_id,
-        school_state: hub_city_mapping.state.downcase,
-        collection: collection
-      )
-    end
-
-    school_collections
-  end
-
-  # Return an array of all SchoolCollections based on what's in hub_city_mapping
-  # Clients that just need a subset, should just use this method (since it'll always need to two queries, one to
-  # get all collections, and one to get the school IDs associated with the collection)
-  #
-  # Results should be cached so these queries execute rarely
-  #
-  #
-  # No longer being used. Turned out to be unnecessary / too much non-needed logic
-  def self.all(state = nil)
-    hub_city_mappings = HubCityMapping.all
-
-    if state.present?
-      hub_city_mappings = hub_city_mappings.select! { |hub_city_mapping| hub_city_mapping.state.match /^#{state}$/i }
-    end
-
-    results = hub_city_mappings.inject([]) do |array, hub_city_mapping|
-      array += SchoolCollection.from_hub_city_mapping(hub_city_mapping)
-      array
-    end
-    results
-  end
-
   # Returns an array of Collection instances for a given school
   def self.for_school(school)
     collection_id = school.school_metadata['collection_id']
