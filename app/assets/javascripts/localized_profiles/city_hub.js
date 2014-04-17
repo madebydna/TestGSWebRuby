801fb7e80f8dcf69b062c847c937353514ad1f09
Hugo Melo <me@hugomelo.com>
2014-04-17 11:32:18 -0700

    refactor use of clearLocalUserCookies

--- app/assets/javascripts/localized_profiles/city_hub.js
+++ app/assets/javascripts/localized_profiles/city_hub.js
@@ -15,6 +15,7 @@ GS.util.isDeveloperWorkstation = function() {
     hostname.match(/^172.18.1.*/) !== null ||
     hostname.match(/^172.21.1.*/) !== null ||
     hostname.indexOf("samson.") != -1 ||
+    hostname.indexOf("hugo.") != -1 ||
     hostname.indexOf("mitchtest.") != -1 ||
     hostname.indexOf("rcox.office.") != -1 ||
     (hostname.match(/.+office.*/) !== null && hostname.indexOf("cpickslay.office") == -1) ||
@@ -23,16 +24,17 @@ GS.util.isDeveloperWorkstation = function() {
 };
 
 GS.hubs.clearLocalUserCookies = function() {
-  // http://www.quirksmode.org/js/cookies.html
-  var date = new Date();
-  date.setTime(date.getTime()+(-2*24*60*60*1000));
-  var expires = "; expires=" + date.toGMTString();
-  var localUserCookieNames = ["hubCity", "hubState", "ishubUser" ,"choosePage", "eventsPage","enrollPage", "eduPage"];
   var isDeveloperWorkstation = GS.util.isDeveloperWorkstation();
+  var localUserCookieNames = ["hubCity", "hubState", "ishubUser" ,"choosePage", "eventsPage","enrollPage", "eduPage"];
   for(var i = 0; i < localUserCookieNames.length; i++) {
-    var localUserCookie = "";
-    localUserCookie = localUserCookieNames[i] + "=" + expires + "; path=/";
-    if(!isDeveloperWorkstation) localUserCookie += "; domain=.greatschools.org";
-    document.cookie = localUserCookie;
+    var opts = { path: '/' }
+    if (!isDeveloperWorkstation) opts['domain'] = '.greatschools.org'
+    $.removeCookie(localUserCookieNames[i])
   }
-};
+}
+
+$(document).ready(function() {
+  $('.js-clear-local-cookies-link').each(function() {
+    $(this).click(GS.hubs.clearLocalUserCookies);
+  });
+});
