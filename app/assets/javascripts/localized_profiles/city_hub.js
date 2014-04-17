b5d0b6c819d2f6a161352a225d354a59048d12f6
Hugo Melo <me@hugomelo.com>
2014-04-17 12:29:18 -0700

    try custom implementation for removing cookies

--- app/assets/javascripts/localized_profiles/city_hub.js
+++ app/assets/javascripts/localized_profiles/city_hub.js
@@ -23,13 +23,31 @@ GS.util.isDeveloperWorkstation = function() {
     hostname.indexOf("macbook") > -1;
 };
 
+
 GS.hubs.clearLocalUserCookies = function() {
+  function createCookie(name, value, days) {
+    var expires;
+
+    if (days) {
+      var date = new Date();
+      date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+      expires = "; expires=" + date.toGMTString();
+    } else {
+      expires = "";
+    }
+    document.cookie = escape(name) + "=" + escape(value) + expires + "; path=/";
+  }
+
+  function eraseCookie(name) {
+    createCookie(name, "", -1);
+  }
+
   var isDeveloperWorkstation = GS.util.isDeveloperWorkstation();
   var localUserCookieNames = ["hubCity", "hubState", "ishubUser" ,"choosePage", "eventsPage","enrollPage", "eduPage"];
   for(var i = 0; i < localUserCookieNames.length; i++) {
     var opts = { path: '/' }
     if (!isDeveloperWorkstation) opts['domain'] = '.greatschools.org'
-    $.removeCookie(localUserCookieNames[i], opts)
+    eraseCookie(localUserCookieNames[i]);
   }
 }
 
