459c7948f85f874c97286e069a656f54c24b065c
Hugo Melo <me@hugomelo.com>
2014-04-17 12:39:15 -0700

    remove unused code

--- app/assets/javascripts/localized_profiles/city_hub.js
+++ app/assets/javascripts/localized_profiles/city_hub.js
@@ -6,23 +6,6 @@ GS.textSwitch = function(el, target, replace) {
 
 GS.uri = GS.uri || {};
 GS.hubs = GS.hubs || {};
-GS.util = GS.util || {};
-
-GS.util.isDeveloperWorkstation = function() {
-  var hostname = window.location.hostname;
-  return hostname.indexOf("localhost") > -1 ||
-    hostname.indexOf("127.0.0.1") > -1 ||
-    hostname.match(/^172.18.1.*/) !== null ||
-    hostname.match(/^172.21.1.*/) !== null ||
-    hostname.indexOf("samson.") != -1 ||
-    hostname.indexOf("hugo.") != -1 ||
-    hostname.indexOf("mitchtest.") != -1 ||
-    hostname.indexOf("rcox.office.") != -1 ||
-    (hostname.match(/.+office.*/) !== null && hostname.indexOf("cpickslay.office") == -1) ||
-    hostname.indexOf("vpn.greatschools.org") != -1 ||
-    hostname.indexOf("macbook") > -1;
-};
-
 
 GS.hubs.clearLocalUserCookies = function() {
   function createCookie(name, value, days) {
@@ -42,11 +25,8 @@ GS.hubs.clearLocalUserCookies = function() {
     createCookie(name, "", -1);
   }
 
-  var isDeveloperWorkstation = GS.util.isDeveloperWorkstation();
   var localUserCookieNames = ["hubCity", "hubState", "ishubUser" ,"choosePage", "eventsPage","enrollPage", "eduPage"];
   for(var i = 0; i < localUserCookieNames.length; i++) {
-    var opts = { path: '/' }
-    if (!isDeveloperWorkstation) opts['domain'] = '.greatschools.org'
     eraseCookie(localUserCookieNames[i]);
   }
 }
