f38c7e76581594d4babbcec80f32f3fdc92c161b
Hugo Melo <me@hugomelo.com>
2014-04-18 08:34:11 -0700

    fix domain assignment logic in cookies concerns

--- app/controllers/concerns/cookie_concerns.rb
+++ app/controllers/concerns/cookie_concerns.rb
@@ -115,7 +115,7 @@ module CookieConcerns
 
     cookie_hash = {}
     cookie_hash[:value] = value
-    cookie_hash[:domain] = config[:domain].presence || :all
+    cookie_hash[:domain] = config[:domain].presence ? config[:domain] : :all
     cookie_hash[:expires] = config[:duration].from_now if config[:duration]
 
     cookies[cookie_name] = cookie_hash
