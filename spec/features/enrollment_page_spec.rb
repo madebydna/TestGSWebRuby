c6aa7161850d64d16f03daa0be6e946abc4dad79
Hugo Melo <me@hugomelo.com>
2014-04-23 15:28:01 -0700

    fix intermittent test failures

--- spec/features/enrollment_page_spec.rb
+++ spec/features/enrollment_page_spec.rb
@@ -1,16 +1,15 @@
 require 'spec_helper'
 
 describe 'Enrollment Page' do
-  before(:each) { visit '/michigan/detroit/enrollment' }
-  before(:all) do
+  before(:each) do
     FactoryGirl.create(:hub_city_mapping)
     FactoryGirl.create(:important_events_collection_config)
     FactoryGirl.create(:collection_nickname, value: 'Detroit')
     FactoryGirl.create(:enrollment_module_configs, quay: 'enrollmentPage_private_preschool_module')
     FactoryGirl.create(:enrollment_module_configs, quay: 'enrollmentPage_public_preschool_module')
   end
-
-  after(:all) { clean_dbs :gs_schooldb }
+  before(:each) { visit '/michigan/detroit/enrollment' }
+  after(:each) { clean_dbs :gs_schooldb }
 
   it 'includes a basic hub page layout' do
     # Header
