2b11a63dbb2114a1b9867f3fe68f9383607abba3
Nanditha Patury <npatury@greatschools.org>
2014-04-23 10:30:44 -0700

    OM-314 made changes to the school cache script

--- script/populate_school_cache_table.rb
+++ script/populate_school_cache_table.rb
@@ -1,37 +1,49 @@
 states = ['mi', 'in', 'wi', 'de']
 state_arg=ARGV[0]
 school_id_arg=ARGV[1]
-cache_key= ARGV[2]
+cache_key_arg= ARGV[2]
+all_cache_keys=['ratings']
 
-#TODO
-#1)use cache_key.Maybe refactor into modules for seperate cache_keys.Also have a enum for keys?
-#2)use logs for debugs in rescue statement
-#3)rescue not unique error?
+def self.create_cache(school, cache_key)
+  begin
+    action = "#{cache_key}_cache_for_school"
+    self.send action, school
+  rescue => error
+    Rails.logger.debug "ERROR: populating school cache for school id: #{school.id} in state: #{school.state}" +
+                           "Exception message: #{error.message}"
+  end
+end
 
 def self.ratings_cache_for_school(school)
   results = TestDataSet.ratings_for_school(school)
   unless (results.nil?)
-    SchoolCache.create(school_id: school.id, name: "ratings_profile", state: school.state, value: results.to_json(:except => [:proficiency_band_id, :school_decile_tops], :methods => [:school_value_text, :school_value_float]))
+    SchoolCache.create(school_id: school.id, name: "ratings", state: school.state, value: results.to_json(:except => [:proficiency_band_id, :school_decile_tops], :methods => [:school_value_text, :school_value_float]))
   end
 end
 
-if !state_arg.nil? && !school_id_arg.nil?
-  school = School.on_db(state_arg.downcase.to_sym).find(school_id_arg)
-  unless (school.nil?)
-    ratings_cache_for_school(school)
-  end
-elsif !state_arg.nil? && school_id_arg.nil?
-  School.on_db(state_arg.downcase.to_sym).all.each do |school|
-    begin
-      ratings_cache_for_school(school)
-    rescue
-      next
+keys = all_cache_keys
+
+if !cache_key_arg.nil?
+  keys.select! { |key| cache_key_arg.to_s.split(',').include?(key) }
+end
+
+keys.each do |cache_key|
+  if !state_arg.nil? && !school_id_arg.nil?
+    school = School.on_db(state_arg.downcase.to_sym).find(school_id_arg)
+    unless (school.nil?)
+      Array(school).each do |school|
+        create_cache(school, cache_key)
+      end
     end
-  end
-else
-  states.each do |state|
-    School.on_db(state.downcase.to_sym).all.each do |school|
-      ratings_cache_for_school(school)
+  elsif !state_arg.nil? && school_id_arg.nil?
+    School.on_db(state_arg.downcase.to_sym).all.each do |school|
+      create_cache(school, cache_key)
+    end
+  else
+    states.each do |state|
+      School.on_db(state.downcase.to_sym).all.each do |school|
+        create_cache(school, cache_key)
+      end
     end
   end
 end
