7247f99456329c3fb795f871d572310f6e5ac34d
Samson Sprouse <ssprouse@greatschools.org>
2014-04-23 23:12:51 -0700

    Reword a few spec examples

--- spec/features/join_features_spec.rb
+++ spec/features/join_features_spec.rb
@@ -1,49 +1,46 @@
 require 'spec_helper'
-describe "Join" do
 
-  describe "/join page" do
+feature "/join page" do
+  feature 'submitting the join form with new email' do
+    before(:each) {
+      clean_models User, SchoolRating
+      clean_models :ca, School, SchoolMetadata
+      visit join_path
+      fill_in 'join-email', with: 'ssprouse+testing@greatschools.org'
+      check 'terms_terms'
+      click_button 'Register email'
+      @email = ActionMailer::Base.deliveries.last
+    }
+    after(:each) do
+      clean_models User, SchoolRating
+      clean_models :ca, School, SchoolMetadata
+    end
 
-    describe 'email verification' do
-      before(:each) {
-        clean_models User, SchoolRating
-        clean_models :ca, School, SchoolMetadata
-        visit join_path
-        fill_in 'join-email', with: 'ssprouse+testing@greatschools.org'
-        check 'terms_terms'
-        click_button 'Register email'
-        @email = ActionMailer::Base.deliveries.last
-      }
-      after(:each) do
-        clean_models User, SchoolRating
-        clean_models :ca, School, SchoolMetadata
+    it 'creates a new user' do
+      expect(User.with_email 'ssprouse+testing@greatschools.org')
+        .to_not be_nil
+    end
+
+    it 'sends an email verification email' do
+      expect(@email).to be_present
+      expect(@email.subject)
+        .to eq 'Please verify your email for GreatSchools'
+    end
+
+    it 'Sends the email to the right person' do
+      expect(@email.to).to include 'ssprouse+testing@greatschools.org'
+    end
+
+    feature 'email verification email' do
+      it 'contains an email verification link' do
+        expect(@email.body).to match verify_email_path
       end
 
-      it 'creates a new user' do
-        expect(User.with_email 'ssprouse+testing@greatschools.org')
-          .to_not be_nil
-      end
-
-      it 'sends an email verification email' do
-        expect(@email).to be_present
-        expect(@email.subject)
-          .to eq 'Please verify your email for GreatSchools' 
-      end
-
-      it 'Sends the email to the right person' do
-        expect(@email.to).to include 'ssprouse+testing@greatschools.org'
-      end
-
-      describe 'email verification email' do
-        it 'contains an email verification link' do
-          expect(@email.body).to match verify_email_path
-        end
-      end
-
-      describe 'visiting the verification link' do
+      feature 'visiting the verification link' do
         let(:verification_link) { @email.body.match(/href=\"(.+)\"/)[1] }
         let(:user) { User.with_email 'ssprouse+testing@greatschools.org' }
-        let(:review) { 
-          FactoryGirl.create(:school_rating, 
+        let(:review) {
+          FactoryGirl.create(:school_rating,
             status: 'pp',
             user: user,
             school: FactoryGirl.create(:school, state: 'ca')
@@ -67,6 +64,6 @@ describe "Join" do
         end
       end
     end
-
+    
   end
 end
