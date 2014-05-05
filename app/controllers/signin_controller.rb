f325539ba30e9049b08cb74f273103962e71a80d
Hugo Melo <me@hugomelo.com>
2014-05-05 16:52:49 -0700

    bring back changes to signin controller

--- app/controllers/signin_controller.rb
+++ app/controllers/signin_controller.rb
@@ -13,10 +13,10 @@ class SigninController < ApplicationController
 
   # gets the join / login form page
   def new
-    set_meta_tags :title => 'Log in to GreatSchools'
-    set_meta_tags :robots => 'noindex'
+    set_meta_tags title: 'Log in to GreatSchools',
+                  robots: 'noindex'
 
-    @active_tab = 'login'
+    @active_tab = params[:tab] || 'login'
     gon.pagename = 'signin/new'
 
     gon.omniture_pagename = 'GS:Admin:Login'
@@ -26,8 +26,8 @@ class SigninController < ApplicationController
   end
 
   def new_join
-    set_meta_tags :title => 'Join GreatSchools'
-    set_meta_tags :robots => 'noindex'
+    set_meta_tags title: 'Join GreatSchools',
+                  robots: 'noindex'
 
     @active_tab = 'join'
     gon.pagename = 'signin/new' # If this is changed, make sure JS is handled, i.e. signin_new-init.js
@@ -63,7 +63,14 @@ class SigninController < ApplicationController
       end
 
       executed_deferred_action
-      redirect_to (overview_page_for_last_school || user_profile_or_home) unless already_redirecting?
+
+      unless already_redirecting?
+        if cookies[:redirect_uri]
+          city_hub_page = URI.decode(cookies[:redirect_uri])
+          delete_cookie :redirect_uri
+        end
+        redirect_to(overview_page_for_last_school || city_hub_page || user_profile_or_home)
+      end
     end
   end
 
@@ -79,7 +86,7 @@ class SigninController < ApplicationController
 
     if logged_in? && redirect_url.present?
       executed_deferred_action
-      redirect_to (redirect_url || overview_page_for_last_school || user_profile_or_home) if !already_redirecting?
+      redirect_to (overview_page_for_last_school || redirect_url || user_profile_or_home) unless already_redirecting?
     else
       redirect_to user_profile_or_home
     end
@@ -97,7 +104,7 @@ class SigninController < ApplicationController
     unless access_token
       Rails.logger.debug('Could not log in with Facebook.')
       flash_error 'Could not log in with Facebook.'
-      redirect_to signin_url
+      redirect_to signin_path
       return nil
     end
 
@@ -107,7 +114,13 @@ class SigninController < ApplicationController
     log_user_in user if error.nil?
 
     executed_deferred_action
-    redirect_to (overview_page_for_last_school || user_profile_or_home) unless already_redirecting?
+    unless already_redirecting?
+      if cookies[:redirect_uri]
+        redirect_uri = URI.decode(cookies[:redirect_uri])
+        delete_cookie :redirect_uri
+      end
+      redirect_to (overview_page_for_last_school || redirect_uri || user_profile_or_home)
+    end
   end
 
   def verify_email
@@ -175,7 +188,6 @@ class SigninController < ApplicationController
   end
 
   def register
-
     user, error = register_user(false, {
       email: params[:email]
     })
