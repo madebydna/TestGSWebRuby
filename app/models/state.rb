6a7277cb35f10e7c55c0abcc2c2f9b6f1a5f8620
Keith McHugh <keith.mchugh@gmail.com>
2014-05-05 17:49:30 -0700

    initial prototyping

--- app/models/state.rb
+++ app/models/state.rb
@@ -1,7 +1,10 @@
 class State < ActiveRecord::Base
   db_magic :connection => :gs_schooldb
   self.table_name = 'state'
+<<<<<<< HEAD
 
   attr_accessible :state, :local, :tier, :tier_week_commitment
   has_many :admin_data_load_schedules, foreign_key: 'state'
+=======
+>>>>>>> initial prototyping
 end
