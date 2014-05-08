b0af663f3c0f0fe92116254f93b055b8eba98935
Christina Liu <cliu@greatschools.org>
2014-05-08 11:17:53 -0700

    PT-429 home page prototype v2 done, need copy, assets, links and route

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,8 +3,7 @@ class HomeController < ApplicationController
 
   def prototype
 
-    @hero_image = "/assets/kitten-hero.jpg"
-    @kitten_1 = "/assets/kitten-article.jpg"
+    @article_1 = "/assets/article_img.jpg"
     @byline = "By Carol Lloyd"
     @parent_img = "/assets/kitten_tiny.jpg"
   end
@@ -17,6 +16,27 @@ class HomeController < ApplicationController
   def format_publications(publications)
     publications.each_value { |pub| pub.create_attributes_for 'title', 'body', 'author' }
     publications
+  end
+
+  def ad_setTargeting_through_gon
+    #if @school.show_ads
+    set_targeting = {}
+    # City, compfilter, county, env, gs_rating, level, school_id, State, type, zipcode, district_id, template
+    set_targeting['City'] = @school.city
+    set_targeting['compfilter'] = 1 + rand(4) # 1-4   Allows ad server to serve 1 ad/page when required by adveritiser
+    set_targeting['county'] = @school.county # county name?
+    set_targeting['env'] = ENV_GLOBAL['advertising_env'] # alpha, dev, product, omega?
+    #set_targeting['gs_rating'] = @school.gs_rating
+    set_targeting['level'] = @school.level_code # p,e,m,h
+    #set_targeting['school_id'] = @school.id
+    set_targeting['State'] = @school.state # abbreviation
+    #set_targeting['type'] = @school.type  # private, public, charter
+    #set_targeting['zipcode'] = @school.zipcode
+    #set_targeting['district_id'] = @school.district.present? ? @school.district.FIPScounty : ""
+    set_targeting['template'] = "ros" # use this for page name - configured_page_name
+
+    gon.ad_set_targeting = set_targeting
+    #end
   end
 
 end
