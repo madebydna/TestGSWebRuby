f5f219119b0980890f107794df947541b57ff235
Keith McHugh <keith.mchugh@gmail.com>
2014-05-08 23:19:42 -0700

    calendar view prototyping

--- app/controllers/admin/data_load_schedules_controller.rb
+++ app/controllers/admin/data_load_schedules_controller.rb
@@ -17,7 +17,7 @@ class Admin::DataLoadSchedulesController < ApplicationController
     @load.year_to_load = p['year_to_load(1i)']
     @load.released = "#{p['released(1i)']}-0#{p['released(2i)']}-0#{p['released(3i)']}"
     @load.acquired = "#{p['acquired(1i)']}-0#{p['acquired(2i)']}-0#{p['acquired(3i)']}"
-    @load.live_by = "#{p['live_by(1i)']}-0#{p['live_by(2i)']}-0#{p['live_by(3i)']}"
+    @load.live_by = "#{p['live_by(1i)']}-0#{p['live_by(2i)']}-#{p['live_by(3i)']}"
     @load.updated_by = p['updated_by']
     if @load.save
       redirect_to '/admin/gsr/data-planning'
--- app/models/admin/data_load_schedule.rb
+++ app/models/admin/data_load_schedule.rb
@@ -3,4 +3,13 @@ class Admin::DataLoadSchedule < ActiveRecord::Base
   self.table_name = 'data_load_schedule'
 
   attr_accessible :state,:description,:load_type,:year_to_load,:released,:acquired,:live_by,:updated,:updated_by
+
+  def live_by_month
+    live_by = self.live_by
+    if live_by.strftime("%d").to_i < 15
+      return "#{live_by.strftime("%B")} 1"
+    else
+      return "#{live_by.strftime("%B")} 15"
+    end
+  end
 end
