882e4030a3c6c0c220a4090a9247d52a5a21d5c5
Harrison <htouw@greatschools.org>
2014-05-21 21:40:11 -0700

    PT-443 Made fragement identifier dynamic in address bar

--- app/assets/javascripts/localized_profiles/page_initializers/signin_new-init.js
+++ app/assets/javascripts/localized_profiles/page_initializers/signin_new-init.js
@@ -1,23 +1,12 @@
 if(gon.pagename == 'signin/new'){
 
     $(function () {
-        var fragment_id = location.hash.substr(1);
-        if (fragment_id == "login" || fragment_id == "join") {
-            if ($('.js-' + fragment_id + '-tab').hasClass('active')) {
-                return
-            } else {
-                $('.js-login-tab').button('toggle');
-                $('.js-join-tab').button('toggle');
-                $('.tab-pane').button('toggle');
-            }
+        
+        if (location.hash.substr(1) == "join") {
+            $('.tab-pane').button('toggle');
+            $('.js-login-tab').button('toggle');
+            $('.js-join-tab').button('toggle');
         }
-
-        $("a[data-toggle='tab']").on("shown", function (e) {
-            var hash = $(e.target).attr("href");
-            if (hash.substr(0,1) == "#") {
-                location.replace("#!" + hash.substr(1));
-            }
-        });
 
         $('#js_login_tab').on('click',function(){
             //Since we are sending data to omniture asynchronously on clicking of the tabs,
@@ -27,6 +16,7 @@ if(gon.pagename == 'signin/new'){
             omnitureObject.pageName = 'GS:Admin:Login';
             omnitureObject.hier1 = 'Account,LogIn';
             sendToOmniture(omnitureObject);
+            location.hash = '';
         });
 
         $('#js_join_tab').on('click',function(){
@@ -37,6 +27,7 @@ if(gon.pagename == 'signin/new'){
             omnitureObject.pageName = 'GS:Admin:CreateAccount';
             omnitureObject.hier1 = 'Account,SignUp';
             sendToOmniture(omnitureObject);
+            location.hash = '#join';
         });
 
         var sendToOmniture = function(omnitureObject){
