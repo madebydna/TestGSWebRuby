212ba804ca5c33c869c635975644ae60032d416d
Hugo Melo <hmelo@greatschools.org>
2014-05-23 13:26:22 -0700

    remove some rails 4 depracation warnings

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -13,8 +13,6 @@ LocalizedProfiles::Application.configure do
   config.assets.precompile << /\.(?:svg|eot|woff|ttf)$/
   config.assets.precompile += ["cycle/jquery.cycle2.js", "cycle/jquery.cycle2.carousel.js", "cycle/carousel_init.js"]
 
-  # Log error messages when you accidentally call methods on nil.
-  config.whiny_nils = true
   # Do not eager load code on boot.
   config.eager_load = false
 
