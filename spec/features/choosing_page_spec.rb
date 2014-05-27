79fd1a033625563a4f01d3f1bd94a9eaed8d7b30
Hugo Melo <hmelo@greatschools.org>
2014-05-27 09:47:57 -0700

    general cleanup

--- spec/features/choosing_page_spec.rb
+++ spec/features/choosing_page_spec.rb
@@ -7,21 +7,20 @@ describe 'Choosing Page' do
 
   context 'on a city choosing page' do
     before(:each) do
-      FactoryGirl.create(:hub_city_mapping)
-      FactoryGirl.create(:choosing_page_links_configs)
-      FactoryGirl.create(:important_events_collection_config)
+      FactoryGirl.create :hub_city_mapping
+      FactoryGirl.create :choosing_page_links_configs
+      FactoryGirl.create :important_events_collection_config
+      visit '/michigan/detroit/choosing-schools/'
     end
 
     it 'displays basic static and dynamic content' do
-      visit '/michigan/detroit/choosing-schools/'
-
       expect(page.title).to eq('Choosing a school in Detroit, MI')
       expect(page).to have_selector('h1', text: '5 simple steps to')
       expect(page).to have_selector('.expandable', count: 4)
 
       step3_links = CollectionConfig.choosing_page_links(configs)
       step3_links.each do |link|
-        expect(page).to have_link(link[:name], href: link[:path])
+        expect(page).to have_link link[:name], href: link[:path]
       end
     end
   end
