8a5e611d9c391ee9eafd7b2178a0e0ef1a3c128d
Nanditha Patury <npatury@GS-0243.local>
2014-05-30 10:35:43 -0700

    OM-445 added ratings breakdowns for state and refactored code

--- app/models/profile_config/ratings_helper.rb
+++ app/models/profile_config/ratings_helper.rb
@@ -7,82 +7,50 @@ class RatingsHelper
     @results = results
   end
 
-  def construct_state_ratings(school)
-    state_rating_configuration = ratings_config.state_rating_configuration
-    state_rating_data_type_ids = ratings_config.state_rating_data_type_ids
+  [:state, :city].each do |var_name|
+    method_name = "construct_#{var_name}_ratings".to_sym
+    define_method method_name do |school|
+      rating_configuration = ratings_config.send "#{var_name}_rating_configuration"
+      rating_data_type_ids = ratings_config.send "#{var_name}_rating_data_type_ids"
 
-    #Hash to hold the state ratings results
-    state_ratings_results = {}
+      #Hash to hold the ratings results
+      ratings_results = {}
+      #Nested hash to hold the rating breakdowns.
+      sub_rating_hash = {}
 
-    #Build a hash of the data_keys to the rating descriptions.
-    description_hash = DataDescription.lookup_table
-    #If configuration exists then loop over the results
-    results.each do |test_data_set|
-      # == 'NULL' is a temporary hack to deal with bad data.
-      if ((state_rating_data_type_ids.include? test_data_set['data_type_id']) && !(test_data_set['school_value_text'].nil?) && !(test_data_set['school_value_text'] == 'NULL'))
-        #Build a hash of the data_keys to the rating descriptions.
-        state_ratings_results = {'overall_rating' => test_data_set['school_value_text'],
-                                 'description' => description_hash[[school.state.upcase,state_rating_configuration['overall']['description_key']]]}
-        break
-      end
-    end
-    methodology_url = get_methodology_url(state_rating_configuration,school)
-    #Only put the url if there is an overall rating.
-    if state_ratings_results['overall_rating'] && methodology_url.present?
-      state_ratings_results['methodology_url'] = methodology_url
-    end
-
-    state_ratings_results
-  end
-
-  def construct_city_ratings(school)
-
-    city_rating_configuration = ratings_config.city_rating_configuration
-    city_rating_data_type_ids = ratings_config.city_rating_data_type_ids
-
-    return {} if city_rating_configuration.nil? || city_rating_data_type_ids.empty?
-
-    #Hash to hold the city ratings results
-    city_ratings_results = {}
-    #Nested hash to hold the rating breakdowns.
-    city_sub_rating_hash = {}
-
-    #Build a hash of the data_keys to the rating descriptions.
-    description_hash = DataDescription.lookup_table
-
-    #Loop over the results
-    results.each do |test_data_set|
-      if (city_rating_data_type_ids.include? test_data_set['data_type_id'])
-
-        if test_data_set['data_type_id'] == city_rating_configuration['overall']['data_type_id']
-          city_ratings_results['overall_rating'] = test_data_set['school_value_text']
-          city_ratings_results['description'] = description_hash[[school.state.upcase,city_rating_configuration['overall']['description_key']]]
-          city_ratings_results['city_rating_label'] = city_rating_configuration['overall']['label']
-        else
-
-          #Loop over the configuration to put the ratings breakdowns in the results.
-          city_rating_configuration['rating_breakdowns'].each do |key, config|
-            if (test_data_set['data_type_id'] == config['data_type_id'] && (!test_data_set['school_value_text'].nil?))
-              city_sub_rating_hash[config['label']] = test_data_set['school_value_text']
+      #Build a hash of the data_keys to the rating descriptions.
+      description_hash = DataDescription.lookup_table
+      #If configuration exists then loop over the results
+      results.each do |test_data_set|
+        if ((rating_data_type_ids.include? test_data_set['data_type_id']))
+          if test_data_set['data_type_id'] == rating_configuration['overall']['data_type_id']
+            ratings_results['overall_rating'] = test_data_set['school_value_text']
+            ratings_results['description'] = description_hash[[school.state.upcase,rating_configuration['overall']['description_key']]]
+            ratings_results["#{var_name}_rating_label"] = rating_configuration['overall']['label']
+          else
+            #Loop over the configuration to put the ratings breakdowns in the results.
+            rating_configuration['rating_breakdowns'].each do |key, config|
+              if (test_data_set['data_type_id'] == config['data_type_id'] && (!test_data_set['school_value_text'].nil?))
+                sub_rating_hash[config['label']] = test_data_set['school_value_text']
+              end
             end
           end
         end
-
       end
-    end
+      methodology_url = get_methodology_url(rating_configuration,school)
 
-    methodology_url = get_methodology_url(city_rating_configuration,school)
-    #Only put the url if there is an overall rating.
-    if city_ratings_results['overall_rating'] && methodology_url.present?
-      city_ratings_results['methodology_url'] = methodology_url
-    end
+      #Only put the url if there is an overall rating.
+      if ratings_results['overall_rating'] && methodology_url.present?
+        ratings_results['methodology_url'] = methodology_url
+      end
 
-    #Only put the sub-ratings if there is an overall rating.
-    if city_ratings_results['overall_rating'] && city_sub_rating_hash.any?
-      city_ratings_results['rating_breakdowns'] = city_sub_rating_hash
-    end
+      #Only put the sub-ratings if there is an overall rating.
+      if ratings_results['overall_rating'] && sub_rating_hash.any?
+        ratings_results['rating_breakdowns'] = sub_rating_hash
+      end
 
-    city_ratings_results
+      ratings_results
+    end
   end
 
 
