5594f3c2f65a3caef1a86a1ca520b1f5a0eef6b6
Hugo Melo <hmelo@greatschools.org>
2014-06-04 14:08:47 -0700

    let programs page render without any data

--- spec/features/programs_page_spec.rb
+++ spec/features/programs_page_spec.rb
@@ -1,47 +1,58 @@
 require 'spec_helper'
 
 describe 'Programs Page' do
+  before { FactoryGirl.create :hub_city_mapping }
   let(:url) { '/michigan/detroit/programs' }
-  before do
-    FactoryGirl.create :hub_city_mapping
-    FactoryGirl.create :programs_heading_config
-    FactoryGirl.create :programs_intro_config
-    FactoryGirl.create :programs_sponsor_config
-    FactoryGirl.create :programs_articles_config
-    FactoryGirl.create :important_events_collection_config
-    visit '/michigan/detroit/programs'
-  end
-  after { clean_dbs :gs_schooldb }
 
+  context 'without data' do
+    before { visit url }
 
-
-  describe 'heading and intro section' do
-    it 'renders the search bar' do
-      expect(page).to have_selector '#js-findByNameBox'
+    it 'renders an empty page' do
+      save_and_open_page
+      expect(page.status_code).to eq(200)
     end
-
-    it 'renders the upcoming events bar' do
-      expect(page).to have_selector '.upcoming-event'
-    end
-
-    it 'displays a configurable heading' do
-      expect(page).to have_selector 'h1', text: 'What makes a great after school or summer program?'
-    end
-
-    it 'renders the intro section' do
-      expect(page).to have_content 'Quality after-school and summer learning opportunities.'
-    end
-
-     it 'shows breadcrumbs' do
-        expect(page).to have_css("span[itemtype='http://data-vocabulary.org/Breadcrumb']", count: 2)
-     end
   end
 
-  it 'renders the sponsor section' do
-    expect(page).to have_content 'description of an excellent sponsor'
-  end
+  context 'by default' do
+    before do
 
-  it 'renders the articles section' do
-    expect(page).to have_selector 'h4', text: 'Facebook Aquires Greatschools'
+      FactoryGirl.create :programs_heading_config
+      FactoryGirl.create :programs_intro_config
+      FactoryGirl.create :programs_sponsor_config
+      FactoryGirl.create :programs_articles_config
+      FactoryGirl.create :important_events_collection_config
+      visit url
+    end
+    after { clean_dbs :gs_schooldb }
+
+    describe 'heading and intro section' do
+      it 'renders the search bar' do
+        expect(page).to have_selector '#js-findByNameBox'
+      end
+
+      it 'renders the upcoming events bar' do
+        expect(page).to have_selector '.upcoming-event'
+      end
+
+      it 'displays a configurable heading' do
+        expect(page).to have_selector 'h1', text: 'What makes a great after school or summer program?'
+      end
+
+      it 'renders the intro section' do
+        expect(page).to have_content 'Quality after-school and summer learning opportunities.'
+      end
+    end
+
+    it 'renders the sponsor section' do
+      expect(page).to have_content 'description of an excellent sponsor'
+    end
+
+    it 'renders the articles section' do
+      expect(page).to have_selector 'h4', text: 'Facebook Aquires Greatschools'
+    end
+
+    it 'shows breadcrumbs' do
+      expect(page).to have_css("span[itemtype='http://data-vocabulary.org/Breadcrumb']", count: 2)
+    end
   end
 end
