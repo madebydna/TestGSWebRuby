3d77873571818da798a728dd0b6c812e364fb66f
Anthony Roy <aroy@greatschools.org>
2014-06-11 11:14:29 -0700

    PT-481 Fix login case I inadvertently broke. Increase rspec coverage

--- app/controllers/signin_controller.rb
+++ app/controllers/signin_controller.rb
@@ -68,7 +68,7 @@ class SigninController < ApplicationController
           city_hub_page = URI.decode(cookies[:redirect_uri])
           delete_cookie :redirect_uri
         end
-        redirect_to (overview_page_for_last_school || city_hub_page || join_url)
+        redirect_to (overview_page_for_last_school || city_hub_page || (should_attempt_login ? home_url : join_url))
       end
     end
   end
