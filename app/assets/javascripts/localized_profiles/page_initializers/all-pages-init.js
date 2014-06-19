d5059385b8a72ac75f85cdd11f856e9dca6113d1
Samson Sprouse <ssprouse@greatschools.org>
2014-06-18 18:56:56 -0700

    OM-518 Add Apply Now button to schools that have an apply_now_url in school_metadata

--- app/assets/javascripts/localized_profiles/page_initializers/all-pages-init.js
+++ app/assets/javascripts/localized_profiles/page_initializers/all-pages-init.js
@@ -1,58 +1,64 @@
 GS.omniture = GS.omniture || function() {
 
-    //Track the start of "review a school".OM-263
-    var track_reviews = function(driver){
-        GS.track.setSPropsAndEvarsInCookies('review_updates_mss_traffic_driver',driver,'evars');
-        GS.track.setEventsInCookies('review_updates_mss_start_event');
-        GS.track.setSPropsAndEvarsInCookies('custom_completion_sprop','PublishReview','sprops');
-    };
+  //Track the start of "review a school".OM-263
+  var track_reviews = function(driver){
+    GS.track.setSPropsAndEvarsInCookies('review_updates_mss_traffic_driver',driver,'evars');
+    GS.track.setEventsInCookies('review_updates_mss_start_event');
+    GS.track.setSPropsAndEvarsInCookies('custom_completion_sprop','PublishReview','sprops');
+  };
 
-    return {
-        track_reviews: track_reviews
-    };
+  return {
+    track_reviews: track_reviews
+  };
 }();
-
 
 //Reads the omniture variables from gon and cookies and sets them.
 GS.track.setOmnitureData();
 
 $(function() {
-    // even though this code is simple, I'd rather it be an actual module, i.e. GS.sendMeUpdates,
-    // since it's easier to test
-    $('.js-send-me-updates-button-header').on('click', function () {
-        $('#js-send-me-updates-form-header').submit();
-    });
+  // even though this code is simple, I'd rather it be an actual module, i.e. GS.sendMeUpdates,
+  // since it's easier to test
+  $('.js-send-me-updates-button-header').on('click', function () {
+      $('#js-send-me-updates-form-header').submit();
+  });
 
-    $('.js-send-me-updates-button-footer').on('click', function () {
-        $('#js-send-me-updates-form-footer').submit();
-    });
+  $('.js-send-me-updates-button-footer').on('click', function () {
+      $('#js-send-me-updates-form-footer').submit();
+  });
 
-    $('.js-save-this-school-button').on('click', function () {
-        $('#js-save-this-school-form').submit();
-    });
+  $('.js-save-this-school-button').on('click', function () {
+      $('#js-save-this-school-form').submit();
+  });
 
-    $('.js-button-link').on('click', function() {
-        window.location.href = $(this).data("link-value");
-    });
+  $('.js-button-link').on('click', function() {
+    var use_new_window = $(this).data('link-use-new-window');
+    var url = $(this).data('link-value');
+    if(use_new_window == true) {
+      window.open(url, '_blank');
+    } else {
+      window.location.href = url;
+    }
+  });
 
-    $('.js_toggle_parent_sib').on('click', function(){
-        $(this).parent().siblings('div').slideToggle('fast');
-        if($(this).html() == 'Close'){
-            $(this).html('Learn More &raquo;');
-        }
-        else{
-            $(this).html('Close');
-        }
-    });
-    $('.js-connect-with-us-buttons').on({
-      mouseenter: function() {
-        var cssClass = $(this).attr('class');
-        $(this).attr('class', cssClass + '-c');
-      },
-      mouseleave: function() {
-        var cssClass = $(this).attr('class');
-        cssClass = cssClass.replace(new RegExp('-c$'), '');
-        $(this).attr('class', cssClass);
-      }
-    }, 'span');
+  $('.js_toggle_parent_sib').on('click', function(){
+    $(this).parent().siblings('div').slideToggle('fast');
+    if($(this).html() == 'Close'){
+      $(this).html('Learn More &raquo;');
+    }
+    else{
+      $(this).html('Close');
+    }
+  });
+
+  $('.js-connect-with-us-buttons').on({
+    mouseenter: function() {
+      var cssClass = $(this).attr('class');
+      $(this).attr('class', cssClass + '-c');
+    },
+    mouseleave: function() {
+      var cssClass = $(this).attr('class');
+      cssClass = cssClass.replace(new RegExp('-c$'), '');
+      $(this).attr('class', cssClass);
+    }
+  }, 'span');
 });
--- spec/features/localized_profiles/school_profile_overview_features_spec.rb
+++ spec/features/localized_profiles/school_profile_overview_features_spec.rb
@@ -3,8 +3,15 @@ require 'features/localized_profiles/school_profile_features_spec'
 
 feature 'School profile overview page' do
   let!(:profile_page) { FactoryGirl.create(:page, name: 'Overview') }
+  let(:school) do
+    FactoryGirl.create(:alameda_high_school)
+  end
+  subject do
+    visit school_path(school)
+    page
+  end
   after do
-    clean_models Page
+    clean_models School, Page
   end
 
   it_behaves_like 'school profile page', 'Overview'
@@ -15,10 +22,6 @@ feature 'School profile overview page' do
         s = FactoryGirl.build(:washington_dc_ps_head_start)
         s.on_db(:dc).save
         s
-      end
-      subject do
-        visit school_path(school)
-        page
       end
       after do
         clean_models :dc, School
@@ -32,16 +35,37 @@ feature 'School profile overview page' do
       let(:school) do
         FactoryGirl.create(:south_san_francisco_high_school)
       end
-      subject do
-        visit school_path(school)
-        page
-      end
-      after do
-        clean_models School
-      end
       scenario 'State and city breadcrumbs says "/ California / San Francisco"' do
         expect(subject).to have_content '/ California / San Francisco'
       end
     end
   end
+
+  feature 'Apply now button' do
+    after do
+      clean_models :ca, School, SchoolMetadata
+    end
+    let(:apply_now_url) do
+      SchoolMetadata.create(
+        school_id: school.id,
+        meta_key: 'apply_now_url',
+        meta_value: 'http://www.schoolchoicede.org/ApplyInfo/AppoKN'
+      )
+    end
+
+    context 'when on a school that has apply now url in school metadata' do
+      before { apply_now_url }
+
+      scenario 'An Apply Now button appears' do
+        expect(subject).to have_button 'Apply now'
+      end
+    end
+
+    context 'when on a school without an apply now url' do
+      scenario 'An Apply Now button does not appear' do
+        expect(subject).to_not have_button 'Apply now'
+      end
+    end
+  end
+
 end
