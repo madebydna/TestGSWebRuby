cfa4b3e023c327c5c88ae99b8a451b64734a50e2
Nanditha Patury <npatury@GS-0243.local>
2014-06-24 16:10:56 -0700

    OM-517 initial work for displaying enrollment section

--- app/helpers/section_helper.rb
+++ app/helpers/section_helper.rb
@@ -1,17 +1,29 @@
 module SectionHelper
   def display_section_head_link(category_placement, link_text=nil)
-    page_title = ['Reviews', 'Details', 'Quality']
+    page_titles = ['reviews', 'details', 'quality']
     return_str = ''
     title = category_placement.title
+    data_config = category_placement.layout_config_json
+    link_text ||= data_config['link_text']
+    link_page = data_config['link_page']
 
-    if title.present? && (page_title.include? title)
+    link_text ||= 'See all '+title.capitalize if title.present?
+    link_page ||= category_placement.title
+
+    if link_text.present? && link_page.present? && (page_titles.include? link_page.downcase)
       return_str << '<div class="fr prm pt8">'
-      return_str << '<a href="'+ title.downcase
-      return_str << '/">'
-      return_str << (link_text.nil? ? 'See all '+title.capitalize  : link_text)
-      return_str << '</a>'
+      return_str << section_header_link(link_page,link_text)
+
       return_str << '</div>'
     end
     return_str
+  end
+
+  def section_header_link(page, link_text)
+    helper_name = 'school_'
+    helper_name << "#{page.downcase}_" if page != 'overview'
+    helper_name << 'path'
+    path = self.send helper_name.to_sym, @school
+    link_to link_text,path
   end
 end
