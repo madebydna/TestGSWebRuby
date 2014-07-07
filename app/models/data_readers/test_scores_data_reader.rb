9dbd50549166d8f6c92ef25a60e62f0e2e892d83
Samson Sprouse <ssprouse@greatschools.org>
2014-07-07 16:12:25 -0700

    Removed a binding.pry that was left in

--- app/models/data_readers/test_scores_data_reader.rb
+++ app/models/data_readers/test_scores_data_reader.rb
@@ -2,9 +2,6 @@ class TestScoresDataReader < SchoolProfileDataReader
 
   def data
     @data ||= TestScoreResults.new.fetch_test_scores school
-    require 'pry-debugger'
-    binding.pry
-    @data
   end
 
 end
