fcc9e73e4a3673b5bb1980a0a7bcada260f112a2
Harrison <htouw@greatschools.org>
2014-07-08 18:04:52 -0700

    Refactored SchoolSearchService Factory

--- spec/decorators/school_search_result_decorator_spec.rb
+++ spec/decorators/school_search_result_decorator_spec.rb
@@ -3,6 +3,6 @@ require 'decorators/concerns/grade_level_concerns_shared'
 
 describe SchoolSearchResultDecorator do
   it_behaves_like 'a school that has grade levels' do
-    let(:school) { SchoolProfileDecorator.decorate(FactoryGirl.build(:school_search_result)) }
+    let(:school) { SchoolSearchResultDecorator.decorate(FactoryGirl.build(:school_search_result)) }
   end
 end
