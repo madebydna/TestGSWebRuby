2b483c9313224a25d7acf445a09d072b69c03f6d
Samson Sprouse <ssprouse@greatschools.org>
2014-07-10 20:51:16 -0700

    PT-558 Added force_review_moderation? method to PropertyConfig. When saving review, if force_review_moderation is true and review would otherwise be published, make it unpublished instead.

--- app/models/property_config.rb
+++ app/models/property_config.rb
@@ -6,4 +6,11 @@ class PropertyConfig < ActiveRecord::Base
     pc_sweepstakes = PropertyConfig.where(quay: 'sweepstakes')
     pc_sweepstakes.present? ? pc_sweepstakes.first.value == 'true' : false
   end
+
+  def self.force_review_moderation?
+    Rails.cache.fetch('PropertyConfig/force_review_moderation', expires_in: 2.minutes) do
+      property = PropertyConfig.where(quay: 'force_review_moderation')
+      property.present? ? property.first.value == 'true' : false
+    end
+  end
 end
