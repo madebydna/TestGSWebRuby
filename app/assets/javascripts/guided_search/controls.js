26ba14c8a02daafe88b158a2e50d6f756f5873ac
Shomi Arora <sarora@greatschools.org>
2014-07-11 10:44:00 -0700

    AT-408 Clean up code.

--- app/assets/javascripts/guided_search/controls.js
+++ app/assets/javascripts/guided_search/controls.js
@@ -1,39 +1,38 @@
 GS.guidedSearch = GS.guidedSearch || {};
 
 $(function() {
+    var displayNone='dn';
+    var active='active';
+    var id_tab_number='.js-tab-number';
+    var id_tab_check='.js-tab-check';
 
     $('.js-guided-search-next').on('click',function(){
-       var next_tab= $(this).data('next-tab');
-       changeTabs(next_tab);
-        $(".tab_"+next_tab).prev().find('.js-tab-number').addClass('dn');
-        $(".tab_"+next_tab).prev().find('.js-tab-check').removeClass('dn');
+        var next_tab= $(this).data('next-tab');
+        $(".tab_"+next_tab).prev().find(id_tab_number).addClass(displayNone);
+        $(".tab_"+next_tab).prev().find(id_tab_check).removeClass(displayNone);
+        changeTabs(next_tab);
     });
     $('.js-guided-search-back').on('click',function(){
         var previous_tab= $(this).data('previous-tab');
-        console.log( $(".tab_"+previous_tab).next());
-        $(".tab_"+previous_tab).next().find('.js-tab-number').removeClass('dn');
-        $(".tab_"+previous_tab).next().find('.js-tab-check').addClass('dn');
-        $(".tab_"+previous_tab).find('.js-tab-number').removeClass('dn');
-        $(".tab_"+previous_tab).find('.js-tab-check').addClass('dn');
-
+        $(".tab_"+previous_tab).next().find(id_tab_number).removeClass(displayNone);
+        $(".tab_"+previous_tab).next().find(id_tab_check).addClass(displayNone);
+        $(".tab_"+previous_tab).find(id_tab_number).removeClass(displayNone);
+        $(".tab_"+previous_tab).find(id_tab_check).addClass(displayNone);
         changeTabs(previous_tab);
 
     });
     $('.js-guided-search-submit').on('click',function(){
-//        $(this).find('.js-tab-number').addClass('dn');
-//        $(this).find('.js-tab-check').removeClass('dn');
+        var current_tab= $(this).data('current-tab');
+        $(".tab_"+current_tab).find(id_tab_number).addClass(displayNone);
+        $(".tab_"+current_tab).find(id_tab_check).removeClass(displayNone);
 
 
     });
 
  var changeTabs=function(tab){
-     $(".tab_"+tab).siblings().removeClass('active');
-     $(".tab_"+tab).addClass('active');
-     $("#"+tab).siblings().removeClass('active');
-     $("#"+tab).addClass('active');
-//     $(".tab_"+tab).prevAll().each(function(){
-//         $(this).find('.js-tab-number').addClass('dn');
-//         $(this).find('.js-tab-check').removeClass('dn');
-//     })
+     $(".tab_"+tab).siblings().removeClass(active);
+     $(".tab_"+tab).addClass(active);
+     $("#"+tab).siblings().removeClass(active);
+     $("#"+tab).addClass(active);
  }
 });
