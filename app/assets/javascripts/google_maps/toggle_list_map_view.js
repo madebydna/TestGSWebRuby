3b430a0dc97cc0bfe7580b0f0e530d3d4ec20111
Christina Liu <cliu@greatschools.org>
2014-07-18 15:50:43 -0700

    PT-563 toggle indicator of active list or map view, with sprites

--- app/assets/javascripts/google_maps/toggle_list_map_view.js
+++ app/assets/javascripts/google_maps/toggle_list_map_view.js
@@ -1,12 +1,12 @@
 $(document).ready(function () {
     $('.js-search-list-view').on("click", function () {
         $('#js-map-canvas').hide('fast');
-        $(this).children('span').addClass('i-24-orange-star').removeClass('i-24-grey-star');
-        $(this).siblings('.js-search-map-view').children('span').addClass('i-24-grey-star').removeClass('i-24-orange-star');
+        $(this).children('span').addClass('i-16-blue-list-view').removeClass('i-16-grey-list-view');
+        $(this).siblings('.js-search-map-view').children('span').addClass('i-16-grey-map-view').removeClass('i-16-blue-map-view');
     });
     $('.js-search-map-view').on("click", function () {
         $('#js-map-canvas').show('slow');
-        $(this).children('span').addClass('i-24-orange-star').removeClass('i-24-grey-star');
-        $(this).siblings('.js-search-list-view').children('span').addClass('i-24-grey-star').removeClass('i-24-orange-star');
-    });
+        $(this).children('span').addClass('i-16-blue-map-view').removeClass('i-16-grey-map-view');
+        $(this).siblings('.js-search-list-view').children('span').addClass('i-16-grey-list-view').removeClass('i-16-blue-list-view');
+});
 });
