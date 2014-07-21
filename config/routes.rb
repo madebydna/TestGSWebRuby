f91aab3f42946e8119854b896d3e6961dbc285ad
mseltzer <mseltzer@greatschools.org>
2014-07-21 13:20:44 -0700

    OM-595 adding link to district boundaries from contact us map

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
+
 LocalizedProfiles::Application.routes.draw do
   require 'states'
   require 'regular_subdomain'
@@ -60,6 +61,7 @@ LocalizedProfiles::Application.routes.draw do
     get '/catalog/pdf/SpringSweepsRules.pdf', as: :sweepstakes_rules
     get '/understanding-common-core-state-standards.topic?content=7802', as: :common_core
     get '/schools/cities/:state_long/:state_short/:letter', as: :city_alphabet
+    get '/school-district-boundaries-map/', as: :district_boundary
   end
 
   namespace :admin, controller: 'admin', path: '/admin/gsr' do
