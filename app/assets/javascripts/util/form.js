559874355b22d52a5fe5aafeec62c69e920b8f43
Christina Liu <cliu@greatschools.org>
2014-07-23 13:24:32 -0700

    PT-575 replaced old checkboxes with custom check boxes

--- app/assets/javascripts/util/form.js
+++ app/assets/javascripts/util/form.js
@@ -42,17 +42,35 @@ $(function() {
     });
     $("body").on('click','.js-gs-popover-checkbox',function(){
 
+
+        $('.js-gs-popover').on('click',function(){
+            alert('I am here');
+            var self=$(this);
+            var checkbox = self.children(".js-icon");
+            var hidden_field = self.siblings(".js-gs-checkbox-value");
+            var gs_checkBox= self.data('gs-checkbox');
+            if (hidden_field.val()== '') {
+                checkbox.removeClass('i-24-checkmark-off').addClass('i-24-checkmark-on');
+                hidden_field.val(gs_checkBox);
+            }else {
+                checkbox.removeClass('i-24-checkmark-on').addClass('i-24-checkmark-off');
+                hidden_field.val('');
+            }
+        });
+    });
+
+    $('.js-gs-checkbox-search').on('click',function(){
         var self=$(this);
         var checkbox = self.children(".js-icon");
         var hidden_field = self.siblings(".js-gs-checkbox-value");
-        var gs_checkBox= self.data('gs-checkbox');
+        var gs_checkBox= self.data('gs-checkbox-value');
+        var gs_checkBoxCategory= self.data('gs-checkbox-category');
         if (hidden_field.val()== '') {
-            checkbox.removeClass('i-24-checkmark-off').addClass('i-24-checkmark-on');
-            hidden_field.val(gs_checkBox);
-        }else {
-            checkbox.removeClass('i-24-checkmark-on').addClass('i-24-checkmark-off');
-            hidden_field.val('');
+            checkbox.removeClass('i-grey-unchecked-box').addClass('i-16-blue-check-box');
+            hidden_field.attr("value", gs_checkBox).attr("name", gs_checkBoxCategory);
+        } else {
+            checkbox.removeClass('i-16-blue-check-box').addClass('i-grey-unchecked-box');
+            hidden_field.removeAttr("value").removeAttr("name");
         }
-
     });
 });
