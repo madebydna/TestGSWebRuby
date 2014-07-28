af60e21400c35b8cc39ceef01966a9b3cc7f58b6
Anthony Roy <aroy@greatschools.org>
2014-07-28 10:12:46 -0700

    PT-569 Search UI: responsive/mobile list item

--- app/decorators/school_search_result_decorator.rb
+++ app/decorators/school_search_result_decorator.rb
@@ -5,4 +5,12 @@ class SchoolSearchResultDecorator < SchoolProfileDecorator
   #ToDo change to decorates :SchoolSearchResult when code is pushed
   decorates :school_search_result
   delegate_all
+
+  def strong_fit?
+    max_fit_score > 0 && (fit_score / max_fit_score) > 0.5
+  end
+
+  def ok_fit?
+    max_fit_score > 0 && fit_score > 0 && (fit_score / max_fit_score) <= 0.5
+  end
 end
