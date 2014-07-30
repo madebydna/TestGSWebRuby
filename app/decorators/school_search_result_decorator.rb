ae8ab8bcde494070181ed98a942f283f8c8f2a46
Anthony Roy <aroy@greatschools.org>
2014-07-30 14:53:31 -0700

    PT-601 Alter fit score algorithm to become three tier

--- app/decorators/school_search_result_decorator.rb
+++ app/decorators/school_search_result_decorator.rb
@@ -6,11 +6,18 @@ class SchoolSearchResultDecorator < SchoolProfileDecorator
   decorates :school_search_result
   delegate_all
 
+  STRONG_FIT_CUTOFF = 0.666
+  OK_FIT_CUTOFF = 0.333
+
   def strong_fit?
-    max_fit_score > 0 && (fit_score / max_fit_score.to_f) > 0.5
+    max_fit_score > 0 && (fit_score / max_fit_score.to_f) >= STRONG_FIT_CUTOFF
   end
 
   def ok_fit?
-    max_fit_score > 0 && fit_score > 0 && (fit_score / max_fit_score.to_f) <= 0.5
+    max_fit_score > 0 && fit_score > 0 && (fit_score / max_fit_score.to_f) >= OK_FIT_CUTOFF && (fit_score / max_fit_score.to_f) < STRONG_FIT_CUTOFF
+  end
+
+  def weak_fit?
+    max_fit_score > 0 && fit_score > 0 && (fit_score / max_fit_score.to_f) < OK_FIT_CUTOFF
   end
 end
