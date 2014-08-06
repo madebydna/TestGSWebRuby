02d8ce6168a78f747ccec78bf4c0ace6b2aaafe0
Keith McHugh <kmchugh@greatschools.org>
2014-08-06 12:55:02 -0700

    PT-596 toggle active state on hover for desktop list/map

--- app/assets/javascripts/google_maps/toggle_list_map_view.js
+++ app/assets/javascripts/google_maps/toggle_list_map_view.js
@@ -8,11 +8,25 @@ $(document).ready(function () {
     var elemMapViewToggle =  $('.js-search-map-view');
     var elemMapViewMobileToggle =  $('.js-search-toggle-map-view');
 
-    var hideMapView = function() {
-        elemMapCanvas.hide('fast');
-        elemListViewToggle.find('span').addClass(listViewHighlighted).removeClass(listViewNormal);
-        elemMapViewToggle .find('span').addClass(mapViewNormal)      .removeClass(mapViewHighlighted);
-        $.cookie('map_view', 'false', { path: '/' });
+    var addActiveToggleStateFor = function (toggleType) {
+        if (toggleType == 'list') {
+            elemListViewToggle.find('span').addClass(listViewHighlighted).removeClass(listViewNormal);
+            elemListViewToggle.addClass('brand-primary');
+        }
+        else if (toggleType == 'map') {
+            elemMapViewToggle .find('span').addClass(mapViewHighlighted).removeClass(mapViewNormal);
+            elemMapViewToggle.addClass('brand-primary');
+        }
+    };
+    var removeActiveToggleStateFor = function (toggleType) {
+        if (toggleType == 'list') {
+            elemListViewToggle.find('span').addClass(listViewNormal)    .removeClass(listViewHighlighted);
+            elemListViewToggle.removeClass('brand-primary');
+        }
+        else if (toggleType == 'map') {
+            elemMapViewToggle .find('span').addClass(mapViewNormal)      .removeClass(mapViewHighlighted);
+            elemMapViewToggle.removeClass('brand-primary');
+        }
     };
     var initAndShowMap = function () {
         GS.search.googleMap.init();
@@ -21,14 +35,36 @@ $(document).ready(function () {
         google.maps.event.trigger(map, 'resize');
         map.setCenter(center);
     };
+    var hideMapView = function() {
+        elemMapCanvas.hide('fast');
+        addActiveToggleStateFor('list');
+        removeActiveToggleStateFor('map');
+        $.cookie('map_view', 'false', { path: '/' });
+    };
     var showMapView = function() {
         elemMapCanvas.show('slow',initAndShowMap);
-        elemMapViewToggle .find('span').addClass(mapViewHighlighted).removeClass(mapViewNormal);
-        elemListViewToggle.find('span').addClass(listViewNormal)    .removeClass(listViewHighlighted);
+        addActiveToggleStateFor('map');
+        removeActiveToggleStateFor('list');
         $.cookie('map_view', 'true', { path: '/' });
     };
     elemListViewToggle.on("click", hideMapView);
+    elemListViewToggle.hover(
+        function () { addActiveToggleStateFor('list') },
+        function () {
+            if($.cookie('map_view') === 'true') {
+                removeActiveToggleStateFor('list');
+            }
+        }
+    );
     elemMapViewToggle.on("click", showMapView);
+    elemMapViewToggle.hover(
+        function () { addActiveToggleStateFor('map') },
+        function () {
+            if($.cookie('map_view') === 'false') {
+                removeActiveToggleStateFor('map');
+            }
+        }
+    );
     elemMapViewMobileToggle.on("click", function() {
         var currentlyVisible = (elemMapCanvas.filter(":visible").length == 1);
         if (currentlyVisible) {
@@ -39,5 +75,8 @@ $(document).ready(function () {
     });
     if ($.cookie('map_view') === 'false') {
         hideMapView();
+    }
+    else {
+        showMapView();
     }
 });
