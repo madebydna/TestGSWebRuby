bd01dfd64a26bddf8fa6a010f6c3c50a9fee872c
mseltzer <mseltzer@greatschools.org>
2014-08-07 11:38:08 -0700

    OM-576 - added check that review_id is numeric

--- app/controllers/simple_ajax_controller.rb
+++ app/controllers/simple_ajax_controller.rb
@@ -6,14 +6,21 @@ class SimpleAjaxController < ApplicationController
 
   def create_helpful_review
     review_id = params[:review_id]
-    ip = remote_ip()
-    hr = HelpfulReview.new
-    hr.review_id = review_id
-    hr.ipaddress = ip
-    hr.save
+    if review_id.numeric?
+      ip = remote_ip()
+      hr = HelpfulReview.new
+      hr.review_id = review_id
+      hr.ipaddress = ip
+      hr.save
 
-    respond_to do |format|
-      format.json { render json:  HelpfulReview.helpful_counts_by_id(review_id)}
+
+      respond_to do |format|
+        format.json { render json:  HelpfulReview.helpful_counts_by_id(review_id)}
+      end
+    else
+      respond_to do |format|
+        format.json { render json:  {'failed' => 'needs number'} }
+      end
     end
   end
 
