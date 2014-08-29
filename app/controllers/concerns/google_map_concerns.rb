80fd0e51856ee6831f0aac796e265ba2ab319244
Christina Liu <cliu@greatschools.org>
2014-08-29 11:47:04 -0700

    refactor google map to own module

---
+++ app/controllers/concerns/google_map_concerns.rb
@@ -0,0 +1,38 @@
+module GoogleMapConcerns
+  extend ActiveSupport::Concern
+
+  def mapping_points_through_gon
+    gon.map_points = @map_schools.map do |school|
+      begin
+        map_points = SchoolSearchResultDecorator.decorate(school).google_map_data_point
+        map_points[:communityRatingStars] = school.community_rating.nil? ? '' : (draw_stars_16 school.community_rating)
+        map_points[:profileUrl] = "/#{@state[:long]}/city/#{school.id}-school"
+        map_points[:reviewUrl] = "#{map_points[:profileUrl]}/reviews"
+        map_points[:zillowUrl] = zillow_url(school)
+        school.latitude.nil? ? next : map_points[:lat] = school.latitude
+        school.longitude.nil? ? next : map_points[:lng] = school.longitude
+        map_points[:numReviews] = school.review_count.nil? ? 0 : school.review_count
+        map_points[:zIndex] = -1 unless school.on_page
+        map_points
+      rescue NoMethodError => e
+        puts e.message
+        puts 'School Not Added as a Map Pin'
+        nil
+      else
+        map_points
+      end
+    end.compact
+  end
+
+  def assign_sprite_files_though_gon
+    sprite_files = {}
+    sprite_files['imageUrlOffPage'] = view_context.image_path('icons/140710-10x10_dots_icons.png')
+    sprite_files['imageUrlOnPage'] = view_context.image_path('icons/140725-29x40_pins.png')
+
+    gon.sprite_files = sprite_files
+
+  end
+
+
+
+end
--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,6 +3,7 @@ class SearchController < ApplicationController
   include MetaTagsHelper
   include ActionView::Helpers::TagHelper
   include PaginationConcerns
+  include GoogleMapConcerns
 
   #Todo move before filters to methods
   before_action :set_verified_city_state, only: [:city_browse, :district_browse]
@@ -344,38 +345,6 @@ class SearchController < ApplicationController
     set_targeting['template'] = 'search' # use this for page name - configured_page_name
 
     gon.ad_set_targeting = set_targeting
-  end
-
-  def mapping_points_through_gon
-    gon.map_points = @map_schools.map do |school|
-      begin
-        map_points = SchoolSearchResultDecorator.decorate(school).google_map_data_point
-        map_points[:communityRatingStars] = school.community_rating.nil? ? '' : (draw_stars_16 school.community_rating)
-        map_points[:profileUrl] = "/#{@state[:long]}/city/#{school.id}-school"
-        map_points[:reviewUrl] = "#{map_points[:profileUrl]}/reviews"
-        map_points[:zillowUrl] = zillow_url(school)
-        school.latitude.nil? ? next : map_points[:lat] = school.latitude
-        school.longitude.nil? ? next : map_points[:lng] = school.longitude
-        map_points[:numReviews] = school.review_count.nil? ? 0 : school.review_count
-        map_points[:zIndex] = -1 unless school.on_page
-        map_points
-      rescue NoMethodError => e
-        puts e.message
-        puts 'School Not Added as a Map Pin'
-        nil
-      else
-        map_points
-      end
-    end.compact
-  end
-
-  def assign_sprite_files_though_gon
-    sprite_files = {}
-    sprite_files['imageUrlOffPage'] = view_context.image_path('icons/140710-10x10_dots_icons.png')
-    sprite_files['imageUrlOnPage'] = view_context.image_path('icons/140725-29x40_pins.png')
-
-    gon.sprite_files = sprite_files
-
   end
 
   def calculate_fit_score(results, params_hash)
