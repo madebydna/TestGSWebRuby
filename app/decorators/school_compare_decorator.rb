d5ef112feb9ced7e3a3833189dc26d50288e9c94
Keith McHugh <kmchugh@greatschools.org>
2014-09-02 11:52:36 -0700

    PT-686 compare: ratings accessors

--- app/decorators/school_compare_decorator.rb
+++ app/decorators/school_compare_decorator.rb
@@ -136,8 +136,20 @@ class SchoolCompareDecorator < SchoolProfileDecorator
     cache_data['ratings'] || {}
   end
 
-  def overall_gs_rating
-    overall_ratings_obj = ratings.find { |rating| rating['data_type_id'] == 174  }
+  def great_schools_rating
+    school_rating_by_data_type_id(174)
+  end
+
+  def test_scores_rating
+    school_rating_by_data_type_id(164)
+  end
+
+  def student_growth_rating
+    school_rating_by_data_type_id(165)
+  end
+
+  def school_rating_by_data_type_id(data_type_id)
+    overall_ratings_obj = ratings.find { |rating| rating['data_type_id'] == data_type_id  }
     overall_ratings_obj['school_value_float'].to_i
   end
 
