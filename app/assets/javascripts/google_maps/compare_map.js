1090cdfd489e94606cbf15ed52f73a01f7456555
Keith McHugh <kmchugh@greatschools.org>
2014-09-03 17:48:49 -0700

    PT-687 change height of compare map

--- app/assets/javascripts/google_maps/compare_map.js
+++ app/assets/javascripts/google_maps/compare_map.js
@@ -2,7 +2,7 @@ if (gon.pagename == "CompareSchoolsPage") {
 
     $(document).ready(function () {
         var elemMapCanvas = $('#js-map-canvas');
-        GS.search.googleMap.setHeightForMap(200);
+        GS.search.googleMap.setHeightForMap(219);
         elemMapCanvas.show('fast', GS.search.googleMap.initAndShowMap);
     });
 }
