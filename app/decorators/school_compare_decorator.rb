19f09b7c3070cb45f1d5b71d9142a36d44fc244f
Keith McHugh <kmchugh@greatschools.org>
2014-09-04 13:15:40 -0700

    PT-686 compare: don't break if no ethnicity data

--- app/decorators/school_compare_decorator.rb
+++ app/decorators/school_compare_decorator.rb
@@ -25,7 +25,7 @@ class SchoolCompareDecorator < SchoolProfileDecorator
   end
 
   def ethnicity_data
-    characteristics['Ethnicity']
+    characteristics['Ethnicity'] || {}
   end
 
   def graduates_high_school
