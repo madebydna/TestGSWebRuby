127197a00667bada62a728921b7dfd8646c269b0
Keith McHugh <kmchugh@greatschools.org>
2014-09-05 16:28:16 -0700

    PT-679 search: specs: test that sorting sorts

--- spec/features/search/sorting_results_spec.rb
+++ spec/features/search/sorting_results_spec.rb
@@ -12,6 +12,7 @@ describe 'Sorting search results' do
 
     it 'should display only rating and fit sorting, in that order' do
       expect(page).to have_content 'Sort by: Rating Fit'
+      expect(page.find('.active-search-sort')).to have_content('Rating')
     end
   end
 
@@ -23,6 +24,7 @@ describe 'Sorting search results' do
 
     it 'should display only rating and fit sorting, in that order' do
       expect(page).to have_content 'Sort by: Rating Fit'
+      expect(page.find('.active-search-sort')).to have_content('Rating')
     end
   end
 
@@ -34,6 +36,7 @@ describe 'Sorting search results' do
 
     it 'should display rating, distance and fit sorting, in that order' do
       expect(page).to have_content 'Sort by: Rating Distance Fit'
+      expect(page.find('.active-search-sort')).to have_content('Distance')
     end
   end
 
@@ -45,6 +48,7 @@ describe 'Sorting search results' do
 
     it 'should display rating, distance and fit sorting, in that order' do
       expect(page).to have_content 'Sort by: Relevance Rating Fit'
+      expect(page.find('.active-search-sort')).to have_content('Relevance')
     end
   end
 
@@ -56,6 +60,47 @@ describe 'Sorting search results' do
 
     it 'should not display sort options' do
       expect(page).to_not have_content 'Sort by:'
+    end
+  end
+
+  context 'sorting results by rating' do
+
+    before do
+      set_up_city_browse('de','dover','sort=rating_desc')
+    end
+
+    it 'should sort by rating descending' do
+      prev_rating = 10
+      page.all(:css, '.iconx24-icons').each do |rating_div|
+        next unless rating_div[:class].include? '-ratings-'
+        rating_div[:class].split(' ').each do |cls|
+          next unless cls.include? 'ratings'
+          rating = cls.split('-')[-1]
+          unless rating == 'nr' || prev_rating == 'nr'
+            rating = rating.to_i
+            expect(rating).to be <= prev_rating
+          end
+          if prev_rating == 'nr'
+            expect(rating).to eq('nr')
+          end
+          prev_rating = rating
+        end
+      end
+    end
+  end
+
+  context 'sorting results by distance' do
+
+    before do
+      set_up_by_location_search('100 North Dupont Road', 'Wilmington', 19807, 'DE', 39.752831, -75.588326, 'sort=distance_asc')
+    end
+
+    it 'should sort by distance ascending' do
+      prev_distance = 0.0
+      page.all('span', text: /miles$/).each do |distance_span|
+        distance = distance_span.text.split(' ').first.to_f
+        expect(distance).to be >= prev_distance
+      end
     end
   end
 end
