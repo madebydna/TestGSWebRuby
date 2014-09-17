4eaa225db777580ad0aa7d0808a17c7a05fb286d
Anthony Roy <aroy@greatschools.org>
2014-09-17 09:48:38 -0700

    PT-716 Remove deprecated gsr route to compare

--- config/routes.rb
+++ config/routes.rb
@@ -25,7 +25,6 @@ LocalizedProfiles::Application.routes.draw do
 
   get '/search/search.page', as: :search, to: 'search#search'
 
-  get '/gsr/school-comparison-tool/results.page', as: :compare_schools_old, to: 'compare_schools#show' #TODO: Remove
   get '/compare', as: :compare_schools, to: 'compare_schools#show'
 
   get '/gsr/search/suggest/school', as: :search_school_suggest, to: 'search#suggest_school_by_name'
