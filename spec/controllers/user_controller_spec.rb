dfdbba5f916f07c8675d55c948c8d4ea80b0711b
Samson Sprouse <ssprouse@greatschools.org>
2014-09-19 10:46:23 -0700

    OM-715 Changed specs for user change password behavior

--- spec/controllers/user_controller_spec.rb
+++ spec/controllers/user_controller_spec.rb
@@ -27,28 +27,40 @@ describe UserController do
     end
 
     context 'when user is signed in' do
-      let(:user) { FactoryGirl.build(:verified_user) }
+      let(:user) do
+        user = FactoryGirl.build(:verified_user)
+        user.password = 'abcdefg'
+        user.save
+        user
+      end
+
       before do
         controller.instance_variable_set(:@current_user, user)
       end
       
+      it 'should validate the user\'s current password' do
+        xhr :post, :change_password, current_password: 'sldkfjsf', new_password: 'foo', confirm_password: 'bar'
+        json_response = JSON.parse(response.body)
+        expect(json_response['success']).to be_falsey
+        expect(json_response['message']).to include('current password')
+      end
 
       it 'should make sure the password and confirmed password match' do
-        xhr :post, :change_password, password: 'foo', confirm_password: 'bar'
+        xhr :post, :change_password, current_password: 'abcdefg', new_password: 'foo', confirm_password: 'bar'
         json_response = JSON.parse(response.body)
         expect(json_response['success']).to be_falsey
         expect(json_response['message']).to include('do not match')
       end
 
       it 'should notify the user if an error occurs' do
-        xhr :post, :change_password, password: 'foo', confirm_password: 'foo'
+        xhr :post, :change_password, current_password: 'abcdefg', new_password: 'foo', confirm_password: 'foo'
         json_response = JSON.parse(response.body)
         expect(json_response['success']).to be_falsey
         expect(json_response['message']).to match(/6 and 14/)
       end
 
       it 'should change the user\'s password' do
-        xhr :post, :change_password, password: '123456', confirm_password: '123456'
+        xhr :post, :change_password, current_password: 'abcdefg', new_password: '123456', confirm_password: '123456'
         json_response = JSON.parse(response.body)
         expect(json_response['success']).to be_truthy
       end
