4ad48da42544f7456e13e3e4347f4509bfe73fa1
Anthony Roy <aroy@greatschools.org>
2014-09-30 08:43:36 -0700

    PT-776 Add rounding to faster_number_with_precision

--- lib/number_utils.rb
+++ lib/number_utils.rb
@@ -1,6 +1,6 @@
 module NumberUtils
   def faster_number_with_precision(number, precision = 1)
     p = 10 ** precision
-    (number * p).to_i.to_f / p
+    ((number * p) + 0.5).to_i.to_f / p
   end
 end
