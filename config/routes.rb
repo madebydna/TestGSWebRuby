e7721bb4af55d4c85c81a103489fa668337fa709
Nanditha Patury <npatury@GS-0243.local>
2014-10-08 10:57:21 -0700

    OM-748 reverted the accounts page

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,7 @@ LocalizedProfiles::Application.routes.draw do
   # Route for Search Prototype
   # get '/gsr/search_prototype', as: :search_prototype, to: 'home#search_prototype'
 
-  get '/account', as: :manage_account, to: 'account_management#show'
+  get '/gsr/account/', as: :manage_account, to: 'account_management#show'
 
   get '/gsr/pyoc', to: 'pyoc#print_pdf' , as: :print_pdf
 
@@ -87,8 +87,9 @@ LocalizedProfiles::Application.routes.draw do
       mount RailsAdmin::Engine => '', :as => 'rails_admin'
     end
 
-    get '/style-guide/*page', to: 'style_guide#index'
-    get '/style-guide/', to: 'style_guide#index'
+    scope '/style-guide/', as: :style_guide, to: :style_guide do
+      get '/index', to: 'style_guide#index'
+    end
 
     scope ':state', constraints: { state: States.any_state_name_regex } do
       resources :schools do
