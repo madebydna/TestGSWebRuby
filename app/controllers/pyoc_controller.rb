ed6be1ed7eae214143d60d4cd41c7c7babf35c78
Shomi Arora <sarora@greatschools.org>
2014-10-14 16:35:05 -0700

    AT-502 fixing enrollment page link

--- app/controllers/pyoc_controller.rb
+++ app/controllers/pyoc_controller.rb
@@ -6,14 +6,14 @@ class PyocController <  ApplicationController
   def print_pdf
     if state_param.present? && (params[:id1].present? || params[:id1].present? || params[:id1].present?)
     @db_schools = School.for_states_and_ids([state_param,state_param, state_param], [params[:id1],params[:id2], params[:id3]])
-    elsif state_param.present? && params[:collection_id].present? && params[:school_batch].present?
+    elsif state_param.present? && params[:collection_id].present?
     @db_schools = School.on_db(state_param).where(active: true).order(name: :asc)
     @db_schools = @db_schools[0..5]
     @db_schools.each do |school|
-      if !(school.collection.present? && school.collection.id == params[:collection_id].to_i)
+      if !(school.collection.present? && school.collection.id == params[:collection_id].to_i  && (school.level_code.to_s.include? "m" or school.level_code.to_s.include? "e" or school.level_code.to_s.include? "p"))
         @db_schools -= Array[school]
       end
-      end
+    end
     elsif state_param.present?
       @db_schools = School.on_db(state_param).where(active: true).order(name: :asc)
       @db_schools = @db_schools[0..5]
