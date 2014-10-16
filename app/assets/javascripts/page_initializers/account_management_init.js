697060e8ba19a14f847229ca973f8ad7ff09f2b3
mseltzer <mseltzer@greatschools.org>
2014-10-16 16:18:09 -0700

    OM-781 - fix for fast clicking on remove link.

--- app/assets/javascripts/page_initializers/account_management_init.js
+++ app/assets/javascripts/page_initializers/account_management_init.js
@@ -1,7 +1,11 @@
 if(gon.pagename == "Account management"){
   $(function () {
-//    GS.select.school.attachAutocomplete();
     GS.accountManagement.changePassword.init();
     GS.accountManagement.PYOC.init();
+
+//    this stops quick clicking on the remove button as it is gone after first click
+    $("a[class^=js-delete-favorite-school-]").on('click', function(){
+      $(this).remove();
+    });
   });
 }
