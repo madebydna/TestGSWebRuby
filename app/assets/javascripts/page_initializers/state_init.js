3e82e7902de80ffa27b505b0e9764b063f5752b0
mseltzer <mseltzer@greatschools.org>
2014-10-22 10:04:20 -0700

    updated to match harrisons' search_results.js change

--- app/assets/javascripts/page_initializers/state_init.js
+++ app/assets/javascripts/page_initializers/state_init.js
@@ -1,6 +1,6 @@
 $(function() {
   if (gon.pagename == "State_Home_Standard") {
     GS.search.init();
-    GS.search.results.init();
+    GS.search.autocomplete.searchAutocomplete.init(gon.state_abbr);
   }
 });
