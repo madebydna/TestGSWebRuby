3547ad406b6609d7027de6b431bec7a18bc4ed2a
Nanditha Patury <npatury@GS-0243.local>
2014-10-23 13:09:15 -0700

    OM-809 added some optimizations for state page

--- app/decorators/state_home_decorator.rb
+++ app/decorators/state_home_decorator.rb
@@ -23,49 +23,14 @@ class StateHomeDecorator < Draper::Decorator
     real_count.round(-2)
   end
 
-  def all_cities
-    @all_cities ||= 
-      City.where(state: abbreviation, active: true).order('name asc')
+  def cities_list
+    @cities_list ||= City.where(state: abbreviation, active: true).
+      order('population desc').limit(NUMBER_OF_CITIES_IN_SHORT_LIST).in_groups(NUMBER_OF_COLUMNS_OF_CITIES, false)
   end
 
-  def short_cities_list
-    short_list = all_cities.sort do |c1, c2|
-      c2.population.to_i <=> c1.population.to_i
-    end[0..NUMBER_OF_CITIES_IN_SHORT_LIST-1]
-    short_list.in_groups(NUMBER_OF_COLUMNS_OF_CITIES, false)
-  end
-
-  def expanded_cities_list
-    if all_cities.size > NUMBER_OF_CITIES_IN_SHORT_LIST
-      all_cities.in_groups(NUMBER_OF_COLUMNS_OF_CITIES, false)
-    else
-      []
-    end
-  end
-
-  def number_of_cities
-    all_cities.size
-  end
-
-  def number_of_districts
-    all_districts.size
-  end
-
-  def all_districts
-    District.on_db(abbreviation.to_sym).where(active: true).order('name asc')
-  end
-
-  def short_districts_list
-    all_districts[0..NUMBER_OF_DISTRICTS_IN_SHORT_LIST-1].
-      in_groups(NUMBER_OF_COLUMNS_OF_DISTRICTS, false)
-  end
-
-  def expanded_districts_list
-    if all_districts.size > NUMBER_OF_DISTRICTS_IN_SHORT_LIST
-      all_districts.in_groups(NUMBER_OF_COLUMNS_OF_DISTRICTS, false)
-    else
-      []
-    end
+  def districts_list
+    @districts_list ||= Array(District.on_db(abbreviation.to_sym).where(active: true).
+      order('name asc').limit(NUMBER_OF_CITIES_IN_SHORT_LIST)).in_groups(NUMBER_OF_COLUMNS_OF_DISTRICTS, false)
   end
 
 end
