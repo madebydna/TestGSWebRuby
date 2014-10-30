eb2ece4bf498c5cd9d8f9245c816ecf9bd8daec3
Harrison <htouw@greatschools.org>
2014-10-29 17:58:24 -0700

    PT-840 Setup Daemon to compare reulsts of daemon writes and java OSP writes

--- lib/data_loading/esp_response_loading/loader.rb
+++ lib/data_loading/esp_response_loading/loader.rb
@@ -2,6 +2,36 @@ class EspResponseLoading::Loader < EspResponseLoading::Base
 
   def load!
     puts self.class
+
+    updates.each do |update|
+      next if update.blank?
+
+      esp_response_update = EspResponseLoading::Update.new(data_type, update)
+
+      value_row = EspResponse
+          .on_db(esp_response_update.shard)
+          .where(esp_response_update.attributes)
+          .first_or_initialize
+
+      errors = []
+      raise "Value row does not exist" unless value_row.id.present?
+
+      errors << "The active column does not match. Values - Java: true Ruby: false" unless value_row.active == true
+      errors << "The school_id column does not match. Values - Java: #{value_row.school_id} Ruby:#{esp_response_update.entity_id}" unless value_row.school_id == esp_response_update.entity_id
+      errors << "The esp_source column does not match. Values - Java: #{value_row.esp_source} Ruby:#{source}" unless value_row.esp_source == source
+      errors << "The response_key column does not match. Values - Java: #{value_row.response_key} Ruby:#{esp_response_update.data_type}" unless value_row.response_key == esp_response_update.data_type
+      errors << "The response_value column does not match. Values - Java: #{value_row.response_value} Ruby:#{esp_response_update.value}" unless value_row.response_value == esp_response_update.value
+      errors << "The member_id column does not match. Values - Java: #{value_row.member_id} Ruby:#{esp_response_update.member_id}" unless value_row.member_id == esp_response_update.member_id
+
+      raise errors.unshift("SCHOOL ##{value_row.school_id} ESP Response").join("\n") if errors.present?
+
+      # value_row.on_db(esp_response_update.shard).update_attributes(
+      #     active: true,
+      #     created: Time.now,
+      #     esp_source: source,
+      #     member_id: esp_response_update.member_id
+      # )
+    end
   end
 
 end
---
+++ lib/data_loading/esp_response_loading/update.rb
@@ -0,0 +1,40 @@
+class EspResponseLoading::Update
+
+  attr_accessor :data_type, :entity_id, :entity_state, :shard, :state, :update_blob, :value, :member_id
+
+  def initialize(data_type, update_blob)
+    @data_type = data_type
+    @update_blob = update_blob
+
+    @update_blob.each do |key, value|
+      instance_variable_set("@#{key}", value)
+    end
+
+    parse_attributes!
+
+    validate_update
+  end
+
+  def parse_attributes!
+    @shard = entity_state.to_s.downcase.to_sym
+
+    @attributes = attributes
+  end
+
+  def attributes
+    @attributes ||=
+        {
+            school_id: entity_id,
+            response_key: data_type,
+            response_value: value
+        }
+  end
+
+  def validate_update
+    raise 'Every esp response update must have an entity_state specified' if entity_state.blank?
+    raise 'Every esp response update must have a data_type specified' if data_type.blank?
+    raise 'Every esp response update must have an value specified' if value.blank?
+    raise 'Every esp response update must have a member_id specified' if member_id.blank?
+    raise 'Every esp response update must have a entity_id specified' if entity_id.blank?
+  end
+end
