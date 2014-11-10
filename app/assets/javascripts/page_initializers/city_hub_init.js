2bacf92d36a6055daf3ef672e11cecae339b2566
Jonathan Wrobel <jwrobel@greatschools.org>
2014-11-10 12:07:56 -0800

    PT-888 move search bar with auto-complete from home page to state and city hub

--- app/assets/javascripts/page_initializers/city_hub_init.js
+++ app/assets/javascripts/page_initializers/city_hub_init.js
@@ -1,5 +1,6 @@
 $(function() {
   if (gon.pagename && gon.pagename.indexOf("GS:City") >= 0) {
       GS.search.schoolSearchForm.setupTabs(); // switch by loc by name
+      GS.search.autocomplete.searchAutocomplete.init(gon.state_abbr);
   }
 });
