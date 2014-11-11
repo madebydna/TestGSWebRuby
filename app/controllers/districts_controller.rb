55a2747c2862c035e00dbbf4ba274e0b1def30bb
Samson Sprouse <ssprouse@greatschools.org>
2014-11-10 17:27:09 -0800

    OM-814 redirect to canonical url on district home page

--- app/controllers/districts_controller.rb
+++ app/controllers/districts_controller.rb
@@ -10,6 +10,7 @@ class DistrictsController < ApplicationController
   before_action :set_hub
   before_action :set_login_redirect
   before_action :write_meta_tags
+  before_action :redirect_to_canonical_url
 
   def show
     gon.pagename = 'DistrictHome'
@@ -17,6 +18,7 @@ class DistrictsController < ApplicationController
     @district = District.find_by_state_and_name(state_param, district_param)
 
     @nearby_districts = @district.nearby_districts
+    @canonical_url = city_district_url(district_params_from_district(@district))
 
     @top_schools = top_schools(@district, 4)
     @params_hash = parse_array_query_string(request.query_string)
@@ -28,6 +30,22 @@ class DistrictsController < ApplicationController
   end
 
   private
+
+  def redirect_to_canonical_url
+    # Add a tailing slash to the request path, only if one doesn't already exist.
+    # Requests made by rspec sometimes contain a trailing slash
+    unless canonical_path == with_trailing_slash(request.path)
+      redirect_to add_query_params_to_url(
+        canonical_path,
+        true,
+        request.query_parameters
+      )
+    end
+  end
+
+  def canonical_path
+    city_district_path(district_params_from_district(@district))
+  end
 
   def set_omniture_data_search_school
     gon.omniture_sprops = {}
