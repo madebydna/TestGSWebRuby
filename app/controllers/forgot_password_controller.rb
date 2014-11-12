094064bda25f67fea5119ac0e99f38d80eda26a6
Nanditha Patury <npatury@GS-0243.local>
2014-11-12 13:37:31 -0800

    OM-718 fixed spacing in text

--- app/controllers/forgot_password_controller.rb
+++ app/controllers/forgot_password_controller.rb
@@ -35,9 +35,9 @@ class ForgotPasswordController < ApplicationController
         verification_email_url = url_for(:controller => 'user', :action => 'send_verification_email', :email => user.email)
         error_msg = (t('forms.errors.email.provisional_resend_email', verification_email_url: verification_email_url)).html_safe
       elsif !user.has_password?
-        error_msg = ("You have an email address on file,but still need to create a free account with GreatSchools.<a href=#{join_url}>Join</a>").html_safe
+        error_msg = ("You have an email address on file, but still need to create a free account with GreatSchools. <a href=#{join_url}>Join today.</a>").html_safe
       elsif !user.is_profile_active?
-        error_msg = ("The account associated with that email address has been disabled.Please contact us.").html_safe
+        error_msg = ("The account associated with that email address has been disabled. Please contact us.").html_safe
       end
     else
        error_msg = 'Please enter an email address.'
