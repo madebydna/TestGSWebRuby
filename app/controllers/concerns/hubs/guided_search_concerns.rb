d07eeff70cf2f0062a30007c87523caee46e2706
Keith McHugh <kmchugh@greatschools.org>
2014-11-13 13:24:02 -0800

    PT-878 Guided search: tweak keywords a bit

--- app/controllers/concerns/hubs/guided_search_concerns.rb
+++ app/controllers/concerns/hubs/guided_search_concerns.rb
@@ -20,7 +20,7 @@ module GuidedSearchConcerns
         set_omniture_data('GS:GuidedSchoolSearch', 'Search,Guided Search',nav_helper.topnav_title)
         set_meta_tags title:       "Your Personalized #{nav_helper.topnav_title} School Search | GreatSchools",
                       keywords:    "#{nav_helper.topnav_title} school wizard, #{nav_helper.topnav_title} schools,
-                                    #{@state[:short].upcase} schools, #{nav_helper.topnav_title} school guided search",
+                                    #{nav_helper.topnav_title} school guided search",
                       description: "Use this 5-step guide to discover #{nav_helper.topnav_title} schools that match your
                                     child\'s unique needs and preferences including programs and extracurriculars, school
                                     focus areas, transportation, and daily schedules."
