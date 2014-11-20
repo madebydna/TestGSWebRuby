220e5ef9379eca1eabc21857efd5002179a92995
Anthony Roy <aroy@greatschools.org>
2014-11-20 14:42:10 -0800

    PT-959 Remove progress_bar dependency from esp_responses temporarily

--- lib/school_cache/cacher.rb
+++ lib/school_cache/cacher.rb
@@ -58,7 +58,7 @@ class Cacher
   def self.cacher_dependencies_for(key)
     {
 
-        esp_responses:    [ProgressBarCaching::ProgressBarCacher]
+        #esp_responses:    [ProgressBarCaching::ProgressBarCacher] # need this dependency off until we're ready to launch progress bar nationwide with realtime updates
 
     }[key.to_s.to_sym]
   end
