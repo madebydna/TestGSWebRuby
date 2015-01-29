d83297b908b94060c2b41982b1890985966e8850
Shomi Arora <sarora@greatschools.org>
2015-01-29 13:58:23 -0800

    AT-565 Changing K to KG as grade in UI and database

--- app/models/student_grade_level.rb
+++ app/models/student_grade_level.rb
@@ -7,7 +7,7 @@ class StudentGradeLevel < ActiveRecord::Base
 
   validates_presence_of :grade, :member_id
 
-  validates :grade, inclusion: { in: ['PK','pk','K','k','1','2','3','4','5','6','7','8','9','10','11','12'],
+  validates :grade, inclusion: { in: ['PK','pk','KG','kg','1','2','3','4','5','6','7','8','9','10','11','12'],
                                 message: "You must specify a valid grade" }
 
 end
