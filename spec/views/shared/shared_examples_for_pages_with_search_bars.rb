1fb0560636aaf21ac9eb7caca3cf2bd0a7680e9f
Harrison <htouw@greatschools.org>
2015-01-30 15:28:10 -0800

    PT-1068 Make State Editable On Mobile

--- spec/views/shared/shared_examples_for_pages_with_search_bars.rb
+++ spec/views/shared/shared_examples_for_pages_with_search_bars.rb
@@ -21,7 +21,31 @@ end
 
 shared_examples_for 'a page with a change location button in the search bar' do
   let(:search_form_element) { page.find(:css, '.js-schoolResultsSearchForm') }
+
   it 'should have the change location anchor tag element' do
-    expect(search_form_element.has_selector?('a', text: 'Change location')).to be_truthy
+    expect(search_form_element.has_selector?('.rs-change_location')).to be_truthy
+  end
+
+  desktop_and_mobile do | window_type, screen_size |
+    describe window_type, js: true do
+      before { page.driver.browser.resize_window(*screen_size) }
+      it 'should have a change location link that is visible' do
+        expect(search_form_element).to have_selector('.rs-change_location', visible: true)
+      end
+      it 'should show list of states when the change location link is clicked' do
+        expect(search_form_element).to have_selector('.rs-search_state_picker', visible: false)
+        click_link 'Change location'
+        expect(search_form_element).to have_selector('.rs-search_state_picker', visible: true)
+      end
+      context 'when the user clicks the link and changes the state' do
+        it 'should change the text saying what state they\'re currently in' do
+          text = search_form_element.find(:css, '.rs-current_location_text').text
+          click_link 'Change location'
+          first('.rs-search_state_picker > li > a').click
+          text2 = search_form_element.find(:css, '.rs-current_location_text').text
+          expect(text).not_to eq(text2)
+        end
+      end
+    end
   end
 end
