a2775df5d83216d37cd90b61104e3866c4551b44
Samson Sprouse <ssprouse@greatschools.org>
2015-02-02 16:35:32 -0800

    PT-1061 Improvements to SafeHtmlUtils

--- lib/safe_html_utils.rb
+++ lib/safe_html_utils.rb
@@ -1,36 +1,45 @@
 # Inspired by https://gwt.googlesource.com/gwt/+/master/user/src/com/google/gwt/safehtml/shared/SafeHtmlUtils.java
-class SafeHtmlUtils
+module SafeHtmlUtils
+  module_function
+
   HTML_ENTITY_REGEX = /\A[a-z]+\z|\A#[0-9]+\z|\A#x[0-9a-fA-F]+\z/
 
-  def self.html_escape(s)
-    result = s.dup
-    result.gsub!('&', '&amp;')
-    result.gsub!('<', '&lt;')
-    result.gsub!('>', '&gt;')
-    result.gsub!('"', '&quot;')
-    result.gsub!("'", '&#39;')
-    return result
+  HTML_ENTITIES = {
+    '&' => '&amp;',
+    '<' => '&lt;',
+    '>' => '&gt;',
+    '"' => '&quot;',
+    "'" => '&#39;'
+  }.freeze
+
+  def html_escape(s)
+    regex_escaped_characters = HTML_ENTITIES.keys.map { |character| Regexp.escape(character) }
+    search_regex = Regexp.new("[#{regex_escaped_characters.join('|')}]")
+    s.gsub(search_regex, HTML_ENTITIES)
   end
 
   # HTML-escapes a string, but does not double-escape HTML-entities already present in the string.
-  def self.html_escape_allow_entities(text)
+  def html_escape_allow_entities(text)
     escaped = ''
-    text.split('&').each_with_index do |segment, index|
-      # The first segment is never part of an entity reference, so we always escape it.
-      # Note that if the input starts with an ampersand, we will get an empty segment before that.
+    text.split('&').each_with_index do |token, index|
+      # The first token is never part of an entity reference, so we always escape it.
+      # Note that if the input starts with an ampersand, we will get an empty token before that.
       if index == 0
-        escaped << html_escape(segment)
+        escaped << html_escape(token)
         next
       end
 
-      entity_end = segment.index(';')
-      if entity_end.present? && entity_end > 0 && !!(segment[0..(entity_end-1)].match(HTML_ENTITY_REGEX))
+      if string_matches_html_entity(token)
         # Append the entity without escaping.
-        escaped << '&' << segment[0..entity_end]
-        escaped << html_escape(segment[(entity_end+1)..-1])
+        semicolon_position = token.index(';')
+        entity = '&' << token[0..semicolon_position]
+        escaped << entity
+        # Escape everything after the entity
+        rest_of_token = token[(semicolon_position+1)..-1]
+        escaped << html_escape(rest_of_token)
       else
-        # The segment did not start with an entity reference, so escape the whole segment.
-        escaped << '&amp;' << html_escape(segment);
+        # The token did not start with an entity reference, so escape the whole token.
+        escaped << '&amp;' << html_escape(token);
       end
     end
 
@@ -40,6 +49,15 @@ class SafeHtmlUtils
     escaped << '&amp;' if text[-1] == '&'
 
     return escaped
+  end
+
+  def string_matches_html_entity(token)
+    semicolon_position = token.index(';')
+    if semicolon_position.present? && semicolon_position > 0
+      stuff_before_semicolon = token[0..(semicolon_position-1)]
+      return !!(stuff_before_semicolon.match(HTML_ENTITY_REGEX))
+    end
+    return false
   end
 
 
