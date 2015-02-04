7781209bbd88e0528b11c6b02fe38769315239c5
mseltzer <mseltzer@greatschools.org>
2015-02-04 10:14:25 -0800

    OM-973 - added morgan stanley to controller for display

--- app/controllers/review_school_chooser_controller.rb
+++ app/controllers/review_school_chooser_controller.rb
@@ -1,5 +1,15 @@
 class ReviewSchoolChooserController < ApplicationController
   def show
+    write_tags_and_gon
+  end
+
+  def morgan_stanley
+    write_tags_and_gon
+    @display_morgan_stanley = ''
+  end
+
+  def write_tags_and_gon
+    @display_morgan_stanley = 'dn'
     gon.pagename = "Write a school review | GreatSchools"
     gon.omniture_pagename = 'GS:Promo:Reviews'
     set_meta_tags :title => "Write a school review | GreatSchools" , :description => "Write a review for your child's school today and you can help other parents make a
