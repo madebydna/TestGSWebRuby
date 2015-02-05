aa891279dbc199f0148c6f91570b9c42c0e818ef
Nanditha Patury <npatury@GS-0243.local>
2015-02-05 11:29:37 -0800

    OM-843 added heir1 to omniture tracking

--- app/controllers/review_school_chooser_controller.rb
+++ app/controllers/review_school_chooser_controller.rb
@@ -13,7 +13,8 @@ class ReviewSchoolChooserController < ApplicationController
     @display_morgan_stanley = 'dn'
     gon.pagename = "Write a school review | GreatSchools"
     gon.omniture_pagename = 'GS:Promo:Reviews'
-    set_meta_tags :title => "Write a school review | GreatSchools" , :description => "Write a review for your child's school today and you can help other parents make a
+    gon.omniture_hier1 = "School,Parent Reviews, Rating Review Marketing Landing Page "
+    set_meta_tags :title => "Write a school review | GreatSchools" , :description => "Write a review for your school today and you can help other parents make a
     more informed choice about which school is right for their family."
   end
 
