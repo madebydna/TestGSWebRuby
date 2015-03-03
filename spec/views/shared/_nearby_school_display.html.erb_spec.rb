ead03cb292e49069ed7c1c791a5107c9717a950c
Jonathan Wrobel <jwrobel@greatschools.org>
2015-03-03 09:40:55 -0800

    OM-966 Updated nearby school module and added unit and view tests

---
+++ spec/views/shared/_nearby_school_display.html.erb_spec.rb
@@ -0,0 +1,53 @@
+require 'spec_helper'
+
+describe 'shared/_nearby_school_display' do
+
+  context 'with ads not turned off' do
+
+    before do
+      mocked_school = {}
+      stub_template  "layouts/_ad_layer.html.erb" => "ad is displayed"
+      stub_template  "school_profile/_nearby_school.html.erb" => "this content"
+      allow_any_instance_of(SchoolProfileDecorator).to receive(:school_zip_location_search_url).and_return("url")
+      @show_ads = true
+      render partial: "shared/nearby_school_display", locals: {title: "Nearby schools", schools: [mocked_school]}
+    end
+
+    it 'should show ads' do
+      expect(rendered).to have_content("ad is displayed")
+    end
+
+    it 'renders the title' do
+      expect(rendered).to have_content("Nearby schools")
+    end
+
+    it 'renders the nearby_school partial' do
+      expect(rendered).to have_content("this content")
+    end
+
+  end
+
+  context 'with ads turned off' do
+    before do
+      mocked_school = {}
+      stub_template  "school_profile/_nearby_school.html.erb" => "this content"
+      allow_any_instance_of(SchoolProfileDecorator).to receive(:school_zip_location_search_url).and_return("url")
+      @show_ads = false
+      render partial: "shared/nearby_school_display", locals: {title: "Nearby schools", schools: [mocked_school] }
+    end
+
+    it 'renders the title' do
+      expect(rendered).to have_content("Nearby schools")
+    end
+
+    it 'renders the nearby_school partial' do
+      expect(rendered).to have_content("this content")
+    end
+
+    it 'should not show ads' do
+      expect(rendered).to_not have_css(".gs_ad_slot")
+    end
+  end
+
+
+end
