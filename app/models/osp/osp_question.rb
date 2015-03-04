7c4b15fa4730f55ba5714a58444bb988ec484603
Shomi Arora <sarora@greatschools.org>
2015-03-03 16:23:52 -0800

    AT-634 adding parsing of jason

--- app/models/osp/osp_question.rb
+++ app/models/osp/osp_question.rb
@@ -8,4 +8,38 @@ class Osp::OspQuestion < ActiveRecord::Base
 
 
 
+  def answer_set
+    # use quality or p_overall(for prek) for star counts and overall
+    # score.OM-209
+    JSON.parse('[{"anserSet":[{"AV1":1},{"AV2":2}]},{"label":"oberride"}]')
+  end
+
+  def answers
+    # binding.pry;
+    result = {}
+    self.question_json_config.each do |attribute|
+      attribute.key("answerSet") == "answerSet"
+      result = attribute
+    end
+    result
+  end
+
+
+  def question_json_config
+    json = read_attribute(:default_config)
+    if json.present?
+      begin results = JSON.parse(json,symbolize_names: true)
+      rescue JSON::ParserError => e
+        results = {}
+        Rails.logger.debug "ERROR: parsing JSON Question Config" +
+                               "Exception message: #{e.message}"
+      end
+
+      results
+    else
+      {}
+    end
+  end
+
+
 end
