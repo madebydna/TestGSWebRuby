8e9a82f7c08dd8e418f7193df6ea05deca11c62b
Shomi Arora <sarora@greatschools.org>
2015-03-05 14:22:14 -0800

    AT-628 Adding the table renaming and data layer

--- app/models/osp/osp_question_group.rb
+++ app/models/osp/osp_question_group.rb
@@ -1,6 +1,6 @@
 class Osp::OspQuestionGroup < ActiveRecord::Base
   db_magic :connection => :gs_schooldb
-  self.table_name = 'osp_question_group'
+  self.table_name = 'osp_question_groups'
 
   attr_accessible :image, :heading, :subtext, :default_config ,:active,  :updated
   belongs_to :osp_display_config, :class_name => 'Osp::OspDisplayConfig'
