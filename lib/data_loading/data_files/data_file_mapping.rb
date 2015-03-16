78b571745e291e36a148acb283ac1aaca78c3113
Keith McHugh <kmchugh@greatschools.org>
2015-03-16 09:33:47 -0700

    PT-1258 Better comments and variable names

--- lib/data_loading/data_files/data_file_mapping.rb
+++ lib/data_loading/data_files/data_file_mapping.rb
@@ -2,7 +2,10 @@
 # Things that are configured are:
 # - location: Where the file exists.
 # - header_rows: How many rows of header information are there before the data starts.
-# - layout: What data is in each column. See parse_layout! for more information about this.
+# - layout: What data is in each column.
+# -- The layout is converted into a mapping of column number to descriptors, but only for value columns.
+# -- A little confusing, but a value column is either labled value or number_tested: what goes into the value row in the db.
+# -- See parse_layout! for more information about how the map is converted to this format.
 #
 # Example file mapping
 # {
@@ -41,13 +44,21 @@ class DataFileMapping
   def parse_layout!
     @base_value_description = {}
     @columns = layout.each_with_object(Hash.new { |h,k| h[k] = {} }) do |(key, map), columns|
+      # As said above, the goal of this method is to convert the layout into a set of
+      # descriptors for each value column, where a value column is either the value or the number_tested.
+      # A descriptor can either be a location to look for information or the information itself.
+      # For instance, { breakdown: 8 } means for each row of the file, the breakdown is the value of column 8
+      # and { breakdown: :latino } means for each row of the file, the breakdown is latino.
+      #
+      # Method mechanics:
       # The key is the column type: like school_id, number_tested, and subject.
       # The map is how that column type is configured. Different objects have different rules:
-      #  Integer/Fixnum: Get the value for this column type from each rows value in that column.
-      #  Array: GSame as Integer/Fixnum, but for multiple columns.
-      #  String/Symbol: The whole file gets the String/Symbol's value for this column type.
-      #  Hash: A mix of the above. Maps the column type to the inner key for the inner values.
-      #   EX: proficiency_band: { null: [20] } yields { 20 => { proficiency_band: :null } }
+      #  Integer/Fixnum: Get the value for this column type from each row's value in that column.
+      #  Array: Same as Integer/Fixnum, but for multiple columns.
+      #  String/Symbol: The whole file gets the String's/Symbol's value for this column type.
+      #  Hash: A mix of the above. Maps the column type to the inner key for listed value columns.
+      #   EX: proficiency_band: { null: [20] } yields { 20 => { proficiency_band: :null, ...other descriptors... } }
+      #   Note that this is only for adding descriptors to value columns.
       parse_map!(columns, key, map)
     end
     @columns = @columns.each_with_object({}) do |(column, description), columns|
@@ -85,8 +96,8 @@ class DataFileMapping
     map.each do |label, col|
       # These parsers are slightly different because hashes have keys and labels
       if col.is_a?(Array) || col.is_a?(Integer)
-        [*col].each do |c|
-          columns[c].merge!({key => label})
+        [*col].each do |value_column|
+          columns[value_column].merge!({key => label})
         end
       else
         raise "Don't know how to handle #{ { key => map } } because '#{col}' is not an array or integer."
