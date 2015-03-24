983f64a003b7e0d337440a8f984e3affc862fe3e
Christina Liu <cliu@greatschools.org>
2015-03-24 16:04:29 -0700

    osp controller spec refactor

--- spec/controllers/osp_controller_spec.rb
+++ spec/controllers/osp_controller_spec.rb
@@ -3,45 +3,52 @@ require 'spec_helper'
 describe Admin::OspController do
 
   let(:current_user) { FactoryGirl.build(:user) }
-  let(:school) { FactoryGirl.create(:alameda_high_school) }
+  let(:school) { FactoryGirl.build(:alameda_high_school) }
+  before do
+    controller.instance_variable_set(:@current_user, current_user)
+    allow(School).to receive(:find_by_state_and_id).and_return school
+  end
+  after do
+    clean_models School
+  end
 
   describe '#show' do
-    before do
-      controller.instance_variable_set(:@current_user, current_user)
-    end
-    after do
-      clean_models School
-    end
+
     it 'should redirect user to account page if user does not have access to osp' do
       allow(current_user).to receive(:provisional_or_approved_osp_user?).and_return false
+
       get :show, state: school.state, schoolId: school.id
       expect(response).to redirect_to(my_account_url)
-      end
-
-    it 'should render Basic Information page if user has access to osp' do
-      allow(current_user).to receive(:provisional_or_approved_osp_user?).and_return true
-      get :show, state: school.state, schoolId: school.id, page: 1
-      expect(response).to render_template('osp/osp_basic_information')
-      end
-
-    it 'should render Academics page if user has access to osp' do
-      allow(current_user).to receive(:provisional_or_approved_osp_user?).and_return true
-      get :show, state: school.state, schoolId: school.id, page: 2
-      expect(response).to render_template('osp/osp_academics')
-      end
-
-    it 'should render Extracurricular & Culture  page if user has access to osp' do
-      allow(current_user).to receive(:provisional_or_approved_osp_user?).and_return true
-      get :show, state: school.state, schoolId: school.id, page: 3
-      expect(response).to render_template('osp/osp_extracurricular_culture')
     end
 
-    it 'should render Facilities & Staff  page if user has access to osp' do
-      allow(current_user).to receive(:provisional_or_approved_osp_user?).and_return true
-      get :show, state: school.state, schoolId: school.id, page: 4
-      expect(response).to render_template('osp/osp_facilities_staff')
+    {
+        osp_basic_information: 1, #page number
+        osp_academics: 2,
+        osp_extracurricular_culture: 3,
+        osp_facilities_staff: 4
+    }.each do |page, page_number|
+      it "should render #{page} page if user has access to osp and page is #{page_number}" do
+        allow(current_user).to receive(:provisional_or_approved_osp_user?).and_return true
+        get :show, state: school.state, schoolId: school.id, page: page_number
+        expect(response).to render_template(page)
+      end
     end
   end
 
-
+  describe '#submit' do
+    before do
+      allow(current_user).to receive(:provisional_or_approved_osp_user?).and_return true
+    end
+    {
+        osp_basic_information: 1, #page number
+        osp_academics: 2,
+        osp_extracurricular_culture: 3,
+        osp_facilities_staff: 4
+    }.each do |page, page_number|
+      it "should redirect user back to #{page} page when submit is clicked on the #{page} page" do
+        get :show, state: school.state, schoolId: school.id, page: page_number
+        expect(response).to render_template(page)
+      end
+    end
+  end
 end
