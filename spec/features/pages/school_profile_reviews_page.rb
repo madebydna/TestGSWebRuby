49bdc8733381dd218da28e1958213975846267aa
Jonathan Wrobel <jwrobel@greatschools.org>
2015-03-31 11:00:58 -0700

    OM-1079 added additional feature specs for topical reviews page

--- spec/features/pages/school_profile_reviews_page.rb
+++ spec/features/pages/school_profile_reviews_page.rb
@@ -1,12 +1,18 @@
 class SchoolProfileReviewsPage < SitePrism::Page
   class ReviewsSection < SitePrism::Section
-    element :questions, 'form'
+    elements :questions, 'form'
+    elements :review_questions, '.js-topical-review-container'
+  end
+
+  class ReviewQuestion < SitePrism::Section
+    elements :review_comment, '.js-gs-review-comment'
   end
 
   set_url_matcher /#{States.any_state_name_regex}\/[a-zA-Z\-.]+\/[0-9]+-[a-zA-Z\-.]+\/reviews\/$/
 
   element :profile_navigation, '#navigation2'
-  section :review_module, ReviewsSection, '.js-topicalReviewQuestionsConatiner'
+  section :review_module, ReviewsSection, '.js-topicalReviewQuestionsContainer'
+  section :review_question, ReviewQuestion, '.js-topical-review-container'
 end
 
 
--- spec/features/pages/school_profile_reviews_page_spec.rb
+++ spec/features/pages/school_profile_reviews_page_spec.rb
@@ -3,7 +3,7 @@ require_relative '../contexts/school_profile_contexts'
 require_relative '../examples/page_examples'
 require_relative '../pages/school_profile_reviews_page'
 
-describe 'School Profile Reviews Page' do
+describe 'School Profile Reviews Page', js: true do
   include_context 'Visit School Profile Reviews'
 
   after do
@@ -11,58 +11,62 @@ describe 'School Profile Reviews Page' do
     clean_models School
   end
 
-  shared_context 'with a topic and question' do
-    # create a topic - requires that a school was set by previous context
+  shared_context 'with two topics and questions' do
+    # create topics- requires that a school was set by previous context
     let(:topic) { FactoryGirl.create(:review_topic, school_level: school.level_code, school_type: school.type) }
+    let(:topic2) { FactoryGirl.create(:review_topic, name: 'Topic2', school_level: school.level_code, school_type: school.type) }
     # create questions for topic
     # Execute immediately
-    let!(:review_question) { FactoryGirl.create(:review_question, review_topic: topic)  }
+    let!(:review_question) { FactoryGirl.create(:review_question, review_topic: topic) }
+    let!(:review_question2) { FactoryGirl.create(:review_question, review_topic: topic2, question: 'How you like Elvis?') }
+
     after do
       clean_models ReviewTopic, ReviewQuestion
+    end
+  end
+
+  shared_context 'Click Question Response' do
+    before do
+      response_option = subject.all(:css, '.js-checkboxContainer', visible: true).first
+      response_option.click
+    end
+  end
+
+  shared_context 'Submit Response' do
+    before do
+      question_submit = subject.all(:css, '.js-review-question-submit', visible: true).first
+      question_submit.click
+      sleep (1)
     end
   end
 
   with_shared_context 'Given basic school profile page', 'Reviews' do
     with_shared_context 'with Alameda High School' do
       include_example 'should be on the correct page'
-
       it 'should show the review module' do
         expect(subject).to have_review_module
       end
-
-      with_shared_context 'with a topic and question' do
+      with_shared_context 'with two topics and questions' do
         it 'should show the first question' do
-
           expect(subject.review_module).to have_questions
-          # expect(subject.review_module_questions.first).to be_visible
+        end
+        it 'should not show the review comment form' do
+          expect(subject.review_question).not_to have_review_comment
+        end
+
+        with_shared_context 'Click Question Response' do
+          it 'should show the review comment section' do
+            expect(subject.review_question).to have_review_comment
+          end
+          with_shared_context 'Submit Response' do
+            it 'should show next question' do
+              question_text = subject.find(:css, '.bg-yellow').text
+              expect(question_text).to eq(review_question2.question)
+            end
+          end
         end
       end
     end
   end
-
-
-  # with_shared_context 'Given basic school profile page' do
-  #   with_shared_context 'with Alameda High School' do
-  #     include_example 'should be on the correct page'
-  #     expect_it_to_have_element(:profile_navigation)
-  #   end
-  #
-  #   with_shared_context 'with an inactive school' do
-  #     it 'should not be on the profile page' do
-  #       pending 'TODO: Do not allow profile page to handle inactive school'
-  #       fail
-  #     end
-  #     # include_example 'should be on the correct page'
-  #   end
-  #
-  #   with_shared_context 'with a demo school' do
-  #     include_example 'should be on the correct page'
-  #     expect_it_to_have_element(:profile_navigation)
-  #     include_example 'should have the noindex meta tag'
-  #     include_example 'should have the nofollow meta tag'
-  #     include_example 'should have the noarchive meta tag'
-  #   end
-  # end
-
 end
 
