a9a230b5f59fdf27b0ee6d6f213acf3a2c563727
Keith McHugh <kmchugh@greatschools.org>
2015-04-07 14:53:40 -0700

    PT-1428 Small syntax update from code review.

--- lib/data_loading/data_files/data_load_mapping.rb
+++ lib/data_loading/data_files/data_load_mapping.rb
@@ -9,24 +9,23 @@ class DataLoadMapping
   validate :files_to_be_array
 
   def files_to_be_array
-    if !@files.kind_of?(Array)
+    unless @files.kind_of?(Array)
       errors.add(:files, "Configuration for Files should be an array")
     end
   end
 
-  
   def initialize(config)
     @config = config
     @name = config[:name]
     @source = config[:source]
-    @files = config[:files].each do |file_mapping|
+    @files = [*config[:files]].each do |file_mapping|
       begin
         DataFileMapping.new(file_mapping)
       rescue => error
         log.error error
         next
       end
-    end if config[:files]
+    end
   end
 
 end
