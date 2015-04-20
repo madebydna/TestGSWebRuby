51cac987efa23ca3abe1dd148e701399634ad625
Samson Sprouse <ssprouse@greatschools.org>
2015-04-20 15:52:11 -0700

    OM-1097 Added some elements to SchoolProfileReviewsPage

--- spec/features/pages/school_profile_reviews_page.rb
+++ spec/features/pages/school_profile_reviews_page.rb
@@ -20,6 +20,19 @@ class SchoolProfileReviewsPage < SitePrism::Page
   element :profile_navigation, '#navigation2'
   section :review_module, ReviewsSection, '.js-topicalReviewQuestionsContainer'
   section :visible_review_question, ReviewQuestionVisible, ".js-topical-review-container.slick-active"
+
+  section :reviews_list, '#js_reviewsList' do
+    sections :reviews, '.cuc_review' do
+      element :review_helpful_button, '.js_reviewHelpfulButton'
+      element :flag_review_link, '.rs-report-review-link'
+      element :review_flagged_text, 'div', text: 'You\'ve reported this review'
+      element :one_star, '.i-16-orange-star .i-16-star-1'
+      element :two_stars, '.i-16-orange-star .i-16-star-2'
+      element :three_stars, '.i-16-orange-star .i-16-star-3'
+      element :four_stars, '.i-16-orange-star .i-16-star-4'
+      element :five_stars, '.i-16-orange-star .i-16-star-5'
+    end
+  end
 end
 
 
