af9e879bd6ea7e13173c270e36d01ce84b2e961b
Jonathan Wrobel <jwrobel@greatschools.org>
2015-05-04 15:03:34 -0700

    OM-1096 fixed order of review topics for links to reviews carousel

--- app/decorators/review_topic_decorator.rb
+++ app/decorators/review_topic_decorator.rb
@@ -14,18 +14,18 @@ class ReviewTopicDecorator < Draper::Decorator
   def topic_key
     {'1'=> {header: "Review your school!",
                subheading: "Families want to hear your honest opinion about your school." },
-     '2'=> {header: "How is homework at your school?",
-                subheading: "Too much? Too little? Share you opinion?"},
-     '3'=> {header: "Share your thoughts on teachers.",
-                subheading: "Great schools begin with great teachers.  How are yours?"},
-     '4'=> {header: "How does your school teach honesty?",
+     '2'=> {header: "How does your school teach honesty?",
                subheading: "Search for your school to share your thoughts."},
-     '5'=> {header: "How does your school build empathy?",
+     '3'=> {header: "How does your school build empathy?",
                subheading: "Search for your school to share your thoughts."},
-     '6'=> {header: "How does your school develop persistence?",
+     '4'=> {header: "How does your school encourage respect?",
+               subheading: "Search for your school to share your thoughts."},
+     '5'=> {header: "How does your school develop persistence?",
             subheading: "Search for your school to share your thoughts."},
-     '7'=> {header: "How does your school encourage respect?",
-               subheading: "Search for your school to share your thoughts."}
+     '6'=> {header: "How is homework at your school?",
+                subheading: "Too much? Too little? Share you opinion."},
+     '7'=> {header: "Share your thoughts on teachers.",
+                subheading: "Great schools begin with great teachers.  How are yours?"}
     }
     end
 end
