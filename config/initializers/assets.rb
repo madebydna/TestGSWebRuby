b89723d24bd094f508a1b72a8b2f17d6256dab82
Christina Liu <cliu@greatschools.org>
2015-05-22 14:37:38 -0700

    AT-602 compling css and js assests

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,1 +1,1 @@
-Rails.application.config.assets.precompile += ['cm_athena_gs_v2.js', 'dropzone.min.js', 'dropzone.css']
+Rails.application.config.assets.precompile += ['cm_athena_gs_v2.js', 'dropzone.min.js', 'dropzone.css','bootstrap-datepicker.min.js', 'bootstrap-datepicker3.css']
