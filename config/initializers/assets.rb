5e52d738629a3d8caf51180a18ed7488851effab
Harrison <htouw@greatschools.org>
2015-06-10 11:45:22 -0700

    BB-166 Fix other asset loading issues with style guide

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -6,6 +6,7 @@ Rails.application.config.assets.precompile += [
     'bootstrap-datepicker3.css',
     'jquery.timepicker.css',
     'jquery.timepicker.min.js',
+    'main.css', #for style-guide
     'main.js', #for style-guide
     'picturefill.min.js' #for style-guide
 ]
