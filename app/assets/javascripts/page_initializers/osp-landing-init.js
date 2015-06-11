29b334d6f9d7458f07afa457fe030fcd6b2b924c
Shomi Arora <sarora@greatschools.org>
2015-06-11 11:37:08 -0700

    AT-776 adding landing page.

--- app/assets/javascripts/page_initializers/osp-landing-init.js
+++ app/assets/javascripts/page_initializers/osp-landing-init.js
@@ -1,6 +1,5 @@
 $(function() {
     if (gon.pagename == "OspLanding") {
-        GS.googleMap.addToInitDependencyCallbacks(GS.util.wrapFunction(GS.search.schoolSearchForm.init, this, []));
-        GS.search.autocomplete.searchAutocomplete.init();
+
     }
 });
