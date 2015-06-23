0968b8c4f0da174150ef26adffd3980ddfe63514
Christina Liu <cliu@greatschools.org>
2015-06-23 10:58:38 -0700

    AT-748 removed @pagename from controller

--- app/controllers/osp_registration_controller.rb
+++ app/controllers/osp_registration_controller.rb
@@ -1,12 +1,14 @@
 class OspRegistrationController < ApplicationController
+  before_action :set_login_redirect
   before_action :set_city_state
+
 
 
   def show
 
     page_title = 'School Account - Register | GreatSchools'
     gon.pageTitle = page_title
-    gon.pagename = @pagename
+    gon.pagename = 'GS:OSP:Register'
     set_omniture_data('GS:OSP:Register', 'Home,OSP,RegisterPage')
     set_meta_tags title: page_title,
                   description:' Register for a school account to edit your school\'s profile on GreatSchools.',
