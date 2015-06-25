746cd081ac1ed1d5a7d56baa074d2cb2d6333765
Jonathan Wrobel <jwrobel@greatschools.org>
2015-06-24 21:25:40 -0700

    OM-1225 updated review flag controller to not allow provisional users to flag reviews until they confirm email; Added specs;

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -13,12 +13,21 @@ class ReviewsController < ApplicationController
       return
     end
 
-    if logged_in?
+    if logged_in? && ! current_user.provisional?
       flag_review_and_redirect review_id: review_id, comment: comment
     else
       save_deferred_action :report_review_deferred, review_id: review_id, comment: comment
+      if ! logged_in?
       flash_error t('actions.report_review.login_required')
-      redirect_to signin_url
+        redirect_to signin_url
+        return
+      end
+      if current_user.provisional?
+        verification_email_url = url_for(:controller => 'user', :action => 'send_verification_email', :email => current_user.email)
+        flash_error t('actions.report_review.email_verification_required', verification_email_url: verification_email_url).html_safe
+        redirect_back
+        return
+      end
     end
   end
 
