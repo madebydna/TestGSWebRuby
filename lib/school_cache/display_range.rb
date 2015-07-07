86d2df00e0d108b17a4cca6dd51dfd00350f11d9
Harrison <htouw@greatschools.org>
2015-07-06 18:29:17 -0700

    AT-847 Code to add colors/display ranges to school cache

---
+++ lib/school_cache/display_range.rb
@@ -0,0 +1,44 @@
+class DisplayRange < ActiveRecord::Base
+
+  db_magic :connection => :gs_schooldb
+
+  attr_accessible :state, :data_type, :data_type_id, :year, :range
+
+  DEFAULT = 'default'
+
+  def self.for(data_type, data_type_id, state, value)
+    return nil unless value.present?
+
+    range = cached_ranges[[data_type, data_type_id, state.downcase]].presence || cached_ranges[[data_type, data_type_id, DEFAULT]]
+
+    range.present? ? get_range_value(range, value) : nil
+  rescue => e
+    #catch error
+    nil
+  end
+
+  def self.cached_ranges
+    #how long should we set cache for?
+    Rails.cache.fetch("display_ranges/#{Time.now.to_i}", expires_in: 1.minute) do
+      display_ranges_map
+    end
+  end
+
+  def self.display_ranges_map
+    #where clause to only grab current and past years
+    display_range_map = all.order(year: :desc).group_by { |dr| [dr.data_type, dr.data_type_id, (dr.state.try(:downcase) || DEFAULT)] }
+    display_range_map.inject({}) do | h, (key, drs) |
+      h.merge({key => JSON.parse(drs.first.range)})
+    end
+  end
+
+  #ex range. {'below average cap' => 32, 'average' => 60, 'above avg' => 101}
+  def self.get_range_value(range, value)
+    value = value.to_i
+    range.sort_by{|k,v| v}.each do |text, cap|
+      return text.chomp('_cap') if value <= cap.to_i
+    end
+    return nil
+  end
+
+end
