78228efaaa2e9db72faf02173ba7704a17780f9e
Jonathan Wrobel <jwrobel@greatschools.org>
2015-07-07 08:43:46 -0700

    OM-1249 fixed failing school_hash_to_url_for_profile spec; Added additional specs for to test for persistence of lang parameters;

--- spec/controllers/concerns/url_helper_spec.rb
+++ spec/controllers/concerns/url_helper_spec.rb
@@ -236,18 +236,42 @@ describe UrlHelper do
         to eq('Naauao')
     end
   end
-  describe '#to_school_profile' do
+  describe '#school_hash_to_url_for_profile' do
     context "when hash with a school's id, name, state abbreviation, and city" do
-      it "should return a url to a school profile" do
-        input = {"id"=>213, "name"=>"Bret Harte Middle School", "city"=>"Oakland", "state"=>"CA"}
-        expect(url_helper.send(:school_hash_to_url_for_profile, input)).
-          to eq('/california/oakland/213-Bret-Harte-Middle-School/')
-      end
-      context "when hash missing one of the school hash attributes" do
-        it "should return a url to a school profile" do
-          input = {"id"=>213, "name"=>"Bret Harte Middle School", "state"=>"CA"}
+      context 'with a lang param set' do
+        let(:params) { {lang: 'es'} }
+        before do
+          allow(url_helper).to receive(:params).and_return(params) 
+        end
+        it "should return a url to a school profile with lang param" do
+          input = {"id"=>213, "name"=>"Bret Harte Middle School", "city"=>"Oakland", "state"=>"CA"}
           expect(url_helper.send(:school_hash_to_url_for_profile, input)).
-              to eq(nil)
+            to eq('/california/oakland/213-Bret-Harte-Middle-School/?lang=es')
+        end
+        context "when hash missing one of the school hash attributes" do
+          it "should return nil" do
+            input = {"id"=>213, "name"=>"Bret Harte Middle School", "state"=>"CA"}
+            expect(url_helper.send(:school_hash_to_url_for_profile, input)).
+                to eq(nil)
+          end
+        end
+      end
+      context 'without a lang param set' do
+        let(:params) { {} }
+        before do
+          allow(url_helper).to receive(:params).and_return(params) 
+        end
+        it "should return a url to a school profile without lang param" do
+          input = {"id"=>213, "name"=>"Bret Harte Middle School", "city"=>"Oakland", "state"=>"CA"}
+          expect(url_helper.send(:school_hash_to_url_for_profile, input)).
+            to eq('/california/oakland/213-Bret-Harte-Middle-School/')
+        end
+        context "when hash missing one of the school hash attributes" do
+          it "should return nil" do
+            input = {"id"=>213, "name"=>"Bret Harte Middle School", "state"=>"CA"}
+            expect(url_helper.send(:school_hash_to_url_for_profile, input)).
+                to eq(nil)
+          end
         end
       end
     end
