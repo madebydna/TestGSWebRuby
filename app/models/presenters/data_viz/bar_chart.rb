9f71401ed6d86d8ba68cb69b19c160e08af19dba
Keith McHugh <kmchugh@greatschools.org>
2015-07-07 11:06:40 -0700

    AT-844 Add specs for the bar chart data viz presenter.

--- app/models/presenters/data_viz/bar_chart.rb
+++ app/models/presenters/data_viz/bar_chart.rb
@@ -2,18 +2,33 @@ class BarChart
 
   attr_accessor :config, :label, :value, :comparison_value, :performance_level, :grey_value
 
-  def initialize(config)
+  FULL_WIDTH = 100
+  SEPERATOR_WIDTH = 0.5
+
+  def initialize(config = {})
     self.config = config
-    parse_config
+    parse_config!
+  end
+
+  def display?
+    value.present?
   end
 
   private
 
-  def parse_config
-    self.label = config[:label] || 'All Students'
-    self.value = config[:value]
-    self.grey_value = 100 - value.to_f - 0.5
+  def parse_config!
+    self.label = config[:label]
     self.comparison_value = config[:comparison_value]
     self.performance_level = config[:performance_level]
+    set_value_fields!
+  end
+
+  def set_value_fields!
+    self.value = config[:value]
+    if [0, FULL_WIDTH].include? value.to_f
+      self.grey_value = FULL_WIDTH - value.to_f
+    else
+      self.grey_value = FULL_WIDTH - value.to_f - SEPERATOR_WIDTH
+    end
   end
 end
