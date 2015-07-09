131a7b727c43baa16f43011ab9542fae9edf82bd
Shomi Arora <sarora@greatschools.org>
2015-07-09 14:41:41 -0700

    PT-1663 cleaning up the cookie logic for show filter menu

--- app/assets/javascripts/page_initializers/search-init.js
+++ app/assets/javascripts/page_initializers/search-init.js
@@ -3,7 +3,6 @@ $(function() {
         GS.search.schoolSearchForm.setupToolTip();
         GS.search.schoolSearchForm.updateFilterState();
         GS.search.schoolSearchForm.setShowFiltersCookieHandler();
-        GS.search.schoolSearchForm.showFiltersMenuOnLoad();
         GS.search.assignedSchools.init();
         GS.googleMap.addToInitDependencyCallbacks(GS.util.wrapFunction(GS.search.schoolSearchForm.init, this, []));
         GS.googleMap.addToInitDependencyCallbacks(GS.util.wrapFunction(GS.search.toggleListMapView.init, this, []));
