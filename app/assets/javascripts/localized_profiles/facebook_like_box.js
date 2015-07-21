982a3bf0fcc2aa354f1dcfa1529c067c7126b72f
Anthony Roy <aroy@greatschools.org>
2015-07-21 09:27:01 -0700

    PT-1673 Update Facebook module to handle detect and handle error in page plugin

---
+++ app/assets/javascripts/localized_profiles/facebook_like_box.js
@@ -0,0 +1,36 @@
+GS.facebook = GS.facebook || {};
+
+/**
+ * Monitors the height of the Facebook plugin iframe, and calls handleIframeTooShort if it consistently drops
+ * below HEIGHT_CUTOFF, where consistently is defined as at least TOO_SHORT_LIMIT measurements.
+ *
+ * In addition, it stops monitoring if the iframe height is at least HEIGHT_CUTOFF for JUST_RIGHT_LIMIT
+ * measurements.
+ */
+GS.facebook.checkIframeHeight = function(opts, $) {
+  var $fbPagePluginDiv = opts.fbPagePluginDiv;
+  var schoolName = opts.schoolName;
+  var fbUrl = opts.fbUrl;
+  var TOO_SHORT_LIMIT = 5; // after this many short measurements, we'll take action
+  var JUST_RIGHT_LIMIT = 20; // after this many good measurements, we'll stop checking
+  var HEIGHT_CUTOFF = 30; // less than this many pixels is "too short"
+
+  var height = $fbPagePluginDiv.find('iframe').height();
+
+  if (height == null) {
+    // nothing to do
+  } else if (height < HEIGHT_CUTOFF) {
+    if (opts.tooShortCounter++ > TOO_SHORT_LIMIT) {
+      window.clearInterval(opts.intervalId);
+      var $grandParent = $fbPagePluginDiv.parent().parent();
+      // .text implicitly escapes the contents for HTML
+      var $newContents = $('<div>').addClass('col-xs-12 tac')
+        .append($('<h3>').addClass('mtn').text('Find out what parents are saying about ' + schoolName))
+        .append($('<a>').attr('target', '_blank').attr('href', fbUrl).text(fbUrl));
+      $grandParent.children().addClass('dn'); // hide existing contents of Facebook module
+      $grandParent.append($newContents); // append a new, visible child linking directly to the FB URL
+    }
+  } else if (opts.justRightCounter++ > JUST_RIGHT_LIMIT) {
+    window.clearInterval(opts.intervalId);
+  }
+};
