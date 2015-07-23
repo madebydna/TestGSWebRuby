1cf3ca21ab0308c0033b166ff3fd6820267dce3a
Anthony Roy <aroy@greatschools.org>
2015-07-23 11:34:08 -0700

    PT-1673 Simplify so that it retains the correct layout relative to the ad

--- app/assets/javascripts/localized_profiles/facebook_like_box.js
+++ app/assets/javascripts/localized_profiles/facebook_like_box.js
@@ -9,8 +9,7 @@ GS.facebook = GS.facebook || {};
  */
 GS.facebook.checkIframeHeight = function(opts, $) {
   var $fbPagePluginDiv = opts.fbPagePluginDiv;
-  var schoolName = opts.schoolName;
-  var fbUrl = opts.fbUrl;
+  var $fbPagePluginFallbackDiv = opts.fbPagePluginFallbackDiv;
   var TOO_SHORT_LIMIT = 5; // after this many short measurements, we'll take action
   var JUST_RIGHT_LIMIT = 20; // after this many good measurements, we'll stop checking
   var HEIGHT_CUTOFF = 30; // less than this many pixels is "too short"
@@ -22,13 +21,8 @@ GS.facebook.checkIframeHeight = function(opts, $) {
   } else if (height < HEIGHT_CUTOFF) {
     if (opts.tooShortCounter++ > TOO_SHORT_LIMIT) {
       window.clearInterval(opts.intervalId);
-      var $grandParent = $fbPagePluginDiv.parent().parent();
-      // .text implicitly escapes the contents for HTML
-      var $newContents = $('<div>').addClass('col-xs-12 tac')
-        .append($('<h3>').addClass('mtn').text('Find out what parents are saying about ' + schoolName))
-        .append($('<a>').attr('target', '_blank').attr('href', fbUrl).text(fbUrl));
-      $grandParent.children().not('.js-nohide').addClass('dn'); // hide existing contents of Facebook module, but not ads
-      $grandParent.prepend($newContents); // prepend a new, visible child linking directly to the FB URL
+      $fbPagePluginDiv.addClass('dn').hide();
+      $fbPagePluginFallbackDiv.removeClass('dn').show();
     }
   } else if (opts.justRightCounter++ > JUST_RIGHT_LIMIT) {
     window.clearInterval(opts.intervalId);
