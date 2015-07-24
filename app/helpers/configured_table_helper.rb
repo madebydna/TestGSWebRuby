acbfc45d3785643ebd88bbc52e1917fef9e383d2
Keith McHugh <kmchugh@greatschools.org>
2015-07-24 10:06:02 -0700

    AT-867 Better variable names for configured table with images. Also ensure that the partial is not given nil after filtering for all students.

--- app/helpers/configured_table_helper.rb
+++ app/helpers/configured_table_helper.rb
@@ -15,7 +15,7 @@ module ConfiguredTableHelper
     output.html_safe
   end
 
-  def filter_for_all_students(data)
+  def filter_for_all_students(data = {})
     data.each do |k, values|
       data[k] = values.select do |value|
         value[:breakdown].nil? || value[:breakdown].to_s.downcase == 'all students'
