fc8a293b6f48220bb78b86a000bb4c0c4f4deadb
Christina Liu <cliu@greatschools.org>
2015-07-24 11:30:35 -0700

    AT-845 using bar chart parent for show hide and html refactor

--- app/assets/javascripts/page_initializers/quality-init.js
+++ app/assets/javascripts/page_initializers/quality-init.js
@@ -22,25 +22,24 @@ if(gon.pagename == "Quality"){
             $('.js-dataVizBarChartContainer').on('click', 'li', function () {
                 var title = $(this).text();
                 var dashedTitle = title.replace(/ /g, "-");
-                $('.js-dataVizBarChart:not(.js-' + dashedTitle + ')').addClass('dn');
-                $('.js-' + dashedTitle).removeClass('dn');
+                var $parent = $(this).closest('.js-dataVizBarChartContainer');
+                $parent.find('.data-viz-bar-chart:not(.js-' + dashedTitle + ')').addClass('dn');
+                $parent.find('.js-' + dashedTitle).removeClass('dn');
             });
         };
-
-        $('.js-dataVizBarChart').first().removeClass('dn');
-        SubgroupCharts();
 
         var toggleBreakdownsHandler = function() {
             $('.js-dataVizBarChartContainer').on('click', '.js-barChartBreakdown', function () {
                 var $self = $(this);
-                var breakdownClass = '.js-' + $self.data('breakdown')
+                var breakdownClass = '.js-' + $self.data('breakdown');
                 var $parent = $self.closest('.js-dataVizBarChart');
 
-                $parent.find('.js-barChart').addClass('dn')
-                $parent.find('.js-barChart' + breakdownClass).removeClass('dn')
+                $parent.find('.js-barChart').addClass('dn');
+                $parent.find('.js-barChart' + breakdownClass).removeClass('dn');
             });
-        }
+        };
 
+        SubgroupCharts();
         toggleBreakdownsHandler()
 
 
