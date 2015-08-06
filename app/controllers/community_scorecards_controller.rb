02edd829d936b7bd1021e91b97c340a2f44616cd
Christina Liu <cliu@greatschools.org>
2015-08-06 15:50:28 -0700

    AT-894 refactor ips scorecard css

--- app/controllers/community_scorecards_controller.rb
+++ app/controllers/community_scorecards_controller.rb
@@ -1,6 +1,8 @@
 class CommunityScorecardsController < ApplicationController
 
-def show
-  @collection = Collection.find(15)
-end
+  before_filter :use_gs_bootstrap
+
+  def show
+    @collection = Collection.find(15)
+  end
 end
