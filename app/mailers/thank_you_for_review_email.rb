bf6280671173b1ef2b39ef6e6f47e71020de3420
Anthony Roy <aroy@greatschools.org>
2015-08-07 10:22:43 -0700

    PT-1687 Let ET handle from header

--- app/mailers/thank_you_for_review_email.rb
+++ app/mailers/thank_you_for_review_email.rb
@@ -2,10 +2,6 @@ require 'addressable/uri'
 class ThankYouForReviewEmail < AbstractExactTargetMailer
 
   self.exact_target_email_key = 'review_posted_trigger'
-  self.from = {
-    address: 'gs-batch@greatschools.org',
-    name: 'GreatSchools'
-  }
   self.priority = 'High' # Valid options = Low | Medium | High
 
   def self.deliver_to_user(user, school, school_review_page_url)
