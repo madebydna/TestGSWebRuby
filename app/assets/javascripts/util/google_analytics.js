dc6d9a70ed51c2d2020f2a8b340f0029b556db1c
Jonathan Wrobel <jwrobel@greatschools.org>
2015-08-17 18:14:51 -0700

    OM-1409 added google tracking to language selection in top nav
      - added data attributes for category, action and label for language
        selection link
      - refactored google_analytics code to remove unused getter methods,
        onload methods, and onload classes and labels
      - updated usage examples to match current use cases

--- app/assets/javascripts/util/google_analytics.js
+++ app/assets/javascripts/util/google_analytics.js
@@ -1,23 +1,18 @@
 /// This will handle events sent on load complete and accept event
 GS.googleAnalytics =  GS.googleAnalytics || {};
 GS.googleAnalytics.tracking = (function() {
-  var gaLoadSelector = '.js-gaLoad';
   var gaClickSelector = '.js-gaClick';
-  var gaLoadLabel = 'gaLoadLabel';
   var gaClickLabel = "gaClickLabel";
-  var gaLoadValue = 'gaLoadValue';
   var gaClickValue = 'gaClickValue';
   var gaClickAction = 'gaClickAction';
   var gaClickCategory = 'gaClickCategory';
 
   var clickEventType = "click";
-  var pageName = '';
 
   var init = function(page){
     //if(GS.util.isBrowserTouch()){
     //  clickEventType = "touchend";
     //}
-    pageName = page;
     onClick();
   };
 
@@ -35,12 +30,6 @@ GS.googleAnalytics.tracking = (function() {
   *
   * */
 
-  var onLoad = function(){
-    $(gaLoadSelector).filter(":visible").each(function () {
-      sendLoadEvent($(this));
-    });
-  };
-
   var onClick = function() {
     $(gaClickSelector).on(clickEventType, function(){
       sendClickEvent($(this));
@@ -54,32 +43,6 @@ GS.googleAnalytics.tracking = (function() {
     var category = getCategory($obj, gaClickCategory);
     send( category, action, label, value);
 
-  };
-
-  var sendLoadEvent = function($obj){
-    var label = getLabel($obj, gaLoadLabel);
-    var value = getValue($obj, gaLoadValue);
-    send( getPageName(), 'load', label, value);
-  };
-
-  var getPageName = function(){
-    return pageName;
-  };
-
-  var getLoadLabelName = function(){
-    return gaLoadLabel;
-  };
-
-  var getClickLabelName = function(){
-    return gaClickLabel;
-  };
-
-  var getLoadValueName = function(){
-    return gaLoadValue;
-  };
-
-  var getClickValueName = function(){
-    return gaClickValue;
   };
 
   var getLabel = function($obj, labelName){
@@ -120,19 +83,25 @@ GS.googleAnalytics.tracking = (function() {
   }
 
 })();
+
+///  Usage
 //
-//  Usage
-//
-//<a class='js-gaLoad js-gaClick'
-//data-ga-load-label='loadlinktest'
-//data-ga-click-label='clickonlinktest'
-//data-ga-load-value='33'
-//data-ga-click-value='99'>TEST</a>
+//<a class='js-gaClick'
+//data-ga-click-category='language selection'
+//data-ga-click-action='global nav bar'
+//data-ga-click-label='spanish'
+//data-ga-click-value=''</a>
 
 // any element
-//<div class="pbl js-gaLoad" data-ga-load-label='bread crumbs on page' data-ga-load-value='989'>
+// <div class='pbl js-gaClick'
+//data-ga-click-category='language selection'
+//data-ga-click-label='spanish'
+//data-ga-click-action='global nav bar'>
+//</div>
 
 // using link_to
 //<%= link_to school_url(@school), class: "js-gaClick", data:
-//{'ga-click-label'=> 'school header link',
-//    'ga-click-value'=>'90', } do %>
+//{ ga-click-category => 'school link',
+//'ga-click-action'=> 'search result',
+//'ga-click-label'=> 'school',
+// 'ga-click-value'=>'90', } do %>/
