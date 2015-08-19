e4271e1068eebd5010569e64d7e1d8eba56cf029
Harrison <htouw@greatschools.org>
2015-08-19 10:32:41 -0700

    AT-905 add styling to sort links

--- app/assets/javascripts/page_initializers/community-scorecards-init.js
+++ app/assets/javascripts/page_initializers/community-scorecards-init.js
@@ -1,5 +1,10 @@
 $(function() {
   if (gon.pagename == GS.CommunityScorecards.Page.pageName) {
     GS.CommunityScorecards.Page.init();
+
+    $('.js-communityScorecard').on('click', '.js-tableSort', function (e) {
+      $('.js-tableSort').addClass('sort-link');
+      $(this).removeClass('sort-link');
+    });
   }
 });
