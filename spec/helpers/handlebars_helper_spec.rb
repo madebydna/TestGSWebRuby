ede333e61db4bcc22182d4891fe1466f08000d73
Keith McHugh <kmchugh@greatschools.org>
2015-08-19 11:13:25 -0700

    AT-892 Add one more spec for good measure.

--- spec/helpers/handlebars_helper_spec.rb
+++ spec/helpers/handlebars_helper_spec.rb
@@ -7,6 +7,7 @@ describe HandlebarsHelper do
     {
       'app/views/handlebars/X/_Y.html.erb' => 'X.Y',
       'app/views/handlebars/X/Y/_Z.html.erb' => 'X.Y.Z',
+      'app/views/handlebars/W/X/Y/_Z.html.erb' => 'W.X.Y.Z',
     }.each do |file_path, scope|
       it "should handle #{file_path}" do
         expect(helper.t_scope_for(file_path)).to eq scope
