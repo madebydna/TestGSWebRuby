da5f31d93dd399ba68f44116fa8e5c091de5c061
Samson Sprouse <ssprouse@greatschools.org>
2015-08-21 18:23:27 -0700

    OM-1350 Add I18n exception handler that logs when translations missing

--- config/initializers/extensions/i18n.rb
+++ config/initializers/extensions/i18n.rb
@@ -15,4 +15,17 @@ module I18n
     locale != default_locale && locale_available?(locale) ? locale.to_s : nil
   end
 
+  class GSLoggingExceptionHandler < ExceptionHandler
+    def call(exception, locale, key, options)
+      if exception.is_a?(MissingTranslation)
+        GSLogger.warn(:i18n, nil, vars: options.merge(key: key, locale: locale), message: "Translation missing for #{key}")
+        super
+      else
+        super
+      end
+    end
+  end
+
 end
+
+I18n.exception_handler = I18n::GSLoggingExceptionHandler.new
