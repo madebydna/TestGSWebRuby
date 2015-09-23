593e6ae8cfe10f875ebec83b8a128176b44baf9a
mseltzer <mseltzer@greatschools.org>
2015-09-23 14:48:15 -0700

    PT-1816 - SIGNIN_PAGENAME missing for omniture.

--- app/assets/javascripts/page_initializers/header_init.js
+++ app/assets/javascripts/page_initializers/header_init.js
@@ -18,3 +18,10 @@ $(function () {
     GS.ad.interstitial.attachInterstitial();
   }
 });
+if (gon.pagename == 'signin/new') {
+
+  var JOIN_PAGENAME = 'GS:Admin:CreateAccount';
+  var JOIN_HIER = 'Account,SignUp';
+  var SIGNIN_PAGENAME = 'GS:Admin:Login';
+  var SIGNIN_HIER = 'Account,LogIn';
+}
