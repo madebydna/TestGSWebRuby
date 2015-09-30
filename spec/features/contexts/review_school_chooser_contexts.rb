4ef3dc87047d51a33ddfdf6b6521a4a2e06e3b53
Jonathan Wrobel <jwrobel@greatschools.org>
2015-09-30 15:35:17 -0700

    OM-1502 fixed broken review_school_chooser feature specs
      - changed cleaning of database model and factory girl mocking to
        associate the reviews to the right topic
      - added a new spec testing that review highlight link navigates to
       school profile reviews page

--- spec/features/contexts/review_school_chooser_contexts.rb
+++ spec/features/contexts/review_school_chooser_contexts.rb
@@ -1,17 +1,15 @@
 require 'spec_helper'
 
 shared_context 'Visit Review School Chooser Page with for topic 1' do
-    let(:overall_topic) { FactoryGirl.create(:overall_topic) }
+    let(:overall_topic) { FactoryGirl.create(:overall_topic, id: 1) }
     let(:school) { FactoryGirl.create(:school) }
-    let!(:overall_rating_question) { FactoryGirl.create(:overall_rating_question, review_topic: overall_topic ) }
-    let!(:reviews) { FactoryGirl.create_list(:five_star_review, 15, review_question_id: 1, school: school ) }
+    let(:overall_rating_question) { FactoryGirl.create(:overall_rating_question, id: 1, review_topic: overall_topic ) }
+    let!(:reviews) { FactoryGirl.create_list(:review, 15, question: overall_rating_question, school: school ) }
     before do
-      pending 'Fails on Jenkins'
-      fail
       visit review_choose_school_path
     end
 
-  after(:all) do
+  after do
     clean_models :gs_schooldb, ReviewTopic, ReviewQuestion, Review, ReviewAnswer
     clean_models :ca, School
   end
--- spec/features/pages/review_school_chooser_page.rb
+++ spec/features/pages/review_school_chooser_page.rb
@@ -1,10 +1,20 @@
 class  ReviewSchoolChooserPage < SitePrism::Page
+
+  class ReviewHighlightSection < SitePrism::Section
+    element :school_link, '.link-darkgray'
+  end
+
+  section :review_highlight_section, ReviewHighlightSection, '.review-highlight'
   element :review_school_chooser_header, 'h1', text: 'Review your school!'
   element :review_highlight, '.review-highlight'
   element :review_school_chooser, '.js-autocompleteContainer'
   section :recent_reviews, '.reviews-list' do
     element :recent_reviews_header, 'h3', text: 'Recent reviews'
     elements :review_modules, '.cuc_review'
+  end
+
+  def click_on_school_link
+    review_highlight_section.school_link.click
   end
 
 end
