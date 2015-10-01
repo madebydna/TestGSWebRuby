2a6ed6717ea8a43f00ca75ac4e4c7d54f088a33a
Jonathan Wrobel <jwrobel@greatschools.org>
2015-10-01 08:23:51 -0700

    OM-1525 added translations for review school chooser landing page
      - translated the headings for each review topic
      - translated the school pickers headers

--- app/decorators/review_topic_decorator.rb
+++ app/decorators/review_topic_decorator.rb
@@ -52,8 +52,8 @@ class ReviewTopicDecorator < Draper::Decorator
       '1' => {
         city: 'Orem',
         state: 'UT',
-        header: "Review your school!",
-        subheading: "Families want to hear your honest opinion about your school.",
+        header: I18n.t('decorators.review_topic_decorator.topic_1_header'),
+        subheading: I18n.t('decorators.review_topic_decorator.topic_1_subheading'),
         sample_review_school: 'Noah Webster Academy',
         sample_review: "Our daughter ... has made leaps and bounds academically, and
          socially. She is safe, appreciated by her teachers and peers, and feels that
@@ -63,8 +63,8 @@ class ReviewTopicDecorator < Draper::Decorator
       '2' => {
         city: 'Jersey City',
         state: 'NJ',
-        header: "How does your school teach honesty?",
-        subheading: "Search for your school to share your thoughts.",
+        header: I18n.t('decorators.review_topic_decorator.topic_2_header'),
+        subheading: I18n.t('decorators.review_topic_decorator.topic_2_subheading'),
         sample_review_school: 'M E T S Charter School',
         sample_review: "... when it comes to my son, I like that he's held
              accountable for his actions. Every mother wants to raise an honest
@@ -74,8 +74,8 @@ class ReviewTopicDecorator < Draper::Decorator
       '3' => {
         city: 'Encinitas',
         state: 'CA',
-        header: "How does your school build empathy?",
-        subheading: "Search for your school to share your thoughts.",
+        header: I18n.t('decorators.review_topic_decorator.topic_3_header'),
+        subheading: I18n.t('decorators.review_topic_decorator.topic_3_subheading'),
         sample_review_school: 'St John Catholic School',
         sample_review: "From day one, there is a strong emphasis on service
              to others, giving back to the community and helping those who are less
@@ -85,8 +85,8 @@ class ReviewTopicDecorator < Draper::Decorator
       '4' => {
         city: 'Kansas City',
         state: 'MO',
-        header: "How does your school encourage respect?",
-        subheading: "Search for your school to share your thoughts.",
+        header: I18n.t('decorators.review_topic_decorator.topic_4_header'),
+        subheading: I18n.t('decorators.review_topic_decorator.topic_4_subheading'),
         sample_review_school: 'Frontier School Of Innovation',
         sample_review: "As a parent, I am extremely pleased with the standards
          the school has for respect among student-student, student-teacher,
@@ -96,8 +96,8 @@ class ReviewTopicDecorator < Draper::Decorator
       '5' => {
         city: 'Seattle',
         state: 'WA',
-        header: "How does your school develop persistence?",
-        subheading: "Search for your school to share your thoughts.",
+        header: I18n.t('decorators.review_topic_decorator.topic_5_header'),
+        subheading: I18n.t('decorators.review_topic_decorator.topic_5_subheading'),
         sample_review_school: 'North Seattle French School',
         sample_review: "But through gentle, caring exposure and lots of support,
            kids learn that they can start out knowing nothing and bit by bit, become
@@ -107,8 +107,8 @@ class ReviewTopicDecorator < Draper::Decorator
       '6' => {
         city: 'Sahuarita',
         state: 'AZ',
-        header: "How is homework at your school?",
-        subheading: "Too much? Too little? Share you opinion.",
+        header: I18n.t('decorators.review_topic_decorator.topic_6_header'),
+        subheading: I18n.t('decorators.review_topic_decorator.topic_6_subheading'),
         student: true,
         sample_review_school: 'Walden Grove High School',
         sample_review: "Many teachers give homework on a semi-weekly or weekly
@@ -119,8 +119,8 @@ class ReviewTopicDecorator < Draper::Decorator
       '7' => {
         city: 'Alexandria',
         state: 'VA',
-        header: "Share your thoughts on teachers.",
-        subheading: "Great schools begin with great teachers.  How are yours?",
+        header: I18n.t('decorators.review_topic_decorator.topic_7_header'),
+        subheading: I18n.t('decorators.review_topic_decorator.topic_7_subheading'),
         sample_review_school: 'John Adams Elementary School',
         sample_review: "The teachers are constantly striving to learn new ways to reach
            out to the students.",
@@ -129,8 +129,8 @@ class ReviewTopicDecorator < Draper::Decorator
       '8' => {
         city: 'West Allis',
         state: 'WI',
-        header: "Are you grateful for your school?",
-        subheading: "Share why. Find your school to start.",
+        header: I18n.t('decorators.review_topic_decorator.topic_8_header'),
+        subheading: I18n.t('decorators.review_topic_decorator.topic_8_subheading'),
         sample_review_school: 'Good Shepherds Lutheran School',
         sample_review: "I would like to thank the school secretary for her warm smile and
           welcoming personality. She brightens my day...",
