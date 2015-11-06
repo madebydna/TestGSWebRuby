f23bb6f6b81d88f0cc0218cb93df86d7f5e81c44
Keith McHugh <kmchugh@greatschools.org>
2015-11-06 13:21:47 -0800

    AT-1105 Fix issue mitigated by hiding subgroup comparison module.
    The real issue was that the data attribute migrator was
    looking for an event that is fired at the end of page render.
    This commit removes that on hook and reverts the hide/show mechanism
    for the module we were using to prevent broken interactions.

--- app/assets/javascripts/util/selectpicker.js
+++ app/assets/javascripts/util/selectpicker.js
@@ -7,25 +7,23 @@ GS.selectpicker = GS.selectpicker || (function() {
   // This function grabs them off of the original select options and moves them
   // on to the corresponding <a>'s that Bootstrap Select creates.
   var updateDataAttributes = function() {
-     $(window).on('load.bs.select.data-api', function () {
-       _($(selectpickerButtons)).forEach(function(button) {
-         var $button = $(button);
-         var id = $button.data('id');
-         var $selectpickerSelect = $(selectpickerSelects + '#' + id);
-         var $selectpickerOptions = $selectpickerSelect.children('option');
-         _($selectpickerOptions).forEach(function(option, index) {
-           var newData = $(option).data();
-           var $correspondingA = correspondingA($button, index);
-           var $spanChildrenOfCorrespondingA = $correspondingA.find('span');
-           $spanChildrenOfCorrespondingA.addClass(GS.util.getJsClasses($correspondingA));
-           _(newData).forEach(function(value, key) {
-             var dashCasedKey = key.replace(/([A-Z])/g, function($1){return "-"+$1.toLowerCase();})
-             $correspondingA.attr('data-' + dashCasedKey, value);
-             $spanChildrenOfCorrespondingA.attr('data-' + dashCasedKey, value);
-           });
-         });
-       });
-     })
+    _($(selectpickerButtons)).forEach(function(button) {
+      var $button = $(button);
+      var id = $button.data('id');
+      var $selectpickerSelect = $(selectpickerSelects + '#' + id);
+      var $selectpickerOptions = $selectpickerSelect.children('option');
+      _($selectpickerOptions).forEach(function(option, index) {
+        var newData = $(option).data();
+        var $correspondingA = correspondingA($button, index);
+        var $spanChildrenOfCorrespondingA = $correspondingA.find('span');
+        $spanChildrenOfCorrespondingA.addClass(GS.util.getJsClasses($correspondingA));
+        _(newData).forEach(function(value, key) {
+          var dashCasedKey = key.replace(/([A-Z])/g, function($1){return "-"+$1.toLowerCase();})
+          $correspondingA.attr('data-' + dashCasedKey, value);
+          $spanChildrenOfCorrespondingA.attr('data-' + dashCasedKey, value);
+        });
+      });
+    });
   };
 
   var correspondingA = function($selectpickerButton, index) {
