3a47b0bf6f475e03b181ebc4bbac1a752ba14232
Jonathan Wrobel <jwrobel@greatschools.org>
2015-11-12 17:22:35 -0800

    JT-91 update character count for review comments
      - Start character count when user gets to 500 characters left
      - update character count language and translattions

--- app/assets/javascripts/topical_reviews/topical_review.js
+++ app/assets/javascripts/topical_reviews/topical_review.js
@@ -388,6 +388,7 @@ GS.topicalReview.characterCount = (function () {
     var TOPICAL_REVIEW_CONTAINER = '.js-topicalReviewContainer';
     var CHARACTER_MESSAGE_DISPLAY = '.js-reviewCharacterDisplay';
     var CHARACTER_COUNT_DISPLAY = '.js-reviewCharacterCount'
+    var START_CHARACTER_COUNT = 1900;
     var MAX_CHARACTERS = 2400;
 
     var init = function (textField) {
@@ -396,8 +397,9 @@ GS.topicalReview.characterCount = (function () {
         var characterCountDisplay = $(reviewContainer).find(CHARACTER_COUNT_DISPLAY);
         var characterCount = textField.value.length;
         var maxCharacters = MAX_CHARACTERS;
+        var startCharacterCount = START_CHARACTER_COUNT;
         var remainingCharacters = maxCharacters - characterCount;
-        if (characterCount > 0) {
+        if (characterCount > startCharacterCount) {
             $(characterDisplay).show();
             $(characterCountDisplay).text(remainingCharacters);
         }
