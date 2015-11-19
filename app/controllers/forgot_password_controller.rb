a53c5fef183915eee3f378bedf4208ad86767281
Shomi Arora <sarora@greatschools.org>
2015-11-19 11:20:41 -0800

    JT-173 Redirecting link

--- app/controllers/forgot_password_controller.rb
+++ app/controllers/forgot_password_controller.rb
@@ -33,7 +33,7 @@ class ForgotPasswordController < ApplicationController
     if hash.present?
       login_from_hash(hash)
       if logged_in?
-        redirect_to manage_account_url(:anchor => 'change-password')
+        redirect_to reset_password_url
       else
         log.error("Error while allowing reset password for hash: #{hash}")
         redirect_to signin_url
