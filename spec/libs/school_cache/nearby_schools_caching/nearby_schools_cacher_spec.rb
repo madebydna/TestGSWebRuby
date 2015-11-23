dfb6815c87fa88ec6094ad6118f3850cef1c5140
Keith McHugh <kmchugh@greatschools.org>
2015-11-23 11:18:16 -0800

    AT-1043 Set up system for configuring nearby schools lists by state.

--- spec/libs/school_cache/nearby_schools_caching/nearby_schools_cacher_spec.rb
+++ spec/libs/school_cache/nearby_schools_caching/nearby_schools_cacher_spec.rb
@@ -2,10 +2,6 @@ require 'spec_helper'
 
 describe NearbySchoolsCaching::NearbySchoolsCacher do
   let(:decorator) { NearbySchoolsCaching::QueryResultDecorator }
-  let(:school) { FactoryGirl.build(:alameda_high_school) }
-  let(:nearby_schools_cacher) do
-    NearbySchoolsCaching::NearbySchoolsCacher.new(school)
-  end
   methodologies = [
     NearbySchoolsCaching::Methodologies::ClosestSchools,
     NearbySchoolsCaching::Methodologies::TopNearbySchools,
@@ -13,19 +9,43 @@ describe NearbySchoolsCaching::NearbySchoolsCacher do
   ]
 
   describe '#build_hash_for_cache' do
-    let(:expected_result) do
-      {
-        closest_schools: [],
-        closest_top_then_top_nearby_schools: [],
-      }
-    end
     before do
       methodologies.each do |methodology|
         allow(methodology).to receive(:results).and_return([])
       end
     end
-    it 'should build the correct structure' do
-      expect(nearby_schools_cacher.build_hash_for_cache).to eq(expected_result)
+
+    context 'for a school in CA' do
+      let(:school) { FactoryGirl.build(:alameda_high_school, state: 'CA') }
+      let(:nearby_schools_cacher) do
+        NearbySchoolsCaching::NearbySchoolsCacher.new(school)
+      end
+      let(:expected_result) do
+        {
+          closest_schools: [],
+          closest_top_then_top_nearby_schools: [],
+        }
+      end
+
+      it 'should build the correct lists' do
+        expect(nearby_schools_cacher.build_hash_for_cache).to eq(expected_result)
+      end
+    end
+
+    context 'for a state not configured for more lists' do
+      let(:school) { FactoryGirl.build(:alameda_high_school, state: 'NH') }
+      let(:nearby_schools_cacher) do
+        NearbySchoolsCaching::NearbySchoolsCacher.new(school)
+      end
+      let(:expected_result) do
+        {
+          closest_schools: [],
+        }
+      end
+
+      it 'should build the correct lists' do
+        expect(nearby_schools_cacher.build_hash_for_cache).to eq(expected_result)
+      end
     end
   end
 
