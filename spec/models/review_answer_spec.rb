454705d87a6826f5084cc967fc8a990429562911
Jonathan Wrobel <jwrobel@greatschools.org>
2015-12-03 17:20:29 -0800

    JT-262 build review data for review bar chart
      - added method to school reviews calculations to create array of
        hashes used in bar chart
      - added unit tests for new method
      - created new methods to access answer label on review and review
        answer and added specs
      - turned on both review modules temporarily

--- spec/models/review_answer_spec.rb
+++ spec/models/review_answer_spec.rb
@@ -3,4 +3,25 @@ require 'spec_helper'
 describe ReviewAnswer do
   it { is_expected.to be_a(ReviewAnswer) }
 
+  after do
+    clean_dbs :gs_schooldb
+  end
+
+  let(:overall_review) { FactoryGirl.build(:five_star_review) }
+  let(:teacher_review) { FactoryGirl.build(:teacher_effectiveness_review) }
+  let(:school) { FactoryGirl.build(:school) }
+  let(:user) { FactoryGirl.build(:user) }
+  let(:overall_answer) { FactoryGirl.build(:review_answer, review: overall_review, value: 5) }
+  let(:teacher_answer) { FactoryGirl.build(:review_answer, review: teacher_review) }
+
+
+  describe '#label' do
+    it 'should return value when not for overall topic' do
+      expect(teacher_answer.label).to eq(teacher_answer.value)
+    end
+
+    it 'should return special label when for overall topic' do
+      expect(overall_answer.label).to eq('5 stars')
+    end
+  end
 end
--- spec/models/review_question_spec.rb
+++ spec/models/review_question_spec.rb
@@ -3,6 +3,8 @@ require_relative 'examples/model_with_active_field'
 
 describe ReviewQuestion do
   let(:review_question) {FactoryGirl.create(:review_question)}
+  let(:overall_question) {FactoryGirl.create(:overall_rating_question)}
+  let(:teacher_question) {FactoryGirl.create(:teacher_question)}
   after do
     clean_dbs :gs_schooldb
   end
@@ -22,6 +24,17 @@ describe ReviewQuestion do
 
    it 'should return an array of the level codes' do
      expect(review_question.level_code_array).to eq(%w(p e m h))
+   end
+ end
+
+ describe '#response_label_array' do
+   it 'should return array with response values when not for overall topic' do
+     expect(teacher_question.response_label_array).to eq(teacher_question.response_array)
+   end
+
+   it 'should return array with star labels for overall topic' do
+     label_array = (2..5).map {|n| n.to_s + ' stars'}.unshift('1 star')
+     expect(overall_question.response_label_array).to eq(label_array)
    end
  end
 
