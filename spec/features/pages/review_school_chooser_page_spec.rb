17d7b812e182c49a79881d98845a8001f8083c65
Jonathan Wrobel <jwrobel@greatschools.org>
2015-12-11 15:31:38 -0800

    JT-444 implemented code review feed back
      - added tests for reviews list on topic 8
      - removed upcase from translation and used css to upcase words

--- spec/features/pages/review_school_chooser_page_spec.rb
+++ spec/features/pages/review_school_chooser_page_spec.rb
@@ -11,7 +11,7 @@ describe 'Review School Chooser Page' do
     it { is_expected.to have_review_highlight }
     it { is_expected.to have_review_school_chooser }
     it { is_expected.to_not have_greater_good_logo_link }
-    on_subject :click_on_school_link do
+    on_subject :click_on_school_link, js: true do
       it 'should navigate to a school profile reviews page' do
         expect(SchoolProfileReviewsPage.new).to be_displayed
       end
@@ -23,12 +23,18 @@ describe 'Review School Chooser Page' do
     include_examples 'should have a footer'
   end
 
-  with_shared_context 'Visit Review School Chooser Page for topic 8', js: true do
+  with_shared_context 'Visit Review School Chooser Page for topic 8' do
     subject(:page_object) { ReviewSchoolChooserPage.new }
     it { is_expected.to have_gratitude_topic_review_school_chooser_header}
     it { is_expected.to have_review_highlight }
     it { is_expected.to have_review_school_chooser }
     it { is_expected.to have_greater_good_logo_link }
+
+    with_subject :recent_reviews do
+      it { is_expected.to have_recent_reviews_header }
+      it { is_expected.to have_review_modules count: 15 }
+    end
+    include_examples 'should have a footer'
   end
 end
 
