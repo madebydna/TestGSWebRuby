2dc14e2516facc851fcb42079e5be1ddf98b8417
Anthony Roy <aroy@greatschools.org>
2015-12-14 13:45:42 -0800

    JT-497 Improve error logging, also CR-534 feedback

--- script/fetch_content.rb
+++ script/fetch_content.rb
@@ -1,14 +1,17 @@
-def fetch_content_usage
+if ARGV.length != 2
   abort %q(
 USAGE: rails runner script/fetch_content key url
 
 Ex: rails runner script/fetch_content homepage_feature http://www.gs.org/gk/json-api/homepage_feature/)
-end
-
-if ARGV.length != 2
-  fetch_content_usage
   exit 1
 end
-content_fetcher = ExternalContentFetcher.new
-rval = content_fetcher.fetch!(ARGV[0], ARGV[1])
-exit (rval ? 0 : 1)
+
+begin
+  content_fetcher = ExternalContentFetcher.new(ARGV[0], ARGV[1], true)
+  rval = content_fetcher.fetch!
+  exit (rval ? 0 : 1)
+rescue => e
+  puts "#{e.class} #{e.message}"
+  puts e.backtrace.join("\n")
+  exit 1
+end
