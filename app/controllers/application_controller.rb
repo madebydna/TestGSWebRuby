743495991442a16aa69f1746b79aecf49d4a05fe
Jonathan Wrobel <jwrobel@greatschools.org>
2015-12-23 10:13:26 -0800

    JT-439 refactor state params methods from appliction controller
      - moved state params method as is into state params concerns module
        that is included in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,6 +14,7 @@ class ApplicationController < ActionController::Base
   include AbTestConcerns
   include TrailingSlashConcerns
   include CityParamsConcerns
+  include StateParamsConcerns
 
   prepend_before_action :set_global_ad_targeting_through_gon
 
@@ -97,18 +98,6 @@ class ApplicationController < ActionController::Base
     super
   end
 
-  def state_param
-    state = (gs_legacy_url_decode(params[:state]) || '').dup
-    state_abbreviation = States.abbreviation(state)
-    state_abbreviation.downcase! if state_abbreviation.present?
-    state_abbreviation
-  end
-
-  def state_param_safe
-    state_param
-  end
-
-
   def path_w_query_string (do_not_append, page_name)
     url = Addressable::URI.parse(request.original_url)
     url.path = url.path + page_name + '/' unless page_name.nil?
@@ -116,18 +105,6 @@ class ApplicationController < ActionController::Base
     url.query_values = nil unless url.query_values.present?
     url.to_s
   end
-
-  def require_state
-    render 'error/page_not_found', layout: 'error', status: 404 if state_param.blank?
-  end
-
-  #todo think of better name than require_state_instance_variable or refactor require_state code
-  def require_state_instance_variable
-    if @state.nil?
-      block_given? ? yield : render('error/page_not_found', layout: 'error', status: 404)
-    end
-  end
-
 
   # Finds school given request param schoolId
   def find_school
@@ -210,19 +187,6 @@ class ApplicationController < ActionController::Base
       short: States.abbreviation(gs_legacy_url_decode(params[:state]))
     } if params[:state]
     @city = gs_legacy_url_decode(params[:city]) if params[:city]
-  end
-
-  def set_verified_city_state
-    if params[:state].present?
-      long_name = States.state_name(gs_legacy_url_decode(params[:state])) || return
-      short_name = States.abbreviation(gs_legacy_url_decode(params[:state])) || return
-      @state = {long: long_name, short: short_name}
-    end
-    if params[:city]
-      city = gs_legacy_url_decode(params[:city])
-      city = City.find_by_state_and_name(@state[:short], city) || return
-      @city = city
-    end
   end
 
   def write_meta_tags
