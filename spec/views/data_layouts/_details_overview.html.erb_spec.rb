45abc144f9b37f99c71cab9acbef848a51e02bb9
Samson Sprouse <ssprouse@greatschools.org>
2016-02-04 23:28:11 -0800

    AT-1237 Fix details module view spec

--- spec/views/data_layouts/_details_overview.html.erb_spec.rb
+++ spec/views/data_layouts/_details_overview.html.erb_spec.rb
@@ -3,22 +3,22 @@ require 'spec_helper'
 describe '_details_overview.html.erb' do
   let(:data) do
     {
-        "Before/After Care" => ['Before care'],
-        "Dress Code" => ['Uniform'],
+        "Before / After care" => ['Before care'],
+        "Dress policy" => ['Uniform'],
         "Transportation" => ['Buses / vans provided for students'],
-        "Academic Focus" => ['Business, Special Ed, Technology'],
-        "Arts" => ['Computer animation, Graphics, Video and Film'],
-        "World Languages" => ['French, German, Spanish, Hungarian, Polish'],
-        "Student Demographics" => {
+        "Academic focus" => ['Business, Special Ed, Technology'],
+        "Arts media" => ['Computer animation, Graphics, Video and Film'],
+        "World languages" => ['French, German, Spanish, Hungarian, Polish'],
+        "Student ethnicity" => {
             "Hispanic" => 88,
             "Black" => 7,
             "White" => 2,
             "Asian/Pacific Islander" => 2,
             "2 or more races" => 1
         },
-        "Free & reduced lunch participants" => 98,
-        "Students w/ disabilities" => 6,
-        "English language learners" => 45,
+        "Free or reduced lunch" => { 'All students' => 78 },
+        "Students with disabilities" => { 'All students' => 6 },
+        "English language learners" => { 'All students' => 45 },
     }
   end
   before do
@@ -27,14 +27,14 @@ describe '_details_overview.html.erb' do
     render 'school_profile/data_layouts/details_overview', data: data
   end
 
-  context 'when given a hash that has basic information data' do
+  context 'when given a hash with school details data' do
     subject { rendered }
 
-    it { is_expected.to have_content 'Before/After Care' }
+    it { is_expected.to have_content 'Before / After care' }
     it { is_expected.to have_content 'Before care' }
-    it { is_expected.to_not have_content 'English language learners' }
-    it { is_expected.to_not have_content 'Student Demographics' }
-    it { is_expected.to have_content 'Dress Code' }
+    it { is_expected.to have_content 'English language learners' }
+    it { is_expected.to have_content 'Student Demographics' }
+    it { is_expected.to have_content 'Dress policy' }
   end
 
   context 'when given a hash that has bad data' do
