ac3a73d78d2c5866132ae5340aecd702efd7754b
Shomi Arora <sarora@greatschools.org>
2016-02-17 11:50:40 -0800

    JT-650 cleaning up test case

--- spec/features/page_objects/district_home_page.rb
+++ spec/features/page_objects/district_home_page.rb
@@ -6,7 +6,5 @@ class DistrictHomePage < SitePrism::Page
   include Breadcrumbs
 
   section :top_rated_schools_section, PageObjects::TopRatedSchools::Section, '#top-rated-schools-in-district'
-  element :district_link, 'a', text: 'District website'
-
   element :email_signup_section, '.js-shared-email-signup'
 end
