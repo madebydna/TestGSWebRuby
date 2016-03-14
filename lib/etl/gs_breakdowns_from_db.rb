e164057a70e8631d5b2ba577bdc2f218a5b47ef9
ray hunter <rhunter@greatschools.org>
2016-03-14 14:19:56 -0700

    AT-1362 -- cleaned up GSBreakdownClass

--- lib/etl/gs_breakdowns_from_db.rb
+++ lib/etl/gs_breakdowns_from_db.rb
@@ -1,32 +1,20 @@
 require 'mysql2'
 
 class GsBreakdownsFromDb
-
-  def initialize
-    @client = Mysql2::Client.new(:host => 'datadev.greatschools.org', :username=>'service', :password=>'service')
-  end
-
-  def breakdown_from_db
+  
+  def self.fetch
 
     all_breakdowns = Hash.new
 
-    results = @client.query("select * from gs_schooldb.TestDataBreakdown;")
+    client = Mysql2::Client.new(:host => 'datadev.greatschools.org', :username=>'service', :password=>'service')
 
-    #require 'pry'; binding.pry
+    results = client.query("select * from gs_schooldb.TestDataBreakdown;")
 
     results.each do |row|
 
       all_breakdowns[ row["id"] ] = row["name"]
 
     end
-
-
-
-    #hash_result = Hash[result.map {|key, value| [key, value]}]
-
-    #puts hash_result.class
-
-    #result
 
     all_breakdowns
 
