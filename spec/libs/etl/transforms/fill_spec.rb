ff5a14820978c4099b76fa34b8a4a7275fc24072
ray hunter <rhunter@greatschools.org>
2016-03-17 10:57:37 -0700

    added spec for fill.rb

---
+++ spec/libs/etl/transforms/fill_spec.rb
@@ -0,0 +1,34 @@
+require 'spec_helper'
+require 'step'
+
+describe Fill do
+
+  # will need to add #initialize test
+
+  describe "#process" do
+
+    context 'with valid hash' do
+      let(:row) do
+        {:c => 50, :d => 75}
+      end
+      let(:fill) do
+        Fill.new({:a => 100, :b => 200})
+      end
+
+      subject { fill.process(row) }
+
+      it 'is expected to be hash as an end result' do
+        expect( subject ).to be_a_kind_of(Hash)
+      end
+
+      it 'is expected to be a merge of two hashes' do
+        expect( subject ).to eq({:c=>50, :d=>75, :a=>100, :b=>200})
+      end
+
+    end
+    
+  end
+
+end
+
+
