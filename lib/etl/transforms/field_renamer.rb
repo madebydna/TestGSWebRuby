0613f0fa1a5081a75b61a390408e16b65fbebaca
Samson Sprouse <ssprouse@greatschools.org>
2016-03-21 14:29:26 -0700

    AT-1363 Allow FieldRenamer spec to accept procs

--- lib/etl/transforms/field_renamer.rb
+++ lib/etl/transforms/field_renamer.rb
@@ -2,15 +2,12 @@ require 'step'
 
 class FieldRenamer < GS::ETL::Step
 
-  def initialize(from, to)
+  def initialize(from, field_name_or_proc)
     self.from = from
-    self.to = to
+    self.to = calculate_destination_field(field_name_or_proc)
   end
 
   def process(row)
-    # unless row.has_key?(@from)
-    #   raise "Tried to rename field '#{@from}' but it did not exist"
-    # end
     if can_process?(row)
       record(:renamed)
       row[@to] = row.delete(@from)
@@ -24,6 +21,22 @@ class FieldRenamer < GS::ETL::Step
     row.has_key?(@from) && ! row.has_key?(@to)
   end
 
+  def event_key
+    "#{@from} -> #{@to}"
+  end
+
+  private
+
+  def calculate_destination_field(field_name_or_proc)
+    if field_name_or_proc.is_a?(String) || field_name_or_proc.is_a?(Symbol)
+      return field_name_or_proc
+    elsif field_name_or_proc.is_a?(Proc)
+      return field_name_or_proc.call(@from)
+    else
+      raise ArgumentError, 'must provide a field name (string, symbol) or proc'
+    end
+  end
+
   def from=(field)
     raise 'Field to rename cannot be nil or empty' if field.empty?
     @from = field
@@ -32,10 +45,6 @@ class FieldRenamer < GS::ETL::Step
   def to=(field)
     raise 'Destination field after rename cannot be nil or empty' if field.empty?
     @to= field
-  end
-
-  def event_key
-    "#{@from} -> #{@to}"
   end
   
 end
