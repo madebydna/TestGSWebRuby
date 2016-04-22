75e482d7ae0ee4aef041675e9fe24bec071d090c
Samson Sprouse <ssprouse@greatschools.org>
2016-04-22 15:31:16 -0700

    AT-1431 Add step to allow us to sum cells across multiple rows

---
+++ lib/etl/transforms/execute_block_when_row_group_changes.rb
@@ -0,0 +1,39 @@
+require_relative '../step'
+require_relative '../source'
+
+class ExecuteBlockWhenRowGroupChanges < GS::ETL::Source
+  attr_accessor :group_by_fields, :join_fields
+
+  def initialize(group_by_fields, options = {}, &key_change_callback)
+    self.group_by_fields = group_by_fields
+    @rows_in_group = []
+    @key_change_callback = key_change_callback
+    @pass_rows_through_on_key_same = true unless options.has_key?(:pass_rows_through_on_key_same)
+    @pass_rows_through_on_key_change = true unless options.has_key?(:pass_rows_through_on_key_change)
+
+    @key_same_callback = proc do |rows_in_group, row|
+      rows_in_group << row
+      @pass_rows_through_on_key_same ? row : nil
+    end
+  end
+
+  def key_for_row(row)
+    row.select { |column| group_by_fields.include?(column) }
+  end
+
+  def process(row)
+    key = key_for_row(row)
+    @last_key ||= key
+    if @last_key && key != @last_key
+      new_rows = @key_change_callback.call(@rows_in_group)
+      new_rows = [new_rows] unless new_rows.is_a?(Array)
+      @rows_in_group = [row]
+      new_rows += [row] if @pass_rows_through_on_key_change
+      @last_key = key
+      return new_rows
+    else
+      @last_key = key
+      return @key_same_callback.call(@rows_in_group, row)
+    end
+  end
+end
