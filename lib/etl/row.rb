1c79c3be169eb235b9b8f61abdd52d1d94df0ed0
Matt Hess <mhess@greatschools.org>
2016-04-26 23:40:58 -0700

    Properly clone row_hash in Row class

--- lib/etl/row.rb
+++ lib/etl/row.rb
@@ -5,7 +5,8 @@ module GS
     class Row
       include Enumerable
       extend Forwardable
-      def_delegators :row_hash, :each, :each_with_object, :[], :[]=, :blank?, :size, :select, :select!, :map, :map!, :-, :has_key?
+      hash_methods = Hash.instance_methods - Object.instance_methods
+      def_delegators :row_hash, *hash_methods
       attr_reader :row_num, :row_hash, :clone_num
 
       def initialize(row_hash, row_num)
@@ -16,6 +17,10 @@ module GS
 
       def method_missing(method, *args)
         @row_hash.send(method, *args)
+      end
+
+      def clone
+        self.class.new(@row_hash.clone, @row_num)
       end
 
       def copy
