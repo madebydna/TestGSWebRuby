ee58ee894e7e2420c7c718b8dd23087bfbec64c9
Samson Sprouse <ssprouse@greatschools.org>
2016-04-27 17:45:38 -0700

    AT-1431 modernize OH processor, fix ExecuteBlockWhenRowGroupChange initializer options

--- lib/etl/oh_test_processor.rb
+++ lib/etl/oh_test_processor.rb
@@ -13,77 +13,22 @@ require 'gs_breakdowns_from_db'
 class OHTestProcessor < GS::ETL::TestProcessor
   attr_reader :runnable_steps, :attachable_input_step, :attachable_output_step, :grade_subject_column_headers
 
-  def build_graph
-
-    return if @graph_built
-
-    @runnable_steps = [
-     district_files,
-      # school_files
-     # state_files
-    ]
-
-    combined_sources_steps = union_steps(
-      districts_pre_union_steps,
-      # schools_pre_union_steps
-      # state_pre_union_steps
-    )
-
-#  Test output for schools and districts
-    # s1 = combined_sources_steps.destination 'output test for districts and schools', CsvDestination, '/tmp/school_district_ohio_test.txt',
-    #   *[ :entity_level, :grade, :subject, :student_group, :state_id, :district_id, :district_name,
-    #      :school_id, :school_name, :value_float ]
-
-#  Test output for state files
-    s1 = combined_sources_steps.destination 'output test for state files', CsvDestination, '/tmp/state_ohio_test.txt',
-      :year, :entity_type, :entity_level, :state_id, :school_id,
-                       :school_name, :district_id, :district_name, :test_data_type,
-                       :test_data_type_id, :grade, :subject, :subject_id,
-                       :breakdown, :breakdown_id, :proficiency_band,
-                       :proficiency_band_id, :level_code,
-                       :number_tested, :value_float
-
-    @graph_built = true
-    self
+  def config_hash
+    {
+     file: 'foo'
+    }
   end
 
-  def run
-    build_graph
-    runnable_steps.each(&:run)
-  end
-
-  def district_files
-    @_districts ||=(
-      tab_delimited_source(
-        [
-          '1415_DIST_ETHNIC.xlsx'
-          # '1415_district_ethnicity_tabs.txt',
-          # '1415_district_gender_tabs.txt',
-          # '1415_district_lep_tabs.txt',
-          # '1415_district_all_tabs.txt'
-        ].map { |file| input_filename(file) }
-      )
-    )
-  end
-
-  def tab_delimited_source(file)
-    source = ExcelSource.new(file, nil, col_sep: "\t", max: @options[:max])
-    file_name = file.first.split('/').last
-    source.description = "Read #{file_name}"
-    source
-  end
-
-  def csv_source(file)
-    source = CsvSource.new(file, nil, max: @options[:max])
-    file_name = file.first.split('/').last
-    source.description = "Read #{file_name}"
-    source
-  end
-
-  def districts_pre_union_steps
-
-    s = district_files
-
+  source(
+    ExcelSource,
+    [
+      '1415_DIST_ETHNIC.xlsx',
+      '1415_district_gender.xlsx',
+      '1415_district_lep_tabs.xlsx',
+      '1415_district_all_tabs.xlsx'
+    ],
+    'district_files'
+  ) do |s|
     s = s.transform 'remove all invalid rows', DeleteRows, :watermark, /Achievement/, /K-3 Literacy data/
 
     s = s.transform 'get value for combined subject and grade', Transposer,
@@ -91,15 +36,18 @@ class OHTestProcessor < GS::ETL::TestProcessor
        :value_float,
        /15.*proficient/i
 
+    s = s.transform 'fill \'district\' for entity level, school id and name', Fill, {
+      entity_level: 'district',
+      school_id: 'district',
+      school_name: 'district'
+    }
 
-    s = s.transform 'remove empty values', DeleteRows, :value_float, 'NC'
-
-    s = s.transform 'convert values with < to negative', WithBlock do |row|
-      row[:value_float] = row[:value_float].gsub(/[<>]/,'-') unless row[:value_float].nil?
+    s = s.transform 'copy district_irn value to state_id', WithBlock do |row|
+      row[:state_id] = row[:district_irn]
       row
     end
 
-    s = s.transform 'delete nil values', DeleteRows, :value_float, nil
+    s = s.transform 'rename district_irn to district_id', FieldRenamer, :district_irn, :district_id
 
     s = s.transform 'split out subject and grade into 2 columns', WithBlock do |row|
       value = row[:subject_grade]
@@ -111,47 +59,19 @@ class OHTestProcessor < GS::ETL::TestProcessor
        row[:grade] = grade
        row
     end
-
-    s = s.transform 'fill \'district\' for entity level, school id and name', Fill, {
-      entity_level: 'district',
-      school_id: 'district',
-      school_name: 'district'
-    }
-
-    s = s.transform 'set district_irn value to state_id', ValueConcatenator, :state_id, :district_irn
-
-    s = s.transform 'rename district_irn to district_id', FieldRenamer, :district_irn, :district_id
-
-    s = s.transform 'add student group All for files w/o student group', WithBlock do |row|
-      row[:student_group] = 'All' unless row.has_key?(:student_group)
-      row
-    end
-
-    s.transform 'select columns', ColumnSelector, :year, :entity_type, :entity_level, :state_id, :school_id,
-                       :school_name, :district_id, :district_name, :test_data_type,
-                       :test_data_type_id, :grade, :subject, :subject_id,
-                       :breakdown, :breakdown_id, :proficiency_band,
-                       :proficiency_band_id, :level_code,
-                       :number_tested, :value_float
-
   end
 
-  def school_files
-    @_school_files ||=(
-      tab_delimited_source(
-        [
-          '1415_school_ethnic_tabs.txt',
-          '1415_school_gender_tabs.txt',
-          '1415_school_lep_tabs.txt',
-          '1415_school_all_tabs.txt'
-          ].map { |file| input_filename(file) }
-      )
-    )
-  end
-
-  def schools_pre_union_steps
-
-    s = school_files
+  source(
+    CsvSource,
+    [
+      '1415_school_ethnic_tabs.txt',
+      '1415_school_gender_tabs.txt',
+      '1415_school_lep_tabs.txt',
+      '1415_school_all_tabs.txt'
+    ],
+    'school_files',
+    col_sep: "\t"
+  ) do |s|
 
     s = s.transform 'remove all invalid rows for schools', DeleteRows, :watermark, /Achievement/, /K-3 Literacy data/
 
@@ -159,15 +79,6 @@ class OHTestProcessor < GS::ETL::TestProcessor
       :subject_grade,
       :value_float,
       /15.*proficient/i
-
-    s = s.transform 'remove empty values for schools', DeleteRows, :value_float, 'NC'
-
-    s = s.transform 'delete nil values for schools', DeleteRows, :value_float, nil
-
-    s = s.transform 'convert values with < to negative for schools', WithBlock do |row|
-      row[:value_float] = row[:value_float].gsub(/[<>]/,'-') unless row[:value_float].nil?
-      row
-    end
 
     s = s.transform 'split out subject and grade into 2 columns for schools', WithBlock do |row|
       value = row[:subject_grade]
@@ -185,67 +96,57 @@ class OHTestProcessor < GS::ETL::TestProcessor
 
     s = s.transform 'set building_irn to state_id for schools', ValueConcatenator, :state_id, :building_irn
 
-    s = s.transform 'add student group All for files w/o student group for schools', WithBlock do |row|
-      row[:student_group] = 'All' unless row.has_key?(:student_group)
-      row
-    end
-
     s = s.transform 'rename district irn, building_name and building irn for schools', MultiFieldRenamer, {
       district_irn: :district_id,
       building_name: :school_name,
       building_irn: :school_id
     }
-
-    s.transform 'select columns before union for schools', ColumnSelector,
-      :value_float, :grade, :subject, :district_id, :state_id, :entity_level, 
-      :student_group, :district_name, :school_id, :school_name, :number_tested
   end
 
-  def state_files
-    @_state_files ||=(
-      csv_source(
-        [
-          '1415_state_gender_proficiency_values.csv',
-          '1415_state_lep_proficiency_values.csv',
-          '1415_state_frl_proficiency_values.csv',
-          '1415_state_race_proficiency_values.csv'
-        ].map { |file| input_filename(file) }
-      )
-    )
-  end
+  source(
+    CsvSource,
+    [
+      '1415_state_gender_proficiency_values.csv',
+      '1415_state_lep_proficiency_values.csv',
+      '1415_state_frl_proficiency_values.csv',
+      '1415_state_race_proficiency_values.csv'
+    ],
+    'school_files',
+    col_sep: ",",
+    quote_char: '"'
+  ) do |s|
 
-  def state_pre_union_steps
-    s = state_files
-
-    s = s.transform 'Set entity type', Fill,
-      entity_type: 'state'
-
-    s = s.transform 'Set year, level code, and test data type', Fill,
-      year: 2015,
-      level_code: 'e,m,h',
-      test_data_type: 'oaa',
-      test_data_type_id: 20
+    s = s.transform 'fill \'state\' into entity type, entity level, and district, and school fields', Fill,
+    {
+      entity_type: 'state',
+      district_id: 'state',
+      state_id: 'state',
+      entity_level: 'state',
+      district_name: 'state',
+      school_id: 'state',
+      school_name: 'state'
+    }
     
     s = s.transform 'get value for combined grade and breakdown for all proficiency columns', Transposer,
       :grade_breakdown,
       :value_float,
       /proficiency_level_pct_of_total/
 
-    s = s.transform 'get value for combined grade and breakdown for all proficiency columns', Transposer,
+    s = s.transform 'get value for combined grade and breakdown for all number_tested columns', Transposer,
       :grade_breakdown_2,
       :number_tested,
       /students_tested/
 
-    s = s.transform 'foo', WithBlock do |row|
+    s = s.transform 'Remove some transposed rows', WithBlock do |row|
        row[:grade_breakdown][0..30] == row[:grade_breakdown_2][0..30] ? row : nil
-    end
-
-    s = s.transform 'remove empty values', DeleteRows, :value_float, nil, '--'
+    end 
 
     s = s.transform 'remove test values for students not matching grade of test', WithBlock do |row|
-      grade_match = row[:test_grade].downcase.gsub(' ','_')
-      if ! row[:grade_breakdown].to_s.include?(grade_match)
-        row = nil
+      if row[:test_grade]
+        grade_match = row[:test_grade].downcase.gsub(' ','_')
+        if ! row[:grade_breakdown].to_s.include?(grade_match)
+          row = nil
+        end
       end
       row
     end
@@ -259,38 +160,46 @@ class OHTestProcessor < GS::ETL::TestProcessor
       row
     end
 
-    s = s.transform 'rename grade and subject column', MultiFieldRenamer, {
-      :test_grade => :grade,
-      :test_subject => :subject
-    }
-
-    s = s.transform 'get numeric grade', WithBlock do |row|
-      row[:grade] = /(\d+)/.match(row[:grade])[1]
-      row
-    end
-
     s = s.transform 'Calculate total number tested per group and update rows in group', 
       ExecuteBlockWhenRowGroupChanges, [
         :district_id,
         :state_id,
-        :grade,
-        :subject
+        :test_grade,
+        :test_subject
       ], 
-      pass_rows_through_on_key_same: false,
-      pass_rows_through_on_key_change: false do |rows|
+      pass_rows_through_on_key_same: true,
+      pass_rows_through_on_key_change: true do |rows|
         rows_by_student_group = rows.group_by { |r| r[:student_group] }
         rows_by_student_group.each_pair do |_, row_group|
           total = row_group.inject(0.0) { |sum, r| sum += r[:number_tested].to_f }
           row_group.each { |r| r[:number_tested] = total }
         end
-        rows
+        nil
+        # rows
       end
+  end
 
-    s = s.transform 'fill \'state\' into district, entity level, and school id', Fill,
-    {
-      district_id: 'state', state_id: 'state',
-      entity_level: 'state', district_name: 'state',
-      school_id: 'state', school_name: 'state'
+  shared do |s|
+     s = s.transform 'Set year, level code, and test data type', Fill,
+       year: 2015,
+       level_code: 'e,m,h',
+       test_data_type: 'oaa',
+       test_data_type_id: 20
+
+    s = s.transform 'add student group All for files w/o student group', WithBlock do |row|
+      row[:student_group] = 'All' unless row.has_key?(:student_group)
+      row
+    end
+
+    s = s.transform 'Rename proficiency band',
+      FieldRenamer, :proficiency_level, :proficiency_band
+
+    s = s.transform 'Rename student group band',
+      FieldRenamer, :student_group, :breakdown
+
+    s = s.transform 'rename grade and subject column', MultiFieldRenamer, {
+      :test_grade => :grade,
+      :test_subject => :subject
     }
 
     s = s.transform 'Look up GS subject ID for each subject', HashLookup,
@@ -303,12 +212,6 @@ class OHTestProcessor < GS::ETL::TestProcessor
         'Writing' => 3,
       },
       to: :subject_id
-
-    s = s.transform 'Rename proficiency band',
-      FieldRenamer, :proficiency_level, :proficiency_band
-
-    s = s.transform 'Rename student group band',
-      FieldRenamer, :student_group, :breakdown
 
     s = s.transform 'Look up breakdown IDs', HashLookup,
       :breakdown,
@@ -349,25 +252,25 @@ class OHTestProcessor < GS::ETL::TestProcessor
       },
       to: :proficiency_band_id
 
-     s = s.transform 'select state columns', ColumnSelector, :year, :entity_type, :entity_level, :state_id, :school_id,
-                       :school_name, :district_id, :district_name, :test_data_type,
-                       :test_data_type_id, :grade, :subject, :subject_id,
-                       :breakdown, :breakdown_id, :proficiency_band,
-                       :proficiency_band_id, :level_code,
-                       :number_tested, :value_float
+    s = s.transform 'get numeric grade', WithBlock do |row|
+      matches = /(\d+)/.match(row[:grade])
+      row[:grade] = matches[1] if matches
+      row
+    end
 
+    s = s.transform 'remove empty values', DeleteRows, :value_float, nil, '--', 'NC'
+
+    s = s.transform 'convert values with < to negative', WithBlock do |row|
+      row[:value_float] = row[:value_float].gsub(/[<>]/,'-') unless row[:value_float].nil?
+      row
+    end
   end
 
   def draw
     require 'graph'
     g = Graph.new
-    steps = [
-      # state_files,
-      district_files,
-      school_files
-    ]
-    steps.each do |source|
-      source.to_a.each { |s| g.node(s.descriptor) }
+    @sources.each do |source|
+      g.node(source.descriptor)
       source.each_edge do |node1, node2|
         g.edge(node1.descriptor, (node2.descriptor))
       end
@@ -380,5 +283,7 @@ end
 
 raise ArgumentError.new('****Requires directory holding prepped files as command line argument****')  unless ARGV[0]
 
-OHTestProcessor.new(ARGV[0], max: (ARGV[1] && ARGV[1].to_i) ).build_graph.draw
-OHTestProcessor.new(ARGV[0], max: (ARGV[1] && ARGV[1].to_i) ).run
+processor = OHTestProcessor.new(ARGV[0], max: (ARGV[1] && ARGV[1].to_i) )
+processor.build_graph
+processor.draw
+OHTestProcessor.new(ARGV[0], max: (ARGV[1] && ARGV[1].to_i), offset: 0).run
--- lib/etl/transforms/execute_block_when_row_group_changes.rb
+++ lib/etl/transforms/execute_block_when_row_group_changes.rb
@@ -5,11 +5,15 @@ class ExecuteBlockWhenRowGroupChanges < GS::ETL::Source
   attr_accessor :group_by_fields, :join_fields
 
   def initialize(group_by_fields, options = {}, &key_change_callback)
+    options = options.merge({
+      pass_rows_through_on_key_change: true,
+      pass_rows_through_on_key_same: true
+    })
     self.group_by_fields = group_by_fields
     @rows_in_group = []
     @key_change_callback = key_change_callback
-    @pass_rows_through_on_key_same = true unless options.has_key?(:pass_rows_through_on_key_same)
-    @pass_rows_through_on_key_change = true unless options.has_key?(:pass_rows_through_on_key_change)
+    @pass_rows_through_on_key_same = options[:pass_rows_through_on_key_same]
+    @pass_rows_through_on_key_change = options[:pass_rows_through_on_key_change]
 
     @key_same_callback = proc do |rows_in_group, row|
       rows_in_group << row
@@ -25,15 +29,23 @@ class ExecuteBlockWhenRowGroupChanges < GS::ETL::Source
     key = key_for_row(row)
     @last_key ||= key
     if @last_key && key != @last_key
-      new_rows = @key_change_callback.call(@rows_in_group)
-      new_rows = [new_rows] unless new_rows.is_a?(Array)
-      @rows_in_group = [row]
-      new_rows += [row] if @pass_rows_through_on_key_change
-      @last_key = key
-      return new_rows
+      result = handle_key_change(row)
     else
-      @last_key = key
-      return @key_same_callback.call(@rows_in_group, row)
+      result = handle_key_same(row)
     end
+    @last_key = key
+    result
+  end
+
+  def handle_key_same(row)
+    @key_same_callback.call(@rows_in_group, row)
+  end
+
+  def handle_key_change(row)
+    new_rows = @key_change_callback.call(@rows_in_group)
+    new_rows = [new_rows] unless new_rows.is_a?(Array)
+    @rows_in_group = [row]
+    new_rows += [row] if @pass_rows_through_on_key_change
+    new_rows
   end
 end
