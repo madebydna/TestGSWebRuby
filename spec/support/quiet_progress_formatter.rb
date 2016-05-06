2771c3712eb1c9e57025653d152e1e73b8c3f2e3
Samson Sprouse <ssprouse@greatschools.org>
2016-05-06 00:41:41 -0700

    Remove pending spec output from QuietProgressFormatter

--- spec/support/quiet_progress_formatter.rb
+++ spec/support/quiet_progress_formatter.rb
@@ -1,19 +1,17 @@
 class QuietProgressFormatter < RSpec::Core::Formatters::ProgressFormatter
-  RSpec::Core::Formatters.register self, :example_passed, :example_pending, :example_failed, :start_dump
-
+  RSpec::Core::Formatters.register self, :example_passed, :example_pending, :example_failed, :dump_pending
+  
   def example_passed(_notification)
-    output.print RSpec::Core::Formatters::ConsoleCodes.wrap('', :success)
   end
 
   def example_pending(_notification)
-    output.print RSpec::Core::Formatters::ConsoleCodes.wrap('', :pending)
   end
 
   def example_failed(_notification)
-    output.print RSpec::Core::Formatters::ConsoleCodes.wrap('F', :failure)
   end
 
-  def start_dump(_notification)
-    output.puts
+  def dump_pending(_notification)
+    # nope
   end
+
 end
