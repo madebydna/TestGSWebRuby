556dfe8e66a71fd70a24e59d6c050bbb23a7a4d2
mseltzer <mseltzer@greatschools.org>
2016-06-09 21:32:35 -0700

    JT-1101  - fixed grade list, slight refactor or view page

--- app/controllers/user_email_preferences_controller.rb
+++ app/controllers/user_email_preferences_controller.rb
@@ -16,7 +16,9 @@ class UserEmailPreferencesController < ApplicationController
     account_meta_tags('My email preferences')
 
     @display_grade_level_array = grade_array_pk_to_8
-    @selected_grade_level = @current_user.student_grade_levels
+    # selected_grade_level = @current_user.student_grade_levels
+    @selected_grade_level = @current_user.student_grade_levels.map(&:grade).join(",")
+
   end
 
 
