523087a95929646eaba90f7e7f0300790f228e52
Samson Sprouse <ssprouse@greatschools.org>
2016-06-29 15:54:51 -0700

    JT-1327 Remove short footer from osp registration confirmation view

--- spec/features/pages/osp_registration_spec.rb
+++ spec/features/pages/osp_registration_spec.rb
@@ -12,6 +12,10 @@ require 'features/examples/footer_examples'
 describe 'OSP Registration page' do
   with_shared_context 'visit registration confirmation page' do
     describe_mobile_and_desktop do
+      describe 'footer' do
+        subject { OspPage.new }
+        include_examples 'should have a footer'
+      end
       include_example 'should have element with text', 'h2', 'Thanks for creating a school account!'
       include_example 'should have element with text', 'h4', "We've sent you a verification email, click the link in the verification email to begin editing your profile."
       include_example 'should have link text on page', "View your school's profile"
