fb2b5343573a9f067e46d0fee9c2fbf11ca8638e
Samson Sprouse <ssprouse@greatschools.org>
2016-08-10 13:44:04 -0700

    JT-1474 Added spec to test that there is no NR rating on assigned school

--- spec/qa/pages/search_page_spec.rb
+++ spec/qa/pages/search_page_spec.rb
@@ -11,3 +11,13 @@ describe 'User sees assigned schools', js:true, type: :feature, remote: true do
   its('assigned_schools.first') { is_expected.to have_gs_rating }
   its('assigned_schools.first.gs_rating') { is_expected.to have_content(9) }
 end
+
+describe 'User doesnt see NR rating for assigned school', js: true, type: :feature, remote: true do
+  before do
+    visit '/search/search.page?lat=59.6525553&lon=-151.5058851&zipCode=99603&state=AK&locationType=street_address&normalizedAddress=1340%20East%20End%20Rd%2C%20Homer%2C%20AK%2099603&city=Homer&sortBy=DISTANCE&locationSearchString=1340%20east%20end%20rd%2C%2099603&distance=5'
+    wait_for_ajax
+  end
+  subject(:page_object) { SearchPage.new }
+  it { is_expected.to have_assigned_schools }
+  its('assigned_schools.first') { is_expected.to_not have_gs_rating }
+end
