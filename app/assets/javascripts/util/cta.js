d9b4244a2101b13c7faeb93f349d802a12a63ad1
Mitchell Seltzer <mseltzer@greatschools.org>
2016-08-16 13:42:20 -0700

    JT-1432 - small cleanup of js

--- app/assets/javascripts/util/cta.js
+++ app/assets/javascripts/util/cta.js
@@ -1,6 +1,6 @@
 var cta_profile_offset;
 $(function () {
-  $(window).on('scroll', _.throttle(manageFixedPositions, 100));
+  $(window).on('scroll', _.throttle(manageFixedPositions, 50));
   // may also need to call this after ads load...
   recaluculateOffsetForCTA();
   $(window).on('resize', _.debounce(recaluculateOffsetForCTA, 100));
@@ -12,41 +12,24 @@ function recaluculateOffsetForCTA() {
 
 
 function manageFixedPositions() {
-  // console.log("throttle execute");
-
-  // console.log("cta_profile_offset:"+cta_profile_offset);
   var cta = $("#cta");
-  // console.log("offset:"+cta.offset().top);
-  // console.log("scrollTop"+$(window).scrollTop());
+
   if (cta_profile_offset <= $(window).scrollTop()) {
     // after cta is scrolled to the top
     var ctaBottomOffset = cta.parent().height() - cta.height() - 60 + cta_profile_offset;
-    // console.log('ctaBottomOffset:'+ctaBottomOffset);
-    // console.log('scrolltop:'+$(window).scrollTop());
 
     if (ctaBottomOffset <= $(window).scrollTop()) {
-      cta.removeClass('cta-fixed-top');
-      cta.removeClass('cta-non-fixed-top');
-      cta.addClass('cta-align-bottom');
+      cta.removeClass('cta-fixed-top').removeClass('cta-non-fixed-top').addClass('cta-align-bottom');
     }
     else {
-      cta.removeClass('cta-non-fixed-top');
-      cta.removeClass('cta-align-bottom');
-      cta.addClass('cta-fixed-top');
+      cta.removeClass('cta-non-fixed-top').removeClass('cta-align-bottom').addClass('cta-fixed-top');
+
     }
   }
   else {
     // before cta is scrolled to the top
-    cta.removeClass('cta-fixed-top');
-    cta.removeClass('cta-align-bottom');
-    cta.addClass('cta-non-fixed-top');
+    cta.removeClass('cta-fixed-top').removeClass('cta-align-bottom').addClass('cta-non-fixed-top');
   }
-
-  //
-  // console.log("cta_profile_offset:"+cta_profile_offset);
-  // console.log("scrolltop:"+$(window).scrollTop());
-  // console.log("parentHeight:"+parentHeight);
-  // console.log("cta height:"+cta.height());
 
 //  when we reach top most point for cta and ad - add a class to fix them.
 // Then when we scroll within a distance of the map or footer we remove the fixed position and
