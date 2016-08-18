b69dafe7b3085cec3ac4ae775e35f25f0828c692
Mitchell Seltzer <mseltzer@greatschools.org>
2016-08-18 06:02:51 -0700

    JT-1432- refactor of css and js.  Added fixed bottom for mobile, first pass.

--- app/assets/javascripts/util/cta.js
+++ app/assets/javascripts/util/cta.js
@@ -1,53 +1,76 @@
-var cta_profile_offset;
-$(function () {
-  $(window).on('scroll', _.throttle(manageFixedPositions, 50));
-  // may also need to call this after ads load...
-  recaluculateOffsetForCTA();
-  $(window).on('resize', _.debounce(recaluculateOffsetForCTA, 100));
-});
+GS.school_profiles = GS.school_profiles || {};
 
-function recaluculateOffsetForCTA() {
-  cta_profile_offset = $("#cta").parent().offset().top;
-}
+GS.school_profiles.CTA = GS.school_profiles.CTA || (function () {
+      var cta_profile_offset;
+      var cta;
+      var transitionToMobile = 767;
 
+      var init = function () {
+        cta_profile_offset = 0;
+        cta = $("#cta");
 
-function manageFixedPositions() {
-  var cta = $("#cta");
+        // only init when cta is on the page. :)
+        if (cta.val() !== undefined) {
+          // set listener to scroll and resize events
+          $(window).on('scroll', _.throttle(manageFixedPositions, 50));
+          $(window).on('resize', _.debounce(recalculateCtaResize, 100));
+          // may also need to call this after ads load...
+          recalculateCtaResize();
+        }
+      };
 
-   console.log("cta_profile_offset:" + cta_profile_offset);
-   console.log("scrolltop:" + $(window).scrollTop());
-  cta.parent().height($("#cta").parent().parent().height());
-  if (cta_profile_offset <= $(window).scrollTop()) {
-    // after cta is scrolled to the top
-     console.log("cta.parent().height():" + cta.parent().height());
-     console.log("cta.height():" + cta.height());
-     console.log("cta_profile_offset:" + cta_profile_offset);
-    var ctaBottomOffset = cta.parent().height() - cta.height() + cta_profile_offset;
-
-    if (ctaBottomOffset <= $(window).scrollTop()) {
-      cta.removeClass('fixed-top')
-      cta.removeClass('non-fixed-top')
-      cta.addClass('align-bottom');
+      var recalculateCtaResize = function () {
+        cta_profile_offset = cta.parent().offset().top;
+        if ($(window).width() > transitionToMobile) {
+          setCTARowHeightToParent();
     }
     else {
-      cta.removeClass('non-fixed-top')
-      cta.removeClass('align-bottom')
-      cta.addClass('fixed-top');
-
+          $("#cta").parent().height('auto');
     }
-  }
-  else {
-    // before cta is scrolled to the top
-    cta.removeClass('fixed-top')
-    cta.removeClass('align-bottom')
-    cta.addClass('non-fixed-top');
-  }
+        manageFixedPositions();
+      };
 
-//  when we reach top most point for cta and ad - add a class to fix them.
-// Then when we scroll within a distance of the map or footer we remove the fixed position and
-// have the components align to the bottom of the column
-//
-//  wait another idea - when the column or parent hits the top we fix our positions
-//  when we are a certain distance from the bottom of the column we remove fixed and
-//  align to the bottom of the column. - so we base everything on the columns position.
-}
+      var setCTARowHeightToParent = function () {
+        cta.parent().height(cta.parent().parent().height());
+      };
+
+      var manageFixedPositions = function () {
+        // console.log("cta_profile_offset:" + cta_profile_offset);
+        // console.log("scrolltop:" + $(window).scrollTop());
+        // cta_profile_offset = offset from top of window to cta
+        // window.scrolltop = current scroll position of page
+        if ($(window).width() > transitionToMobile) {
+          if (cta_profile_offset <= $(window).scrollTop()) {
+            // after cta is scrolled to the top
+            //  console.log("cta.parent().height():" + cta.parent().height());
+            //  console.log("cta.height():" + cta.height());
+            //  console.log("cta_profile_offset:" + cta_profile_offset);
+            var ctaBottomOffset = cta.parent().height() - cta.height() + cta_profile_offset;
+
+            if (ctaBottomOffset <= $(window).scrollTop()) {
+              // desktop align to the bottom of the right rail when scroll reaches bottom of right rail
+              cta.removeClass('fixed-top').removeClass('non-fixed-top').addClass('align-bottom');
+            }
+            else {
+              // desktop fix cta to top when scroll reaches the top of window
+              cta.removeClass('non-fixed-top').removeClass('align-bottom').addClass('fixed-top');
+            }
+          }
+          else {
+            // desktop default before cta is scrolled to the top
+            cta.removeClass('fixed-top').removeClass('align-bottom').addClass('non-fixed-top');
+          }
+        }
+        else {
+          cta.removeClass('fixed-top').removeClass('align-bottom').removeClass('non-fixed-top');
+        }
+      };
+
+      return {
+        init: init
+      };
+    })();
+
+$(function () {
+  GS.school_profiles.CTA.init();
+});
