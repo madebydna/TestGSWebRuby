187b2cb93e3dead728ac4edf414a773c506fd5a6
Mitchell Seltzer <mseltzer@greatschools.org>
2016-08-19 05:53:31 -0700

    JT-1432- formatting issues with js - trying again.  somehow format keeps reverting

--- app/assets/javascripts/util/school_profile_sticky_cta.js
+++ app/assets/javascripts/util/school_profile_sticky_cta.js
@@ -1,97 +1,101 @@
 GS.schoolProfiles = GS.schoolProfiles || {};
 
 GS.schoolProfiles.CTA = GS.schoolProfiles.CTA || (function ($) {
-      var ctaProfileOffset;
-      var cta;
-      var transitionToMobile = 767;
+  var ctaProfileOffset;
+  var cta;
+  var transitionToMobile = 767;
 
-      var init = function () {
-        cta = $("#cta");
-
-        // only init when cta is on the page. :)
-        if (isCTADefined()) {
-          // set listener to scroll and resize events
-          $(window).on('scroll', _.throttle(manageFixedPositions, 50));
-          $(window).on('resize', _.debounce(recalculateCTAResize, 100));
-          // may also need to call this after ads load...
-          recalculateCTAResize();
+  var init = function () {
+    cta = $("#cta");
+    // only init when cta is on the page. :)
+    if (isCTADefined()) {
+      $(window).on('scroll', _.throttle(manageFixedPositions, 50));
+      $(window).on('resize', _.debounce(recalculateCTAResize, 100));
+      
+      // may also need to call this after ads load...
+      recalculateCTAResize();
     }
-      };
+  };
 
-      var recalculateCTAResize = function () {
-        setCTAProfileOffset();
-        if (isDesktopWidth()) {
-          setCTARowHeightToParent();
-        } else {
-          setCTARowToDefault();
+  var recalculateCTAResize = function () {
+    setCTAProfileOffset();
+    if (isDesktopWidth()) {
+      setCTARowHeightToParent();
+    } else {
+      setCTARowToDefault();
     }
-        manageFixedPositions();
-      };
+    manageFixedPositions();
+  };
 
-      var setCTAProfileOffset = function () {
-        ctaProfileOffset = cta.parent().offset().top + 30;
-      };
-
-      var setCTARowHeightToParent = function () {
-        cta.parent().height(cta.parent().parent().height());
-      };
-
-      var setCTARowToDefault = function () {
-        $("#cta").parent().height('auto');
-      };
-
-      var manageFixedPositions = function () {
-        if (isDesktopWidth()) {
-          if (isScrollAboveTop()) {
-            if (scrollBelowBottom()) {
-              alignToBottom();
-            } else {
-              alignFixedTop();
-        }
-          } else {
-            alignDefault();
-          }
+  var manageFixedPositions = function () {
+    if (isDesktopWidth()) {
+      if (isScrollAboveTop()) {
+        if (scrollBelowBottom()) {
+          alignToBottom();
         } else {
-          alignMobile();
+          alignFixedTop();
         }
-      };
+      } else {
+        alignDefault();
+      }
+    } else {
+      alignMobile();
+    }
+  };
+  
+  // is cta is defined on page
+  var isCTADefined = function () {
+    return (cta.val() !== undefined);
+  };
+  
+  // setters
+  var setCTAProfileOffset = function () {
+    ctaProfileOffset = cta.parent().offset().top + 30;
+  };
 
-      var isCTADefined = function () {
-        return (cta.val() !== undefined);
-      };
-      var isScrollAboveTop = function () {
-        return (ctaProfileOffset <= $(window).scrollTop());
-      };
+  var setCTARowHeightToParent = function () {
+    cta.parent().height(cta.parent().parent().height());
+  };
 
-      var isDesktopWidth = function () {
-        return ($(window).width() > transitionToMobile);
-      };
+  var setCTARowToDefault = function () {
+    $("#cta").parent().height('auto');
+  };
 
-      var scrollBelowBottom = function () {
-        var ctaBottomOffset = cta.parent().height() - cta.height() + ctaProfileOffset - 30;
-        return (ctaBottomOffset <= $(window).scrollTop());
-      };
+  // branching
+  var isScrollAboveTop = function () {
+    return (ctaProfileOffset <= $(window).scrollTop());
+  };
 
-      var alignToBottom = function () {
-        cta.removeClass('fixed-top').removeClass('non-fixed-top').addClass('align-bottom');
-      };
+  var isDesktopWidth = function () {
+    return ($(window).width() > transitionToMobile);
+  };
 
-      var alignFixedTop = function () {
-        cta.removeClass('non-fixed-top').removeClass('align-bottom').addClass('fixed-top');
-      };
+  var scrollBelowBottom = function () {
+    var ctaBottomOffset = cta.parent().height() - cta.height() + ctaProfileOffset - 30;
+    return (ctaBottomOffset <= $(window).scrollTop());
+  };
+  
+  // align cta for each case
+  var alignToBottom = function () {
+    cta.removeClass('fixed-top').removeClass('non-fixed-top').addClass('align-bottom');
+  };
 
-      var alignDefault = function () {
-        cta.removeClass('fixed-top').removeClass('align-bottom').addClass('non-fixed-top');
-      };
+  var alignFixedTop = function () {
+    cta.removeClass('non-fixed-top').removeClass('align-bottom').addClass('fixed-top');
+  };
 
-      var alignMobile = function () {
-        cta.removeClass('fixed-top').removeClass('align-bottom').removeClass('non-fixed-top');
-      };
+  var alignDefault = function () {
+    cta.removeClass('fixed-top').removeClass('align-bottom').addClass('non-fixed-top');
+  };
 
-      return {
-        init: init
-      };
-    })(jQuery);
+  var alignMobile = function () {
+    cta.removeClass('fixed-top').removeClass('align-bottom').removeClass('non-fixed-top');
+  };
+
+  return {
+    init: init
+  };
+})(jQuery);
 
 $(function () {
   GS.schoolProfiles.CTA.init();
