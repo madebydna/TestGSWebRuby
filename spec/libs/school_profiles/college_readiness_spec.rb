c9468ddfc4d11a070fa7a31d7d39152c5bb81a07
Samson Sprouse <ssprouse@greatschools.org>
2016-08-30 08:02:31 -0700

    JT-1438 Fix college_readiness_spec I broke in recent commit

--- spec/libs/school_profiles/college_readiness_spec.rb
+++ spec/libs/school_profiles/college_readiness_spec.rb
@@ -5,7 +5,6 @@ describe SchoolProfiles::CollegeReadiness do
   let(:school_cache_data_reader) { double("school_cache_data_reader") }
   subject(:hero) do
     SchoolProfiles::CollegeReadiness.new(
-      school,
       school_cache_data_reader: school_cache_data_reader
     )
   end
@@ -19,11 +18,11 @@ describe SchoolProfiles::CollegeReadiness do
   end
 
   describe "#rating_label" do
-    SchoolProfiles::CollegeReadiness::RatingLabelMap
+    SchoolProfiles::CollegeReadiness::RATING_LABEL_MAP
       .each do |rating, label|
       it "returns correct #{label} for rating score: #{rating}" do
         allow(subject).to receive(:rating).and_return(rating.to_i)
-        expect(subject.rating_label).to eq(SchoolProfiles::CollegeReadiness::RatingLabelMap[rating])
+        expect(subject.rating_label).to eq(SchoolProfiles::CollegeReadiness::RATING_LABEL_MAP[rating])
       end
     end
   end
