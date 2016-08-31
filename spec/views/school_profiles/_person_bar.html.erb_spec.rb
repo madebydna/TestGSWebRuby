7019a196e3c181e0e747133ee8b85579213944c0
Samson Sprouse <ssprouse@greatschools.org>
2016-08-31 11:49:43 -0700

    JT-1704 add view template spec and make two vis partials handle invalid percentages

---
+++ spec/views/school_profiles/_person_bar.html.erb_spec.rb
@@ -0,0 +1,52 @@
+
+require 'spec_helper'
+
+describe 'school_profile/_nearby_school' do
+  let(:valid_props) do
+    {
+      score: 1,
+      score_rating: 1
+    }
+  end
+
+  def render_partial(props)
+    render partial: 'school_profiles/person_bar', locals: props
+  end
+
+  (1..10).to_a.each do |input|
+    describe "when valid score_rating is #{input}" do
+      it 'renders the visualization' do
+        render_partial(valid_props.merge(score_rating: input))
+        expect(rendered).to have_css('.person-bar')
+      end
+    end
+  end
+
+  [-1, 0, 11].each do |input|
+    describe "when invalid score_rating is #{input}" do
+      it 'renders nothing' do
+        render_partial(valid_props.merge(score_rating: input))
+        expect(rendered.strip).to be_empty
+      end
+    end
+  end
+
+  (1..100).to_a.each do |input|
+    describe "when valid score is #{input}" do
+      it 'renders the visualization' do
+        render_partial(valid_props.merge(score: input))
+        expect(rendered).to have_css('.person-bar')
+      end
+    end
+  end
+
+  [-1, 0, 101].each do |input|
+    describe "when invalid score is #{input}" do
+      it 'renders nothing' do
+        render_partial(valid_props.merge(score: input))
+        expect(rendered.strip).to be_empty
+      end
+    end
+  end
+
+end
