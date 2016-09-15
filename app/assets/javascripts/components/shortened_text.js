df5db8cc845269dbccfcc12ce37b236cb94023a1
Samson Sprouse <ssprouse@greatschools.org>
2016-09-15 08:58:18 -0700

    JT-1798 2nd pass at code to JS and partial to abbreviate comments

--- app/assets/javascripts/components/shortened_text.js
+++ app/assets/javascripts/components/shortened_text.js
@@ -2,7 +2,7 @@ $(function() {
   $('.js-shortened-text').each(function() {
     var $text = $(this);
     var extraText = $text.data('shortened-text-rest');
-    $text.find('a').on('click', function() {
+    $text.find('span').on('click', function() {
       $(this).hide();
       $text.html($text.html() + extraText);
     });
