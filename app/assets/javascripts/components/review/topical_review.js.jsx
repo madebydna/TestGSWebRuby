e8db6654d7421f2f4749a6e0e9e308792b490d31
Anthony Roy <aroy@greatschools.org>
2016-10-06 08:42:10 -0700

    JT-1942 Refactor code to simplify state management

---
+++ app/assets/javascripts/components/review/topical_review.js.jsx
@@ -0,0 +1,69 @@
+class TopicalReview extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {reportReviewOpen: false}
+  }
+
+  topicalReviewReportLink() {
+    var isReported = !!this.props.userAlreadyReported;
+    if (isReported) {
+      return (
+          <div className="reported">Review Reported</div>
+      )
+    } else {
+      return (
+          <a href="#" onClick={this.handleReportReviewClick.bind(this)}>Report</a>
+      )
+    }
+  }
+
+  handleReportReviewClick(event) {
+    event.preventDefault();
+    this.setState({reportReviewOpen: !this.state.reportReviewOpen});
+  }
+
+  handleCancelClick() {
+    this.setState({reportReviewOpen: false});
+  }
+
+  handleReviewReported() {
+    // TODO: Actually report the review to the server, error handling, etc.
+    // For now, assume it was successful. Close the form and update the parent
+    this.setState({reportReviewOpen: false});
+    this.props.reportedCallback();
+  }
+
+  render() {
+    const review = this.props.review;
+    return(
+        <div className="topical-review" key={review.id}>
+          <div className="average-rating-column">
+            <span className={"answer-icon " + review.answer }></span>
+          </div>
+          <div className="text-column">
+            <div className="answer">
+              { review.answer_label }
+            </div>
+            <div className="comment">
+              <ShortenText text={review.comment} length={200} key={review.text} />
+            </div>
+            <div className="topical-review-button-bar">
+            <div className="topical-review-report">
+              { this.topicalReviewReportLink(review.id) }
+            </div>
+            </div>
+            <ReportReview open={this.state.reportReviewOpen}
+                          cancelCallback={ this.handleCancelClick.bind(this) }
+                          reportedCallback={ this.handleReviewReported.bind(this, review.id) }
+            />
+          </div>
+        </div>
+    )
+  }
+}
+
+ReportReview.propTypes = {
+  review: React.PropTypes.object,
+  reportedCallback: React.PropTypes.func,
+  userAlreadyReported: React.PropTypes.bool,
+};
--- app/assets/javascripts/components/review/user_reviews.js.jsx
+++ app/assets/javascripts/components/review/user_reviews.js.jsx
@@ -1,18 +1,7 @@
 class UserReviews extends React.Component {
   constructor(props) {
     super(props);
-    var reportReviewState = {};
-    var fiveStarReview = this.props.five_star_review;
-    if (fiveStarReview) {
-      reportReviewState[fiveStarReview.id] = {open: false};
-    }
-    if (this.props.topical_reviews) {
-      for (var i=0; i < this.props.topical_reviews.length; i++) {
-        var topicalReview = this.props.topical_reviews[i];
-        reportReviewState[topicalReview.id] = {open: false}
-      }
-    }
-    this.state = {reportReviewState: reportReviewState};
+    this.state = {reportReviewOpen: false};
   }
 
   fiveStars(numberFilled) {
@@ -32,49 +21,15 @@ class UserReviews extends React.Component {
     )
   }
 
-  topicalReviewReportLink(reviewId) {
-    var isReported = this.props.current_user_reported_reviews.includes(reviewId);
-    if (isReported) {
-      return (
-          <span className="reported">Review Reported</span>
-      )
-    } else {
-      return (
-          <a href="#" onClick={this.handleReportReviewClick.bind(this, reviewId)}>Report</a>
-      )
-    }
-  }
-
-  topicalReview(review) {
-    return(
-      <div className="topical-review" key={review.id}>
-        <div className="average-rating-column">
-          <span className={"answer-icon " + review.answer }></span>
-        </div>
-        <div className="text-column">
-          <div className="answer">
-            { review.answer_label }
-          </div>
-          <div className="comment">
-            <ShortenText text={review.comment} length={200} key={review.text} />
-          </div>
-          <div className="topical-review-button-bar">
-            <span className="topical-review-report">
-              { this.topicalReviewReportLink(review.id) }
-            </span>
-          </div>
-          <ReportReview open={this.state.reportReviewState[review.id].open}
-                        cancelCallback={ this.handleCancelReportReviewClick.bind(this, review.id) }
-                        reportedCallback={ this.handleReviewReported.bind(this, review.id) }
-          />
-        </div>
-      </div>
-    )
-  }
-
   topicalReviews() {
     if(this.props.topical_reviews) {
-      return this.props.topical_reviews.map(this.topicalReview.bind(this));
+      return this.props.topical_reviews.map(function (review) {
+        var userAlreadyReported = this.props.current_user_reported_reviews.includes(review.id);
+        return (<TopicalReview review={review} key={review.id}
+                               reportedCallback={this.handleReviewReported.bind(this, review.id)}
+                               userAlreadyReported={userAlreadyReported}
+        />)
+      }, this);
     }
   }
 
@@ -125,8 +80,8 @@ class UserReviews extends React.Component {
     var review = this.props.five_star_review;
     if (review !== undefined) {
       return (
-          <ReportReview open={this.state.reportReviewState[review.id].open}
-                        cancelCallback={ this.handleCancelReportReviewClick.bind(this, review.id) }
+          <ReportReview open={this.state.reportReviewOpen}
+                        cancelCallback={ this.handleCancelReportReviewClick.bind(this) }
                         reportedCallback={ this.handleReviewReported.bind(this, review.id) }
           />
       );
@@ -135,24 +90,17 @@ class UserReviews extends React.Component {
 
   handleReportReviewClick(reviewId, event) {
     if (!this.props.current_user_reported_reviews.includes(reviewId)) {
-      var newAttr = {};
-      var currentState = this.state.reportReviewState[reviewId].open;
-      newAttr[reviewId] = {open: !currentState};
-      var newReportReviewState = Object.assign({}, this.state.reportReviewState, newAttr);
-      this.setState({reportReviewState: newReportReviewState});
+      this.setState({reportReviewOpen: !this.state.reportReviewOpen});
     }
     event.preventDefault();
   }
 
-  handleCancelReportReviewClick(reviewId) {
-    var newAttr = {};
-    newAttr[reviewId] = {open: false};
-    var newReportReviewState = Object.assign({}, this.state.reportReviewState, newAttr);
-    this.setState({reportReviewState: newReportReviewState});
+  handleCancelReportReviewClick() {
+    this.setState({reportReviewOpen: false});
   }
 
   handleReviewReported(reviewId) {
-    this.handleCancelReportReviewClick(reviewId);
+    this.handleCancelReportReviewClick();
     this.props.review_reported_callback(reviewId);
   }
 
