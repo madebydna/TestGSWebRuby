0aab34b3563d39b53a80fb2c9989be7d85d1b7a2
Samson Sprouse <ssprouse@greatschools.org>
2016-10-07 14:04:51 -0700

    JT-1946 Add GS.session.getCurrentSession method to get user session data

--- app/assets/javascripts/util/session.js
+++ app/assets/javascripts/util/session.js
@@ -1,12 +1,27 @@
 GS = GS || {};
-GS.session = GS.session || (function() {
+GS.session = GS.session || (function(gon) {
 
   var isSignedIn = function() {
     return $.cookie('community_www') != null || $.cookie('community_dev') != null;
   };
 
-  return {
-    isSignedIn: isSignedIn
+  // returns a jQuery promise
+  var getCurrentSession = function() {
+    uri = gon.links.session;
+    if (uri === undefined) {
+      throw new Error('uri is undefined in getCurrentSession');
+    }
+    return GS.util.memoizeAjaxRequest(
+      'session',
+      function() {
+        return $.get(uri, null, 'json')
+      }
+    );
   };
 
-})();
+  return {
+    isSignedIn: isSignedIn,
+    getCurrentSession: getCurrentSession
+  };
+
+})(gon);
