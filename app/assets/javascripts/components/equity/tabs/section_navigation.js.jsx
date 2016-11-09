05b751fdb9c9b75cd94da5904042d9abe9c04e74
Mitchell Seltzer <mseltzer@greatschools.org>
2016-11-09 11:15:51 -0800

    JT-2292 - added tab tracking in equity section.  Need to have GTM published on production

--- app/assets/javascripts/components/equity/tabs/section_navigation.js.jsx
+++ app/assets/javascripts/components/equity/tabs/section_navigation.js.jsx
@@ -5,8 +5,11 @@ class SectionNavigation extends React.Component {
     var items = _.map(this.props.items, function(item, index) {
       return <a href="javascript:void(0)"
                 key={index}
-                className={'nav-title ' + (active === index ? 'tab_selected' : '')}
-                onClick={this.onClick.bind(this, index)}>
+                className={'nav-title js-gaClick' + (active === index ? ' tab_selected' : '')}
+                onClick={this.onClick.bind(this, index)}
+                data-ga-click-category='Profile'
+                data-ga-click-action='Equity Ethnicity Tabs'
+                data-ga-click-label={item.section_title}>
         {item.section_title}
       </a>;
     }.bind(this));
