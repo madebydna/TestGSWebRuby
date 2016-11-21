1dda2d7bece6c7fbfae2033b6c7029ba9fe83cb7
Anthony Roy <aroy@greatschools.org>
2016-11-21 08:44:39 -0800

    JT-2408 Add conditional

--- app/controllers/api/school_user_digests_controller.rb
+++ app/controllers/api/school_user_digests_controller.rb
@@ -1,11 +1,14 @@
 class Api::SchoolUserDigestsController < ApplicationController
 
   def show
-    render json: {}, status: :forbidden unless logged_in? # :forbidden = 403
-    state =  school_user_digest_params[:state]
-    school_id = school_user_digest_params[:school_id]
-    school = School.find_by_state_and_id(state, school_id)
-    @school_user_digest = SchoolUserDigest.new(current_user.id, school).create
+    if logged_in?
+      state = school_user_digest_params[:state]
+      school_id = school_user_digest_params[:school_id]
+      school = School.find_by_state_and_id(state, school_id)
+      @school_user_digest = SchoolUserDigest.new(current_user.id, school).create
+    else
+      render json: {}, status: :forbidden # :forbidden = 403
+    end
   end
 
   private
