2da7ef380f18bfd62db494e3e1bc2bb25188b465
Samson Sprouse <ssprouse@greatschools.org>
2016-12-14 17:25:24 -0800

    Rename application.jsx entry file to application.jsx

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -13,7 +13,7 @@ const config = {
     'es5-shim/es5-shim',
     'es5-shim/es5-sham',
     'babel-polyfill',
-    './app/bundles/GSWeb/registration'
+    './app/bundles/GSWeb/application'
   ],
 
   output: {
