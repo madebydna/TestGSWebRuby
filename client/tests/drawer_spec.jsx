215a73aa3c9eff75b1fcc3766cb0f6c09f31ffb5
Samson Sprouse <ssprouse@greatschools.org>
2016-12-15 17:33:36 -0800

    JT-2620 Add webpack config for JS testing, add first react component test

---
+++ client/tests/drawer_spec.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import sinon from 'sinon';
+import { expect } from 'chai';
+
+import Drawer from '../app/bundles/GSWeb/components/drawer.jsx';
+
+describe('<Drawer />', function() {
+  it('when given content it says "Show more"', function() {
+    const wrapper = shallow(<Drawer content="<div class='.some-class'></div>" />);
+    expect(wrapper.text()).to.have.contain('Show more');
+  });
+});
