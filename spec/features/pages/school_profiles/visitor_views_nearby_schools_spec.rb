5a33f84e9d6aa61cfb87f64cab6fea63cf436631
Anthony Roy <aroy@greatschools.org>
2016-12-19 09:23:29 -0800

    JT-2618 Mark as pending

--- spec/features/pages/school_profiles/visitor_views_nearby_schools_spec.rb
+++ spec/features/pages/school_profiles/visitor_views_nearby_schools_spec.rb
@@ -1,34 +1,35 @@
-require "spec_helper"
-require "features/page_objects/school_profiles_page"
+require 'spec_helper'
+require 'features/page_objects/school_profiles_page'
 
-describe "Visitor" do
+describe 'Visitor' do
   let(:page_object) { SchoolProfilesPage.new }
+  let (:school) { create(:school_with_new_profile) }
   after do
     clean_dbs(:gs_schooldb)
     clean_models(:ca, School)
   end
 
-  scenario "does not see nearby schools if no data present" do
-    school = create(:school_with_new_profile)
+  scenario 'does not see nearby schools if no data present' do
     visit school_path(school)
     expect(page_object).to_not have_nearby_schools
   end
 
-  scenario "sees nearby schools" do
-    school = create(:school_with_new_profile)
-    create(:nearby_schools, school_id: school.id, state: school.state)
-    visit school_path(school)
-    expect(page_object).to have_nearby_schools
-    expect(page_object.nearby_schools.title).to have_text('Nearest high-performing schools')
+  context 'with a nearby top performing school' do
+    before do
+      create(:nearby_schools, school_id: school.id, state: school.state)
+      pending('Needs support for AJAX fetching')
+    end
+
+    scenario 'sees nearby schools' do
+      visit school_path(school)
+      expect(page_object).to have_nearby_schools
+      expect(page_object.nearby_schools.title).to have_text('Nearest high-performing schools')
+    end
+
+    scenario 'sees closest top schools and other nearby schools' do
+      visit school_path(school)
+      expect(page_object.nearby_schools.root_element).to have_text('Arise High School')
+      expect(page_object.nearby_schools.root_element).to have_text('Alameda Science And Technology Institute')
+    end
   end
-
-  scenario "sees closest top schools and other nearby schools" do
-    school = create(:school_with_new_profile)
-    create(:nearby_schools, school_id: school.id, state: school.state)
-    visit school_path(school)
-    expect(page_object.nearby_schools.root_element).to have_text('Arise High School')
-    expect(page_object.nearby_schools.root_element).to have_text('Alameda Science And Technology Institute')
-  end
-
-
 end
