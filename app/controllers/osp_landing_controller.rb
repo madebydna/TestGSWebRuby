fab06eef7755a6d55b376e7d060d8c68dcb13885
Anthony Roy <aroy@greatschools.org>
2017-01-13 10:39:43 -0800

    JT-2814 JT-2815 Ruby page for OSP superusers and demigods

--- app/controllers/osp_landing_controller.rb
+++ app/controllers/osp_landing_controller.rb
@@ -2,6 +2,8 @@ class OspLandingController < ApplicationController
   before_action :set_login_redirect
   before_action :login_required, only: [:dashboard]
 
+  layout 'application', only: [:dashboard]
+
   def show
     page_title = 'Edit your school profile at GreatSchools'
     gon.pageTitle = page_title
@@ -22,36 +24,44 @@ class OspLandingController < ApplicationController
     # If state/schoolId params are provided, redirect directly to that school's form
     if params[:state] && params[:schoolId]
       redirect_to(osp_page_path(:state =>params[:state], :schoolId => params[:schoolId], :page => 1))
+    elsif current_user.is_esp_superuser? || approved_memberships.size > 1
+      @superuser = current_user.is_esp_superuser?
+      @schools = schools
+      render 'osp/dashboard' # demigod disambiguation list and superuser form
+    elsif single_membership # includes provisional
+      redirect_to(osp_page_path(:state =>single_membership.state, :schoolId => single_membership.school_id, :page => 1))
     else
-      # TODO: If superuser, render superuser form
-      # if current_user.is_esp_superuser?
-      #   @superuser = true
-      #   render 'osp/dashboard'
-      #   return
-      # end
+      redirect_to(my_account_path)
+    end
+  end
 
-      memberships = current_user.esp_memberships
-      approved_memberships = memberships.select(&:approved?)
-      provisional_memberships = memberships.select(&:provisional?)
-      if approved_memberships.size == 1
-        # If approved and list.size == 1, redirect to that school's form
-        membership = approved_memberships.first
-        redirect_to(osp_page_path(:state =>membership.state, :schoolId => membership.school_id, :page => 1))
-      elsif approved_memberships.size > 1
-        # Otherwise render demigod list
-        # get ordered list of memberships (by school name ignoring state)
-        #   for each membership get the school
-        #   Sort memberships by school name
-        #   Send list of schools to view
-        redirect_to(my_account_path) # TODO implement correct behavior
-      elsif !provisional_memberships.empty?
-        # If provisional OSP user, get first provisional membership and redirect to that school's form
-        membership = provisional_memberships.first
-        redirect_to(osp_page_path(:state =>membership.state, :schoolId => membership.school_id, :page => 1))
-      else
-        # If other user, block access
-        redirect_to(my_account_path)
+  private
+
+  def single_membership
+    return @_single_membership if defined?(@_single_membership)
+    @_single_membership = if approved_memberships.size == 1 || !provisional_memberships.empty?
+                            (approved_memberships + provisional_memberships).compact.first
+                          end
+  end
+
+  def esp_memberships
+    @_esp_memberships ||= current_user.esp_memberships
+  end
+
+  def approved_memberships
+    @_approved_memberships ||= esp_memberships.select(&:approved?)
+  end
+
+  def provisional_memberships
+    @_provisional_memberships ||= esp_memberships.select(&:provisional?)
+  end
+
+  def schools
+    @_schools ||= begin
+      schools = approved_memberships.map do |m|
+        School.on_db(m.state.downcase.to_sym).find_by_id(m.school_id) if m.state.present? && m.school_id.present?
       end
+      schools.compact.select { |s| s.active == 1 }.sort_by(&:name)
     end
   end
 end
--- spec/controllers/osp_landing_controller_spec.rb
+++ spec/controllers/osp_landing_controller_spec.rb
@@ -2,8 +2,6 @@ require 'spec_helper'
 require 'controllers/contexts/osp_shared_contexts'
 
 describe OspLandingController do
-  #include ActionView::Helpers::TranslationHelper
-
   describe '#dashboard' do
     it 'redirects to signin if no user' do
       get :dashboard
@@ -48,6 +46,125 @@ describe OspLandingController do
         get :dashboard
         expect(response).to redirect_to(osp_page_path(:state =>state, :schoolId => school_id, :page => 1))
       end
+    end
+
+    with_shared_context 'user esp_membership status is', :super do
+      before { allow(controller).to receive(:current_user).and_return(current_user) }
+
+      it 'superuser, it renders the disambiguation dashboard' do
+        get :dashboard
+        expect(response).to render_template('osp/dashboard')
+      end
+    end
+
+    context 'with two or more active memberships' do
+      before do
+        user = FactoryGirl.create(:user)
+        allow(controller).to receive(:current_user).and_return(user)
+        FactoryGirl.create(:esp_membership, :with_approved_status,school_id: 1,state: 'ca',member_id: user.id)
+        FactoryGirl.create(:esp_membership, :with_approved_status,school_id: 2,state: 'ca',member_id: user.id)
+      end
+      after do
+        clean_models User, EspMembership
+      end
+
+      it 'renders the disambiguation dashboard' do
+        get :dashboard
+        expect(response).to render_template('osp/dashboard')
+      end
+    end
+  end
+
+  describe '#single_membership' do
+    let (:subject) { controller.send(:single_membership) }
+    let (:user) { FactoryGirl.create(:user) }
+
+    before { allow(controller).to receive(:current_user).and_return(user) }
+
+    after do
+      clean_models User, EspMembership
+    end
+
+    it 'returns an approved memberships if you have exactly one' do
+      FactoryGirl.create(:esp_membership, :with_approved_status, school_id: 2, state: 'ca', member_id: user.id)
+
+      expect(subject).to_not be_nil
+      expect(subject.school_id).to eq(2)
+    end
+
+    it 'returns nil if you have more than one approved membership' do
+      FactoryGirl.create(:esp_membership, :with_approved_status, school_id: 2, state: 'ca', member_id: user.id)
+      FactoryGirl.create(:esp_membership, :with_approved_status, school_id: 1, state: 'ca', member_id: user.id)
+
+      expect(subject).to be_nil
+    end
+
+    it 'returns a provisional membership if you have exactly one' do
+      FactoryGirl.create(:esp_membership, :with_provisional_status, school_id: 2, state: 'ca', member_id: user.id)
+
+      expect(subject).to_not be_nil
+      expect(subject.school_id).to eq(2)
+      expect(subject.provisional?).to be_truthy
+    end
+
+    it 'returns a provisional membership if you have more than one' do
+      FactoryGirl.create(:esp_membership, :with_provisional_status, school_id: 2, state: 'ca', member_id: user.id)
+      FactoryGirl.create(:esp_membership, :with_provisional_status, school_id: 1, state: 'ca', member_id: user.id)
+
+      expect(subject).to_not be_nil
+      expect(subject.provisional?).to be_truthy
+    end
+
+    it 'returns approved membership if you have exactly one along with some provisionals' do
+      FactoryGirl.create(:esp_membership, :with_approved_status, school_id: 2, state: 'ca', member_id: user.id)
+      FactoryGirl.create(:esp_membership, :with_provisional_status, school_id: 1, state: 'ca', member_id: user.id)
+
+      expect(subject).to_not be_nil
+      expect(subject.school_id).to eq(2)
+      expect(subject.approved?).to be_truthy
+    end
+
+    it 'returns nil if you have no memberships' do
+      expect(subject).to be_nil
+    end
+  end
+
+  describe '#schools' do
+    let (:subject) { controller.send(:schools) }
+
+    before do
+      user = FactoryGirl.create(:user)
+      allow(controller).to receive(:current_user).and_return(user)
+      FactoryGirl.create(:esp_membership, :with_approved_status, school_id: 10, state: 'ca', member_id: user.id)
+      FactoryGirl.create(:esp_membership, :with_approved_status, school_id: 11, state: 'ca', member_id: user.id)
+      FactoryGirl.create(:esp_membership, :with_approved_status, school_id: 12, state: 'ca', member_id: user.id)
+    end
+
+    after do
+      clean_models User, EspMembership, School
+    end
+
+    it 'Orders schools by name' do
+      s1 = FactoryGirl.create(:school, id: 10, name: 'Def')
+      s2 = FactoryGirl.create(:school, id: 11, name: 'Ghi')
+      s3 = FactoryGirl.create(:school, id: 12, name: 'Abc')
+
+      expect(subject).to eq([s3, s1, s2])
+    end
+
+    it 'Removes nil (school not found)' do
+      s2 = FactoryGirl.create(:school, id: 11, name: 'Ghi')
+      s3 = FactoryGirl.create(:school, id: 12, name: 'Abc')
+
+      expect(subject).to eq([s3, s2])
+    end
+
+    it 'Removes inactive schools' do
+      s1 = FactoryGirl.create(:school, id: 10, name: 'Def')
+      s2 = FactoryGirl.create(:school, id: 11, name: 'Ghi')
+           FactoryGirl.create(:school, id: 12, name: 'Abc', active: false)
+
+      expect(subject).to eq([s1, s2])
     end
   end
 end
