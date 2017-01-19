2c22e80f73af35a68556daaf3c0336cfd136d562
Anthony Roy <aroy@greatschools.org>
2017-01-19 10:21:12 -0800

    JT-2790 Add BpCensus and BpZip models using STI

---
+++ app/models/bp_census.rb
@@ -0,0 +1,23 @@
+class BpCensus < ActiveRecord::Base
+  self.table_name = 'bp_census'
+  self.inheritance_column = 'Place_type'
+
+  db_magic :connection => :us_geo
+
+  alias_attribute :zip, :Zip
+  alias_attribute :lat, :Lat
+  alias_attribute :lon, :Lon
+  alias_attribute :name, :Name
+
+  def self.discriminate_class_for_record(record)
+    if record[inheritance_column].downcase == 'zi'
+      BpZip
+    else
+      BpCensus
+    end
+  end
+
+  def state
+    self.State ? self.State.downcase.to_sym : nil
+  end
+end
