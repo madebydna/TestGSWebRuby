7c99cc6544e315d932cc42749f122a2eb8974ca7
Derek Walters <dwalters@Dereks-MacBook-Pro.local>
2017-01-20 11:11:42 -0800

    JT-2588 add title tags

--- app/controllers/cities_list_controller.rb
+++ app/controllers/cities_list_controller.rb
@@ -5,6 +5,8 @@ class CitiesListController < ApplicationController
   before_filter :require_valid_state
 
   def show
+    gon.pageTitle = meta_title
+    set_meta_tags title: meta_title
     @cities = dcl.cities(state)
     @state_names = dcl.state_names(state)
     @dropdown_info = dcl.dropdown_info
@@ -22,6 +24,10 @@ class CitiesListController < ApplicationController
     unless States.abbreviations.include?(state.downcase)
       render "error/page_not_found", layout: "error", status: 404
     end
+  end
+
+  def meta_title
+    "#{dcl.state_full_name(state)} School information by City: Popular Cities"
   end
 
 end
--- app/controllers/districts_list_controller.rb
+++ app/controllers/districts_list_controller.rb
@@ -5,6 +5,8 @@ class DistrictsListController < ApplicationController
   before_filter :require_valid_state
 
   def show
+    gon.pageTitle = meta_title
+    set_meta_tags title: meta_title
     @districts_cities_counties = dcl.districts_cities_counties(state)
     @state_names = dcl.state_names(state)
     @dropdown_info = dcl.dropdown_info
@@ -22,6 +24,10 @@ class DistrictsListController < ApplicationController
     unless States.abbreviations.include?(state.downcase)
       render "error/page_not_found", layout: "error", status: 404
     end
+  end
+
+  def meta_title
+    "All school districts in #{dcl.state_full_name(state)}, #{state}"
   end
 
 end
