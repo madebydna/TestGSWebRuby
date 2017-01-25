3dc55bb30d3e6839f6d2495b446cbb8e58555570
Samson Sprouse <ssprouse@greatschools.org>
2017-01-25 12:33:23 -0800

    JT-2885 Improvements to info window styling

--- client/app/bundles/GSWeb/react_components/district_boundaries/info_window.jsx
+++ client/app/bundles/GSWeb/react_components/district_boundaries/info_window.jsx
@@ -2,25 +2,40 @@ import React, { PropTypes } from 'react';
 import jsxToString from 'jsx-to-string';
 
 export default function createInfoWindow(entity) {
-  let contentString = (<div class="rating-container__rating">
-    <div class="module-header">
-      <div class={'circle-rating--' + entity.rating + ' circle-rating--medium'}>
-        {entity.rating}<span class="rating-circle-small">/10</span>
-      </div>
-      <div class="title-container" style="width:300px">
-        <div>
-          <span class="title">
-            {entity.name}
-          </span>
-        </div>
-        {entity.address &&
-          <div>
-            <div>{entity.address.street1}</div>
-            <div>{entity.address.city}, {entity.address.state} {entity.address.zip}</div>
+
+  // let homesForSaleHref;
+  // if (typeof entity.state && entity.address && entity.address.zip) {
+  //   homesForSaleHref = 'https://www.zillow.com/' + state + '-' + zip.split("-")[0] + '?cbpartner=Great+Schools&utm_source=Great_Schools&utm_medium=referral&utm_campaign=schoolsearch';
+  // } else {
+  //   homesForSaleHref = 'https://www.zillow.com/?cbpartner=Great+Schools&utm_source=Great_Schools&utm_medium=referral&utm_campaign=schoolsearch';
+  // }
+
+  let contentString = (
+    <div class="infowindow" style="padding:10px">
+      <div class="clearfix">
+        { entity.rating != 'NR' &&
+          <div class={'circle-rating--' + entity.rating + ' circle-rating--medium rating'} style="float:left; margin-right: 20px">
+            {entity.rating}<span class="rating-circle-small">/10</span>
           </div>
         }
+        <div style="float:left;">
+          <div>
+            <span class="title">
+              <a href={entity.show}>{entity.name}</a>
+            </span>
+          </div>
+          {entity.address &&
+            <div>
+              <div>{entity.address.street1}</div>
+              <div>{entity.address.city + ','} {entity.state} {entity.address.zip}</div>
+            </div>
+          }
+        </div>
       </div>
+      <hr/>
+      <a href="" rel="nofollow">Homes for sale</a>
+      <a href="">View school details</a>
     </div>
-  </div>);
+  );
   return jsxToString(contentString);
 }
