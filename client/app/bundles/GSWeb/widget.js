b80fbc41f2e4e8331a0f8503567cbfbd49ae503b
Samson Sprouse <ssprouse@greatschools.org>
2017-02-16 10:14:38 -0800

    JT-2701 Clean up widget JS

--- client/app/bundles/GSWeb/widget.js
+++ client/app/bundles/GSWeb/widget.js
@@ -1,26 +1,3 @@
-// TODO: import Uri
-// import { geocode } from './components/geocoding';
-// import { google_maps } from './components/map/google_maps';
-
-// const updateIFrameUrl = function(paramName, value) {
-//   let $iframe = $('.preview iframe');
-//   var url = $iframe.attr('src');
-//   debugger;
-//   url = GS.uri.Uri.putIntoQueryString(url, paramName, value);
-//   $iframe.src = url;
-// };
-
-// $(function() {
-//   google_maps.init(function() {
-//     $('.widget-customization input[name=address]').on('change', function(e) {
-//       updateIFrameUrl(e.target.name, e.target.value);
-//     });
-//     $('.widget-customization input').on('change', function(e) {
-//       updateIFrameUrl(e.target.name, e.target.value);
-//     });
-//   });
-// });
-
 import ReactOnRails from 'react-on-rails';
 import Widget from './react_components/widget';
 ReactOnRails.register({
