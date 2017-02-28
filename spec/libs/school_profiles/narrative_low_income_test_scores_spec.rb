3b5aadc8ddab2943b8115f68e71b9770746d514f
Anthony Roy <aroy@greatschools.org>
2017-02-28 09:42:50 -0800

    JT-3099 Low income narration generates for any valid data

--- spec/libs/school_profiles/narrative_low_income_test_scores_spec.rb
+++ spec/libs/school_profiles/narrative_low_income_test_scores_spec.rb
@@ -12,6 +12,16 @@ describe SchoolProfiles::NarrativeLowIncomeTestScores do
 
       expect(school_cache_data_reader.test_scores).to eq(result_hash)
     end
+
+    it 'should change a test score hash with alternate level code and subject names correctly' do
+      school_cache_data_reader = double(test_scores: test_scores_hash_2)
+
+      narrativeLowIncomeTestScores = SchoolProfiles::NarrativeLowIncomeTestScores.new(school_cache_data_reader: school_cache_data_reader)
+      allow(narrativeLowIncomeTestScores).to receive(:key_for_yml).and_return '4_3'
+      narrativeLowIncomeTestScores.auto_narrative_calculate_and_add
+
+      expect(school_cache_data_reader.test_scores).to eq(result_hash_2)
+    end
   end
 
   def mock_school_cache_data_reader
@@ -127,6 +137,109 @@ describe SchoolProfiles::NarrativeLowIncomeTestScores do
     }
   end
 
+  def result_hash_2
+    {"15" =>
+     {
+       "All"=>
+       {
+         "grades"=>
+         {
+           "All"=>
+           {
+             "level_code"=>
+             {
+               "m,h"=>
+               {
+                 "Some subject"=>
+                 {
+                   "2015"=>
+                   {
+                     "number_students_tested"=>344,
+                     "score"=>72.0,
+                     "state_average"=>44.0
+                   }
+                 },
+                 "Some other subject"=>
+                 {
+                   "2015"=>
+                   {
+                     "number_students_tested"=>343,
+                     "score"=>55.0,
+                     "state_average"=>33.0
+                   }
+                 }
+               }
+             }
+           }
+         }
+       },
+       "Not economically disadvantaged"=>
+       {
+         "grades"=>
+         {"All"=>
+          {
+            "level_code"=>
+            {"m,h"=>
+             {"Some subject"=>
+              {"2015"=>
+               {"number_students_tested"=>245,
+                "score"=>78.0,
+                "state_average"=>64.0
+               }
+              },
+              "Some other subject"=>
+              {"2015"=>
+               {
+                 "number_students_tested"=>245,
+                 "score"=>58.0,
+                 "state_average"=>52.0
+               }
+              }
+             }
+            }
+          }
+         }
+       },
+       "Economically disadvantaged"=>
+       {
+         "grades"=>
+         {
+           "All"=>
+           {
+             "level_code"=>
+             {
+               "m,h"=>
+               {
+                 "Some subject"=>
+                 {
+                   "2015"=>
+                   {
+                     "number_students_tested"=>99,
+                     "score"=>56.0,
+                     "state_average"=>30.0,
+                     "narrative" => string_key_for_key_4_3_and_subject_some_subject
+
+                   }
+                 },
+                 "Some other subject"=>
+                 {
+                   "2015"=>
+                   {
+                     "number_students_tested"=>98,
+                     "score"=>47.0,
+                     "state_average"=>21.0,
+                     "narrative" => string_key_for_key_4_3_and_subject_some_other_subject
+                   }
+                 }
+               }
+             }
+           }
+         }
+       }
+     }
+    }
+  end
+
   def test_scores_hash
     {"236" =>
      {
@@ -231,6 +344,104 @@ describe SchoolProfiles::NarrativeLowIncomeTestScores do
     }
   end
 
+  def test_scores_hash_2
+    {"15" =>
+     {
+       "All"=>
+      {
+        "grades"=>
+          {
+            "All"=>
+              {
+                "level_code"=>
+                  {
+                    "m,h"=>
+                      {
+                        "Some subject"=>
+                          {
+                            "2015"=>
+                              {
+                                "number_students_tested"=>344,
+                                "score"=>72.0,
+                                "state_average"=>44.0
+                              }
+                          },
+                        "Some other subject"=>
+                              {
+                                "2015"=>
+                                  {
+                                    "number_students_tested"=>343,
+                                    "score"=>55.0,
+                                    "state_average"=>33.0
+                                  }
+                              }
+                      }
+                  }
+              }
+          }
+     },
+  "Not economically disadvantaged"=>
+     {
+       "grades"=>
+       {"All"=>
+         {
+          "level_code"=>
+            {"m,h"=>
+              {"Some subject"=>
+                {"2015"=>
+                  {"number_students_tested"=>245,
+                    "score"=>78.0,
+                    "state_average"=>64.0
+                  }
+                },
+              "Some other subject"=>
+                {"2015"=>
+                  {
+                    "number_students_tested"=>245,
+                    "score"=>58.0,
+                    "state_average"=>52.0
+                  }
+                }
+              }
+            }
+          }
+       }
+      },
+    "Economically disadvantaged"=>
+       {
+         "grades"=>
+         {
+          "All"=>
+          {
+            "level_code"=>
+              {
+                "m,h"=>
+                  {
+                    "Some subject"=>
+                      {
+                        "2015"=>
+                        {
+                          "number_students_tested"=>99,
+                          "score"=>56.0,
+                          "state_average"=>30.0}
+                        },
+                      "Some other subject"=>
+                        {
+                          "2015"=>
+                            {
+                              "number_students_tested"=>98,
+                              "score"=>47.0,
+                              "state_average"=>21.0}
+                        }
+                  }
+              }
+          }
+        }
+       }
+     }
+    }
+  end
+
   def string_key_for_key_4_3_and_subject_english
     "<p class=\"narrative-content\"><span class=\"positive\">A positive sign!</span> The English scores of low-income students at this school are at or above the state average for all kids. Still, there's a <span class=\"negative\">large gap between how low-income kids</span> are doing compared to their classmates with more financial resources.</p> <p class=\"footnote\"><span class=\"emphasis\">Parents should ask</span> the school what it’s doing to help all students succeed.</p>\n"
     end
@@ -238,5 +449,14 @@ describe SchoolProfiles::NarrativeLowIncomeTestScores do
 
   def string_key_for_key_4_3_and_subject_math
     "<p class=\"narrative-content\"><span class=\"positive\">A positive sign!</span> The math scores of low-income students at this school are at or above the state average for all kids. Still, there's a <span class=\"negative\">large gap between how low-income kids</span> are doing compared to their classmates with more financial resources.</p> <p class=\"footnote\"><span class=\"emphasis\">Parents should ask</span> the school what it’s doing to help all students succeed.</p>\n"
+  end
+
+  def string_key_for_key_4_3_and_subject_some_subject
+    "<p class=\"narrative-content\"><span class=\"positive\">A positive sign!</span> The some subject scores of low-income students at this school are at or above the state average for all kids. Still, there's a <span class=\"negative\">large gap between how low-income kids</span> are doing compared to their classmates with more financial resources.</p> <p class=\"footnote\"><span class=\"emphasis\">Parents should ask</span> the school what it’s doing to help all students succeed.</p>\n"
+    end
+
+
+  def string_key_for_key_4_3_and_subject_some_other_subject
+    "<p class=\"narrative-content\"><span class=\"positive\">A positive sign!</span> The some other subject scores of low-income students at this school are at or above the state average for all kids. Still, there's a <span class=\"negative\">large gap between how low-income kids</span> are doing compared to their classmates with more financial resources.</p> <p class=\"footnote\"><span class=\"emphasis\">Parents should ask</span> the school what it’s doing to help all students succeed.</p>\n"
   end
 end
