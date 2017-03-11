3739d892d32701a7a4978fe9a57e1aa834fb1983
Anthony Roy <aroy@greatschools.org>
2017-03-10 21:10:14 -0800

    JT-3203 Clean up method a bit

--- lib/school_profiles/equity_gsdata.rb
+++ lib/school_profiles/equity_gsdata.rb
@@ -23,22 +23,42 @@ module SchoolProfiles
       data = @school_cache_data_reader.gsdata_data(*COURSES_DATA_TYPES)
       max_year = data.reduce(0, &max_year_across_keys)
       data.each_with_object({}) do |(data_type_name, array_of_hashes), output_hash|
-        has_values = array_of_hashes.select(&has_school_value)
-        matching_years = has_values.select(&matching_source_year(max_year))
-        matching_breakdowns = matching_years.select(&breakdowns_blank_or_includes(ethnicity_breakdowns.keys))
+        matching_breakdowns = array_of_hashes.select(&matching_values(max_year))
         unless matching_breakdowns.empty?
-          display_hashes = matching_breakdowns.map(&hash_for_display)
-          subject_name = I18n.t(data_type_name, scope: 'lib.equity_gsdata', default: data_type_name)
-          output_hash[subject_name] = {
-              narration: I18n.t(data_type_name, scope: 'lib.equity_gsdata.data_point_info_texts', default: ''),
-              values: display_hashes.sort_by(&percentage_desc)
-          }
-          # note sources
-          @sources[subject_name] = {
-              info_text: I18n.t(data_type_name, scope: 'lib.equity_gsdata.data_point_info_texts', default: ''),
-              sources: matching_breakdowns.map { |hash| {name: hash['source_name'], year: hash['source_year'] }}.uniq
-          }
+          output_hash.merge!(subject_hash(data_type_name, matching_breakdowns))
+
+          @sources.merge!(sources_hash(data_type_name, matching_breakdowns))
         end
+      end
+    end
+
+    def subject_name(data_type_name)
+      I18n.t(data_type_name, scope: 'lib.equity_gsdata', default: data_type_name)
+    end
+
+    def subject_hash(data_type_name, value_hashes)
+      {
+          subject_name(data_type_name) => {
+              narration: I18n.t(data_type_name, scope: 'lib.equity_gsdata.data_point_info_texts', default: ''),
+              values: value_hashes.map(&hash_for_display).sort_by(&percentage_desc)
+          }
+      }
+    end
+
+    def sources_hash(data_type_name, value_hashes)
+      {
+          subject_name(data_type_name) => {
+              info_text: I18n.t(data_type_name, scope: 'lib.equity_gsdata.data_point_info_texts', default: ''),
+              sources: value_hashes.map { |hash| {name: hash['source_name'], year: hash['source_year'] }}.uniq
+          }
+      }
+    end
+
+    def matching_values(max_year)
+      lambda do |hash|
+        hash.has_key?('school_value') &&
+            hash['source_year'].to_i == max_year &&
+            (hash['breakdowns'].blank? || ethnicity_breakdowns.keys.include?(hash['breakdowns']))
       end
     end
 
@@ -66,18 +86,6 @@ module SchoolProfiles
 
     def percentage_desc
       ->(hash) { -hash[:percentage] }
-    end
-
-    def has_school_value
-      ->(hash) { hash.has_key?('school_value') }
-    end
-
-    def matching_source_year(year)
-      ->(hash) { hash['source_year'].to_i == year }
-    end
-
-    def breakdowns_blank_or_includes(valid_breakdowns)
-      ->(hash) { hash['breakdowns'].blank? || valid_breakdowns.include?(hash['breakdowns']) }
     end
 
     def ethnicity_breakdowns
