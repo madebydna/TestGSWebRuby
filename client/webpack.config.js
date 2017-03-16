891d0abb48c72a44e487637212719265020b1def
Samson Sprouse <ssprouse@greatschools.org>
2017-03-16 15:43:00 -0700

    JT-3270 address es7 polyfill to suport Object.values and Object.entries

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -54,6 +54,10 @@ const config = {
         test: /\.jsx?$/,
         loader: 'babel-loader',
         exclude: /node_modules/,
+        query: {
+          plugins: ['transform-runtime'],
+          presets: ['es2015', 'stage-0', 'react'],
+        }
       },
     ],
   },
