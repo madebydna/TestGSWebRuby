75e553e7448088002a33a593a6d4c04af5a9103f
Anthony Roy <aroy@greatschools.org>
2017-03-23 14:12:53 -0700

    JT-3157 Trim city name too per Greg request

--- spec/controllers/cities_list_controller_spec.rb
+++ spec/controllers/cities_list_controller_spec.rb
@@ -42,6 +42,16 @@ describe CitiesListController do
     end
   end
 
+  context 'trims city name' do
+    it 'redirects to city home' do
+      get :old_homepage, controller: 'cities_list', city: '__New_Trenton___', state_abbr: 'NJ', s_cid: 'wsbay93'
+
+      target = city_path(city: 'new-trenton', state: 'new-jersey', s_cid: 'wsbay93')
+      expect(response).to redirect_to(target)
+      expect(response).to have_http_status(301)
+    end
+  end
+
   context 'invalid redirection' do
     it 'redirects to root' do
       get :old_homepage, :controller => 'cities_list', :city => 'none', :state_abbr => 'CZ'
