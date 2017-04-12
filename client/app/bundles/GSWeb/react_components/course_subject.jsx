e6abd7b9191d2c32afcad5405d81e23ce365fb39
Derek Walters <dwalters@Dereks-MacBook-Pro.local>
2017-04-12 14:19:43 -0700

    JT-3274 styling tweaks

--- client/app/bundles/GSWeb/react_components/course_subject.jsx
+++ client/app/bundles/GSWeb/react_components/course_subject.jsx
@@ -32,7 +32,7 @@ export default class CourseSubject extends React.Component {
         <div className="course-subject" onClick={this.toggle}>
           <span>{this.props.name}</span>
           <span>
-            <span className={'gs-rating circle-rating--xtra-small circle-rating--' + this.props.rating}>{this.props.rating}<span class="denominator">/10</span></span>
+            <span className={'gs-rating-inline circle-rating--xtra-small circle-rating--' + this.props.rating}>{this.props.rating}<span class="denominator">/10</span></span>
           </span>
           { this.props.courses && <span><a href="javascript:void(0)">{this.props.courses.length} courses</a></span> }
           <span className="icon-caret-down show-classes" />
