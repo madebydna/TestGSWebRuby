449f5b562dfcf808ef4f8fc4271e9d0ccb909ea4
Samson Sprouse <ssprouse@greatschools.org>
2017-04-26 17:31:27 -0700

    JT-3067 Fix two low-income test score specs

--- spec/libs/school_profiles/narrative_low_income_test_scores_spec.rb
+++ spec/libs/school_profiles/narrative_low_income_test_scores_spec.rb
@@ -7,7 +7,7 @@ describe SchoolProfiles::NarrativeLowIncomeTestScores do
       school_cache_data_reader = mock_school_cache_data_reader
 
       narrativeLowIncomeTestScores = SchoolProfiles::NarrativeLowIncomeTestScores.new(school_cache_data_reader: school_cache_data_reader)
-      allow(narrativeLowIncomeTestScores).to receive(:key_for_yml).and_return '4_3'
+      allow(narrativeLowIncomeTestScores).to receive(:key_for_yml).and_return '4'
       narrativeLowIncomeTestScores.auto_narrative_calculate_and_add
 
       expect(school_cache_data_reader.test_scores).to eq(result_hash)
@@ -17,7 +17,7 @@ describe SchoolProfiles::NarrativeLowIncomeTestScores do
       school_cache_data_reader = double(test_scores: test_scores_hash_2)
 
       narrativeLowIncomeTestScores = SchoolProfiles::NarrativeLowIncomeTestScores.new(school_cache_data_reader: school_cache_data_reader)
-      allow(narrativeLowIncomeTestScores).to receive(:key_for_yml).and_return '4_3'
+      allow(narrativeLowIncomeTestScores).to receive(:key_for_yml).and_return '4'
       narrativeLowIncomeTestScores.auto_narrative_calculate_and_add
 
       expect(school_cache_data_reader.test_scores).to eq(result_hash_2)
@@ -113,7 +113,7 @@ describe SchoolProfiles::NarrativeLowIncomeTestScores do
                      "number_students_tested"=>99,
                      "score"=>56.0,
                      "state_average"=>30.0,
-                     "narrative" => string_key_for_key_4_3_and_subject_english
+                     "narrative" => t('4', 'English')
 
                    }
                  },
@@ -124,7 +124,7 @@ describe SchoolProfiles::NarrativeLowIncomeTestScores do
                      "number_students_tested"=>98,
                      "score"=>47.0,
                      "state_average"=>21.0,
-                     "narrative" => string_key_for_key_4_3_and_subject_math
+                     "narrative" => t('4', 'math') 
                    }
                  }
                }
@@ -217,7 +217,7 @@ describe SchoolProfiles::NarrativeLowIncomeTestScores do
                      "number_students_tested"=>99,
                      "score"=>56.0,
                      "state_average"=>30.0,
-                     "narrative" => string_key_for_key_4_3_and_subject_some_subject
+                     "narrative" => t('4', 'Some subject')
 
                    }
                  },
@@ -228,7 +228,7 @@ describe SchoolProfiles::NarrativeLowIncomeTestScores do
                      "number_students_tested"=>98,
                      "score"=>47.0,
                      "state_average"=>21.0,
-                     "narrative" => string_key_for_key_4_3_and_subject_some_other_subject
+                     "narrative" => t('4', 'Some other subject')
                    }
                  }
                }
@@ -442,21 +442,7 @@ describe SchoolProfiles::NarrativeLowIncomeTestScores do
     }
   end
 
-  def string_key_for_key_4_3_and_subject_english
-    "<p class='title positive'>A positive sign!</p> <p class=\"narrative-content\">The English scores of low-income students at this school are at or <span class=\"emphasis\">above</span> the state average for all kids. Still, there’s a <span class=\"emphasis\">large gap</span> between how low-income kids are doing compared to their classmates with more financial resources.</p> <hr /> <p class=\"parent-tip\">Parent tip:</p> <p class=\"footnote\">Ask the school what it’s doing to help all students succeed.</p>\n"
-  end
-
-
-  def string_key_for_key_4_3_and_subject_math
-    "<p class='title positive'>A positive sign!</p> <p class=\"narrative-content\">The math scores of low-income students at this school are at or <span class=\"emphasis\">above</span> the state average for all kids. Still, there’s a <span class=\"emphasis\">large gap</span> between how low-income kids are doing compared to their classmates with more financial resources.</p> <hr /> <p class=\"parent-tip\">Parent tip:</p> <p class=\"footnote\">Ask the school what it’s doing to help all students succeed.</p>\n"
-  end
-
-  def string_key_for_key_4_3_and_subject_some_subject
-    "<p class='title positive'>A positive sign!</p> <p class=\"narrative-content\">The Some subject scores of low-income students at this school are at or <span class=\"emphasis\">above</span> the state average for all kids. Still, there’s a <span class=\"emphasis\">large gap</span> between how low-income kids are doing compared to their classmates with more financial resources.</p> <hr /> <p class=\"parent-tip\">Parent tip:</p> <p class=\"footnote\">Ask the school what it’s doing to help all students succeed.</p>\n"
-  end
-
-
-  def string_key_for_key_4_3_and_subject_some_other_subject
-    "<p class='title positive'>A positive sign!</p> <p class=\"narrative-content\">The Some other subject scores of low-income students at this school are at or <span class=\"emphasis\">above</span> the state average for all kids. Still, there’s a <span class=\"emphasis\">large gap</span> between how low-income kids are doing compared to their classmates with more financial resources.</p> <hr /> <p class=\"parent-tip\">Parent tip:</p> <p class=\"footnote\">Ask the school what it’s doing to help all students succeed.</p>\n"
+  def t(yml_key, subject)
+    I18n.t(yml_key + '_html', scope: 'lib.test_scores.narrative.low_income', subject: I18n.t(subject, scope: 'lib.equity_gsdata', default: subject))
   end
 end
