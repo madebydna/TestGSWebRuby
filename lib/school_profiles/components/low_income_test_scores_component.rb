c248439ef283a8bab0cadccf38e0c22680a5f6b7
Anthony Roy <aroy@greatschools.org>
2017-05-22 10:11:27 -0700

    JT-3757 Order low income breakdowns

--- lib/school_profiles/components/low_income_test_scores_component.rb
+++ lib/school_profiles/components/low_income_test_scores_component.rb
@@ -13,6 +13,19 @@ module SchoolProfiles
 
         t(yml_key + '_html', scope: 'lib.test_scores.narrative.low_income', subject: t(data_type))
       end
+
+      # Keep "All" or "All students" on top
+      # Then "Economically disadvantaged"
+      # Finally "Not economically disadvantaged"
+      #
+      # Keep in mind breakdown string must not be translated yet
+      def comparator(h1, h2)
+        return -2 if h1[:breakdown] == 'All students'
+        return 2 if h2[:breakdown] == 'All students'
+        return -1 if h1[:breakdown] == 'Economically disadvantaged'
+        return 1 if h2[:breakdown] == 'Economically disadvantaged'
+        return h2[:percentage].to_f <=> h1[:percentage].to_f
+      end
     end
   end
 end
