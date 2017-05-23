8aededb7247f75828dd54199b5a700d1f3caf533
Samson Sprouse <ssprouse@greatschools.org>
2017-05-23 09:10:57 -0700

    JT-3749 Use rails cache key to cache census description and not class var
    - Also clear caches when census data is loaded

--- lib/school_cache/characteristics_caching/base.rb
+++ lib/school_cache/characteristics_caching/base.rb
@@ -1,25 +1,32 @@
 class CharacteristicsCaching::Base < Cacher
 
-  @@characteristics_data_types = Hash[CensusDataType.all.map { |f| [f.id, f] }]
-  @@characteristics_data_breakdowns = Hash[CensusDataBreakdown.all.map { |f| [f.id, f] }]
-  @@characteristics_descriptions = Hash[CensusDescription.all.map { |f| [f.census_data_set_id.to_s+f.state, f] }]
+  def self.clear_rails_caches
+    Rails.cache.delete('CharacteristicsCaching::Base.characteristics_descriptions');
+  end
 
-  cattr_accessor :characteristics_data_types, :characteristics_data_breakdowns, :characteristics_descriptions
-
+  def self.characteristics_data_types
+    Rails.cache.fetch('CharacteristicsCaching::Base.characteristics_data_types', expires_in: 2.weeks) do
+      Hash[CensusDataType.all.map { |f| [f.id, f] }]
+    end
+  end
   def characteristics_data_types
-    @@characteristics_data_types
+    self.class.characteristics_data_types
   end
 
   def configured_characteristics_data_types
     Hash[CensusDataConfigEntry.on_db(school.shard).all.map { |f| [f.data_type_id, f] }]
   end
 
-  def characteristics_descriptions
-    @@characteristics_descriptions
+  def self.characteristics_descriptions
+    Rails.cache.fetch('CharacteristicsCaching::Base.characteristics_descriptions', expires_in: 2.weeks) do
+      Hash[CensusDescription.all.map { |f| [f.census_data_set_id.to_s+f.state, f] }]
+    end
   end
 
-  def characteristics_data_breakdowns
-    @@characteristics_data_breakdowns
+  def self.characteristics_data_breakdowns
+    Rails.cache.fetch('CharacteristicsCaching::Base.characteristics_data_breakdowns', expires_in: 2.weeks) do
+      Hash[CensusDataBreakdown.all.map { |f| [f.id, f] }]
+    end
   end
 
   def self.listens_to?(data_type)
