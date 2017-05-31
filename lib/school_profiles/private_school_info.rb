991e86e8ba6d4a2ec7dcef801da6d8eac55d4b9b
Mitchell Seltzer <mseltzer@greatschools.org>
2017-05-31 14:12:22 -0700

    JT-3679 - fix for private school issue with translation missing - it was getting a period as a value.

--- lib/school_profiles/private_school_info.rb
+++ lib/school_profiles/private_school_info.rb
@@ -24,7 +24,7 @@ module SchoolProfiles
       osp_question_metadata.slice(*cache_keys).each_with_object([]) do |(response_key, response_value), accum|
         next if (response_value[:level_code] & school_level_code).empty?
         data = private_school_cache_data.slice(*cache_keys)
-        next if (keys_to_hide_if_no_data.include?(response_key) || data[response_key].nil?)
+        next if (keys_to_hide_if_no_data.include?(response_key))
         responses = data[response_key].present? ? data[response_key].keys : Array(NO_DATA_TEXT)
         translated_responses = responses.map{|response| data_label(response)}
         accum << {
