5e6098f2261f216e331c0437682360d8783d8d9d
Mitchell Seltzer <mseltzer@greatschools.org>
2017-06-02 09:21:59 -0700

    JT-2880 - trying inline style sheet with override in linked style sheet

--- app/assets/javascripts/header/header_init.js
+++ app/assets/javascripts/header/header_init.js
@@ -1,16 +1,8 @@
 (function() {
   if ( document.getElementsByClassName("header_un").length > 0 ) {
-
     GS.nav.menu.init();
     GS.nav.searchBar.init();
     GS.nav.featured.init();
     GS.nav.language.init();
-    var topHeaderNavigationWP = document.getElementById("header_top_navigation_wp");
-    if(topHeaderNavigationWP) {
-      setTimeout(function () {
-        topHeaderNavigationWP.style.display = null;
-      }, 500);
-
-    }
   }
 })();
