01337b7554d6276bfa55de4bd0721949b6947da7
Anthony Roy <aroy@greatschools.org>
2017-06-07 11:25:13 -0700

    JT-3804 Drop tipso from FAQ, only remodal

--- client/app/bundles/GSWeb/react_components/info_text_and_circle.jsx
+++ client/app/bundles/GSWeb/react_components/info_text_and_circle.jsx
@@ -3,8 +3,8 @@ import React, { PropTypes } from 'react';
 export default class InfoTextAndCircle extends React.Component {
 
   static propTypes = {
-    cta: React.PropTypes.string.isRequired,
-    content: React.PropTypes.string.isRequired
+    cta: PropTypes.string.isRequired,
+    content: PropTypes.string.isRequired
   };
 
   constructor(props) {
@@ -21,7 +21,7 @@ export default class InfoTextAndCircle extends React.Component {
     return(
     <div className="info-text-and-circle">
       <a data-remodal-target="modal_info_box" data-content-type="info_box"
-         data-content-html={this.props.content} className="speech-bubble gs-tipso" href="javascript:void(0)">
+         data-content-html={this.props.content} className="speech-bubble" href="javascript:void(0)">
         {this.props.cta}
         <span className="info-circle icon-question"/>
       </a>
