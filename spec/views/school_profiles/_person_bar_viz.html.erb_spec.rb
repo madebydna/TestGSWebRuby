113af86e069f9f82f145eef6d7b6adb43c65211e
Mitchell Seltzer <mseltzer@greatschools.org>
2017-06-09 10:49:07 -0700

    JT-3864 - specs are a mess but trying to persevere.

--- spec/views/school_profiles/_person_bar_viz.html.erb_spec.rb
+++ spec/views/school_profiles/_person_bar_viz.html.erb_spec.rb
@@ -2,33 +2,50 @@ require 'spec_helper'
 
 describe 'school_profile/_person_bar_viz' do
   let(:visualization_class) { '.person-bar-viz' }
-  let(:state_visualization_class) { '.arrow-down' }
+  let(:state_visualization_class) { '.arrow-up' }
   let(:valid_props) do
     {
-        score: 1,
-        state_score: 1
+        score_data: {
+            score: SchoolProfiles::DataPoint.new('90'),
+            state_score: 1,
+            range:(0..100),
+            score_label:'English',
+            info_text:'tooltip content'
+        }
     }
+  end
+
+  def props(value)
+    p = valid_props.clone
+    p[:score_data] = OpenStruct.new(p[:score_data].merge(value))
+    p
   end
 
   def render_partial(props)
     stub_template "_ten_person_icons.html.erb" => "persons"
+    stub_template "_info_circle.html.erb" => "info_text"
     render partial: 'school_profiles/person_bar_viz', locals: props
   end
 
   describe 'score' do
     [0,1,14,37,50,88,99,100].each do |input|
-      describe "when valid score is #{input}" do
-        it 'renders the visualization' do
-          render_partial(valid_props.merge(score: input))
-          expect(rendered).to have_css(visualization_class)
+      it 'renders the visualization' do
+        score_data = SchoolProfiles::RatingScoreItem.new.tap do |rsi|
+          rsi.score = SchoolProfiles::DataPoint.new(input)
         end
+        render_partial(props(score_data: score_data))
+        expect(rendered).to have_css(visualization_class)
       end
     end
+
 
     [-1, 101].each do |input|
       describe "when invalid score is #{input}" do
         it 'renders nothing' do
-          render_partial(valid_props.merge(score: input))
+          score_data = SchoolProfiles::RatingScoreItem.new.tap do |rsi|
+            rsi.score = SchoolProfiles::DataPoint.new(input)
+          end
+          render_partial(props(score_data: score_data))
           expect(rendered.strip).to be_empty
         end
       end
@@ -36,7 +53,10 @@ describe 'school_profile/_person_bar_viz' do
 
     describe 'when score is nil' do
       it 'renders nothing' do
-        render_partial(valid_props.merge(score: nil))
+        score_data = SchoolProfiles::RatingScoreItem.new.tap do |rsi|
+          rsi.score = SchoolProfiles::DataPoint.new(nil)
+        end
+        render_partial(props(score_data: score_data))
         expect(rendered.strip).to be_empty
       end
     end
@@ -53,7 +73,10 @@ describe 'school_profile/_person_bar_viz' do
         100 => 10
     }.each do |score, expected_rating|
       it "renders #{expected_rating} when score is #{score}" do
-        render_partial(valid_props.merge(score: score))
+        score_data = SchoolProfiles::RatingScoreItem.new.tap do |rsi|
+          rsi.score = SchoolProfiles::DataPoint.new(score)
+        end
+        render_partial(props(score_data: score_data))
         expect(rendered).to have_css(".rating_color_#{expected_rating}")
       end
     end
@@ -62,7 +85,10 @@ describe 'school_profile/_person_bar_viz' do
   describe 'state average' do
     describe 'when within range' do
       it 'renders the arrow' do
-        render_partial(valid_props)
+        score_data = SchoolProfiles::RatingScoreItem.new.tap do |rsi|
+          rsi.score = SchoolProfiles::DataPoint.new(60)
+        end
+        render_partial(props(score_data: score_data))
         expect(rendered).to have_css(state_visualization_class)
       end
     end
@@ -85,15 +111,26 @@ describe 'school_profile/_person_bar_viz' do
   end
 
   describe 'when custom ranges are specified' do
-    let(:props) { valid_props.merge(range: (600..2400)) }
+    # let(:props) { valid_props.merge(range: (600..2400)) }
 
     it 'valid score is rendered' do
-      render_partial(props.merge(score: 1553, state_score: 1500))
+      score_data = SchoolProfiles::RatingScoreItem.new.tap do |rsi|
+        rsi.score = SchoolProfiles::DataPoint.new(input)
+        rsi.state_score = 1500
+        rsi.range = (600..2400)
+      end
+      render_partial(props(score_data: score_data))
+      # render_partial(props.merge(score: 1553, state_score: 1500))
       expect(rendered).to have_css(visualization_class)
     end
 
     it 'valid state average is rendered' do
-      render_partial(props.merge(score: 1553, state_score: 1500))
+      score_data = SchoolProfiles::RatingScoreItem.new.tap do |rsi|
+        rsi.score = SchoolProfiles::DataPoint.new(input)
+        rsi.state_score = 1500
+        rsi.range = (600..2400)
+      end
+      render_partial(props(score_data: score_data))
       expect(rendered).to have_css(state_visualization_class)
     end
 
@@ -103,7 +140,12 @@ describe 'school_profile/_person_bar_viz' do
     end
 
     it 'state average arrow when outside range is not rendered' do
-      render_partial(props.merge(score: 1553, state_score: 65))
+      score_data = SchoolProfiles::RatingScoreItem.new.tap do |rsi|
+        rsi.score = SchoolProfiles::DataPoint.new(input)
+        rsi.state_score = 65
+        rsi.range = (600..2400)
+      end
+      render_partial(props(score_data: score_data))
       expect(rendered.strip).not_to be_empty
       expect(rendered).not_to have_css(state_visualization_class)
     end
