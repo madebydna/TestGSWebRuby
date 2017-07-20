80a50a0d79c55eb01ad7267972d6c790ddf78d6f
Alex Thaler <thaler@greatschools.org>
2017-07-20 11:16:01 -0700

    jt-4037 Use FactoryGirl instead of District.new

--- spec/controllers/modules/districts_meta_tags_concerns_spec.rb
+++ spec/controllers/modules/districts_meta_tags_concerns_spec.rb
@@ -11,12 +11,12 @@ describe DistrictsMetaTagsConcerns do
   context 'with a state, city, and district instance vars set' do
     before do
       subject.instance_variable_set(:@state, {:short => 'ca'})
-      subject.instance_variable_set(:@city, 'berkeley')
-      subject.instance_variable_set(:@district, District.new(name: 'Berkeley Unified School District'))
+      subject.instance_variable_set(:@city, 'oakland')
+      subject.instance_variable_set(:@district, FactoryGirl.build(:oakland_unified))
     end
 
     it 'should return the correct title string' do
-      expect(subject.send(:districts_show_title)).to eql("Berkeley Unified School District in Berkeley, CA | GreatSchools")
+      expect(subject.send(:districts_show_title)).to eql("Oakland Unified School District in Oakland, CA | GreatSchools")
     end
   end
 
