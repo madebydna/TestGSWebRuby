cc8a8d3ad896e8583fd4fe8703b9023835f8cd9b
Samson Sprouse <ssprouse@greatschools.org>
2017-07-27 14:05:10 -0700

    JT-4215 Rename legacy_application to school-profile-blocking

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -10,7 +10,7 @@ const nodeEnv = devBuild ? 'development' : 'production';
 
 const config = {
   entry: {
-    'legacy_application': ['jquery', 'jquery-ujs', 'jquery.cookie', 'lodash', 'jquery-unveil'],
+    'school-profile-blocking': ['jquery', 'jquery-ujs', 'jquery.cookie', 'lodash', 'jquery-unveil'],
     'widget': ['./app/bundles/GSWeb/widget'],
     'district-boundaries': ['./app/bundles/GSWeb/district_boundaries'],
     'webpack': [
