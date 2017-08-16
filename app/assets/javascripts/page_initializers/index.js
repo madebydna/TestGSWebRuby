f2b80eb9a6a33a9da78485dcdd7ef351ce9974d1
Samson Sprouse <ssprouse@greatschools.org>
2017-08-16 14:02:09 -0700

    JT-3991 Remove old profile related code

--- app/assets/javascripts/page_initializers/index.js
+++ app/assets/javascripts/page_initializers/index.js
@@ -16,7 +16,6 @@
 //= require ./overview_init
 //= require ./quality_init
 //= require ./review_school_chooser_init
-//= require ./reviews_init
 //= require ./reviews_new_init
 //= require ./search_init
 //= require ./signin_new_init
