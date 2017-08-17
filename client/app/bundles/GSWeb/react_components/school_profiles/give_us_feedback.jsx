ccca06de7c44a9dbf451b294702ccf51e1c358de
Samson Sprouse <ssprouse@greatschools.org>
2017-08-17 11:07:11 -0700

    JT-4229 Update qualaroo feedback cta to have yes/no buttons

--- client/app/bundles/GSWeb/react_components/school_profiles/give_us_feedback.jsx
+++ client/app/bundles/GSWeb/react_components/school_profiles/give_us_feedback.jsx
@@ -2,11 +2,16 @@ import React from 'react';
 import { t } from '../../util/i18n';
 
 const GiveUsFeedback = ({className='', content, children}) => {
+  let qualaroo_yes_url = content + '&a=0';
+  let qualaroo_no_url = content + '&a=1';
   return (
-      <div className="module_feedback">
-        <span className="module_feedback_desktop_divider">&nbsp;|&nbsp;</span>{t('was_this_useful')}&nbsp;
-        <a href={content} target="_blank"><span className="source-link">{t('give_us_your_feedback')}</span></a>
-      </div>
+    <div className="module_feedback">
+      <span className="module_feedback_desktop_divider">&nbsp;|&nbsp;</span>{t('was_this_useful')}&nbsp;
+      <span>
+        <a href={qualaroo_yes_url} className='anchor-button'>{t('yes')}</a>
+        <a href={qualaroo_no_url} className='anchor-button'>{t('no')}</a>
+      </span>
+    </div>
   );
 };
 
