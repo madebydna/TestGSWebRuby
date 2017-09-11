14b1b04ded6b2c20d29e560b092df16ebd1f371d
Alex Thaler <athaler@greatschools.org>
2017-09-11 16:16:55 -0700

    Add summary rating to cacher

--- lib/school_cache/gsdata_caching/gsdata_cacher.rb
+++ lib/school_cache/gsdata_caching/gsdata_cacher.rb
@@ -19,7 +19,7 @@ class GsdataCaching::GsdataCacher < Cacher
   # 159: Academic Progress Rating
   DISCIPLINE_ATTENDANCE_IDS = [161, 162, 163, 164]
 
-  DATA_TYPE_IDS = [23, 27, 35, 55, 59, 63, 71, 83, 91, 95, 99, 119, 133, 149, 150, 151, 152, 154, 158, 159, 175, 176, 177, 178, 179, 180, 181, 182, 186].freeze
+  DATA_TYPE_IDS = [23, 27, 35, 55, 59, 63, 71, 83, 91, 95, 99, 119, 133, 149, 150, 151, 152, 154, 158, 159, 160, 175, 176, 177, 178, 179, 180, 181, 182, 186].freeze
 
   BREAKDOWN_TAG_NAMES = [
     'ethnicity',
