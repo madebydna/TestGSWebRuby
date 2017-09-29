5ea40b4fd08d30e93435903c737937d214a9a720
Samson Sprouse <ssprouse@greatschools.org>
2017-09-29 14:20:23 -0700

    JT-4376 Change format of webpack fingerprinted image names

--- client/webpack.config.babel.js
+++ client/webpack.config.babel.js
@@ -77,7 +77,7 @@ const config = {
           {
             loader: 'file-loader',
             options: {
-              name: '[name]-[hash].[ext]'
+              name: '[name]_[hash].[ext]'
             }
           },
           {
