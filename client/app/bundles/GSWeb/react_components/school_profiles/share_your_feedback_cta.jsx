078b0cec314f09374bfc0e5df53749939d8ed669
Samson Sprouse <ssprouse@greatschools.org>
2017-10-19 11:53:38 -0700

    JT-4873 Fix up styling on Share Your Feedback CTA, add new image

--- client/app/bundles/GSWeb/react_components/school_profiles/share_your_feedback_cta.jsx
+++ client/app/bundles/GSWeb/react_components/school_profiles/share_your_feedback_cta.jsx
@@ -3,16 +3,16 @@ import PropTypes from 'prop-types';
 import { t } from '../../util/i18n';
 import AnchorButton from 'react_components/anchor_button';
 import ConnectedReviewDistributionModal from 'react_components/connected_review_distribution_modal';
+import communityAvatar from 'icons/community-avatar.png';
 
 const ShareYourFeedbackCta = ({questionText, buttonClicked}) => {
   return (
     <div className="share-your-feedback-cta">
       <hr/>
       <p className="parent-tip">
-        <img src="/assets/school_profiles/owl.png" />
-        <span className="speech-bubble left">Share your feedback</span>
+        <img src={communityAvatar} />
+        <span className="speech-bubble left">{questionText}</span>
       </p>
-      <p>{questionText}</p>
       <div className="actions">
         <AnchorButton href="javascript:void(0)" onClick={buttonClicked}>Answer</AnchorButton>
         <ConnectedReviewDistributionModal
