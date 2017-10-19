9694ad84d26916aa616ee0eefc2b8918c28faa5c
Mitchell Seltzer <mseltzer@greatschools.org>
2017-10-19 13:03:44 -0700

    JT-4858 -  update og and image for facebook on the home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -16,10 +16,19 @@ class HomeController < ApplicationController
     # Description lives in view because the meta-tags gem truncates description at 200 chars. See https://github.com/kpumuk/meta-tags
     set_meta_tags title: 'GreatSchools: School Ratings and Reviews for Public and Private Schools',
                   og: {
-                      title: 'GreatSchools: School Ratings and Reviews for Public and Private Schools',
-                      type: 'website',
-                      url: home_url,
-                      site_name: 'GreatSchools'
+                      title: "K-12 school quality information and parenting resources.",
+                      description: "We're an independent nonprofit that provides parenting resources and in-depth school quality information families can use to choose the right school and support their child's learning and development.",
+                      site_name: 'GreatSchools.org',
+                      image: {
+                        url: ActionController::Base.helpers.asset_path('assets/share/logo-ollie-large.png'),
+                        secure_url: ActionController::Base.helpers.asset_path('assets/share/logo-ollie-large.png'),
+                        height: 600,
+                        width: 1200,
+                        type: 'image/png',
+                        alt: 'GreatSchools is a non profit organization providing school quality information'
+                      },
+                      type: 'place',
+                      url: home_url
                   },
                   twitter: {
                       card: 'Summary',
