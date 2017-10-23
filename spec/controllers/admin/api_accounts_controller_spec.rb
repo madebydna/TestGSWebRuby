9ff1c21b76777e4ef6db337825fae0a8d34eb5c7
Alex Thaler <athaler@greatschools.org>
2017-10-23 15:47:02 -0700

    jt-4817 Add spec coverage, refactor pagination method to avoid side-effects, add api to script that generates test db tables

--- spec/controllers/admin/api_accounts_controller_spec.rb
+++ spec/controllers/admin/api_accounts_controller_spec.rb
@@ -3,20 +3,60 @@ require 'spec_helper'
 describe Admin::ApiAccountsController do
 
   describe '#display_selected_api_accounts' do
-    let (:params) {
-      {all: 'true'}
-    }
-
-    let (:subject) { controller.send(:index) }
-
+    test_size = 23
     before do
-      allow(controller).to receive(:params).and_return(params)
+      test_size.times {|num| FactoryGirl.create(:api_account, account_added: Time.now + num)}
+    end
+    let (:params) {
+      {all: nil, start: nil}
+    }
+    after do
+      clean_models ApiAccount
     end
 
-    describe 'calls the main pagination method' do
-      it { should receive(:display_selected_api_accounts)}
+    let (:subject) {controller.send(:selected_api_accounts)}
+
+    context 'when start is not specified' do
+      it 'loads with 0 offset' do
+        params = {}
+        allow(controller).to receive(:params).and_return(params)
+        expect(subject.size).to eq(test_size)
+      end
     end
 
 
+    context 'when start is specified' do
+
+      it 'calculates offset by reference to OFFSET constant' do
+        smaller_offset = 5
+        stub_const("Admin::ApiAccountsController::OFFSET", smaller_offset)
+        params[:start] = 6
+        allow(controller).to receive(:params).and_return(params)
+        expect(subject).to eq(ApiAccount.offset(smaller_offset).limit(smaller_offset))
+      end
+
+      it 'defaults to 0 if start is negative' do
+        params[:start] = -5
+        allow(controller).to receive(:params).and_return(params)
+        expect(subject.first).to eq(ApiAccount.first)
+      end
+
+      it 'defaults to 0 if start is larger than total # of accounts' do
+        params[:start] = test_size + 1
+        allow(controller).to receive(:params).and_return(params)
+        expect(subject.first).to eq(ApiAccount.first)
+      end
+
+    end
+
+    context 'when all is specified' do
+      it 'loads all api accounts' do
+        params[:all] = true
+        allow(controller).to receive(:params).and_return(params)
+        expect(subject.size).to eq(test_size)
+      end
+    end
+    
   end
 end
+
