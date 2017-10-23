da96ca25ec8198eec1438b7ace471df367bada60
Alex Thaler <athaler@greatschools.org>
2017-10-23 16:45:29 -0700

    jt-4817 refactor spec

--- spec/controllers/admin/api_accounts_controller_spec.rb
+++ spec/controllers/admin/api_accounts_controller_spec.rb
@@ -3,13 +3,12 @@ require 'spec_helper'
 describe Admin::ApiAccountsController do
 
   describe '#display_selected_api_accounts' do
-    test_size = 23
+    test_size = 101
     before do
       test_size.times {|num| FactoryGirl.create(:api_account, account_added: Time.now + num)}
+      allow(controller).to receive(:params).and_return(params)
     end
-    let (:params) {
-      {all: nil, start: nil}
-    }
+    let (:params) {{}}
     after do
       clean_models ApiAccount
     end
@@ -18,9 +17,7 @@ describe Admin::ApiAccountsController do
 
     context 'when start is not specified' do
       it 'loads with 0 offset' do
-        params = {}
-        allow(controller).to receive(:params).and_return(params)
-        expect(subject.size).to eq(test_size)
+        expect(subject.first.id).to eq(ApiAccount.first.id)
       end
     end
 
@@ -31,19 +28,16 @@ describe Admin::ApiAccountsController do
         smaller_offset = 5
         stub_const("Admin::ApiAccountsController::OFFSET", smaller_offset)
         params[:start] = 6
-        allow(controller).to receive(:params).and_return(params)
         expect(subject).to eq(ApiAccount.offset(smaller_offset).limit(smaller_offset))
       end
 
       it 'defaults to 0 if start is negative' do
         params[:start] = -5
-        allow(controller).to receive(:params).and_return(params)
         expect(subject.first).to eq(ApiAccount.first)
       end
 
       it 'defaults to 0 if start is larger than total # of accounts' do
         params[:start] = test_size + 1
-        allow(controller).to receive(:params).and_return(params)
         expect(subject.first).to eq(ApiAccount.first)
       end
 
@@ -51,12 +45,19 @@ describe Admin::ApiAccountsController do
 
     context 'when all is specified' do
       it 'loads all api accounts' do
-        params[:all] = true
-        allow(controller).to receive(:params).and_return(params)
+        params[:all] = 'true'
         expect(subject.size).to eq(test_size)
       end
+
+      it 'it loads all even if start is also specified' do
+        smaller_offset = 5
+        stub_const("Admin::ApiAccountsController::OFFSET", smaller_offset)
+        params[:all] = 'true'
+        params[:start] = 6
+        expect(subject.size).to eq(ApiAccount.count)
+      end
     end
-    
+
   end
 end
 
