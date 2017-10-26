4864c398cde65c9d3a31e0c51fa3aa0be357b374
Mitchell Seltzer <mseltzer@greatschools.org>
2017-10-26 16:01:14 -0700

    JT-4861 - adding some console logs

--- client/app/bundles/GSWeb/util/scrolling.js
+++ client/app/bundles/GSWeb/util/scrolling.js
@@ -1,8 +1,12 @@
 export function scrollToElement(selector, doneCallback) {
+  console.log("selector: "+selector);
   if($(selector).length > 0) {
     let y = $(selector).offset().top;
     let widthWithSearchBar = 767;
     let searchBarHeight = 48;
+    console.log("document.documentElement.clientWidth: "+document.documentElement.clientWidth);
+    console.log("y: "+y);
+    console.log("searchBarHeight: "+searchBarHeight);
     if(document.documentElement.clientWidth <= widthWithSearchBar) {
       // html needed in selector because firefox overflows at html node
       $('html,body').animate({scrollTop:y-searchBarHeight}, 500, 'swing', doneCallback);
