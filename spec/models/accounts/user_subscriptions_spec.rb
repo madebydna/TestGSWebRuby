2c8341a5ef4f15248ffc1b668219cf5fbf5a5ab3
Alex Thaler <athaler@greatschools.org>
2017-11-30 12:25:35 -0800

    jt-5205 Add spec coverage

--- spec/models/accounts/user_subscriptions_spec.rb
+++ spec/models/accounts/user_subscriptions_spec.rb
@@ -3,11 +3,15 @@ require 'spec_helper'
 describe UserSubscriptions do
 
   describe '#get' do
+    user = FactoryGirl.create(:verified_user)
+    subscriptions = Subscription::SUBSCRIPTIONS.map(&:first)
+    subscriptions.each {|list_type| FactoryGirl.create(:subscription, user: user , list: list_type)}
+
     after do
       clean_dbs :gs_schooldb
     end
+
     context 'when external code changes data from underneath class' do
-      let(:user) { FactoryGirl.create(:verified_user) }
       let!(:subscription) do
         FactoryGirl.create(:subscription, user: user , list: 'greatnews')
       end
@@ -16,6 +20,19 @@ describe UserSubscriptions do
         expect do
           Subscription.destroy_all
         end.to_not change { UserSubscriptions.new(user).get.size }
+      end
+    end
+
+    context 'with different subscription types' do
+      it 'successfully retrieves all user subscriptions' do
+        expect(UserSubscriptions.new(user).get.sort).to eq(subscriptions.sort)
+      end
+    end
+
+    context 'with a disallowed subscription type' do
+      FactoryGirl.create(:subscription, user: user , list: 'DeafPig')
+      it 'does not retrieve subscriptions not included in whitelist' do
+        expect(UserSubscriptions.new(user).get.any? {|list_type| list_type == 'DeafPig'}).to be_falsey
       end
     end
   end
