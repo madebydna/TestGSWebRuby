a85a71088b32cf972b6559e5344fa64f796b3607
Alex Thaler <athaler@greatschools.org>
2017-12-04 15:56:51 -0800

    jt-5205 Comment out debugging script, enable production mode

--- script/fix_grade_by_grade_subscriptions.rb
+++ script/fix_grade_by_grade_subscriptions.rb
@@ -1,19 +1,17 @@
 ActiveRecord::Base.establish_connection(ActiveRecord::Base.configurations['mysql_production_rw'])
 sql = "select distinct s.member_id from gs_schooldb.student s LEFT JOIN gs_schooldb.list_active la on s.member_id = la.member_id where s.updated > '2017-07-13' and la.list in ('greatnews', 'sponsor');"
 member_ids = ActiveRecord::Base.connection.execute(sql).to_a.flatten
-filtered_ids = []
+# filtered_ids = []
 member_ids.each do |id|
   #Try to prevent duplicate rows caused by users signing up again for a grade
   unless Subscription.where(member_id: id, list: 'greatkidsnews').present?
-    filtered_ids << id
-    # Save to file and debug
-    # Subscription.create(member_id: id, list: 'greatkidsnews', updated: Time.now)
+    # filtered_ids << id
+    Subscription.create(member_id: id, list: 'greatkidsnews', updated: Time.now)
   end
 end
 
-puts filtered_ids
-puts "No duplicates? #{filtered_ids.sort == filtered_ids.uniq.sort}"
-puts "Includes Bill Jackson? #{filtered_ids.include?(6780760)}"
+# p filtered_ids
+# puts "No duplicates? #{filtered_ids.sort == filtered_ids.uniq.sort}"
+# puts "Includes Bill Jackson? #{filtered_ids.include?(6780760)}"
 
-# Verify no dups
-# Verify that after script is run everyone in array now has two subscriptions
+
