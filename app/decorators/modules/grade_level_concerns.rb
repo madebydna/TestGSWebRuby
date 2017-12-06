467745e566e5a96d58e174965db2906ac3fb1019
Mitchell Seltzer <mseltzer@greatschools.org>
2017-12-05 19:35:07 -0800

    JT-5273 - directory feeds - build cache with level not including Ungraded and if only Ungraded change to 'n/a'

--- app/decorators/modules/grade_level_concerns.rb
+++ app/decorators/modules/grade_level_concerns.rb
@@ -9,6 +9,14 @@ module GradeLevelConcerns
     GradeLevelConcerns.human_readable_level(level)
   end
 
+  # extra processing to remove ungraded or replace it with n/a if alone
+  def process_level_feeds
+    level_human = GradeLevelConcerns.human_readable_level(level)
+    return 'n/a' if level_human == 'Ungraded'
+    level_human.slice! ' & Ungraded'
+    return level_human
+  end
+
   def self.human_readable_level(level)
     level.is_a?(String) ? level_array = level.split(',') : level_array = level
     if level_array.blank?
