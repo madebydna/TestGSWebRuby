6b4d9800662112f71d05626a61c04e3703d23e33
Alex Thaler <athaler@greatschools.org>
2017-12-07 15:34:11 -0800

    jt-5290 Refactor render logic

--- client/app/bundles/GSWeb/react_components/equity/equity_content_pane.jsx
+++ client/app/bundles/GSWeb/react_components/equity/equity_content_pane.jsx
@@ -16,13 +16,27 @@ export default class EquityContentPane extends React.Component {
     return this.props.text
   }
 
+  // Column header labels (i.e. 'Students', '% Proficient') will be added to every pane unless included in this
+  // blacklist, which references anchor props.
+  hasColumnHeader() {
+    return !['Overview', 'UC/CSU eligibility', 'Graduation rates', 'Advanced_coursework', 'Advanced courses',
+      'Percentage AP enrolled grades 9-12', 'Percentage of students suspended out of school', 'College readiness',
+      'Percentage of students chronically absent (15+ days)'].includes(this.props.anchor);
+  }
+
+  getColumnHeader() {
+    if (this.hasColumnHeader()) {
+      return <div>{<ColumnHeader anchor={this.props.anchor} />}</div>
+    }
+  }
+
   render() {
     let hr_style = ''
     return(
       <div className={'tabs-panel tabs-panel_selected'}>
         <div className="row">
           <div className="top-content">{this.get_narrative()}<hr  /></div>
-          <div>{<ColumnHeader anchor={this.props.anchor} />}</div>
+          {this.getColumnHeader()}
           <div>{this.props.graph}</div>
         </div>
       </div>
--- client/app/bundles/GSWeb/react_components/school_profiles/column_header.jsx
+++ client/app/bundles/GSWeb/react_components/school_profiles/column_header.jsx
@@ -13,29 +13,15 @@ export default class ColumnHeader extends React.Component {
     super(props);
   }
 
-  // These reference anchor props. If you've created a new module tab that you don't want column header labels to show
-  // up in, add its anchor here.
-  hasColumnHeader() {
-    return !['Overview', 'UC/CSU eligibility', 'Graduation rates', 'Advanced_coursework', 'Advanced courses',
-      'Percentage AP enrolled grades 9-12', 'Percentage of students suspended out of school', 'College readiness',
-      'Percentage of students chronically absent (15+ days)'].includes(this.props.anchor);
-  }
-
-  getGraphColumnHeader(){
-    if (this.hasColumnHeader()) {
-      return <div className="row bar-graph-display">
+  render() {
+    return(
+      <div className="row bar-graph-display">
         <div className="test-score-container clearfix">
           <div className="col-sm-5 header-text">{ this.props.leftLabel || capitalize(t('students')) }</div>
           <div className="col-sm-1"></div>
           <div className="col-sm-6 header-text">% { this.props.rightLabel || t('test_scores.at_grade_level')}</div>
         </div>
       </div>
-    }
-  }
-
-  render() {
-    return(
-      <div>{this.getGraphColumnHeader()}</div>
     )
   }
 }
