c96e7be695720b5d7270c49b3657c2016c386832
Samson Sprouse <ssprouse@greatschools.org>
2017-12-15 07:32:36 -0800

    Add script to install configured ruby version, run rbenv rehash, install bundler

---
+++ update_my_dev_environment.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+gem_install_or_update() {
+  if gem list "$1" --installed > /dev/null; then
+    echo "Updating just $@ if needed"
+    gem update "$@"
+  else
+    echo Installing "$@"
+    gem install "$@"
+  fi
+}
+
+configured_version=`cat .ruby-version`
+
+# rbenv install $configured_version
+if `rbenv versions 2> /dev/null | grep --quiet $configured_version`
+then
+  echo ruby $configured_version already installed
+else
+  echo ruby $configured_version not installed. Installing with rbenv
+  rbenv install $configured_version
+  rbenv rehash
+fi
+
+gem_install_or_update "bundler"
+gem_install_or_update "overcommit"
+overcommit --sign
+
+bundle install
+
+npm install
