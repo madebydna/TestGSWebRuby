a541d53c5e4273d4c06706e20cb12f818a5d9fc3
Samson Sprouse <ssprouse@greatschools.org>
2017-12-15 08:12:51 -0800

    Made a few more edits to dev environment script

--- update_my_dev_environment.sh
+++ update_my_dev_environment.sh
@@ -1,5 +1,24 @@
 #!/usr/bin/env bash
 
+# This script runs on Macs, tested with macOS Sierra 10.12.5
+# This script assumes the homebrew package manager for OSX is already installed
+#
+# Xcode and command line tools should be updated to latest versions beforehand
+
+read -p "Is xcode installed/updated to latest version? " -n 1 -r
+echo
+if ! [[ $REPLY =~ ^[Yy]$ ]]
+then
+  exit 1
+fi
+
+read -p "Are osx command line tools installed/updated to latest version? (command 'xcode-select --install' or use Mac App Store updates tab) " -n 1 -r
+echo
+if ! [[ $REPLY =~ ^[Yy]$ ]]
+then
+  exit 1
+fi
+
 gem_install_or_update() {
   if gem list "$1" --installed > /dev/null; then
     echo "Updating just $@ if needed"
@@ -11,6 +30,8 @@ gem_install_or_update() {
 }
 
 configured_version=`cat .ruby-version`
+
+brew upgrade rbenv ruby-build
 
 if `rbenv versions 2> /dev/null | grep --quiet $configured_version`
 then
