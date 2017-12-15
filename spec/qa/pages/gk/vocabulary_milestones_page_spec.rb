0cfcae95cf316eb8da1afff8ee9390a44ebbb36e
Samson Sprouse <ssprouse@greatschools.org>
2017-12-14 16:49:27 -0800

    JT-2682 Add high school and vocabulary milestones specs

---
+++ spec/qa/pages/gk/vocabulary_milestones_page_spec.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require 'remote_spec_helper'
+require 'features/page_objects/gk_vocabulary_milestones_page'
+
+describe 'User visits GK high school milestones', type: :feature, remote: true, safe_for_prod: true do
+  before { visit '/gk/category/milestones-topics/vocabulary/' }
+  let(:page_object) { GkVocabularyMilestones.new }
+  subject { page_object }
+  its(:heading) { is_expected.to have_text('Vocabulary') }
+  it { is_expected.to have_videos }
+  its('videos.size') { is_expected.to eq(6) }
+end
