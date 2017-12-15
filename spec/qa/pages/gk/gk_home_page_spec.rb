b71c97326eaaff8723c45c3cccf18cffa30d2ed9
Samson Sprouse <ssprouse@greatschools.org>
2017-12-15 10:23:55 -0800

    JT-2682 mark sign_up_for_newsletters as not safe for prod

--- spec/qa/pages/gk/gk_home_page_spec.rb
+++ spec/qa/pages/gk/gk_home_page_spec.rb
@@ -18,7 +18,7 @@ describe 'User visits GK home', type: :feature, remote: true, safe_for_prod: tru
   it { is_expected.to have_newsletter_signup_section }
   it { is_expected.to have_footer }
 
-  when_I :sign_up_for_newsletters do
+  when_I :sign_up_for_newsletters, safe_for_prod: false do
     it { is_expected.to have_newsletter_success_modal }
   end
   
