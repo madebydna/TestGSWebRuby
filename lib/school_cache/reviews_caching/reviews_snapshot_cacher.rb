2d3480b8731bc9c582e6bf2fe190e065fc070d21
Alex Thaler <athaler@greatschools.org>
2017-12-18 11:40:52 -0800

    JT-4649 Start building reviews cache. Stopped here for testing

--- lib/school_cache/reviews_caching/reviews_snapshot_cacher.rb
+++ lib/school_cache/reviews_caching/reviews_snapshot_cacher.rb
@@ -4,7 +4,7 @@ class ReviewsCaching::ReviewsSnapshotCacher < Cacher
 
   def school_reviews
     @school_reviews ||= (
-      school.reviews.extend(ReviewScoping).extend(ReviewCalculations)
+      SchoolProfiles::Reviews.new(school, SchoolProfiles::ReviewQuestions.new(school))
     )
   end
 
@@ -13,10 +13,66 @@ class ReviewsCaching::ReviewsSnapshotCacher < Cacher
         avg_star_rating: school_reviews.five_star_rating_reviews.average_score.round,
         num_ratings: school_reviews.five_star_rating_reviews.count_having_numeric_answer,
         num_reviews: school_reviews.size
-    }
+    }.merge(reviews_hash)
+  end
+
+  def reviews_hash
+    school_reviews.reviews_list.map do |review|
+      { :reviews =>
+        {
+          :avatar => review["avatar"],
+          :id => review["id"],
+          :topic_label => review["topic_label"],
+          :links => {
+            :flag => ""
+          },
+          :topical_reviews => [],
+          :most_recent_date => review["most_recent_date"],
+          :user_type_label => review["user_type_label"],
+          :comment => review["comment"]
+        }
+      }
+    end
   end
   
   def self.listens_to?(data_type)
     :school_reviews == data_type
   end
 end
+
+# reviews [
+#   {
+#     avatar:
+#       current_user_reported_reviews: []
+# review_reported_callback: func
+#     five_star_review: {
+#       answer:
+#       answer_label:
+#       answer_value:
+#       comment:
+#       date_published:
+#       id:
+#       links: {
+#         flag:
+#       }
+#       topic_label:
+#     }
+#     id: (if saved)
+#     most_recent_date:
+#     school_user_digest:
+#     user_type_label:
+#     topical_reviews: [
+#     {
+#      answer:
+#        answer_value:
+#        comment:
+#        date_published:
+#        id:
+#        links: {
+#         flag:
+#         }
+#        topic_label:
+#     }
+#     ]
+#   }
+# ]
