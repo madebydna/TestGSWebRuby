ba648d0f9720b63895c96fc48ce814804200d607
Alex Thaler <athaler@greatschools.org>
2018-01-16 13:54:02 -0800

    Add back gk specific specs

--- script/test/test_qa_gk.sh
+++ script/test/test_qa_gk.sh
@@ -1,8 +1,9 @@
 CAPYBARA_HOST=https://qa.greatschools.org \
 CAPYBARA_PORT=80 \
 BLACK_BOX=true \
-bundle exec rspec ./spec/ \
+bundle exec rspec \
 --require spec_helper.rb \
 --tag=remote \
+--pattern="spec/qa/pages/gk/**{,/*/**}/*_spec.rb" \
 
 
