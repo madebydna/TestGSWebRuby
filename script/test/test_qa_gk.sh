72026b5893ad3b0b4973883cf04724c656f58ca2
Alex Thaler <athaler@greatschools.org>
2018-01-16 13:49:33 -0800

    rename qa spec runner file for gk

--- script/test/test_qa_gk.sh
+++ script/test/test_qa_gk.sh
@@ -1,8 +1,8 @@
 CAPYBARA_HOST=https://qa.greatschools.org \
 CAPYBARA_PORT=80 \
 BLACK_BOX=true \
-bundle exec rspec \
+bundle exec rspec ./spec/ \
 --require spec_helper.rb \
 --tag=remote \
---pattern="spec/qa/pages/gk/**{,/*/**}/*_spec.rb"
+
 
