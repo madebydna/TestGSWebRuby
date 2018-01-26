07c67aec5291f0e4240d4a14b3c0822d228fb3da
Alex Thaler <athaler@greatschools.org>
2018-01-25 16:59:23 -0800

    JT-5523 Finish working version of pre-flight checker with refactored grouping and iteration

--- lib/data_loading/pre_flight_checker.rb
+++ lib/data_loading/pre_flight_checker.rb
@@ -10,58 +10,66 @@ require 'csv'
 # Example:  ruby -I '.' -e "require 'pre_flight_checker.rb'; PreFlightChecker.run('sample_source_file.csv', 1,3,5)"
 
 class PreFlightChecker
-  def self.run(source_file, *state_id_columns)
-    new(source_file, state_id_columns).orchestrate_script
+  attr_reader :source_file
+
+  def self.run(source_file, suppression_value=nil, *state_id_columns)
+    new(source_file, supression_value, state_id_columns).orchestrate_script
   end
 
-  def initialize(source_file, state_id_column_array)
+  def initialize(source_file, suppression_value, state_id_column_array)
     @source_file = source_file
     @state_id_column_array = state_id_column_array.map(&:to_i)
+    @suppression_value = suppression_value
   end
 
-  def each_row(&block)
-    CSV.foreach(@source_file, headers: true).with_index do |row, idx|
+  def each_row(file=@source_file, &block)
+    CSV.foreach(file, headers: true).with_index do |row, idx|
       yield(row, idx)
     end
   end
 
-  # def each_row(&block)
-  #   CSV.open(@source_file, headers: true) do |csv|
-  #     csv.each_with_index do |row,idx|
-  #       yield(row, idx)
-  #     end
-  #   end
-  # end
-
-  def id_to_row_mapping
-    # Format of dups hash is state_id => [dup 1 line number, dup 2 line number, etc.]
-    dups = {}
-    each_row do |row, idx|
-      state_id = assemble_id_from_row(row)
-      if dups.has_key? state_id
-        dups[state_id] << idx
-      else
-        dups[state_id] = [idx]
+  def create_copy_with_state_id
+    File.open('source_file_with_state_id.csv', 'w+') do |f|
+      each_row do |row,idx|
+        state_id = assemble_id_from_row(row)
+        row_with_state_id = row.to_s.prepend ("#{state_id.to_s},")
+        f.puts row_with_state_id
       end
     end
-    dups
+    puts 'Created new file with STATE_ID column (\'source_file_with_state_id.csv\'). Sorting file by state id...'
   end
 
-  def process_dups(id_to_row_hash)
-    # iterates through the id-to-row-mapping, finds the matching rows in the csv file and grabs their full row data, then
-    # runs some comparisons on them.
-    compared_dups = []
-    id_to_row_hash.each_value do |index|
-      row_data = []
-        each_row do |row, index2|
-          if index.include?(index2)
-            row_data << [row, index2]
-          end
-        end
-      comparison_hash = compare_dups(row_data)
-      unless comparison_hash.values.all?(&:blank?)
-        compared_dups << comparison_hash
+  def sort_by_state_id
+    sorted_array = source_file_with_state_id.sort_by {|line| line.split(',').first.to_i}
+    #after sorting, add in the headers with state_id column
+    sorted_array.unshift headers.prepend("STATE_ID,")
+    File.open('source_file_with_state_id.csv', 'w+') {|f| sorted_array.each {|row| f.puts row}}
+    puts 'Successfully sorted file. Moving on to dup checks...'
+  end
+
+  def headers
+    CSV.read(@source_file, headers: true).headers.join(',')
+  end
+
+  def source_file_with_state_id
+    @_source_file_with_state_id ||= File.readlines('source_file_with_state_id.csv')
+  end
+
+  def line_count
+    source_file_with_state_id.length - 1
+  end
+
+  def process_dups
+    current_state_id = nil; row_data = []; compared_dups = []
+    each_row('source_file_with_state_id.csv') do |row, idx|
+      #Step through sorted file until state_id changes. If state_id has changed, run comparison on accumulated row data
+      if (id_from(row) != current_state_id || idx >= line_count) && current_state_id
+        comparison_hash = compare_dups(row_data)
+        compared_dups << comparison_hash unless comparison_hash.values.all?(&:blank?)
+        row_data = []
       end
+      row_data << [row, idx] if add_to_row_data?(row, current_state_id)
+      current_state_id = id_from row
     end
     compared_dups
   end
@@ -73,16 +81,29 @@ class PreFlightChecker
     }
   end
 
+  def add_to_row_data?(row, current_id)
+    id_from(row) == current_id || current_id.nil?
+  end
+
+  def id_from(row)
+    row[0]
+  end
+
+# Row_data represents all the rows for a school id. Here's the format: [[row_values, row number], [row_values, row number], etc.]
+
   def find_rows_with_empty_data(row_data)
-    # row_data.select {|line| line[0][0].to_s.gsub(/[->*<%,+.]/,'').strip.empty?}.map(&:last)
-    # row_data is an array in which the first element is the full row and the second element is its line number in the
-    # csv file. We want to strip out the non-alphanumeric characters and check to see if anything is left (i.e. the row is empty).
-    # We add two primarily so the user can more easily find the line (editors start at one and we've removed the headers)
-    row_data.select {|line| line[0][0].to_s.gsub(/[^0-9a-z]/i, '').empty?}.map {|array| array.last + 2}
+    # Remove state_id data and see if there's anything else. Add 2 for readability in a csv viewer/editor
+    indices_for_deletion = @state_id_column_array.map {|num| num + 1}.unshift(0)
+    empty_rows = row_data.select do |row|
+      row.first.to_s.chomp.split(',')
+        .delete_if.with_index {|_,index|indices_for_deletion.include? index}
+        .map {|cell| cell.gsub(/[^0-9a-z]/i, '')}
+        .all? {|val| val.empty? || val == @suppression_value}
+    end
+    empty_rows.map{|array| array.last + 2}
   end
 
   def find_rows_with_identical_data(row_data)
-    # Row data includes the line number of each row.  Add two to each index for easier lookup in a text editor.
     row_data.group_by{ |row| row[0].to_s }.select { |k, v| v.length > 1 }.map {|key,val| val.map {|array| array.last + 2}}
   end
 
@@ -96,12 +117,17 @@ class PreFlightChecker
     response = gets.chomp.downcase until ['y', 'n'].include?(response)
 
     if response == 'y'
-      puts 'Ok, checking for duplicate ids (this may take a while)...'
+      puts 'Getting started (this may take a while)...'
       return
     elsif response == 'n'
       puts "Ok. You can try again with different column numbers or consult pre_flight_checker.rb for examples. Exiting now."
       exit
     end
+  end
+
+  def sort_input_file
+    create_copy_with_state_id
+    sort_by_state_id
   end
 
   def first_five_ids
@@ -112,7 +138,8 @@ class PreFlightChecker
 
   def orchestrate_script
     verify_well_formed_ids
-    puts process_dups(id_to_row_mapping)
+    sort_input_file
+    puts process_dups
   end
 
 end
