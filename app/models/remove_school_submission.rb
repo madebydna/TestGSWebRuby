6e5950c492d5fd74db5a3a275bc2d682cd731731
Alex Thaler <athaler@greatschools.org>
2018-02-07 11:08:54 -0800

    JT-5561 Add basic validations to remove_school_submission

--- app/models/remove_school_submission.rb
+++ app/models/remove_school_submission.rb
@@ -4,11 +4,13 @@ class RemoveSchoolSubmission < ActiveRecord::Base
   self.table_name = 'remove_school_submissions'
   db_magic :connection => :gs_schooldb
 
-  validates :submitter_email, :submitter_role, presence: true
+  validates :submitter_email, length: {maximum: 100 }, presence: true
+  validates :submitter_role, presence: true
+  validates :evidence_url, length: {maximum: 100}, allow_blank: true
   validate :well_formed_gs_url?
 
   def well_formed_gs_url?
-    unless /www.greatschools.org/.match?(gs_url)
+    unless /www.greatschools.org/.match?(gs_url) && gs_url.length <= 100
       errors.add(:greatschools, 'web link is not valid.')
     end
   end
