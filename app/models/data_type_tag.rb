6f6cada610eb95a82f80832b94b8c81240d7b3bf
Alex Thaler <athaler@greatschools.org>
2018-02-22 14:46:37 -0800

    JT-4972 Add back data_type_ids_for(tag_names)

--- app/models/data_type_tag.rb
+++ app/models/data_type_tag.rb
@@ -7,4 +7,8 @@ class DataTypeTag < ActiveRecord::Base
 
   belongs_to :data_type, class_name: 'DataType', foreign_key: :data_type_id
 
+  def self.data_type_ids_for(tag_names)
+    where(tag: tag_names).pluck(:data_type_id)
+  end
+
 end
