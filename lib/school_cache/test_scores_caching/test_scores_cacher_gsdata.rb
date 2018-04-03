79ef32e8e7a4ffcae25e8d07999af1d61c0d00d4
Samson Sprouse <ssprouse@greatschools.org>
2018-04-02 20:09:20 -0700

    JT-5745 Filter to proficiency band 1 for test scores gsdata

--- lib/school_cache/test_scores_caching/test_scores_cacher_gsdata.rb
+++ lib/school_cache/test_scores_caching/test_scores_cacher_gsdata.rb
@@ -34,13 +34,16 @@ class TestScoresCaching::TestScoresCacherGsdata < Cacher
       begin
         DataValue
           .find_by_school_and_data_type_tags(school, data_type_tags)
+          .where(proficiency_band_id: 1)
       end
   end
 
   def state_results_hash
     @_state_results_hash ||= begin
-      state_values = DataValue.find_by_state_and_data_types(school.state,
-                                                            data_type_ids)
+      state_values = DataValue
+        .find_by_state_and_data_types(school.state, data_type_ids)
+        .where(proficiency_band_id: 1)
+
       state_values.each_with_object({}) do |result, hash|
         state_key = result.datatype_breakdown_year
         hash[state_key] = result
@@ -50,9 +53,10 @@ class TestScoresCaching::TestScoresCacherGsdata < Cacher
 
   def district_results_hash
     @_district_results_hash ||= begin
-      district_values = DataValue.find_by_district_and_data_types(school.state,
-                                                                  school.district_id,
-                                                                  data_type_ids)
+      district_values = DataValue
+        .find_by_district_and_data_types(school.state, school.district_id, data_type_ids)
+        .where(proficiency_band_id: 1)
+
       district_values.each_with_object({}) do |result, hash|
         district_key = result.datatype_breakdown_year
         hash[district_key] = result.value
