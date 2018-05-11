b4d8d47695d0d659b9f37716eb831eb98b27791c
Samson Sprouse <ssprouse@greatschools.org>
2018-05-11 08:12:50 -0700

    JT-6286 Upgrade ReactCSSTransitionGroup and fix usage on review form

--- client/app/bundles/GSWeb/react_components/review/form/question.jsx
+++ client/app/bundles/GSWeb/react_components/review/form/question.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import ReactCSSTransitionGroup from 'react-transition-group';
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
 import TextArea from './text_area';
 import { t } from '../../../util/i18n';
 
 export default class Question extends React.Component {
-
   static propTypes = {
     id: PropTypes.number.isRequired,
     questionCounter: PropTypes.number.isRequired,
@@ -24,118 +23,120 @@ export default class Question extends React.Component {
     this.handleTextBlur = this.handleTextBlur.bind(this);
     this.handleTellUsWhyClick = this.handleTellUsWhyClick.bind(this);
     this.state = {
-      shouldDisplayTextArea: (this.props.textValue && this.props.textValue != ''),
-      shouldDisplayTellUsLink: !(this.props.textValue && this.props.textValue != ''),
+      shouldDisplayTextArea: this.props.textValue && this.props.textValue != '',
+      shouldDisplayTellUsLink: !(
+        this.props.textValue && this.props.textValue != ''
+      ),
       textFocus: true
     };
   }
 
   renderLayout() {
-    return(this.props.layout);
+    return this.props.layout;
   }
 
   displayTextArea() {
-    this.setState(
-      {
-        shouldDisplayTellUsLink: false,
-        shouldDisplayTextArea: true
-      });
+    this.setState({
+      shouldDisplayTellUsLink: false,
+      shouldDisplayTextArea: true
+    });
   }
 
   handleTextBlur() {
-    if ((! this.props.textValue || this.props.textValue == '') && this.state.textValue) {
-      this.setState(
-        {
-          shouldDisplayTextArea: false,
-          shouldDisplayTellUsLink: true
-        })
+    if (
+      (!this.props.textValue || this.props.textValue == '') &&
+      this.state.textValue
+    ) {
+      this.setState({
+        shouldDisplayTextArea: false,
+        shouldDisplayTellUsLink: true
+      });
     }
   }
 
   renderErrorMessage() {
-    return(
-      <div className="error-message">
-        { this.props.errorMessage }
-      </div>
-    );
+    return <div className="error-message">{this.props.errorMessage}</div>;
   }
 
   handleTellUsWhyClick() {
-   this.setState({ textFocus: true})
-   this.displayTextArea();
+    this.setState({ textFocus: true });
+    this.displayTextArea();
   }
 
   renderTextArea() {
-    return(<TextArea
-      questionId = {this.props.id}
-      onTextValueChanged = {this.props.textValueChanged}
-      handleTextBlur = { this.handleTextBlur }
-      autoFocus = { this.state.textFocus }
-      errorMessage = { this.props.errorMessage }
-      textValue = { this.props.textValue }
-      placeholder = 'Tell us why...'
-    />);
-    this.setState({ textFocus: false});
+    return (
+      <TextArea
+        questionId={this.props.id}
+        onTextValueChanged={this.props.textValueChanged}
+        handleTextBlur={this.handleTextBlur}
+        autoFocus={this.state.textFocus}
+        errorMessage={this.props.errorMessage}
+        textValue={this.props.textValue}
+        placeholder="Tell us why..."
+      />
+    );
   }
 
   handleTextBoxChange(event) {
-    this.setState({textValue: event.target.value})
-    this.props.textValueChanged(event.target.value, this.props.id)
+    this.setState({ textValue: event.target.value });
+    this.props.textValueChanged(event.target.value, this.props.id);
   }
 
   renderTellUsLink() {
-    return(
+    return (
       <div className="tell-us-link" onClick={this.handleTellUsWhyClick}>
-        <span className="icon-pencil"></span>
+        <span className="icon-pencil" />
         {t('Tell us why')}&hellip;
-       </div>
+      </div>
     );
   }
 
   renderTellUsWhy() {
-    return(
-      <div className="tell-us-why">
-        <div className="tell-us-text">
-          <ReactCSSTransitionGroup
-            transitionName="textarea"
-            transitionEnterTimeout={800}
-            transitionLeaveTimeout={100}>
-            {this.renderTextArea() }
-            <div className="required-or-optional">Optional</div>
-          </ReactCSSTransitionGroup>
+    return (
+      <CSSTransition classNames="textarea" timeout={400}>
+        <div className="tell-us-why">
+          <div className="tell-us-text">
+            <CSSTransition classNames="textarea" timeout={800}>
+              <React.Fragment>
+                {this.renderTextArea()}
+                <div className="required-or-optional">Optional</div>
+              </React.Fragment>
+            </CSSTransition>
+          </div>
         </div>
-      </div>
+      </CSSTransition>
     );
   }
 
   renderSubtext() {
-    return (
-      <div className="subtext">
-        { this.props.subtext }
-      </div>
-    );
+    return <div className="subtext">{this.props.subtext}</div>;
   }
 
   render() {
     return (
-      <div className={'review-question clearfix ' + 'question_' + this.props.id}>
+      <div
+        className={`${'review-question clearfix ' + 'question_'}${
+          this.props.id
+        }`}
+      >
         <div>
-          <div className="review-counter"><span>{ this.props.questionCounter }</span></div>
+          <div className="review-counter">
+            <span>{this.props.questionCounter}</span>
+          </div>
         </div>
         <div>
           <div>
-            <div dangerouslySetInnerHTML={{__html: this.props.title }} />
-            { this.props.subtext ? this.renderSubtext() : null }
+            <div dangerouslySetInnerHTML={{ __html: this.props.title }} />
+            {this.props.subtext ? this.renderSubtext() : null}
           </div>
-          { this.renderLayout() }
-          <ReactCSSTransitionGroup
-            transitionName="textarea"
-            transitionEnterTimeout={400}
-            transitionLeaveTimeout={400}>
-            { (this.props.value && this.props.shouldDisplayTextArea) ? this.renderTellUsWhy() : null }
-          </ReactCSSTransitionGroup>
+          {this.renderLayout()}
+          <TransitionGroup>
+            {this.props.value && this.props.shouldDisplayTextArea
+              ? this.renderTellUsWhy()
+              : null}
+          </TransitionGroup>
         </div>
       </div>
-    )
+    );
   }
 }
