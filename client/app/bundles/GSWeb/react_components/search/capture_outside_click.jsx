112d319f9a58a59a013b8e4b8721e37a782b60bf
Samson Sprouse <ssprouse@greatschools.org>
2018-05-15 10:32:13 -0700

    JT-6027 add comment

--- client/app/bundles/GSWeb/react_components/search/capture_outside_click.jsx
+++ client/app/bundles/GSWeb/react_components/search/capture_outside_click.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+// The purpose of this is to keep track of a dom reference on mount,
+// and fire a callback if a click event occurs anywhere outside of the
+// childrens' dom tree. Used for things like clicking a dropdown
+// menu when user clicks outside of it.
 export default class CaptureOutsideClick extends React.Component {
   static propTypes = {
     callback: PropTypes.func.isRequired,
