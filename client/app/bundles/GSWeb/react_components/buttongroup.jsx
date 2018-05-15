b252babc04d342dd5489ed780ec4aa17e85f425e
Samson Sprouse <ssprouse@greatschools.org>
2018-05-15 13:20:16 -0700

    JT-6027 Make buttongroup a functional component

--- client/app/bundles/GSWeb/react_components/buttongroup.jsx
+++ client/app/bundles/GSWeb/react_components/buttongroup.jsx
@@ -2,44 +2,44 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Selectable from './selectable';
 
-export default class ButtonGroup extends React.Component {
-  static propTypes = {
-    options: PropTypes.object.isRequired,
-    onSelect: PropTypes.func.isRequired,
-    activeOption: PropTypes.string
-  };
+const ButtonGroup = ({ options, onSelect, activeOption }) => {
+  const newOpts = Object.keys(options).map(key => ({
+    key,
+    value: key,
+    label: options[key]
+  }));
 
-  render() {
-    const newOpts = Object.keys(this.props.options).map(key => ({
-      key,
-      value: key,
-      label: this.props.options[key]
-    }));
+  return (
+    <span className="button-group">
+      <Selectable
+        options={newOpts}
+        activeOptions={[activeOption]}
+        onSelect={key => onSelect(key)}
+        className="button-group"
+        keyFunc={o => o.key}
+      >
+        {opts =>
+          opts.map(({ option, active, select }) => (
+            <label
+              key={option.key}
+              className={active ? 'active' : ''}
+              onClick={select}
+              onKeyPress={select}
+              role="button"
+            >
+              {option.label}
+            </label>
+          ))
+        }
+      </Selectable>
+    </span>
+  );
+};
 
-    return (
-      <span className="button-group">
-        <Selectable
-          options={newOpts}
-          activeOptions={[this.props.activeOption]}
-          onSelect={key => this.props.onSelect(key)}
-          className="button-group"
-          keyFunc={o => o.key}
-        >
-          {opts =>
-            opts.map(({ option, active, select }) => (
-              <label
-                key={option.key}
-                className={active ? 'active' : ''}
-                onClick={select}
-                onKeyPress={select}
-                role="button"
-              >
-                {option.label}
-              </label>
-            ))
-          }
-        </Selectable>
-      </span>
-    );
-  }
-}
+ButtonGroup.propTypes = {
+  options: PropTypes.object.isRequired,
+  onSelect: PropTypes.func.isRequired,
+  activeOption: PropTypes.string.isRequired
+};
+
+export default ButtonGroup;
