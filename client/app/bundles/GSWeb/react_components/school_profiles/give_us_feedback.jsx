363175b2dd56238261e80d7ae1789c873dd6ba95
Samson Sprouse <ssprouse@greatschools.org>
2018-05-15 18:32:30 -0700

    JT-6300 Clean up more propType warnings

--- client/app/bundles/GSWeb/react_components/school_profiles/give_us_feedback.jsx
+++ client/app/bundles/GSWeb/react_components/school_profiles/give_us_feedback.jsx
@@ -3,25 +3,39 @@ import PropTypes from 'prop-types';
 import { t } from '../../util/i18n';
 import { qualarooLink } from 'util/qualaroo';
 
-const GiveUsFeedback = ({className='', content, module=null, divider=true, children}) => {
-  if(module) {
-    content = qualarooLink(module)
+const GiveUsFeedback = ({ className, content, module, divider }) => {
+  if (module) {
+    content = qualarooLink(module);
   }
-  let qualaroo_yes_url = content + '&a=0';
-  let qualaroo_no_url = content + '&a=1';
+  const qualaroo_yes_url = `${content}&a=0`;
+  const qualaroo_no_url = `${content}&a=1`;
   return (
     <div className="module_feedback">
       {t('was_this_useful')}&nbsp;
       <span>
-        <a href={qualaroo_yes_url} className='anchor-button' target='_blank'>{t('yes')}</a>
-        <a href={qualaroo_no_url} className='anchor-button' target='_blank'>{t('no')}</a>
+        <a href={qualaroo_yes_url} className="anchor-button" target="_blank">
+          {t('yes')}
+        </a>
+        <a href={qualaroo_no_url} className="anchor-button" target="_blank">
+          {t('no')}
+        </a>
       </span>
     </div>
   );
 };
 
 GiveUsFeedback.propTypes = {
-  content: PropTypes.string.isRequired
-}
+  className: PropTypes.string,
+  content: PropTypes.string,
+  module: PropTypes.string,
+  divider: PropTypes.bool
+};
+
+GiveUsFeedback.defaultProps = {
+  className: '',
+  content: undefined,
+  module: null,
+  divider: true
+};
 
 export default GiveUsFeedback;
--- client/app/bundles/GSWeb/react_components/school_profiles/module_sub_tab.jsx
+++ client/app/bundles/GSWeb/react_components/school_profiles/module_sub_tab.jsx
@@ -1,31 +1,50 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const ModuleSubTab = ({title, google_tracking=title, flagged = false, highlight = false, anchorLink='', onClick}) => {
+const ModuleSubTab = ({
+  title,
+  google_tracking = title,
+  flagged,
+  highlight,
+  anchorLink,
+  onClick
+}) => {
   let addJSHashUpdate = '';
-  if(anchorLink.length > 0){
+  if (anchorLink.length > 0) {
     addJSHashUpdate = ' js-updateLocationHash';
   }
   return (
-    <a href="javascript:void(0)"
+    <a
+      href="javascript:void(0)"
       data-anchor={anchorLink}
       onClick={onClick}
-      className={'sub-nav-item js-gaClick' + addJSHashUpdate + (highlight ? ' sub-tab-selected' : '')}
-      data-ga-click-category='Profile'
-      data-ga-click-action={'Equity ' + google_tracking +' Tabs'}
-      data-ga-click-label={title}>
+      className={`sub-nav-item js-gaClick${addJSHashUpdate}${
+        highlight ? ' sub-tab-selected' : ''
+      }`}
+      data-ga-click-category="Profile"
+      data-ga-click-action={`Equity ${google_tracking} Tabs`}
+      data-ga-click-label={title}
+    >
       {title}
-      {flagged && <span className="red icon-flag"/>}
+      {flagged && <span className="red icon-flag" />}
     </a>
-  )
+  );
 };
 
 ModuleSubTab.propTypes = {
   title: PropTypes.string.isRequired,
-  google_tracking: PropTypes.string.isRequired,
+  google_tracking: PropTypes.string,
   flagged: PropTypes.bool,
   anchorLink: PropTypes.string,
-  onClick: PropTypes.func
-}
+  onClick: PropTypes.func,
+  highlight: PropTypes.bool
+};
+
+ModuleSubTab.defaultProps = {
+  highlight: false,
+  anchorLink: '',
+  flagged: false,
+  onClick: undefined
+};
 
 export default ModuleSubTab;
