eba875f7557be247ede70b43e3c08ac2dfbdc77e
Samson Sprouse <ssprouse@greatschools.org>
2018-05-16 10:05:41 -0700

    JT-6219 Add react tests for school component

--- client/app/bundles/GSWeb/react_components/search/school.jsx
+++ client/app/bundles/GSWeb/react_components/search/school.jsx
@@ -1,35 +1,98 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { capitalize } from 'util/i18n';
 
-const renderRating = function(rating) {
-  let className = 'circle-rating--small circle-rating--' + rating;
-  return <div className={className}>
-    {rating}
-    <span className="rating-circle-small">/10</span>
-  </div>;
-}
+const renderRating = rating => {
+  const className = `circle-rating--small circle-rating--${rating}`;
+  return (
+    <div className={className}>
+      {rating}
+      <span className="rating-circle-small">/10</span>
+    </div>
+  );
+};
 
-const homesForSaleHref = function(state, address) {
-  let homesForSaleHref = null;
-  homesForSaleHref = 'https://www.zillow.com/' + state + '-' + address.zip.split("-")[0] + '?cbpartner=Great+Schools&utm_source=GreatSchools&utm_medium=referral&utm_campaign=districtbrowsemap';
-  return homesForSaleHref;
-}
+const getHomesForSaleHref = (state, address) => {
+  if (state && address && address.zip) {
+    let homesForSaleHref = null;
+    homesForSaleHref = `https://www.zillow.com/${state}-${
+      address.zip.split('-')[0]
+    }?cbpartner=Great+Schools&utm_source=GreatSchools&utm_medium=referral&utm_campaign=districtbrowsemap`;
+    return homesForSaleHref;
+  }
+  return null;
+};
 
-const School = ({id, state, name, address={}, city, schoolType, gradeLevels, enrollment=null, rating=null, active=false, links={}} = {}) => {
+const studentsPhrase = enrollment => {
+  if (enrollment > 1) {
+    return `${enrollment} students`;
+  }
+  return `${enrollment} student`;
+};
+
+const School = ({
+  id,
+  state,
+  name,
+  address = {},
+  schoolType,
+  gradeLevels,
+  enrollment = null,
+  rating = null,
+  active = false,
+  links
+}) => {
+  const homesForSaleHref = getHomesForSaleHref(state, address);
+  let addressPhrase = [address.street1, address.city, state, address.zip]
+    .filter(s => !!s && s.length > 0)
+    .join(', ');
+  if (!address.city || !state) {
+    addressPhrase = null;
+  }
+
   return (
     <React.Fragment key={state + id}>
-      { rating && <span>{renderRating(rating)}</span> }
+      {rating && <span>{renderRating(rating)}</span>}
       <span>
-        <a href={links.profile} className="name" target="_blank">{name}</a>
-        <br/>
-        <div>{address.street1}, {address.city}, {state}, {address.zip}</div>
-        <div>{capitalize(schoolType)}, {gradeLevels} | {enrollment} students</div>
-        <div className="icon active icon-house">
-          <a href={homesForSaleHref(state, address)} target="_blank">Homes for sale</a>
+        <a href={links.profile} className="name" target="_blank">
+          {name}
+        </a>
+        <br />
+        {addressPhrase && <div className="address">{addressPhrase}</div>}
+        <div>
+          {capitalize(schoolType)}, {gradeLevels} | {studentsPhrase(enrollment)}
         </div>
+        {homesForSaleHref && (
+          <div className="icon active icon-house">
+            <a href={homesForSaleHref} target="_blank">
+              Homes for sale
+            </a>
+          </div>
+        )}
       </span>
     </React.Fragment>
   );
-}
+};
+
+School.propTypes = {
+  id: PropTypes.number.isRequired,
+  state: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  address: PropTypes.shape({}).isRequired,
+  schoolType: PropTypes.oneOf(['public', 'private', 'charter']).isRequired,
+  gradeLevels: PropTypes.string.isRequired,
+  enrollment: PropTypes.number,
+  rating: PropTypes.number,
+  active: PropTypes.bool,
+  links: PropTypes.shape({
+    profile: PropTypes.string.isRequired
+  }).isRequired
+};
+
+School.defaultProps = {
+  enrollment: null,
+  rating: null,
+  active: false
+};
 
 export default School;
---
+++ client/tests/react_components/search/school_spec.jsx
@@ -0,0 +1,82 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import sinon from 'sinon';
+import { expect } from 'chai';
+import { describe, it } from 'mocha';
+
+import School from 'react_components/search/school';
+
+describe('<School />', () => {
+  it('renders the school name', () => {
+    const wrapper = shallow(<School name="A High School" />);
+    expect(wrapper.text()).to.contain('A High School');
+  });
+
+  it('links to school profile', () => {
+    const wrapper = shallow(
+      <School name="A High School" links={{ profile: 'foo' }} />
+    );
+    const link = wrapper.find({ href: 'foo' });
+    expect(link.text()).to.contain('A High School');
+  });
+
+  it('Correctly formats number of students', () => {
+    let wrapper = shallow(<School enrollment={2} />);
+    expect(wrapper.text()).to.match(/.*\b2 students.*/);
+    wrapper = shallow(<School enrollment={1} />);
+    expect(wrapper.text()).to.match(/.*1 student\b.*/);
+  });
+
+  describe('Address', () => {
+    let state = 'CA';
+    let address = {};
+    const school = () => shallow(<School state={state} address={address} />);
+    const addressDiv = () => school().find('.address');
+    const hasAddress = () => addressDiv().length > 0;
+    const renderedAddress = () => addressDiv().text();
+
+    it('Correctly formats standard address', () => {
+      address = {
+        street1: '1999 Harrison st',
+        city: 'Oakland',
+        zip: '94612'
+      };
+      expect(renderedAddress()).to.contain(
+        '1999 Harrison st, Oakland, CA, 94612'
+      );
+    });
+
+    it('Correctly formats address with blank street1', () => {
+      address = {
+        street1: '',
+        city: 'Oakland',
+        zip: '94612'
+      };
+      expect(renderedAddress()).to.eq('Oakland, CA, 94612');
+    });
+
+    it('Correctly formats address with blank street and zipcode', () => {
+      address = {
+        street1: '',
+        city: 'Oakland',
+        zip: ''
+      };
+      expect(renderedAddress()).to.eq('Oakland, CA');
+    });
+
+    it('Omits address if missing city or state', () => {
+      address = {
+        street1: '123 main st',
+        zip: '94612'
+      };
+      expect(hasAddress()).to.eq(false);
+      address = {
+        street1: '123 main st',
+        city: 'Oakland',
+        zip: '94612'
+      };
+      state = null;
+      expect(hasAddress()).to.eq(false);
+    });
+  });
+});
