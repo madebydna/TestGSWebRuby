04bc2d27e7d5eeb8dac0bb67b6fdedff399cb6b1
Samson Sprouse <ssprouse@greatschools.org>
2018-05-17 08:41:12 -0700

    JT-6206 Move search list markup into its own component

---
+++ client/app/bundles/GSWeb/react_components/search/school_list.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import SpinnyOverlay from '../spinny_overlay';
+import School from './school';
+
+const SchoolList = ({ schools, isLoading, pagination }) => (
+  <SpinnyOverlay spin={isLoading}>
+    {({ createContainer, spinny }) =>
+      createContainer(
+        <section className="school-list">
+          {spinny}
+          <ol>
+            {schools.map(s => (
+              <li key={s.state + s.id} className={s.active ? 'active' : ''}>
+                <School {...s} />
+              </li>
+            ))}
+            {pagination && (
+              <li>
+                <div className="pagination-buttons button-group">
+                  {pagination}
+                </div>
+              </li>
+            )}
+          </ol>
+        </section>
+      )
+    }
+  </SpinnyOverlay>
+);
+
+SchoolList.propTypes = {
+  schools: PropTypes.arrayOf(School.propTypes).isRequired,
+  isLoading: PropTypes.bool,
+  pagination: PropTypes.element
+};
+SchoolList.defaultProps = {
+  isLoading: false,
+  pagination: null
+};
+export default SchoolList;
--- client/app/bundles/GSWeb/react_components/search/search.jsx
+++ client/app/bundles/GSWeb/react_components/search/search.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { validSizes as validViewportSizes } from 'util/viewport';
-import SpinnyOverlay from '../spinny_overlay';
 import FilterBar from './filter_bar';
 import SearchContext from './search_context';
 import School from './school';
@@ -10,6 +9,7 @@ import SearchLayout from './search_layout';
 import ListMapDropdown from './list_map_dropdown';
 import PaginationButtons from './pagination_buttons';
 import Map from './map';
+import SchoolList from './school_list';
 
 class Search extends React.Component {
   static defaultProps = {
@@ -64,42 +64,21 @@ class Search extends React.Component {
         )}
         renderAd={() => <div className="ad-bar">Advertisement</div>}
         renderList={() => (
-          <React.Fragment>
-            <SpinnyOverlay spin={this.props.loadingSchools}>
-              {({ createContainer, spinny }) =>
-                createContainer(
-                  <section className="school-list">
-                    {spinny}
-                    <ol>
-                      {this.props.schools.map(s => (
-                        <li
-                          key={s.state + s.id}
-                          className={s.active ? 'active' : ''}
-                        >
-                          <School {...s} />
-                        </li>
-                      ))}
-                      {this.props.totalPages > 1 && (
-                        <li>
-                          <div className="pagination-buttons button-group">
-                            <PaginationButtons
-                              page={this.props.page}
-                              totalPages={this.props.totalPages}
-                              onPageChanged={this.props.onPageChanged}
-                            />
-                          </div>
-                        </li>
-                      )}
-                    </ol>
-                  </section>
-                )
-              }
-            </SpinnyOverlay>
-          </React.Fragment>
+          <SchoolList
+            schools={this.props.schools}
+            pagination={
+              this.props.totalPages > 1 ? (
+                <PaginationButtons
+                  page={this.props.page}
+                  totalPages={this.props.totalPages}
+                  onPageChanged={this.props.onPageChanged}
+                />
+              ) : null
+            }
+          />
         )}
         renderMap={() => (
           <Map
-            school={this.props.school}
             schools={this.props.schools}
             isLoading={this.props.loadingSchools}
           />
