a579d2375d0869b7aa755d97c7d40b6749703cfc
Katherine Pannell <kpannell@greatschools.org>
2018-05-23 13:25:20 -0700

    testing apostrophe change to source name

--- lib/etl/destinations/output_lib/test_score_queue_daemon_json_blob.rb
+++ lib/etl/destinations/output_lib/test_score_queue_daemon_json_blob.rb
@@ -47,7 +47,7 @@ class TestScoreQueueDaemonJsonBlob
 
   def source_hash
     {
-        source_name: @row[:source_name] || @source[:source_name].gsub("'","\\\\\\\\'"),
+        source_name: @row[:source_name] || @source[:source_name].gsub("'","\\\\\\\'"),
         date_valid: @row[:date_valid] || @source[:date_valid],
         notes: @row[:notes] || @source[:notes],
         description: @row[:description] || @source[:description]
