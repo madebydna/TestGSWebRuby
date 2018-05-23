fe6844a497c6268be6550e5e78f86241fc44a2f0
Katherine Pannell <kpannell@greatschools.org>
2018-05-23 14:08:29 -0700

    testing apostrophe change to source name

--- lib/etl/destinations/output_lib/queue_daemon_insert_statement.rb
+++ lib/etl/destinations/output_lib/queue_daemon_insert_statement.rb
@@ -2,7 +2,7 @@
 
 module QueueDaemonInsertStatement
   def self.build(source, blob, priority=4)
-   "insert into gs_schooldb.update_queue (source,status,priority,update_blob,created,updated) values (#{source},'todo',#{priority},#{blob},NOW(),NOW());\n";
+   "insert into gs_schooldb.update_queue (source,status,priority,update_blob,created,updated) values (#{source.gsub(/'/) {|x| "\\#{x}"}},'todo',#{priority},#{blob},NOW(),NOW());\n";
   end
 end
 
