212e172bdca8fb7633e7067406d7a95cb706eb99
Samson Sprouse <ssprouse@greatschools.org>
2018-05-24 19:09:05 -0700

    JT-6212 Add load transition to school list. Remove spinny for now

--- client/app/bundles/GSWeb/react_components/search/school_list.jsx
+++ client/app/bundles/GSWeb/react_components/search/school_list.jsx
@@ -2,18 +2,21 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import SpinnyOverlay from '../spinny_overlay';
 import School from './school';
+import { CSSTransition } from 'react-transition-group';
 
 const SchoolList = ({ schools, isLoading, pagination }) => (
   <SpinnyOverlay spin={isLoading}>
     {({ createContainer, spinny }) =>
       createContainer(
-        <section className="school-list">
-          {spinny}
+        <section className={`school-list ${isLoading ? 'loading' : ''}`}>
+          {/* spinny */}
           <ol>
             {schools.map(s => (
-              <li key={s.state + s.id} className={s.active ? 'active' : ''}>
-                <School {...s} />
-              </li>
+              <CSSTransition classNames="school-list" in={!isLoading}>
+                <li key={s.state + s.id} className={s.active ? 'active' : ''}>
+                  <School {...s} />
+                </li>
+              </CSSTransition>
             ))}
             {pagination && (
               <li>
