bedd97ca2f52bc0c8288e1b2eb08bdc576e6d745
Samson Sprouse <ssprouse@greatschools.org>
2018-06-11 17:25:58 -0700

    JT-6378 code review feedback -- make specs test correct thing

--- spec/requests/new_search_page_requests_spec.rb
+++ spec/requests/new_search_page_requests_spec.rb
@@ -5,20 +5,28 @@ require 'spec_helper'
 describe 'New search page' do
   context 'with no solr results' do
     before do
-      stub_request(:post, /\/main\/select/).
-        to_return(status: 200, body: {response: {docs: []}}.to_json, headers: {})
+      stub_request(:any, /\/main\/select/)
+        .to_return(status: 200, body: {response: {docs: [], numFound: 0}}.to_json, headers: {})
+
+      FactoryGirl.create(:city, name: 'Alameda', state: 'ca')
+      FactoryGirl.create(:district, name: 'Alameda Unified School District', city: 'Alameda', state: 'ca')
+    end
+
+    after do
+      clean_models(City, District)
     end
 
     [
       ['/search/search.page?newsearch&q=foo', true],
       ['/search/search.page?newsearch&lat=1&lon=2', true],
       ['/search/search.page?newsearch&lat=1&lon=2&radius=3', true],
-      ['/search/california/alameda/?newsearch', false],
-      ['/search/california/alameda/?newsearch&q=bay', false],
-      ['/search/california/alameda/alameda-unified-school-district/?newsearch&q=bay', false]
+      ['/california/alameda/schools/?newsearch', false],
+      ['/california/alameda/schools/?newsearch&q=bay', false],
+      ['/california/alameda/alameda-unified-school-district/schools/?newsearch&q=bay', false]
     ].each do |path, should_write_noindex|
       it "the uri: #{path} should #{should_write_noindex ? '' : 'not'} include noindex tag" do
         get path
+        expect(response.status).to eq(200)
         if should_write_noindex
           expect(response.body).to include('<meta name="robots" content="noindex, nofollow" />')
         else
