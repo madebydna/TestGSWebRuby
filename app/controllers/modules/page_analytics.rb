7eb19f4bb40aa5b52ec8aa18466af021b3125d9c
Samson Sprouse <ssprouse@greatschools.org>
2018-06-18 17:22:11 -0700

    Revert "JT-6308 Add cookie for tracking when user arrives at search from autocomplete"
    This reverts commit 13c0885141912b74493c65855e6864b5655ffdcb.

--- app/controllers/modules/page_analytics.rb
+++ app/controllers/modules/page_analytics.rb
@@ -5,20 +5,10 @@ module PageAnalytics
   SEARCH_TERM = 'search_term'
   SEARCH_TYPE = 'search_type'
   SEARCH_HAS_RESULTS = 'search_has_results'
-  LINKED_FROM = 'linked_from'
 
   def set_page_analytics_data
-    hash = {
-      LINKED_FROM => read_and_unset_linked_from_cookie
-    }
-    page_specific_hash = page_analytics_data
-    data_layer_gon_hash.merge!(hash.merge(page_specific_hash))
-  end
-
-  def read_and_unset_from_cookie
-    val = cookies[:_ga_linked_from]
-    cookies.delete(:_ga_linked_from)
-    return val
+    hash = page_analytics_data
+    data_layer_gon_hash.merge!(hash)
   end
 
   # Should be overridden in controller
