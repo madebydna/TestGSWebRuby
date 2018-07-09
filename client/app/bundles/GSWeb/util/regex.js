d407458dd5853ab1ecd67042687fd86b28468d8a
Samson Sprouse <ssprouse@greatschools.org>
2018-07-09 15:32:43 -0700

    Temporary fix for search term bolding bug

--- client/app/bundles/GSWeb/util/regex.js
+++ client/app/bundles/GSWeb/util/regex.js
@@ -1,15 +1,7 @@
-
-const escapeRegexChars = (str) => {
-  return str.replace(/([-()\[\]{}+?*.$\^|,:#<!\\])/g, '\\$1').
-  replace(/\x08/g, '\\x08');
-}
-
-const everythingButHTML = (str) => {
-  let match = str.match(/(?<!<[^>]*).*/)
-  return (match ? match[0] : undefined)
-}
-
-export {
-  escapeRegexChars,
-  everythingButHTML
-}
+const escapeRegexChars = str => str;
+// return str.replace(/([-()\[\]{}+?*.$\^|,:#<!\\])/g, '\\$1').
+// replace(/\x08/g, '\\x08');
+const everythingButHTML = str => str;
+// let match = str.match(/(?<!<[^>]*).*/)
+// return (match ? match[0] : undefined)
+export { escapeRegexChars, everythingButHTML };
