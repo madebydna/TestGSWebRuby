21b55ca2cc684d04fea2bff3652b1fc7ec2d73ec
Samson Sprouse <ssprouse@greatschools.org>
2018-07-10 21:50:11 -0700

    JT-5930 clean up some styling and fix background color on table

--- client/app/bundles/GSWeb/react_components/search/search_layout.jsx
+++ client/app/bundles/GSWeb/react_components/search/search_layout.jsx
@@ -170,7 +170,7 @@ class SearchLayout extends React.Component {
     return (
       <div className="menu-bar filters" ref={this.header}>
         {this.props.searchBox}
-        <div style={{ margin: 'auto', padding: '0 10px' }}>
+        <div style={{ margin: 'auto' }}>
           <span className="menu-item">{this.props.entityTypeDropdown}</span>
           <span className="menu-item">{this.props.gradeLevelButtons}</span>
           {this.props.distanceFilter ? (
