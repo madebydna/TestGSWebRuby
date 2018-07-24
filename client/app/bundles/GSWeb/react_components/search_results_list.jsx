5f29fecb50cb93bf4ea83a1b60ae319f79d0505e
Samson Sprouse <ssprouse@greatschools.org>
2018-07-24 16:14:58 -0700

    JT-6725 Attempt to fix issues with autosuggest rendering

--- client/app/bundles/GSWeb/react_components/search_results_list.jsx
+++ client/app/bundles/GSWeb/react_components/search_results_list.jsx
@@ -9,33 +9,42 @@ const boldSearchTerms = (string, substring) => {
   // string when fed a group-capturing regex (compare 'Some string'.split(' '), which returns ['some','string'], not ['some',' ','string']
   const matchesAndNonMatches = string.split(
     new RegExp(`\\b(${tokens.join('|')})`, 'gi')
-  )
+  );
   return matchesAndNonMatches.map(token => {
     const queryContainsToken = tokens.find(
       item => item.toLowerCase() === token.toLowerCase()
     );
     if (queryContainsToken) {
-      return <span key={token} className="match">{token}</span>;
+      return (
+        <span key={token} className="match">
+          {token}
+        </span>
+      );
     }
     return token;
-  })
-}
+  });
+};
 
 // This component is responsible for formatting and rendering a payload of search results (listGroups) into a dropdown.
 // Noteworthy behavior: 1) within the title of each listItem, it will bold substrings that match the searchTerm,
 // 2) onclick, it will invoke the onSelect callback if a listItem does not have a url. In the current implementation, SearchBox
 // houses the callback, and updates the value of the input with the value of the listItem, then submits a search. 3) if a list item
 // is selected and the user hits the return key, that event is handled by the search box.
-const SearchResultsList = ({selectedListItem, navigateToSelectedListItem, onSelect, listGroups, searchTerm}) => {
-  const groupNameListItem = (name) => {
-      return (<li key={`category ${name}`} className="search-results-list-group-name">
-        {name[0].toUpperCase() + name.slice(1)}
-      </li>)
-  }
+const SearchResultsList = ({
+  selectedListItem,
+  onSelect,
+  listGroups,
+  searchTerm
+}) => {
+  const groupNameListItem = name => (
+    <li key={`category ${name}`} className="search-results-list-group-name">
+      {name[0].toUpperCase() + name.slice(1)}
+    </li>
+  );
 
-  const groupListItems = (listItems,order) => {
-    return listItems.map((listItem) => {
-      order.counter += 1
+  const groupListItems = (listItems, order) =>
+    listItems.map(listItem => {
+      order.counter += 1;
       return (
         <li
           onClick={listItem.url ? () => {} : () => onSelect(listItem)}
@@ -51,43 +60,36 @@ const SearchResultsList = ({selectedListItem, navigateToSelectedListItem, onSele
         </li>
       );
     }, this);
-  }
 
   // The last <li> is always an option to do a full search using the current search term (i.e. without clicking the search icon)
-  const allResultsListItem = () => {
-    return (
-      <li
-        className="search-results-list-item"
-        onClick={() => onSelect({value: searchTerm})}
-      >
-        {`Search all results for "${searchTerm}"`}
-      </li>
-    );
-  }
+  const allResultsListItem = () => (
+    <li
+      className="search-results-list-item"
+      onClick={() => onSelect({ value: searchTerm })}
+    >
+      {`Search all results for "${searchTerm}"`}
+    </li>
+  );
 
   const renderList = () => {
-    let order = {counter: -1}
+    const order = { counter: -1 };
     return Object.keys(listGroups)
-    .filter(k => listGroups[k] && listGroups[k].length > 0)
-    .map(
-      group => (
-        <React.Fragment key={`fragment ${listGroups[group]}`}>
+      .filter(k => listGroups[k] && listGroups[k].length > 0)
+      .map(group => (
+        <React.Fragment key={group}>
           {groupNameListItem(group)}
-          {groupListItems(listGroups[group],order)}
+          {groupListItems(listGroups[group], order)}
         </React.Fragment>
-      )
-    )
-  }
+      ));
+  };
 
   return (
     <ul>
       {renderList()}
       {allResultsListItem()}
     </ul>
-  )
-    
-  
-}
+  );
+};
 
 SearchResultsList.propTypes = {
   onSelect: PropTypes.func.isRequired,
