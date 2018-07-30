5a13394723cdfa5cd71a811e807c699562f0df5b
Samson Sprouse <ssprouse@greatschools.org>
2018-07-30 08:23:10 -0700

    JT-6217 Update webpack to v4.
    - Update webpack
    - Update webpack plugins
    - Split out the code that inits jQuery and dependencies needed
      for inline scripts / GTM into separate bundle. jQuery and
      jQuery plugins remain in their own bundle.
    - Update page layouts to use the new/correct bundles

--- client/app/bundles/GSWeb/util/advertising.js
+++ client/app/bundles/GSWeb/util/advertising.js
@@ -1,7 +1,3 @@
-import {
-  onAdFilled as onMobileOverlayAdFilled,
-  onAdNotFilled as onMobileOverlayAdNotFilled
-} from 'components/ads/mobile_overlay';
 import log from 'util/log';
 import { remove } from 'lodash';
 
--- client/webpack.config.babel.js
+++ client/webpack.config.babel.js
@@ -6,6 +6,7 @@ import AssetMapPlugin from 'asset-map-webpack-plugin';
 
 const webpack = require('webpack');
 const path = require('path');
+const fs = require('fs');
 
 const devBuild = process.env.NODE_ENV !== 'production';
 const nodeEnv = devBuild ? 'development' : 'production';
@@ -16,42 +17,74 @@ const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
 const config = {
   entry: {
-    'commons-blocking': [
-      'jquery',
-      'jquery-ujs',
-      'jquery.cookie',
-      './app/bundles/GSWeb/misc_all_page_blocking'
-    ],
-    commons: [
-      './app/bundles/GSWeb/vendor/tipso',
-      './app/bundles/GSWeb/vendor/remodal',
-      './app/bundles/GSWeb/header'
-    ],
-    'react-redux': [
-      './app/bundles/GSWeb/polyfills',
-      'react',
-      'react-dom',
-      'redux',
-      'react-redux',
-      'react-transition-group'
-    ],
     widget: ['./app/bundles/GSWeb/widget'],
     'mobile-overlay-ad': ['./app/bundles/GSWeb/components/ads/mobile_overlay'],
     interstitial: ['./app/bundles/GSWeb/interstitial'],
     'district-boundaries': ['./app/bundles/GSWeb/district_boundaries'],
     'school-profiles': ['./app/bundles/GSWeb/school_profiles'],
     home: ['./app/bundles/GSWeb/home'],
-    jquery: ['jquery'],
+    'commons-blocking-loader': ['./app/bundles/GSWeb/misc_all_page_blocking'],
+    'jquery-loader': ['jquery'],
     'admin-tools': ['./app/bundles/GSWeb/admin_tools'],
     'add-schools': ['./app/bundles/GSWeb/pages/add_schools'],
     search: ['./app/bundles/GSWeb/search'],
     'search-box': ['./app/bundles/GSWeb/react_components/search_box_wrapper']
   },
 
+  optimization: {
+    splitChunks: {
+      chunks: 'async',
+      minSize: 999999, // these all just prevent splitChunks from dynamically making chunk bundles for us
+      maxSize: 0,
+      minChunks: 1999,
+      maxAsyncRequests: 5,
+      maxInitialRequests: 3,
+      automaticNameDelimiter: '~',
+      name: true,
+      cacheGroups: {
+        vendors: false,
+        // make these custom defined chunk bundles
+        'commons-blocking': {
+          test: /jquery/,
+          name: 'commons-blocking',
+          enforce: true,
+          reuseExistingChunk: true,
+          chunks(module, chunks) {
+            if (module.name == 'mobile-overlay-ad') {
+              // mobile overlay ad exists on "old" layouts w/ old deprecated JS, which means it cannot depend on webpack commons blocking
+              return false;
+            }
+            return true;
+          }
+        },
+        'react-redux': {
+          chunks(module, chunks) {
+            if (module.name == 'search-box') {
+              // leave react-redux stuff in search-box bundle so GK can use it without DLing multiple files
+              return false;
+            }
+            return true;
+          },
+          test: /polyfills|\breact\b|react-dom|redux|react-redux|react-transition-group/,
+          name: 'react-redux',
+          enforce: true,
+          reuseExistingChunk: true
+        },
+        commons: {
+          chunks: 'all',
+          test: /tipso|remodal|typeahead_modified.bundle.js|header|handlebars/,
+          name: 'commons',
+          enforce: true,
+          reuseExistingChunk: true
+        }
+      }
+    }
+  },
+
   output: {
     filename: '[name]-bundle_[chunkhash].js',
     chunkFilename: '[name]-bundle_[chunkhash].js',
-    path: '../app/assets/webpack',
+    path: path.resolve(__dirname, '../app/assets/webpack'),
     publicPath: '/assets/'
   },
 
@@ -68,36 +101,33 @@ const config = {
     ]
   },
   plugins: [
-    new AssetMapPlugin('asset_map.json', null),
+    {
+      apply(compiler) {
+        compiler.plugin('done', (stats, done) => {
+          const assets = {};
+          for (const chunkGroup of stats.compilation.chunkGroups) {
+            if (chunkGroup.name) {
+              let files = [];
+              for (const chunk of chunkGroup.chunks) {
+                files = files.concat(chunk.files);
+              }
+              assets[chunkGroup.name] = files;
+            }
+          }
 
-    new webpack.optimize.CommonsChunkPlugin({
-      name: 'commons-blocking',
-      chunks: [
-        'commons',
-        'react-redux',
-        'school-profiles',
-        'district-boundaries',
-        'widget',
-        'search'
-      ],
-      minChunks: Infinity
-    }),
-    new webpack.optimize.CommonsChunkPlugin({
-      name: 'commons',
-      chunks: [
-        'react-redux',
-        'school-profiles',
-        'district-boundaries',
-        'widget',
-        'search'
-      ],
-      minChunks: Infinity
-    }),
-    new webpack.optimize.CommonsChunkPlugin({
-      name: 'react-redux',
-      chunks: ['school-profiles', 'district-boundaries', 'widget', 'search'],
-      minChunks: Infinity
-    }),
+          fs.writeFile(
+            'webpack.stats.json',
+            JSON.stringify({
+              assetsByChunkName: assets,
+              publicPath: stats.compilation.outputOptions.publicPath
+            }),
+            done
+          );
+          console.log(assets);
+        });
+      }
+    },
+    new AssetMapPlugin('asset_map.json', null),
     new webpack.DefinePlugin({
       'process.env': {
         NODE_ENV: JSON.stringify(nodeEnv)
@@ -108,12 +138,8 @@ const config = {
   module: {
     rules: [
       {
-        test: require.resolve('jquery'),
-        loader: 'expose-loader?$!expose-loader?jQuery'
-      },
-      {
         test: /\.(jpe?g|png|gif|svg)$/i,
-        loaders: [
+        use: [
           {
             loader: 'file-loader',
             options: {
@@ -133,12 +159,39 @@ const config = {
       },
       {
         test: /\.jsx?$/,
-        loader: 'babel-loader',
-        exclude: /node_modules/,
-        options: {
-          plugins: ['lodash', 'transform-runtime'],
-          presets: [['es2015', { modules: false }], 'react', 'stage-0']
-        }
+        use: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: require.resolve('jquery.cookie'),
+        use: [
+          {
+            loader: 'expose-loader',
+            options: 'jquery.cookie'
+          }
+        ]
+      },
+      {
+        test: require.resolve('jquery-ujs'),
+        use: [
+          {
+            loader: 'expose-loader',
+            options: 'jquery-ujs'
+          }
+        ]
+      },
+      {
+        test: require.resolve('jquery'),
+        use: [
+          {
+            loader: 'expose-loader',
+            options: 'jQuery'
+          },
+          {
+            loader: 'expose-loader',
+            options: '$'
+          }
+        ]
       }
     ]
   },
@@ -160,6 +213,11 @@ if (devBuild) {
       }
     })
   );
+  // config["externals"] = {
+  //   jquery: 'jQuery',
+  //   "jquery-ujs": "jquery-ujs",
+  //   'jquery.cookie': "jquery.cookie"
+  // }
   console.log('Webpack production build for Rails'); // eslint-disable-line no-console
 }
 
