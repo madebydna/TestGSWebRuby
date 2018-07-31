544d4a19803485900c9fa256d00318e427f83a12
Alex Thaler <athaler@greatschools.org>
2018-07-31 15:00:17 -0700

    Remove subheadings spec - it's optional

--- spec/qa/pages/gk/gk_home_page_spec.rb
+++ spec/qa/pages/gk/gk_home_page_spec.rb
@@ -10,7 +10,6 @@ describe 'User visits GK home', type: :feature, remote: true, safe_for_prod: tru
   it { is_expected.to have_heading }
   its('heading.text') { is_expected.to be_present }
   it { is_expected.to have_subheadings }
-  its('subheadings.first.text') { is_expected.to be_present }
   it { is_expected.to have_cue_cards }
   it { is_expected.to have_recommended_articles }
   its('recommended_articles.content_tiles.size') { is_expected.to eq(4) }
