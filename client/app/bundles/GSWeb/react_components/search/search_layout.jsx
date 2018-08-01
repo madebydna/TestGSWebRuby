0e893c02e1687b64da82dc3db17d3150f3ae116c
Alex Thaler <athaler@greatschools.org>
2018-08-01 15:03:34 -0700

    JT-6773 Add pressed state to mobile filter button in search

--- client/app/bundles/GSWeb/react_components/search/search_layout.jsx
+++ client/app/bundles/GSWeb/react_components/search/search_layout.jsx
@@ -227,7 +227,7 @@ class SearchLayout extends React.Component {
                     active={isOpen}
                     onClick={toggle}
                     onKeyPress={toggle}
-                    className="js-filter-button"
+                    className={`js-filter-button${isOpen ? ' active' : ''}`}
                   />
                 </span>
               </span>
