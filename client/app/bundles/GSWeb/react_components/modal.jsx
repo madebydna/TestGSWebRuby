ba06651e7573dab24b2559be466972443daaae2a
Samson Sprouse <ssprouse@greatschools.org>
2018-08-02 16:06:04 -0700

    JT-6771 First pass at new interstitial modal on profiles

---
+++ client/app/bundles/GSWeb/react_components/modal.jsx
@@ -0,0 +1,72 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import CaptureOutsideClick from 'react_components/search/capture_outside_click';
+import OpenableCloseable from 'react_components/openable_closeable';
+
+class Modal extends React.Component {
+  static propTypes = {
+    children: PropTypes.func,
+    renderTrigger: PropTypes.func,
+    closeOnOutsideClick: PropTypes.bool
+  };
+
+  static defaultProps = {
+    closeOnOutsideClick: true,
+    renderTrigger: () => {},
+    children: () => {}
+  };
+
+  overlay = () => (
+    <div
+      className="remodal-overlay remodal-is-opened"
+      style={{ display: 'block' }}
+    />
+  );
+
+  wrapperProps = isOpen => ({
+    className: isOpen ? 'remodal-wrapper remodal-is-opened' : null,
+    style: { display: isOpen ? 'block' : 'none' }
+  });
+
+  modalProps = () => ({
+    className: 'remodal modal_info_box remodal-is-initialized remodal-is-opened'
+  });
+
+  render() {
+    return (
+      <OpenableCloseable>
+        {(isOpen, { openForDuration, open, close, toggle }) => (
+          <React.Fragment>
+            {this.props.renderTrigger(isOpen, {
+              openForDuration,
+              open,
+              close,
+              toggle
+            })}
+            <div>
+              {isOpen ? this.overlay() : null}
+              <div {...this.wrapperProps(isOpen)}>
+                <CaptureOutsideClick
+                  callback={this.props.closeOnOutsideClick ? close : () => {}}
+                >
+                  <div {...this.modalProps()}>
+                    <button
+                      data-remodal-action="close"
+                      className="remodal-close"
+                      onClick={close}
+                    />
+                    <div className="remodal-content">
+                      {this.props.children({ openForDuration, close })}
+                    </div>
+                  </div>
+                </CaptureOutsideClick>
+              </div>
+            </div>
+          </React.Fragment>
+        )}
+      </OpenableCloseable>
+    );
+  }
+}
+
+export default Modal;
---
+++ client/app/bundles/GSWeb/react_components/school_profiles/profile_interstitial_ad.jsx
@@ -0,0 +1,83 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Modal from 'react_components/modal';
+import Ad from 'react_components/ad';
+import { get as getCookie, set as setCookie } from 'js-cookie';
+import { t } from 'util/i18n';
+
+const COOKIE_NAME = 'gs_interstitial';
+const INTERSTITIAL_VIEWED = 'viewed';
+
+const trackInterstitialViewed = () => {
+  setCookie(COOKIE_NAME, INTERSTITIAL_VIEWED);
+  return true;
+};
+
+const searchRegExp = /search\.page/;
+const browseExp = /\/schools\/|\?/;
+
+const referrerIsSearch = () =>
+  searchRegExp.test(window.document.referrer) ||
+  browseExp.test(window.document.referrer);
+
+const hasViewedInterstitial = () =>
+  getCookie(COOKIE_NAME) === INTERSTITIAL_VIEWED;
+
+const shouldShowInterstitial = () =>
+  referrerIsSearch() && !hasViewedInterstitial();
+
+const ProfileInterstitialAd = ({ loaded }) =>
+  shouldShowInterstitial() && loaded ? (
+    <Modal closeOnOutsideClick={false}>
+      {({ openForDuration, close }) => (
+        <Ad
+          sizeName="prestitial"
+          slot="Prestitial"
+          onFill={() => trackInterstitialViewed() && openForDuration(10000)}
+        >
+          <p>
+            {t(
+              'GreatSchools thanks the foundations and advertisers that make it possible to provide our site free to millions of parents. To skip ad,'
+            )}
+            <a onClick={close} style={{ cursor: 'pointer' }}>
+              {t('click here')} »
+            </a>
+          </p>
+        </Ad>
+      )}
+    </Modal>
+  ) : (
+    <div />
+  );
+
+ProfileInterstitialAd.propTypes = {
+  loaded: PropTypes.bool.isRequired
+};
+
+const profileInterstitialLoader = {
+  shouldLoad: false,
+  load: () => {
+    profileInterstitialLoader.shouldLoad = true;
+  }
+};
+
+class ProfileInterstitialWrapper extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loaded: profileInterstitialLoader.shouldLoad
+    };
+    profileInterstitialLoader.load = () => {
+      this.setState({
+        loaded: true
+      });
+    };
+  }
+  render() {
+    return <ProfileInterstitialAd loaded={this.state.loaded} />;
+  }
+}
+
+export default ProfileInterstitialWrapper;
+
+export { shouldShowInterstitial, profileInterstitialLoader };
