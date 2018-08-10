ef5637d7fc0234e700e5f985609cd5f94999031d
Samson Sprouse <ssprouse@greatschools.org>
2018-08-10 10:53:42 -0700

    JT-6217 Make babel decide on polyfills based on browser versions to support

--- client/app/bundles/GSWeb/polyfills.js
+++ client/app/bundles/GSWeb/polyfills.js
@@ -1,7 +1,8 @@
+import 'babel-polyfill';
 import 'raf/polyfill';
-import 'core-js/es6/map';
-import 'core-js/es6/set';
-import 'core-js/fn/array/find';
-import 'core-js/fn/object/entries';
-import 'core-js/fn/object/values';
-import 'core-js/fn/object/assign';
+// import 'core-js/es6/map';
+// import 'core-js/es6/set';
+// import 'core-js/fn/array/find';
+// import 'core-js/fn/object/entries';
+// import 'core-js/fn/object/values';
+// import 'core-js/fn/object/assign';
