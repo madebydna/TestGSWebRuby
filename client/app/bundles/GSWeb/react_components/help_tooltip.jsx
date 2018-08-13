9ca10221865896b33d5b640a184790f4d5a01b48
Andy Luo <aluo@greatschools.org>
2018-08-13 12:05:11 -0700

    Fix language locale switching between EN/ES by inserting in the html elements using dangerouslySetInnerHTML

--- client/app/bundles/GSWeb/react_components/help_tooltip.jsx
+++ client/app/bundles/GSWeb/react_components/help_tooltip.jsx
@@ -10,48 +10,47 @@ const renderHelpCircle = (numArr) => (
   numArr.map(num => <div className={`help-circle circle-rating--${num}`}>{num}</div>)
 )
 
-const content = (close) => (
-  <div className='rating-help-container'>
+const content = close => (
+  <div className="rating-help-container">
     <span
       className="icon-close btn-close"
       onClick={close}
       onKeyPress={close}
       role="button"
     />
-    <h4>{t('search_help.help')}</h4>
-    <p>{t('search_help.greatschool_rating')}
-    </p>
-    <div className='ratings-scale-container'>
-      <div className='block-container'>
-        <div className='rating-scale'>
-          {renderHelpCircle([1, 2, 3, 4])}
-        </div>
-        <p className='word-scale'>{t('search_help.rating.below_average')}</p>
+    <h4>{t("search_help.help")}</h4>
+    <p>{t("search_help.greatschool_rating")}</p>
+    <div className="ratings-scale-container">
+      <div className="block-container">
+        <div className="rating-scale">{renderHelpCircle([1, 2, 3, 4])}</div>
+        <p
+        dangerouslySetInnerHTML={{__html: t("search_help.rating.below_average_html")}}
+          className="word-scale"
+        />
       </div>
-      <div className='block-container'>
-        <div className='rating-scale'>
-          {renderHelpCircle([5, 6])}
-        </div>
-        <p className='word-scale'>{t('search_help.rating.average')}</p>
+      <div className="block-container">
+        <div className="rating-scale">{renderHelpCircle([5, 6])}</div>
+        <p className="word-scale" dangerouslySetInnerHTML={{ __html: t("search_help.rating.average_html") }}/>
       </div>
-      <div className='block-container'>
-        <div className='rating-scale'>
-          {renderHelpCircle([7, 8, 9, 10])}
-        </div>
-        <p className='word-scale'>{t('search_help.rating.above_average')}</p>
+      <div className="block-container">
+        <div className="rating-scale">{renderHelpCircle([7, 8, 9, 10])}</div>
+        <p className="word-scale" dangerouslySetInnerHTML={{ __html: t("search_help.rating.above_average_html") }} />
       </div>
     </div>
     <hr />
     <p>
-      <div className='circle-nr circle-rating--gray unrated'/>
-      {t('search_help.currently_rated')} <br/> <br/>
-      {t('search_help.currently_rated_info')}
+      <div className="circle-nr circle-rating--gray unrated" />
+      {t("search_help.currently_rated")} <br /> <br />
+      {t("search_help.currently_rated_info")}
     </p>
     <hr />
-    <p><span className="bold">{t('search_help.search_suggestions')}</span>
+    <p>
+      <span className="bold">{t("search_help.search_suggestions")}</span>
       <br />
     </p>
-    <a href={Links.zendesk} target='_blank'>{t('search_help.send_feedback')}</a>
+    <a href={Links.zendesk} target="_blank">
+      {t("search_help.send_feedback")}
+    </a>
   </div>
 );
 
