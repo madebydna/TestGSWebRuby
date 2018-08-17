7676b384667a12aab6f4b4eed754537da67938f5
Samson Sprouse <ssprouse@greatschools.org>
2018-08-16 17:57:25 -0700

    JT-6950 Not sure why I thought I could remove jquery_ujs from wordpress_modals

--- app/assets/javascripts/wordpress-modals.js
+++ app/assets/javascripts/wordpress-modals.js
@@ -1,5 +1,6 @@
 //= require_self
 //= require jquery.cookie.js
+//= require jquery_ujs
 //= require util/uri
 //= require util/i18n
 //= require resources/parsley.remote
@@ -17,16 +18,14 @@
 var $ = jQuery;
 
 GS = GS || {};
-GS.session =
-  GS.session ||
-  (function(gon) {
-    var isSignedIn = function() {
-      return (
-        $.cookie('community_www') != null || $.cookie('community_dev') != null
-      );
-    };
+GS.session = GS.session || (function(gon) {
 
-    return {
-      isSignedIn: isSignedIn
-    };
-  })();
+  var isSignedIn = function() {
+    return $.cookie('community_www') != null || $.cookie('community_dev') != null;
+  };
+
+  return {
+    isSignedIn: isSignedIn
+  };
+
+})();
