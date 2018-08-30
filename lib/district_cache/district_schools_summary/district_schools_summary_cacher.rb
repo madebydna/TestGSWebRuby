43c70825fd910aede632fb2a4d537ccdab2b2ede
Alex Thaler <athaler@greatschools.org>
2018-08-30 11:33:52 -0700

    JT-7058 First pass at refactored district schools summary cache with school type data

--- lib/district_cache/district_schools_summary/district_schools_summary_cacher.rb
+++ lib/district_cache/district_schools_summary/district_schools_summary_cacher.rb
@@ -1,41 +1,48 @@
 # cacher to cache summary data for a district's schools
 # writes out district schools summary value for district cache:
 # {
-# "school counts by level code" => {
-#  {
-#    e: 2,
-#    m: 3,
-#    h: 2
+#   "school counts by level code": {
+#     "p": 0,
+#     "e": 12,
+#     "m": 9,
+#     "h": 7
+#   },
+#   "school counts by type": {
+#     "public": 17,
+#     "charter": 5
 #   }
 # }
 class DistrictSchoolsSummary::DistrictSchoolsSummaryCacher < DistrictCacher
   CACHE_KEY = "district_schools_summary".freeze
   SCHOOLS_COUNTS_BY_LEVEL_CODE_KEY = "school counts by level code".freeze
+  SCHOOLS_COUNTS_BY_TYPE = "school counts by type".freeze
 
   def build_hash_for_cache
     {
-      SCHOOLS_COUNTS_BY_LEVEL_CODE_KEY => schools_count_by_level_code
+      SCHOOLS_COUNTS_BY_LEVEL_CODE_KEY => count_of_schools_by_level_code,
+      SCHOOLS_COUNTS_BY_TYPE => count_of_schools_by_type,
     }
   end
 
   private
 
-  def schools_count_by_level_code
-    count_of_schools_at_each_level(count_of_schools_for_each_level_code)
+  def schools_within_district
+    @_schools_within_district ||= School.within_district(district)
   end
 
-  def count_of_schools_for_each_level_code
-    district.on_db(district.state.downcase.to_sym)
-      .schools
-      .group("level_code").count
-  end
-
-  def count_of_schools_at_each_level(count_of_level_code_set_values)
-    count_of_level_code_set_values
-      .each_with_object(Hash.new(0)) do |(level_code_set, count), h|
-      level_code_set.split(",").each do |level_code|
-        h[level_code.to_sym] += count
-      end
+  def count_of_schools_by_level_code
+    level_code_counts = LevelCode::LEVEL_LOOKUP.keys.each_with_object({}) {|lc, hash| hash[lc] = 0}
+    schools_within_district.pluck('level_code').each do |level_codes|
+      split_lc = level_codes.split(',')
+      split_lc.each {|lc| level_code_counts[lc.strip.downcase] += 1}
     end
+    level_code_counts
   end
+
+  def count_of_schools_by_type
+    school_type_counts = {'public' => 0, 'charter' => 0}
+    schools_within_district.pluck('type').each {|st| school_type_counts[st.strip.downcase] += 1}
+    school_type_counts
+  end
+
 end
