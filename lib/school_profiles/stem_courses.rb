2712702a88b179448d4a154d2879818159c05731
Anthony Roy <aroy@greatschools.org>
2018-09-06 07:58:50 -0700

    Another max year fix. Fix decimal formatting

--- lib/school_profiles/stem_courses.rb
+++ lib/school_profiles/stem_courses.rb
@@ -21,7 +21,7 @@ module SchoolProfiles
       {
           'Percentage algebra 1 enrolled grades 7-8' => :PersonBar,
           'Percentage passing algebra 1 grades 7-8' => :SingleBar,
-          'Percentage AP math enrolled grades 9-12' => :SingleBar,
+          'Percentage AP math enrolled grades 9-12' => :PersonBar,
           'Percentage AP science enrolled grades 9-12' => :PersonBar
       }
     end
@@ -31,38 +31,33 @@ module SchoolProfiles
     end
 
     def stem_data
-      @_stem_data ||= @school_cache_data_reader.gsdata_data(*data_types)
+      @_stem_data ||= @school_cache_data_reader.decorated_gsdata_datas(*data_types)
     end
 
     def stem_courses_hashes
       stem_data.each_with_object([]) do |(data_type, bd_hashes), accum|
-        bd_hashes.each do |bd_hash|
-          data_value = GsdataCaching::GsDataValue.from_hash(bd_hash.merge(data_type: data_type))
-          if data_value.all_students? && data_value.school_value_as_float > 0
-            accum << {
-                breakdown: I18n.t(data_type, scope: 'school_profiles.stem_courses', default:{})[:label],
-                score: data_value.school_value.to_f,
-                label: "#{data_value.school_value}",
-                state_average: data_value.state_value.to_f,
-                visualization: data_types_and_visualizations[data_type],
-                tooltip_html: I18n.t(data_type, scope: 'school_profiles.stem_courses', default:{})[:tooltip_html]
-            }
-          end
+        bd_hashes.for_all_students.having_school_value.having_most_recent_date.each do |data_value|
+          val = data_value.school_value.to_f.round
+          accum << {
+              breakdown: I18n.t(data_type, scope: 'school_profiles.stem_courses', default:{})[:label],
+              score: val,
+              label: val < 1 ? '<1' : "#{val}",
+              state_average: data_value.state_value.to_f.round,
+              visualization: data_types_and_visualizations[data_type],
+              tooltip_html: I18n.t(data_type, scope: 'school_profiles.stem_courses', default:{})[:tooltip_html]
+          }
         end
       end
     end
 
     def stem_courses_sources
       stem_data.each_with_object([]) do |(data_type, bd_hashes), accum|
-        bd_hashes.each do |bd_hash|
-          data_value = GsdataCaching::GsDataValue.from_hash(bd_hash.merge(data_type: data_type))
-          if data_value.all_students?
-            accum << {
-              data_type: I18n.t(data_type, scope: 'school_profiles.stem_courses', default:{})[:label],
-              source_year: data_value.source_year,
-              source_name: I18n.db_t(data_value.source_name)
-            }
-          end
+        bd_hashes.for_all_students.having_school_value.having_most_recent_date.each do |data_value|
+          accum << {
+            data_type: I18n.t(data_type, scope: 'school_profiles.stem_courses', default:{})[:label],
+            source_year: data_value.source_year,
+            source_name: I18n.db_t(data_value.source_name)
+          }
         end
       end.uniq
     end
