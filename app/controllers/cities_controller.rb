19fbd75c4489fc165d20dc72fb4257f790a126e1
Andy Luo <aluo@greatschools.org>
2018-09-18 19:48:16 -0700

    Add safe navigation operator for finding largest district

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -80,8 +80,8 @@ class CitiesController < ApplicationController
         cp[:county] = county_record&.name
         cp[:searchResultBrowseUrl] = search_city_browse_path(city_params(state, city))
         cp[:zip] = get_zip
-        cp[:lat] = fetch_district(@districts&.first.fetch(:id,nil))[:lat] || city_record&.lat
-        cp[:lon] = fetch_district(@districts&.first.fetch(:id,nil))[:lon] || city_record&.lon
+        cp[:lat] = fetch_district(@districts&.first&.fetch(:id, nil))&.fetch(:lat, nil) || city_record&.lat
+        cp[:lon] = fetch_district(@districts&.first&.fetch(:id, nil))&.fetch(:lon, nil) || city_record&.lon
       end
     end
   end
