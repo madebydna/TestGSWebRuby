46d31f2347d475e3017dfae09c8c5d0dfb135d2a
Andy Luo <aluo@greatschools.org>
2018-09-21 00:31:16 -0700

    Add logic to same component

--- client/app/bundles/GSWeb/react_components/search/choose_table_buttons.jsx
+++ client/app/bundles/GSWeb/react_components/search/choose_table_buttons.jsx
@@ -3,12 +3,27 @@ import ButtonGroup from 'react_components/buttongroup';
 import { t } from 'util/i18n';
 import ChooseTableContext from './choose_table_context';
 import { SM, validSizes } from 'util/viewport';
-import TableSelect from './table_select';
+import Select from '../select';
 
-const options = {
-  Overview: t('School Overview'),
-  Academic: t('Academic and Equity ratings'),
-  Equity: t('Equity Test Score ratings')
+const optionsArray = [
+    {
+        key: 'Overview',
+        label: t('School Overview')
+    },
+    {
+        key: 'Academic',
+        label: t('Academic and Equity ratings')
+    },
+    {
+        key: 'Equity',
+        label: t('Equity Test Score ratings')
+    }
+];
+
+const optionsObject = {
+    Overview: t('School Overview'),
+    Academic: t('Academic and Equity ratings'),
+    Equity: t('Equity Test Score ratings')
 };
 
 
@@ -24,14 +39,27 @@ const renderTableButtonsFilters = (tableView, updateTableView, size) => {
     if(size > SM){
         return(
             <ButtonGroup
-                options={options}
+                options={optionsObject}
                 activeOption={tableView}
                 onSelect={updateTableView}
             />
         )
     }else{
         return(
-            <TableSelect/>
+            <ChooseTableContext.Consumer>
+                {({ tableView, updateTableView }) => (
+                    <Select
+                        objects={optionsArray}
+                        labelFunc={d => d.label}
+                        keyFunc={d => d.key}
+                        onChange={d => updateTableView(d.key)}
+                        defaultLabel={
+                            (optionsArray.find(obj => obj.key === tableView) || optionsArray[0]).label
+                        }
+                        defaultValue={tableView}
+                    />
+                )}
+            </ChooseTableContext.Consumer>
         )
     }
 };
