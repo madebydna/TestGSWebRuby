1d5d0d733770feaa0befb1c72a020b78568dce5c
Andy Luo <aluo@greatschools.org>
2018-10-23 14:58:52 -0700

    Remove additional unused functions

--- client/app/bundles/GSWeb/react_components/search/state_select_dropdown.jsx
+++ client/app/bundles/GSWeb/react_components/search/state_select_dropdown.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { t, capitalize } from 'util/i18n';
 import { SM, validSizes } from 'util/viewport';
 import Select from '../select';
 import { name as stateName } from 'util/states';
@@ -7,10 +6,6 @@ import PropTypes from 'prop-types';
 import SearchContext from './search_context';
 import { mySchoolList } from 'api_clients/schools';
 import { startCase } from 'lodash';
-
-const capitalizeStateName = (arrayOfStrings) => console.log(arrayOfStrings) || (
-  arrayOfStrings.map(str => capitalize(str)).join(" ")
-);
 
 const StateSelectDropdown = () => {
   return(
