4992f6b86d8fe7767f6ec24efc0f2281539b07bd
Andy Luo <aluo@greatschools.org>
2018-11-02 07:27:34 -0700

    Added safe navigation to equity_header_hashes

--- app/controllers/modules/search/search_table_concerns.rb
+++ app/controllers/modules/search/search_table_concerns.rb
@@ -19,7 +19,7 @@ module SearchTableConcerns
   def equity_header_hash(schools)
     headers = []
     schools.each do |school|
-      headers << school.ethnicity_information&.map{ |hash| hash[:label] }
+      headers << school&.ethnicity_information&.map{ |hash| hash[:label] }
     end
     h = headers.flatten.compact.uniq
     h.map do |title|
