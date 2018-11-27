976615ea01a49abbd02bc4987a13e643d3a63562
Alex Thaler <athaler@greatschools.org>
2018-11-27 09:43:09 -0800

    JT-7402 First pass at sticky table headers

--- client/app/bundles/GSWeb/react_components/compare/compare_school_table.jsx
+++ client/app/bundles/GSWeb/react_components/compare/compare_school_table.jsx
@@ -5,76 +5,98 @@ import School from 'react_components/search/school';
 import LoadingOverlay from 'react_components/search/loading_overlay';
 import CompareSchoolTableRow from './compare_school_table_row';
 import SchoolTableColumnHeader from 'react_components/search/school_table_column_header';
+import { keepInViewport } from 'util/sticky';
 
-const tableHeaders = (headerArray = []) => {
-  const schoolHeader = [
-    <SchoolTableColumnHeader
-      key={`school`}
-      colName={capitalize(t('school'))}
-      classNameTH="school"
-      tooltipContent=""
-    />
-  ];
-  let headers = headerArray.map(hash => (
-    <SchoolTableColumnHeader
-      key={hash.key}
-      colName={hash.title}
-      classNameTH={hash.className}
-      tooltipContent={hash.tooltip}
-    />
-  ));
-  headers = schoolHeader.concat(headers);
-  return (
-    <thead>
+class CompareSchoolTable extends React.Component {
+  static propTypes = {
+    schools: PropTypes.arrayOf(PropTypes.shape(School.propTypes)).isRequired,
+    isLoading: PropTypes.bool,
+    compareTableHeaders: PropTypes.array,
+    tableView: PropTypes.string
+  };
+
+  static defaultProps = {
+    isLoading: false,
+    tableView: 'Overview',
+    compareTableHeaders: {}
+  };
+
+  constructor(props) {
+    super(props);
+    this.headers = React.createRef();
+  };
+
+  componentDidMount(){
+    keepInViewport(this.headers, {
+      initialTop: 162,
+      setTop: true,
+      setBottom: false
+    });
+  }
+
+  tableHeaders(headerArray = []) {
+    const schoolHeader = [
+      <SchoolTableColumnHeader
+        key={`school`}
+        colName={capitalize(t('school'))}
+        classNameTH="school"
+        tooltipContent=""
+      />
+    ];
+    let headers = headerArray.map(hash => (
+      <SchoolTableColumnHeader
+        key={hash.key}
+        colName={hash.title}
+        classNameTH={hash.className}
+        tooltipContent={hash.tooltip}
+      />
+    ));
+    headers = schoolHeader.concat(headers);
+    return (
+      <thead>
       <tr>{headers}</tr>
-    </thead>
-  );
+      </thead>
+    );
+  };
+
+  renderCompareSchoolTable(){
+    let {schools, isLoading, compareTableHeaders} = this.props;
+    const pinnedSchool = schools.filter(s => s.pinned)[0];
+    const otherSchools = schools.filter(s => !s.pinned);
+
+    return (
+      <section className="school-table">
+        {
+          /* would prefer to just not render overlay if not showing it,
+           but then loader gif has delay, and we would need to preload it */
+          <LoadingOverlay
+            visible={isLoading && schools.length > 0}
+            numItems={schools.length}
+          />
+        }
+        <div className={isLoading ? 'loading' : undefined}>
+          <table>
+          {this.tableHeaders(compareTableHeaders)}
+            <tbody>
+            <CompareSchoolTableRow columns={compareTableHeaders}
+                                   key={pinnedSchool.state + pinnedSchool.id} {...pinnedSchool}/>
+            {otherSchools.map(s => (
+              <CompareSchoolTableRow
+                columns={compareTableHeaders}
+                key={s.state + s.id}
+                {...s}
+              />
+            ))}
+            </tbody>
+          </table>
+        </div>
+      </section>
+    )
+  }
+
+  render() {
+    return this.renderCompareSchoolTable();
+  };
 };
 
-const CompareSchoolTable = ({
-  schools,
-  isLoading,
-  compareTableHeaders
-}) => {
-  const pinnedSchool = schools.filter(s=>s.pinned)[0];
-  const otherSchools = schools.filter(s=>!s.pinned);
-  return (
-    <section className="school-table">
-      {
-        /* would prefer to just not render overlay if not showing it,
-        but then loader gif has delay, and we would need to preload it */
-        <LoadingOverlay
-          visible={isLoading && schools.length > 0}
-          numItems={schools.length}
-        />
-      }
-      <table className={isLoading ? 'loading' : undefined}>
-        {tableHeaders(compareTableHeaders)}
-        <tbody>
-          <CompareSchoolTableRow columns={compareTableHeaders} key={pinnedSchool.state + pinnedSchool.id} {...pinnedSchool}/>
-          {otherSchools.map(s => (
-            <CompareSchoolTableRow
-              columns={compareTableHeaders}
-              key={s.state + s.id}
-              {...s}
-            />
-          ))}
-        </tbody>
-      </table>
-    </section>
-  );
-};
-
-CompareSchoolTable.propTypes = {
-  schools: PropTypes.arrayOf(PropTypes.shape(School.propTypes)).isRequired,
-  isLoading: PropTypes.bool,
-  compareTableHeaders: PropTypes.array,
-  tableView: PropTypes.string
-};
-
-CompareSchoolTable.defaultProps = {
-  isLoading: false,
-  tableView: 'Overview',
-  compareTableHeaders: {}
-};
 export default CompareSchoolTable;
