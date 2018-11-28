2cd4ffaf00f4079bb793d0adfbca091841905408
Anthony Roy <aroy@greatschools.org>
2018-11-28 13:48:02 -0800

    JT-7500 Update title/description for state pages

--- spec/controllers/modules/states_meta_tags_concerns_spec.rb
+++ spec/controllers/modules/states_meta_tags_concerns_spec.rb
@@ -1,20 +1,49 @@
+# frozen_string_literal: true
+
 require 'spec_helper'
 
 describe StatesMetaTagsConcerns do
-
-  subject do
+  subject(:state_module) do
     o = Object.new
     o.singleton_class.instance_eval { include StatesMetaTagsConcerns }
     o
   end
 
-  describe '#states_show_title' do
-    context 'with a state instance var set' do
-      before { subject.instance_variable_set(:@state, {:short => 'az', :long => 'arizona'}) }
-      it 'should return the correct title string' do
-        expect(subject.send(:states_show_title)).to eql("Arizona Schools - Arizona State School Ratings - Public and Private")
-      end
+  context 'With state set to Arizona' do
+    before { state_module.instance_variable_set(:@state, {:short => 'az', :long => 'arizona'}) }
+
+    describe '#states_show_title' do
+      subject { state_module.states_show_title }
+      it { is_expected.to eql("2019 Arizona Schools | Arizona Schools | Public & Private Schools") }
+    end
+
+    describe '#states_show_description' do
+      subject { state_module.states_show_description }
+      it { is_expected.to eql("2019 Arizona school rankings, all AZ public and private schools in Arizona ranked. Click here for Arizona school information plus read ratings and reviews for Arizona schools.") }
     end
   end
 
+  context 'With state set to New Jersey' do
+    before { state_module.instance_variable_set(:@state, {:short => 'nj', :long => 'new jersey'}) }
+
+    describe '#states_show_title' do
+      subject { state_module.states_show_title }
+      it { is_expected.to eql("2019 New Jersey Schools | New Jersey Schools | Public & Private Schools") }
+    end
+
+    describe '#states_show_description' do
+      subject { state_module.states_show_description }
+      it { is_expected.to eql("2019 New Jersey school rankings, all NJ public and private schools in New Jersey ranked. Click here for New Jersey school information plus read ratings and reviews for New Jersey schools.") }
+    end
+  end
+
+  describe 'current year' do
+    before { state_module.instance_variable_set(:@state, {:short => 'az', :long => 'arizona'}) }
+    it 'should be updated before 2020' do
+      # If this fails then whoever currently owns our SEO strategy should be informed that these
+      # title/meta tags are out of date. They wanted it to say 2019 when we launched in 2018
+      # which is why we aren't using Time.now.year currently.
+      expect(state_module.states_show_title.scan(/\d+/).first.to_i).to be >= Time.now.year
+    end
+  end
 end
