e830d81f98d7b6a8a4ba5a70cb6d7753ece5e63f
Andy Luo <aluo@greatschools.org>
2018-11-29 10:50:51 -0800

    JT-7402 Remove ads from no compare school list result

--- client/app/bundles/GSWeb/react_components/compare/no_compare_school_list_result.jsx
+++ client/app/bundles/GSWeb/react_components/compare/no_compare_school_list_result.jsx
@@ -2,14 +2,12 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { t, capitalize } from 'util/i18n';
 import noResultsOwlPng from 'search/no-results-owl.png';
-import Ad from 'react_components/ad';
 import CompareContext from './compare_context';
 
 const NoCompareSchoolListResult = () => (
   <CompareContext.Consumer>
     {({ breakdown }) =>
       <div className="no-results">
-        <Ad sizeName="thin_banner" slot="Search_NoResults_Top" />
         <div className="body">
           <img src={noResultsOwlPng} />
           <div>
@@ -25,7 +23,6 @@ const NoCompareSchoolListResult = () => (
             <hr />
           </div>
         </div>
-        <Ad sizeName="thin_banner" slot="Search_NoResults_Bottom" />
       </div>
     }
   </CompareContext.Consumer>
