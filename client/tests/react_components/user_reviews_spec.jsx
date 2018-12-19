6be79ce1c65c0f3bd108b4a2cdd4e06819e95bfa
Alexina Chong <achong@dhcp-183.office.greatschools.org>
2018-12-19 10:15:06 -0800

    JT-7634 Fix typos in specs

--- client/tests/react_components/user_reviews_spec.jsx
+++ client/tests/react_components/user_reviews_spec.jsx
@@ -10,19 +10,19 @@ import ReviewsList from 'react_components/review/reviews_list';
 describe('<UserReviews />', ()=> {
   const recentReview = {
     avatar: 1,
-    five_star_review: [
+    five_star_review: {
       answer: '3',
       answer_label: null, 
       answer_value: '3',
       comment: 'Excellent. My kids love going to school every day.',
       date_published: 'September 05, 2018',
       id: 3604492,
-      links: [
+      links: {
         flag: '/gsr/reviews/3604492/flag'
-      ],
+      },
       school_id: 36,
       topic_label: 'Overall experience'
-    ],
+    },
     id: 1403387618630687500,
     most_recent_date: 'September 05, 2018',
     school_name: 'Oxford Elementary School',
@@ -39,12 +39,12 @@ describe('<UserReviews />', ()=> {
     expect(wrapper.containsMatchingElement(<UserReviews />));
   });
 
-  context('five star reviews', () => {
+  context('Displays five star reviews', () => {
     it('displays the correct number of filled-in stars corresponding to the rating', () => {
       const wrapper = shallow(
         <UserReviews {...recentReview} />
       );
-      expect(wrapper.find('.answer .five-stars .icon-star.filled-star')).to.equal(3);
+      expect(wrapper.find('.answer .five-stars .icon-star.filled-star').length).to.equal(3);
     });
 
     it('displays five stars total (filled and empty)', () => {
@@ -54,7 +54,7 @@ describe('<UserReviews />', ()=> {
       const filledStars = wrapper.find('.answer .five-stars .icon-star.filled-star');
       const emptyStars = wrapper.find('.answer .five-stars .icon-star.empty-star');
 
-      expect(filledStars + emptyStars).to.equal(5);
+      expect(filledStars.length + emptyStars.length).to.equal(5);
     });
   });
 });
