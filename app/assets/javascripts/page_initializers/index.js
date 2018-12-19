cebca47135860285843215e1baaa3539ca0eea24
Samson Sprouse <ssprouse@greatschools.org>
2018-12-18 17:16:45 -0800

    JT-7635 Remove old unused compare code

--- app/assets/javascripts/page_initializers/index.js
+++ app/assets/javascripts/page_initializers/index.js
@@ -1,8 +1,6 @@
 //= require ./admin
 //= require ./all_pages_init
 //= require ./city_hub_init
-//= require ./compare_schools_init
-//= require ./details_init
 //= require ./district_home_init
 //= require ./enrollment_init
 //= require ./forgot_password_init
@@ -10,8 +8,6 @@
 //= require ./osp_init
 //= require ./osp_landing_init
 //= require ./osp_registration_init
-//= require ./overview_init
-//= require ./quality_init
 //= require ./review_school_chooser_init
 //= require ./reviews_new_init
 //= require ./search_init
