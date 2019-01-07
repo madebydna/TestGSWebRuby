27e2f1bc05a98392e92ff93d3f0d466e9aa968ea
Samson Sprouse <ssprouse@greatschools.org>
2019-01-07 09:53:15 -0800

    JT-7574 Update text on compare no results view

--- client/app/bundles/GSWeb/react_components/compare/no_compare_school_list_result.jsx
+++ client/app/bundles/GSWeb/react_components/compare/no_compare_school_list_result.jsx
@@ -13,11 +13,11 @@ const NoCompareSchoolListResult = () => (
           <img src={noResultsOwlPng} />
           <div>
             <hr />
-            <p>{t('no_compare_school_title')}</p>
+            <p>{t('no_compare_school_results', { parameters: { breakdown: t(breakdown) }})}</p>
             <ul>
                 <li key={breakdown}
                   dangerouslySetInnerHTML={{ 
-                    __html: t('no_compare_school_results', { parameters: { breakdown: t(breakdown) }})
+                    __html: t('try_increasing_distance')
                   }}
                 />
             </ul>
