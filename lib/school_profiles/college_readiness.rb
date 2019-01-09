c3a5d67f8e0963f9fadcce76243188540684d6d2
Alex Thaler <athaler@greatschools.org>
2019-01-09 11:12:26 -0800

    JT-7643 Cleanup translation code per code review suggestions

--- lib/school_profiles/college_readiness.rb
+++ lib/school_profiles/college_readiness.rb
@@ -177,19 +177,10 @@ module SchoolProfiles
 
     def get_props(component)
       return {} if component.empty_data?
-      hash = {
-          title: I18n.t('title', scope: component.scope),
-          anchor: component.tab.capitalize,
-          data: component.college_data_array
-      }
-      hash.merge!(csa_props(component)) if component.tab == 'college_success' && component.csa_badge?
-      hash
-    end
-
-    def csa_props(component)
-      return nil unless component.csa_badge?
       {
-        csa_badge: I18n.t(:csa_badge_html, scope: 'lib.college_readiness').html_safe
+        title: I18n.t('title', scope: component.scope),
+        anchor: component.tab.capitalize,
+        data: component.college_data_array
       }
     end
 
--- lib/school_profiles/college_success.rb
+++ lib/school_profiles/college_success.rb
@@ -20,7 +20,7 @@ module SchoolProfiles
     end
 
     def csa_props
-      return nil unless cs_component.csa_badge?
+      return {} unless cs_component.csa_badge?
       {
         csa_badge: I18n.t(:csa_badge_html, scope: 'lib.college_readiness').html_safe
       }
@@ -34,14 +34,14 @@ module SchoolProfiles
           anchor: cs_component.tab.capitalize,
           data: cs_component.college_data_array
         }
-        hash.merge!(csa_props) if cs_component.csa_badge?
+        hash.merge!(csa_props)
         Array.wrap(hash)
       end
     end
 
     def sources
       content = '<div class="sourcing">'
-      content += '<h1>' + data_label('title') + '</h1>'
+      content += '<h1>' + translate('title') + '</h1>'
       data_array = cs_component.data_type_hashes
       content += data_array.reduce('') do |string, hash|
         string += sources_text(hash)
@@ -53,10 +53,10 @@ module SchoolProfiles
       year = hash['year'] || ((hash['source_date_valid'] || '')[0..3]).presence || hash['source_year']
       source = hash['source'] || hash['source_name']
       str = '<div>'
-      str += '<h4>' + data_label(hash['data_type']) + '</h4>'
-      str += "<p>#{data_label_info_text(hash['data_type'])}</p>"
+      str += '<h4>' + translate(hash['data_type']) + '</h4>'
+      str += "<p>#{info_text(hash['data_type'])}</p>"
       if year && source
-        str += '<p><span class="emphasis">' + data_label('source')+ '</span>: ' + I18n.db_t(source, default: source) + ', ' + year.to_s + '</p>'
+        str += '<p><span class="emphasis">' + translate('source') + '</span>: ' + translate(source) + ', ' + year.to_s + '</p>'
       else
         GSLogger.error( :misc, nil, message: "Missing source or missing year", vars: hash)
       end
@@ -90,16 +90,16 @@ module SchoolProfiles
       qualaroo_iframe(:college_success, @school_cache_data_reader.school.state, @school_cache_data_reader.school.id.to_s)
     end
 
-    def data_label(key)
+    def translate(key)
       I18n.t(key.to_sym, scope: 'lib.college_readiness', default: I18n.db_t(key, default: key))
     end
 
-    def data_label_info_text(key)
+    def info_text(key)
       I18n.t(key.to_sym, scope: 'lib.college_readiness.data_point_info_texts')
     end
 
     def school_csa_badge?
-      school_cache_data_reader.csa_badge?
+      csa_props.present?
     end
 
     def visible?
