369dbc95dd321a5f1edff204d7cf9ccbc54952ff
Mitchell Seltzer <mseltzer@greatschools.org>
2019-01-23 14:51:50 -0800

    JT-7678- more load query options

--- app/models/gsdata/load.rb
+++ app/models/gsdata/load.rb
@@ -45,26 +45,92 @@ class Load < ActiveRecord::Base
     where(q)
   end
 
+  def self.with_configuration_new(config)
+    q = "loads.configuration like '%#{config}%'"
+    if config.is_a?(Array)
+      q =''
+      config.each_with_index  do | c, i |
+        if i > 0
+          q += ' or '
+        end
+        q += "loads.configuration like '%#{c}%'"
+      end
+    end
+    q
+  end
+
+  def self.data_type_tags_to_loads_new(tags, configuration)
+    config = configuration.is_a?(Array) ? configuration.join(',') : configuration
+    tag_conf = tags + config
+    @_data_type_tags_to_loads ||= {}
+    @_data_type_tags_to_loads[tag_conf] ||= begin
+      find_by_sql("select loads.id, loads.data_type_id, loads.configuration,
+        loads.date_valid, loads.description, (sources_new.name) as 'source_new_name',
+        (data_types.name) as 'data_type_name', (data_types.short_name) as 'data_types_short_name'
+        from gsdata.loads, gsdata.sources_new, gsdata.data_types, gsdata.data_type_tags
+        where loads.data_type_id = data_types.id and loads.source_id = sources_new.id
+        and data_type_tags.data_type_id = data_types.id and data_type_tags.tag = #{tags}
+        and (#{with_configuration_new(configuration)})")
+    end
+  end
+
   def self.data_type_ids_to_loads(data_type_ids, configuration)
+    config = configuration.is_a?(Array) ? configuration.join(',') : configuration
+    dtis = data_type_ids.join(',')
+    hash_key = dtis + config
+    @_data_type_ids_to_loads ||= {}
+    @_data_type_ids_to_loads[hash_key] ||= begin
+      find_by_sql("select loads.id,
+        loads.data_type_id,
+        loads.configuration,
+        loads.date_valid,
+        loads.description,
+        (sources_new.name) as 'source_name',
+        (data_types.name) as 'data_type_name',
+        (data_types.short_name) as 'data_type_short_name'
+        from loads, sources_new, data_types
+        where loads.data_type_id = data_types.id and loads.source_id = sources_new.id
+        and loads.data_type_id in (#{dtis})
+        and (#{with_configuration_new(configuration)})")
+      end
+  end
+
+
+  def self.data_type_ids_to_loads_old(data_type_ids, configuration)
+    config = configuration.is_a?(Array) ? configuration.join(',') : configuration
+    dtis = data_type_ids.join(',')
+    hash_key = dtis + config
+    @_data_type_ids_to_loads ||= {}
+    @_data_type_ids_to_loads[hash_key] ||= begin
     load_and_source_and_data_type.
         from(
-            Load.with_data_type_ids(data_type_ids)
+            Load.with_data_type_ids(dtis)
                 .with_configuration(configuration),
             :loads)
         .with_data_types.with_sources
+                                  end
   end
 
   def self.data_type_tags_to_loads(tags, configuration)
-    load_and_source_and_data_type
-        .with_data_types.with_data_type_tags(tags)
-        .with_sources
+    config = configuration.is_a?(Array) ? configuration.join(',') : configuration
+    hash_key = tags + config
+    @_data_type_tags_to_loads ||= {}
+    @_data_type_tags_to_loads[hash_key] ||= begin
+    load_and_source_and_data_type.
+        from(
+            Load.with_configuration(configuration)
+            .with_data_types
+            .with_data_type_tags(tags),
+            :loads)
+        .with_data_types.with_sources
+                                         end
   end
 
   def self.load_and_source_and_data_type
     load_and_source_and_data_type_values = <<-SQL
       loads.id, loads.data_type_id, loads.configuration,
-      loads.date_valid, loads.description, (sources_new.name) as "source_name", (data_types.name) as "data_type_name", 
-      (data_types.short_name) as "short_name"
+      loads.date_valid, loads.description, (sources_new.name) as "source_name", 
+      (data_types.name) as "data_type_name", (data_types.short_name) as "short_name"
     SQL
     select(load_and_source_and_data_type_values)
   end
