0d02ef1bbaa41ce0adac7e3bb042d0477ef76193
Alexina Chong <achong@dhcp-183.office.greatschools.org>
2019-01-24 17:18:55 -0800

    JT-7100 Remove District Tandem Results model

--- app/models/district_tandem_results.rb
+++
@@ -1,6 +0,0 @@
-class DistrictTandemResults < ActiveRecord::Base
-  self.table_name = 'district_tandem_results'
-  db_magic :connection => :gs_schooldb
-
-  attr_accessible :state, :district_id, :district_name, :nces, :results, :status 
-end
