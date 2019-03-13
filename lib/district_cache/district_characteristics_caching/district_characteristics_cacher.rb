76888c4f77997b8f4db2e458c97999fd856cea87
Andy Luo <aluo@greatschools.org>
2019-03-13 14:33:19 -0700

    JT-8002 pdated typos on district cacher

--- lib/district_cache/district_characteristics_caching/district_characteristics_cacher.rb
+++ lib/district_cache/district_characteristics_caching/district_characteristics_cacher.rb
@@ -4,7 +4,7 @@ class DistrictCharacteristicsCacher < DistrictCacher
   include DistrictCacheValidation
 
   CACHE_KEY = 'district_characteristics'
-  DIRECTORY_CENSUS_DATA_TYPES = [9, 17, 76. 77, 78, 102, 178, 179, 182, 262, 273, 277, 296, 298, 319, 320, 321, 325, 417, 419]
+  DIRECTORY_CENSUS_DATA_TYPES = [9, 17, 76, 77, 78, 102, 178, 179, 182, 262, 273, 277, 296, 298, 319, 320, 321, 325, 417, 419]
   # 1 - Percentage of teachers in their first year
   # 2 - Bachelor's degree
   # 3 - Master's degree
