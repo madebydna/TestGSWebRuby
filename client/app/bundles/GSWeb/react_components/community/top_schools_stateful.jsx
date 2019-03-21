1b9015788b72c607c302f41829ced1294a9f7d35
Alexina Chong <achong@GSs-MacBook-Pro.local>
2019-03-21 14:57:54 -0700

    JT-8001 Add CSA school list filter to Top Schools module

--- client/app/bundles/GSWeb/react_components/community/top_schools_stateful.jsx
+++ client/app/bundles/GSWeb/react_components/community/top_schools_stateful.jsx
@@ -4,6 +4,11 @@ import TopSchools from "./top_schools";
 import School from "react_components/search/school";
 import { SM, validSizes as validViewportSizes } from "util/viewport";
 import * as APISchools from 'api_clients/schools';
+import SectionNavigation from '../equity/tabs/section_navigation';
+import ModuleTab from 'react_components/school_profiles/module_tab';
+import DataModule from 'react_components/data_module';
+import { handleAnchor, handleThirdAnchor, addAnchorChangeCallback, removeAnchorChangeCallback, formatAnchorString, formatAndJoinAnchors } from '../../components/anchor_router';
+import { t } from "util/i18n";
 
 class TopSchoolsStateful extends React.Component {
   static propTypes = {
@@ -16,34 +21,43 @@ class TopSchoolsStateful extends React.Component {
 
   static defaultProps = {
     schoolsData: {},
-    schoolLevels: {}
+    schoolLevels: {},
+    active: 0
   };
 
   constructor(props) {
     super(props);
     this.state = {
       size: props.size,
-      schoolLevels: props.schoolLevels
+      schoolLevels: props.schoolLevels,
+      active: props.active 
     };
     this.initialSchoolLoad(props.schoolsData);
     this.handleGradeLevel = this.handleGradeLevel.bind(this);
+    this.handleTabClick = this.handleTabClick.bind(this);
+    this.renderTabsContainer = this.renderTabsContainer.bind(this);
   }
 
-  initialSchoolLoad({elementary, middle, high}){
+  initialSchoolLoad({elementary, middle, high, csa}){
     if (elementary.length > 0) {
       this.state = {
         levelCodes: 'e',
         schools: elementary
       }
-    }else if(middle.length > 0){
+    } else if (middle.length > 0) {
       this.state = {
         levelCodes: 'm',
         schools: middle
       }
-    }else if(high.length > 0){
+    } else if (high.length > 0) {
       this.state = {
         levelCodes: 'h',
         schools: high
+      }
+    } else if (csa.length > 0) {
+      this.state = {
+        levelCodes: 'h',
+        schools: csa
       }
     }
   }
@@ -60,16 +74,49 @@ class TopSchoolsStateful extends React.Component {
     })
   }
 
+  handleTabClick(index) {
+    this.setState({active: index});
+    if(index === 1) {
+      this.setState({
+        levelCodes: 'h',
+        schools: this.props.schoolsData.csa
+      })
+    }
+  }
+  
+  tabs() {
+    let tabs = [t('top_schools.top_schools')];
+    if (this.props.schoolsData.csa.length > 0) {
+      tabs.push(t('csa_winners'));
+    }
+    return tabs.map(function (item, index) {
+      return <ModuleTab title={item} key={index} />
+    }.bind(this));
+  }
+    
+  renderTabsContainer() {
+    return (
+      <div className="tab-buttons">
+        <SectionNavigation active={this.state.active} onTabClick={this.handleTabClick} badge={this.props.csa_badge} >
+          { this.tabs() }
+        </SectionNavigation>
+      </div>
+    );
+  }
+
   render() {
     return (
        <TopSchools
         schools={this.state.schools}
         schoolLevels={this.props.schoolLevels}
         handleGradeLevel={this.handleGradeLevel}
+        handleTabClick={this.handleTabClick}
+        renderTabsContainer={this.renderTabsContainer}
         size={this.props.size}
         levelCodes={this.state.levelCodes}
         community={this.props.community}
         locality={this.props.locality}
+        currentTab={this.state.active}
       />
     );
   }
