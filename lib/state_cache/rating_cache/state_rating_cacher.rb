07248b4c64118a5cc60e2c5b63ed1b2f5757642f
Mitchell Seltzer <mseltzer@greatschools.org>
2019-04-01 12:59:19 -0700

    JT-8066 - state rating cache and revised feed build using cache

--- lib/state_cache/rating_cache/state_rating_cacher.rb
+++ lib/state_cache/rating_cache/state_rating_cacher.rb
@@ -4,17 +4,34 @@
 class StateRatingCacher < StateCacher
   CACHE_KEY = 'ratings'
 
+  SUMMARY_RATING_DATA_TYPE_ID = 160
+  TEST_SCORES_RATING_DATA_TYPE_ID = 155
+  SUMMARY_RATING_NAME = 'Summary Rating'
+  TEST_SCORES_RATING_NAME = 'Test Score Rating'
+
   # needs to contain
   # - rating type - summary or test
   # - description
   # - year or date
   #
   # Summary description is currently not in database and is static across states
+  SUMMARY_DESCRIPTION = "The GreatSchools Rating helps parents compare schools within a state based on a variety of school quality indicators and provides a helpful picture of how effectively each school serves all of its students. Ratings are on a scale of 1 (below average) to 10 (above average) and can include test scores, college readiness, academic progress, advanced courses, equity, discipline and attendance data. We also advise parents to visit schools, consider other information on school performance and programs, and consider family needs as part of the school selection process."
   # Test scores rating description is in database and varies across states
-  # SELECT v.state, l.data_type_id, l.date_valid, l.description FROM loads l
-  # LEFT JOIN data_values v on v.load_id = l.id
-  # WHERE l.data_type_id = 155
-  # GROUP BY l.id;
+
+
+  def test_rating
+    Load.find_by_sql("SELECT l.data_type_id, l.date_valid, l.description FROM loads l
+      LEFT JOIN data_values v on v.load_id = l.id
+      WHERE l.data_type_id = #{TEST_SCORES_RATING_DATA_TYPE_ID} and v.state = '#{state}' and l.description is not NULL
+      GROUP BY l.id order by l.date_valid desc")
+  end
+
+  def summary_rating
+    Load.find_by_sql("SELECT l.data_type_id, l.date_valid, l.description FROM loads l
+      LEFT JOIN data_values v on v.load_id = l.id
+      WHERE l.data_type_id = #{SUMMARY_RATING_DATA_TYPE_ID} and v.state = '#{state}'
+      GROUP BY l.id order by l.date_valid desc")
+  end
   #
   # Determine if it is summary or test by looking at all caches for all schools in a state and if any of them have a
   # Summary rating than it is a summary rating state.
@@ -23,130 +40,27 @@ class StateRatingCacher < StateCacher
   # and max date on results
   #
 
+  def build_hash_for_cache
+    s = summary_rating&.first
+    if s&.date_valid
+      description = s.description.present? ? s.description : SUMMARY_DESCRIPTION
+      result_to_hash(s.date_valid.year, description, SUMMARY_RATING_NAME)
+    else
+      tr = test_rating&.first
+      if tr&.date_valid
+        result_to_hash(tr.date_valid.year, tr.description, TEST_SCORES_RATING_NAME)
+      else
+        {}
+      end
+    end
+  end
 
+  def result_to_hash(year, description, type)
+    {}.tap do |h|
+      h[:year] = year
+      h[:description] = description
+      h[:type] = type
+    end
+  end
 
-
-
-
-  #
-  # DATA_TYPES INCLUDED
-  # 31: In school suspension ---- not used currently JT-3276
-  # 35: Out of school suspension
-  # 55: %AP enrollment for students in grades 9-12
-  # 71: Percentage SAT/ACT participation grades 11-12
-  # 83: Percentage of students passing 1 or more AP exams grades 9-12
-  # 91: Absent the rate of absenteeism
-  # 95: Ration of students to full time teachers
-  # 99: Percentage of full time teachers who are certified
-  # 119: Ratio of students to full time counselors
-  # 133: Ratio of teacher salary to total number of teachers
-  # 149: Percentage of teachers with less than three years experience
-  # 152: Number of advanced courses per student
-  # 154: Percentage of Students Enrolled
-
-#   DISCIPLINE_ATTENDANCE_IDS = [161, 162, 163, 164]
-#
-#   DATA_TYPE_IDS = [23, 27, 35, 55, 59, 63, 71, 83, 91, 95, 99, 119, 133, 149, 152, 154].freeze
-#
-#   # BREAKDOWN_TAG_NAMES = %w(
-#   #   ethnicity
-#   #   gender
-#   #   language_learner
-#   #   disability
-#   #   all_students
-#   # )
-#   #
-#   # COURSE_ENROLLMENT_DATA_TYPE_ID = 150
-#
-#   # ACADEMIC_TAG_NAMES = %w(
-#   #   course_subject_group
-#   #   advanced
-#   #   stem_index
-#   #   arts_index
-#   #   vocational_hands_on_index
-#   #   ela_index
-#   #   fl_index
-#   #   hss_index
-#   #   business_index
-#   #   health_index
-#   # )
-#
-#   def data_type_ids
-#     self.class::DATA_TYPE_IDS
-#   end
-#
-#   def build_hash_for_cache
-#     state_cache_hash = Hash.new { |h, k| h[k] = [] }
-#     r = state_results
-#     r.each_with_object(state_cache_hash) do |result, cache_hash|
-#       result_hash = result_to_hash(result)
-#       validate_result_hash(result_hash, result.data_type_id)
-#       cache_hash[result.name] << result_hash
-#     end
-#   end
-#
-#   def self.listens_to?(data_type)
-#     data_type == :gsdata
-#   end
-#
-#   def state_results
-#     @_school_results ||=
-#       DataValue.find_by_state_and_data_types(state, data_type_ids)
-#   end
-#
-#   private
-#
-#   def result_to_hash(result)
-#     breakdowns = result.breakdown_names
-#     breakdown_tags = result.breakdown_tags
-#     academics = result.academic_names if result.respond_to?(:academic_names)
-#     academic_tags = result.academic_tags if result.respond_to?(:academic_tags)
-#     academic_types = result.academic_types if result.respond_to?(:academic_types)
-#     # display_range = display_range(result)
-#
-#     {}.tap do |h|
-#       # switch back to only breakdowns when the code down stream can handle academics
-#       h[:breakdowns] = breakdowns if breakdowns
-#       h[:breakdown_tags] = breakdown_tags if breakdown_tags
-#       h[:academics] = academics if academics
-#       h[:academic_tags] = academic_tags if academic_tags
-#       h[:academic_types] = academic_types if academic_types
-#
-#       h[:source_date_valid] = result.date_valid
-#
-#       h[:state_value] = result.value
-#       h[:source_name] = result.source
-#       h[:grade] = result.grade if result.grade
-#       h[:cohort_count] = result.cohort_count if result.cohort_count
-#       h[:proficiency_band_id] = result.proficiency_band_id if result.proficiency_band_id
-#     end
-#   end
-#
-#   def validate_result_hash(result_hash, data_type_id)
-#     result_hash = result_hash.reject { |_,v| v.blank? }
-#     required_keys = %i(school_value source_date_valid source_name)
-#     missing_keys = required_keys - result_hash.keys
-#     if missing_keys.count > 0
-#       GSLogger.error(
-#         :state_cache,
-#         message: "#{self.class.name} cache missing required keys",
-#         vars: { state: state,
-#                 data_type_id: data_type_id,
-#         }
-#       )
-#     end
-#     return missing_keys.count == 0
-#   end
-#
-#   # after display range strategy is chosen will need to update method below
-#   def display_range(_result)
-#     nil
-#     # DisplayRange.for({
-#     #   data_type:    'gsdata',
-#     #   data_type_id: result.data_type_id,
-#     #   state:        result.state,
-#     #   year:         year,
-#     #   value:        result.value
-#     # })
-#   end
 end
--- script/feeds/feed_builders/rating/test_rating_feed_data_reader.rb
+++ script/feeds/feed_builders/rating/test_rating_feed_data_reader.rb
@@ -1,10 +1,10 @@
 module Feeds
   module TestRatingFeedDataReader
 
-    def get_school_data_for_ratings(school,ratings_id)
+    def get_school_data_for_ratings(school,ratings_type)
       data = school.try(:school_cache).cache_data['ratings']
       state = school['state'].downcase if school.present? && school['state'].present?
-      get_rating_data_for_school_feed(data, state, ratings_id) if data.present?
+      get_rating_data_for_school_feed(data, state, ratings_type) if data.present?
     end
 
     def get_district_data_for_ratings(district, ratings_id)
@@ -12,8 +12,9 @@ module Feeds
       get_rating_data_for_district_feed(data,ratings_id)
     end
 
-    def get_ratings_master_data(state, rating_id)
-      TestDataSet.ratings_config_for_state(state,rating_id)
+    def get_ratings_master_data(state)
+      state_info = StateCache.for_state('ratings', state)
+      JSON.parse(state_info.value)
     end
 
     private
@@ -22,44 +23,26 @@ module Feeds
       date.slice(0..3).to_i
     end
 
-    def data_type_id_to_name
+    def name_to_data_type_id
       {
-          174 => 'Summary Rating',
-          164 => 'Test Score Rating'
+          'Summary Rating' => 174,
+          'Test Score Rating' => 164
       }
     end
 
-    def get_rating_data_for_school_feed(ratings_cache_data, state, ratings_id)
-      hash = {'data_type_id'=>ratings_id, 'year'=>0, 'school_value_text'=>nil, 'school_value_float'=>nil, 'test_data_type_display_name'=>'GreatSchools rating'}
-      rating_name = data_type_id_to_name[ratings_id]
-      ratings_hashes = ratings_cache_data[rating_name]
-      rating_data = nil
+    def get_rating_data_for_school_feed(ratings_cache_data, state, ratings_type)
+      hash = {'data_type_id'=>name_to_data_type_id[ratings_type], 'year'=>0, 'school_value_float'=>nil, 'test_data_type_display_name'=>'GreatSchools rating'}
+      ratings_hashes = ratings_cache_data[ratings_type]
+      # rating_data = nil
       if ratings_hashes
         all_values = RatingsCaching::Value.from_array_of_hashes(ratings_hashes)
         rating_data = all_values.having_school_value.for_all_students.expect_only_one(
-            "Expecting only one #{rating_name}", state: state
+            "Expecting only one #{ratings_type}", state: state
         )
       end
-      if rating_data.present?
-        hash['school_value_float'] = rating_data.school_value_as_int
-        hash['year'] = rating_data.source_year
-      elsif ratings_id == 174 && test_score_rating_used?(state)
-        hash['data_type_id'] = 164
-        ratings_hashes = ratings_cache_data['Test Score Rating']
-        value_objects = RatingsCaching::Value.from_array_of_hashes(ratings_hashes)
-        overall_test_score = value_objects.having_school_value.for_all_students.expect_only_one(
-          "Should only have found one Test Score Rating",
-          state: state
-        )
-        hash['school_value_float'] = overall_test_score.school_value_as_int if overall_test_score.present?
-        hash['year'] = overall_test_score.source_year if overall_test_score.present?
-      end
-      hash['school_value_float'].present? || hash['school_value_text'].present? ? hash : nil
-    end
-
-    def test_score_rating_used?(state)
-      whitelist = %w(ak id me nd nh sd vt)
-      whitelist.include? state
+      hash['school_value_float'] = rating_data&.school_value_as_int
+      hash['year'] = rating_data&.source_year
+      hash['school_value_float'].present? ? hash : nil
     end
 
     def get_rating_data_for_district_feed(ratings_cache_data,ratings_id_for_feed)
