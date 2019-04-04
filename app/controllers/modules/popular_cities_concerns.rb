33494a344dbfb8a0ee75dd5c09b490d9552bd539
Alexina Chong <achong@GSs-MacBook-Pro.local>
2019-04-03 17:05:00 -0700

    JT-8000 Add query for city browse list on State page

--- app/controllers/modules/popular_cities_concerns.rb
+++ app/controllers/modules/popular_cities_concerns.rb
@@ -9,4 +9,11 @@ module PopularCitiesConcerns
     )
   end
 
+  def browse_top_cities
+    return @_browse_top_cities if defined?(@_browse_top_cities)
+    @_browse_top_cities = (
+      City.popular_cities(@state[:short], limit: 7)
+    )
+  end
+
 end
