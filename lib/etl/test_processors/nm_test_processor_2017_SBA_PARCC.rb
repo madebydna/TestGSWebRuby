6a24c443463a406e508c8f2da61bbc22560aab3e
Nina Magnuson <nmagnuson@Admins-MacBook-Pro-2.local>
2019-04-04 11:32:48 -0700

    DXT-2878 NM test

--- lib/etl/test_processors/nm_test_processor_2017_SBA_PARCC.rb
+++ lib/etl/test_processors/nm_test_processor_2017_SBA_PARCC.rb
@@ -15,8 +15,8 @@ class NMTestProcessor2017SBAPARCC < GS::ETL::TestProcessor
      'American Indian' => 18,
      'Asian' => 16,
      'Caucasian' => 21,
-     'Economically Disadvantaged' => 23,
-     'English Language Learners' => 32,
+     'Economically Disadvanta³d' => 23,
+     'English Langua³ Learners, Current' => 32,
      'Female' => 26,
      'Hispanic' => 19,
      'Male' => 25,
@@ -40,205 +40,14 @@ class NMTestProcessor2017SBAPARCC < GS::ETL::TestProcessor
      'NMSBA' => 244
    }
 
- source("ACC_Webfiles_2017_Proficiencies_ALL_ByStateByDistrictBySchool.txt",[], col_sep: "\t") do |s|
-   s.transform("Set entity", WithBlock) do |row|
-     if row[:state_or_district] == 'Statewide'
-       row[:entity_level] = 'state'
-     elsif row[:school] == 'Districtwide'
-       row[:entity_level] = 'district'
-     else row[:entity_level] = 'school'
-     end
-     row
-    # require 'byebug'
-    # byebug
-   end
-   .transform("Creating StateID", WithBlock) do |row|
-     if row[:entity_level] == 'state'
-       row[:state_id] = 'state'
-     else row[:state_id] = row[:code]
-     end
-     row
-   end
-   .transform("Adding column breakdown_id from group",
-    HashLookup, :group, map_nm_breakdown, to: :breakdown_id)
-   .transform("Remove Migrant", WithBlock) do |row|
-     if row[:group] != 'Migrant'
-       row[:breakdown] = row[:group]
-       row
-     end
-   end
-   .transform('Fill missing default fields', Fill, {
-     entity_type: 'public_charter',
-     proficiency_band: 1,
-     proficiency_band_gsdata_id: 'null',
-     level_code: 'e,m,h',
-     grade: 'All',
-     year: 2017
+ source("ACC_Webfiles_2017_Proficiencies_All_ByStateByDistrictBySchool.txt",[], col_sep: "\t") do |s|
+   s.transform("Fill grade all", Fill,{
+    grade: 'All'
    })
-   .transform("Adding school and district names", WithBlock) do |row|
-     if row[:entity_level] == 'school'
-       row[:school_id] = row[:code]
-       row[:school_name] = row[:school]
-       row[:district_name] = row[:state_or_district]
-     elsif row[:entity_level] == 'district'
-       row[:district_name] = row[:state_or_district]
-     end
-       row
-   end
-   .transform("Adding district ids", WithBlock) do |row|
-     if row[:entity_level] == 'district'
-       row[:district_id] = row[:code]
-     end
-     row
-   end
-   .transform("Renaming subject value columns", MultiFieldRenamer,
-     {
-       reading_proficientabove: :reading,
-       math_proficientabove: :math,
-       science_proficientabove: :science
-     })
-   .transform("Transpose", Transposer, :subject,:value_float,:reading,:math,:science)
-   .transform("Filling in number_tested", WithBlock) do |row|
-     if row[:subject] == :reading
-       row[:number_tested] = row[:reading_count]
-     elsif row[:subject] == :math
-       row[:number_tested] = row[:math_count]
-     elsif row[:subject] == :science
-       row[:number_tested] = row[:science_count]
-     end
-     row
-   end
-   .transform("Filling in subject ids",
-    HashLookup, :subject, map_nm_subject, to: :academic_id)
-   .transform("Filling in test names",
-    HashLookup, :subject, map_nm_test, to: :test_data_type)
-   .transform("Filling in test ids",
-    HashLookup, :test_data_type, map_nm_test_id, to: :gsdata_test_data_type_id)
-   .transform("Padding School and District level ID's to 6 digits", WithBlock) do |row|
-       if row[:entity_level] == 'school'
-          row[:state_id] = '%06i' % (row[:state_id].to_i)
-          row[:school_id] = '%06i' % (row[:school_id].to_i)
-          row[:district_id] = '%06i' % (row[:state_id].to_i)
-       elsif row[:entity_level] == 'district'
-          row[:state_id] = '%06i' % (row[:state_id].to_i)
-          row[:district_id] = '%06i' % (row[:district_id].to_i)
-       elsif row[:entity_levl] =='state'
-          row[:state_id] = 'state'
-       end
-       row
-   end
-   # .transform('remove %',WithBlock,) do |row|
-   #   unless row[:number_tested].nil?
-   #          row[:number_tested] = row[:number_tested].tr('"','')
-   #   end
-   #   if row[:number_tested].nil?
-   #      row[:number_tested] = 'skip'
-   #   end
-   #   row
-   # end
-   .transform('remove blank value rows', DeleteRows, :number_tested, 'skip')
-   .transform("Fixing district ids to first 3 numbers", WithBlock) do |row|
-     if row[:entity_level] == 'school'
-       row[:district_id] = row[:district_id].slice!(0,3)
-     elsif row[:entity_level] == 'district'
-       row[:state_id] = row[:state_id].slice!(0,3)
-       row[:district_id]=row[:district_id].slice!(0,3)
-     end
-     row
-   end
  end
 
-
- source('nm_SBA_PARCC_2016_by_grade.txt',[], col_sep: "\t") do |s|
-   s.transform("Set entity", WithBlock) do |row|
-     if row[:state_or_district] == 'Statewide'
-       row[:entity_level] = 'state'
-     elsif row[:school] == 'Districtwide'
-       row[:entity_level] = 'district'
-     else row[:entity_level] = 'school'
-     end
-     row
-    # require 'byebug'
-    # byebug
-   end
-   .transform("Creating StateID", WithBlock) do |row|
-     if row[:entity_level] == 'state'
-       row[:state_id] = 'state'
-     else row[:state_id] = row[:code]
-     end
-     row
-   end
-   .transform("Removing grades KN,1,2", DeleteRows,:grade,'KN','1','2','12')
-   .transform("Adding school and district names", WithBlock) do |row|
-     if row[:entity_level] == 'school'
-       row[:school_id] = row[:code]
-       row[:school_name] = row[:school]
-       row[:district_name] = row[:state_or_district]
-     elsif row[:entity_level] == 'district'
-       row[:district_name] = row[:state_or_district]
-     end
-       row
-   end
-   .transform("Adding district ids", WithBlock) do |row|
-     if row[:entity_level] == 'district'
-       row[:district_id] = row[:code]
-     end
-     row
-   end
-   .transform("Renaming subject value columns", MultiFieldRenamer,
-     {
-       reading_proficientabove: :reading,
-       math_proficientabove: :math,
-       science_proficientabove: :science,
-       grade: :grade
-     })
-   .transform("Transpose", Transposer, :subject,:value_float,:reading,:math,:science)
-   .transform("Filling in number_tested", WithBlock) do |row|
-     if row[:subject] == :reading
-       row[:number_tested] = row[:reading_count]
-     elsif row[:subject] == :math
-       row[:number_tested] = row[:math_count]
-     elsif row[:subject] == :science
-       row[:number_tested] = row[:science_count]
-     end
-     row
-   end
-   .transform("Filling in subject ids",
-    HashLookup, :subject, map_nm_subject, to: :subject_id)
-   .transform("Filling in test names",
-    HashLookup, :subject, map_nm_test, to: :test_data_type)
-   .transform("Filling in test ids",
-    HashLookup, :test_data_type, map_nm_test_id, to: :gsdata_test_data_type_id)
-   .transform("Padding School and District level ID's to 6 digits", WithBlock) do |row|
-       if row[:entity_level] == 'school'
-          row[:state_id] = '%06i' % (row[:state_id].to_i)
-          row[:school_id] = '%06i' % (row[:school_id].to_i)
-          row[:district_id] = '%06i' % (row[:state_id].to_i)
-       elsif row[:entity_level] == 'district'
-          row[:state_id] = '%06i' % (row[:state_id].to_i)
-          row[:district_id] = '%06i' % (row[:district_id].to_i)
-       end
-       row
-   end
-   .transform('remove %',WithBlock,) do |row|
-     unless row[:number_tested].nil?
-            row[:number_tested] = row[:number_tested].tr('"','')
-     end
-     if row[:number_tested].nil?
-        row[:number_tested] = 'skip'
-     end
-     row
-   end
-   .transform('remove blank value rows', DeleteRows, :number_tested, 'skip')
-   .transform("Fixing district ids to first 3 numbers", WithBlock) do |row|
-     if row[:entity_level] == 'school'
-       row[:district_id] = row[:district_id].slice!(0,3)
-     elsif row[:entity_level] == 'district'
-       row[:state_id] = row[:state_id].slice!(0,3)
-       row[:district_id]=row[:district_id].slice!(0,3)
-     end
-     row
-   end
+  source('ACC_Webfiles_2017_Proficiencies_All_ByStateByDistrictBySchoolByGrade.txt',[], col_sep: "\t") do |s|
+   s.transform("Removing grades KN,1,2", DeleteRows,:grade,'KN','1','2','12')
  end
 
   shared do |s|
@@ -250,30 +59,33 @@ class NMTestProcessor2017SBAPARCC < GS::ETL::TestProcessor
       proficiency_band: 'proficient and above',
       notes: 'DXT-2878: NM PARCC SBA test',
     })
-    .transform("Set entity", WithBlock) do |row|
-     if row[:state_or_district] == 'Statewide'
-       row[:entity_level] = 'state'
-     elsif row[:school] == 'Districtwide'
-       row[:entity_level] = 'district'
-     else row[:entity_level] = 'school'
-     end
-     row
-   end
     .transform('Rename column headers', MultiFieldRenamer,{
-    group: :breakdown,
+      group: :breakdown,
     })
-    .transform("Adding column breakdown_id from group",
-      HashLookup, :group, map_nm_breakdown, to: :breakdown_gsdata_id)
-    .transform("Skip missing values", DeleteRows, :tested_percentage, '*')
     .transform("Skip migrant", DeleteRows, :breakdown, 'Migrant')
+    .transform("Adding column breakdown_id from group",
+      HashLookup, :breakdown, map_nm_breakdown, to: :breakdown_gsdata_id)
     .transform("Renaming subject value columns", MultiFieldRenamer,
      {
        reading_proficientabove: :reading,
        math_proficientabove: :math,
        science_proficientabove: :science,
-       grade: :grade
      })
     .transform("Transpose", Transposer, :subject,:value_float,:reading,:math,:science)
+    .transform("Filling in subject ids",
+    HashLookup, :subject, map_nm_subject, to: :academic_gsdata_id)
+    .transform("Filling in test names",
+    HashLookup, :subject, map_nm_test, to: :test_data_type)
+    .transform("Filling in test ids",
+    HashLookup, :test_data_type, map_nm_test_id, to: :gsdata_test_data_type_id)
+    .transform("Filling in description", WithBlock) do |row|
+     if row[:gsdata_test_data_type_id] == 245
+       row[:description] = 'In 2016-2017, New Mexico used the PARCC assessment to test students in grades 3-11 in Math and Reading.'
+     elsif row[:gsdata_test_data_type_id] == 244
+       row[:description] = 'In 2016-2017, New Mexico used the New Mexico Standards-Based Assessment (NMSBA) to test students in grades 4, 7 and 11 in Science.'
+     end
+     row
+    end
     .transform("Filling in number_tested", WithBlock) do |row|
      if row[:subject] == :reading
        row[:number_tested] = row[:reading_count]
@@ -284,45 +96,48 @@ class NMTestProcessor2017SBAPARCC < GS::ETL::TestProcessor
      end
      row
     end
-    .transform('remove %',WithBlock,) do |row|
-     unless row[:number_tested].nil?
-            row[:number_tested] = row[:number_tested].tr('"','')
-     end
-     if row[:number_tested].nil?
-        row[:number_tested] = 'skip'
+    .transform("Remove , in number_tested", WithBlock) do |row|
+      unless row[:number_tested].nil?
+          row[:number_tested] = row[:number_tested].gsub(/[\,]/, '')
      end
      row
-   end
-   
-   .transform("Filling in subject ids",
-    HashLookup, :subject, map_nm_subject, to: :subject_id)
-   .transform("Filling in test names",
-    HashLookup, :subject, map_nm_test, to: :test_data_type)
-   .transform("Filling in test ids",
-    HashLookup, :test_data_type, map_nm_test_id, to: :gsdata_test_data_type_id)
-    .transform("fix special cases for prof and above", WithBlock) do |row|
+    end
+   .transform("Delete rows where number tested is less than 10 and blank ",DeleteRows, :number_tested, '0','1','2','3','4','5','6','7','8','9')
+   .transform("fix special cases for prof and above", WithBlock) do |row|
           if row[:value_float].nil?
             row[:value_float]=row[:value_float]
-          elsif row[:value_float] < 0
-            row[:value_float] = 0
-          elsif row[:value_float] > 100
-            row[:value_float] = 100
-          elsif row[:value_float].between?(0,1)
-            row[:value_float]=row[:value_float].round(2)
+          elsif row[:value_float] == '≤ 1'
+            row[:value_float] == '1'
+          elsif row[:value_float] == '≥95'
+            row[:value_float] == '95'
           end
      row
     end
     .transform("Skip missing prof and above values", DeleteRows, :value_float, nil)
-    .transform("Padding School and District level ID's to 6 digits", WithBlock) do |row|
-       if row[:entity_level] == 'school'
-          row[:state_id] = '%06i' % (row[:state_id].to_i)
-          row[:school_id] = '%06i' % (row[:school_id].to_i)
-          row[:district_id] = '%06i' % (row[:state_id].to_i)
-       elsif row[:entity_level] == 'district'
-          row[:state_id] = '%06i' % (row[:state_id].to_i)
-          row[:district_id] = '%06i' % (row[:district_id].to_i)
-       end
-       row
+    .transform("Skip range values", DeleteRows, :value_float, '³ 80', '² 10', '² 20', '³ 90', '² 5', '² 2')
+    .transform("Skip range values", DeleteRows, :value_float, ' ≥ 80', '≤ 10', '≤ 20', ' ≥ 90', '≤ 5', '≤ 2')
+    .transform("Set entity", WithBlock) do |row|
+     if row[:state_or_district] == 'Statewide'
+       row[:entity_level] = 'state'
+     elsif row[:school] == 'Districtwide'
+       row[:entity_level] = 'district'
+     else row[:entity_level] = 'school'
+     end
+     row
+   end
+   .transform("Creating StateID, district and school id and dist and sch names", WithBlock) do |row|
+     if row[:entity_level] == 'state'
+       row[:state_id] = 'state'
+     elsif row[:entity_level] == 'district'
+        row[:state_id] = '%03i' % (row[:code].to_i)
+        row[:district_id] = row[:code]
+        row[:district_name] = row[:state_or_district]
+     elsif row[:entity_level] == 'school'
+        row[:state_id] = '%06i' % (row[:code].to_i)
+        row[:school_id] = row[:code]
+        row[:school_name] = row[:school]
+     end
+     row
    end
 end
 
@@ -330,9 +145,10 @@ end
 
   def config_hash
     {
-        source_id: 35,
+        gsdata_source_id: 35,
         state: 'nm',
         source_name: 'New Mexico Public Education Department',
+        date_valid: '2017-01-01 00:00:00',
         notes: 'DXT-2878: NM SBA PARCC 2017 test load.',
         url: 'http://ped.state.nm.us/AssessmentAccountability/AcademicGrowth/NMSBA.html',
         file: 'nm/2016/output/nm.2017.1.public.charter.[level].txt',
