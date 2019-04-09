7e02b395ba187126de0635290bc3716276b73c25
Alexina Chong <achong@GSs-MacBook-Pro.local>
2019-04-09 11:35:57 -0700

    JT-8000 Add See More button to Districts In State module

--- client/app/bundles/GSWeb/react_components/community/districts_in_state.jsx
+++ client/app/bundles/GSWeb/react_components/community/districts_in_state.jsx
@@ -2,31 +2,28 @@ import React from "react";
 import PropTypes from "prop-types";
 import { t, capitalize } from "util/i18n";
 
-const renderButton = () => {
-  <div className="more-school-btn">
-          <a href={this.props.locality.districtsBrowseUrl}>
-            <button>See All Districts</button>
-          </a>
-        </div>
-}
-
-const DistrictsInState = ({districts}) => {
-  const districtItems = districts.map((district, idx )=> (
-    <li key={district.districtName}>
-      <a href={district.url}>{district.districtName}</a>
-      <div>
-        {district.enrollment ? <span><span>{district.enrollment.toLocaleString()} {t("students")}<span className="display-desktop"> | </span></span><div className="display-mobile"></div></span> : null}
-        <span>{t("Grades")}: {district.grades} | </span>
-        <span>{district.numSchools} {district.numSchools === 1 ? t("school" ): t("schools")}</span>
-      </div>
-      {idx !== districts.length - 1 ? <div className="blue-line" /> : null}
-    </li>
-  ));
+const DistrictsInState = ({districts, locality}) => {
+  // const districtItems = districts.map((district, idx )=> (
+  //   <li key={district.districtName}>
+  //     <a href={district.url}>{district.districtName}</a>
+  //     <div>
+  //       {district.enrollment ? <span><span>{district.enrollment.toLocaleString()} {t("students")}<span className="display-desktop"> | </span></span><div className="display-mobile"></div></span> : null}
+  //       <span>{t("Grades")}: {district.grades} | </span>
+  //       <span>{district.numSchools} {district.numSchools === 1 ? t("school" ): t("schools")}</span>
+  //     </div>
+  //     {idx !== districts.length - 1 ? <div className="blue-line" /> : null}
+  //   </li>
+  // ));
   return(
     <section className="districts-in-city-module">
       <ul>
-        {districtItems}
+        {"list" || districtItems}
       </ul>
+      <div className="more-school-btn">
+        <a href={locality.districtsBrowseUrl}>
+          <button>See All Districts</button>
+        </a>
+      </div>
     </section>
   )
 }
