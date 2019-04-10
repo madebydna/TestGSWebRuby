2b7119d89f1af3b085c87cd46d065df53c681270
Samson Sprouse <ssprouse@greatschools.org>
2019-04-10 14:11:16 -0700

    Fix buttongroup jsx spec

--- client/tests/react_components/buttongroup_spec.jsx
+++ client/tests/react_components/buttongroup_spec.jsx
@@ -12,91 +12,115 @@ describe('<ButtonGroup />', () => {
   let label;
   let allowDeselect;
   const onSelect = sinon.spy();
-  const options = {
-    e: 'Elementary',
-    m: 'Middle',
-    h: 'High',
-    p: 'Preschool'
-  };
+  const options = [
+    { key: 'e', label: 'Elementary' },
+    { key: 'm', label: 'Middle' },
+    { key: 'h', label: 'High' },
+    { key: 'p', label: 'Preschool' }
+  ];
   const buttonGroup = () =>
-      mount(<ButtonGroup options={options}
-                         onSelect={onSelect}
-                         activeOption={activeOption}
-                         multiple={multiple}
-                         label={label}
-                         allowDeselect={allowDeselect}
-      />);
+    mount(
+      <ButtonGroup
+        options={options}
+        onSelect={onSelect}
+        activeOption={activeOption}
+        multiple={multiple}
+        label={label}
+        allowDeselect={allowDeselect}
+      />
+    );
 
-  afterEach(() => (onSelect.reset()));
+  afterEach(() => onSelect.reset());
 
   context('With four options', () => {
-    it('Expect four buttons', () => expect(buttonGroup().find('button').length).to.eq(4));
+    it('Expect four buttons', () =>
+      expect(buttonGroup().find('button').length).to.eq(4));
 
     context('With elementary selected', () => {
       before(() => (activeOption = 'e'));
 
       it('Expect elementary to be selected', () => {
-        let activeButton = buttonGroup().find('button.active');
+        const activeButton = buttonGroup().find('button.active');
         expect(activeButton.length).to.eq(1);
         expect(activeButton.text()).to.eq('Elementary');
       });
 
       context('With allowDeselect false', () => {
-        before(() => allowDeselect = false);
+        before(() => (allowDeselect = false));
 
         context('When trying to click on elementary', () => {
           it('Nothing happens', () => {
-            buttonGroup().find('button.active').simulate('click');
+            buttonGroup()
+              .find('button.active')
+              .simulate('click');
             expect(onSelect.calledOnce).to.be.false;
           });
         });
 
         context('When trying to click on middle', () => {
           it('Triggers onSelect', () => {
-            buttonGroup().find('button').not('.active').first().simulate('click');
+            buttonGroup()
+              .find('button')
+              .not('.active')
+              .first()
+              .simulate('click');
             expect(onSelect.calledOnce).to.be.true;
           });
         });
       });
 
       context('With allowDeselect true', () => {
-        before(() => allowDeselect = true);
+        before(() => (allowDeselect = true));
 
         context('When trying to click on elementary', () => {
           it('Triggers onSelect', () => {
-            buttonGroup().find('button.active').simulate('click');
+            buttonGroup()
+              .find('button.active')
+              .simulate('click');
             expect(onSelect.calledOnce).to.be.true;
           });
         });
 
         context('When trying to click on middle', () => {
           it('Triggers onSelect', () => {
-            buttonGroup().find('button').not('.active').first().simulate('click');
+            buttonGroup()
+              .find('button')
+              .not('.active')
+              .first()
+              .simulate('click');
             expect(onSelect.calledOnce).to.be.true;
           });
         });
       });
 
       context('With multiple true', () => {
-        before(() => multiple = true);
+        before(() => (multiple = true));
 
         context('When clicking on middle', () => {
           it('should call the onSelect function with both e and m', () => {
-            buttonGroup().find('button').not('.active').first().simulate('click');
+            buttonGroup()
+              .find('button')
+              .not('.active')
+              .first()
+              .simulate('click');
             expect(onSelect.calledWith(['e', 'm'])).to.be.true;
-          })
+          });
         });
       });
 
       context('With multiple false', () => {
-        before(() => multiple = false);
+        before(() => (multiple = false));
 
         context('When clicking on middle', () => {
           it('should call the onSelect function with just m', () => {
-            buttonGroup().find('button').not('.active').first().simulate('click');
+            buttonGroup()
+              .find('button')
+              .not('.active')
+              .first()
+              .simulate('click');
             expect(onSelect.calledWith(['e', 'm'])).to.be.false;
             expect(onSelect.calledWith('m')).to.be.true;
-          })
+          });
         });
       });
     });
@@ -105,30 +129,33 @@ describe('<ButtonGroup />', () => {
       before(() => (activeOption = ['e', 'm']));
 
       it('Expect elementary and middle to be selected', () => {
-        let activeButton = buttonGroup().find('button.active');
+        const activeButton = buttonGroup().find('button.active');
         expect(activeButton.length).to.eq(2);
-        expect(activeButton.map((node) => node.text())).to.include('Elementary');
-        expect(activeButton.map((node) => node.text())).to.include('Middle');
-        expect(activeButton.map((node) => node.text())).to.not.include('High');
+        expect(activeButton.map(node => node.text())).to.include('Elementary');
+        expect(activeButton.map(node => node.text())).to.include('Middle');
+        expect(activeButton.map(node => node.text())).to.not.include('High');
       });
     });
 
     context('When choosing elementary', () => {
       before(() => (activeOption = ''));
 
-      it('should call the onSelect function with \'e\'', () => {
-        buttonGroup().find('button').first().simulate('click');
+      it("should call the onSelect function with 'e'", () => {
+        buttonGroup()
+          .find('button')
+          .first()
+          .simulate('click');
         expect(onSelect.calledWith('e')).to.be.true;
       });
     });
 
     context('With garbage selected', () => {
-      before(() => activeOption = 'Private');
+      before(() => (activeOption = 'Private'));
 
       it('Expect nothing to be selected', () => {
-        let activeButton = buttonGroup().find('button.active');
+        const activeButton = buttonGroup().find('button.active');
         expect(activeButton.length).to.eq(0);
       });
-    })
+    });
   });
 });
