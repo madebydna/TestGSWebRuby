bdeeafea3d6a1745e0cb53fa50bac811ed9ff329
Nina Magnuson <nmagnuson@Admins-MacBook-Pro-2.local>
2019-04-11 11:13:40 -0700

    DXT-3040 ND test

---
+++ lib/etl/test_processors/nd_test_processor_2015_NDSA.rb
@@ -0,0 +1,152 @@
+require_relative "../test_processor"
+
+class NDTestProcessor2015NDSA < GS::ETL::TestProcessor
+
+  def initialize(*args)
+    super
+    @year = 2015
+  end
+
+
+   map_nd_breakdown = {
+     'All' => 1,
+     'Black' => 17,
+     'Native American' => 18,
+     'Asian American' => 16,
+     'White' => 21,
+     'Low Income' => 23,
+     'English Learner' => 32,
+     'Female' => 26,
+     'Hispanic' => 19,
+     'Male' => 25,
+     'IEP (student with disabilities)' => 27,
+     'Native Hawaiian or Pacific Islander' => 20,
+     'Non-English Learner' => 33,
+     'Non-IEP' => 30,
+     'Non-Low Income' => 24,
+   }
+
+   map_nd_subject = {
+     :reading => 2,
+     :math => 5,
+     :science => 19
+   }
+
+
+ source("State_Level_NDSA_15.txt",[], col_sep: "\t") do |s|
+   s.transform("Fill values", Fill,{
+    entity_level: 'state'
+    })
+ end
+
+  source("School_Level_NDSA_15.txt",[], col_sep: "\t") do |s|
+   s.transform("Fill values", Fill,{
+    entity_level: 'school'
+   })
+    .transform('Rename column headers', MultiFieldRenamer,{
+     entity_name: :school_name,
+     entity_id: :school_id
+    })
+ end
+
+  source("District_Level_NDSA_15.txt",[], col_sep: "\t") do |s|
+   s.transform("Fill values", Fill,{
+    entity_level: 'district'
+   })
+   .transform('Rename column headers', MultiFieldRenamer,{
+     entity_name: :district_name,
+     entity_id: :district_id
+    })
+ end
+
+shared do |s|
+    s.transform('Fill missing default fields', Fill, {
+      entity_type: 'public_charter',
+      level_code: 'e,m,h',
+      proficiency_band_gsdata_id: 1,
+      proficiency_band: 'proficient and above',
+      test_data_type: 'NDSA',
+      test_data_type_id: 206,
+      year: 2015,
+      description: 'In 2014-2015 North Dakota used the North Dakota State Assessment (NDSA) to test students in grades 3 through 8 and 11 in reading and math, and in science in grades 4, 8 and 11.  2015 science results are unavailable at present.  Results represent students enrolled in the school for the entire academic year.  The NDSA is a standards-based test, which means it measures how well students are mastering the specific skills defined for each grade by the state of North Dakota.  The goal is for all students to score at or above the proficient level.',
+      notes: 'DXT-3040: ND 2015 NDSA'
+    })
+    .transform('Rename column headers', MultiFieldRenamer,{
+      subgroup_desc: :breakdown,
+      grades: :grade,
+      total_students: :number_tested,
+      entity_id: :state_id
+    })
+    .transform("Skip bad subgroups", DeleteRows, :breakdown, 'All Others', 'Migrant', 'Non-Migrant')
+    .transform("Adding column breakdown_id from group",
+      HashLookup, :breakdown, map_nd_breakdown, to: :breakdown_gsdata_id)
+    .transform("Filling in subject ids",
+    HashLookup, :subject, map_nd_subject, to: :academic_gsdata_id)
+    .transform("Fixing grade All name", WithBlock) do |row|
+     if row[:grade] =~ /All Grades/
+       row[:grade] = 'All'
+     else row[:grade] = row[:grade]
+     end
+     row
+   end
+   .transform("Delete rows where number tested is less than 10 and blank ",DeleteRows, :number_tested, '0','1','2','3','4','5','6','7','8','9')
+    .transform("Calc prof and above, ignore missing values", WithBlock) do |row|
+      if row[:pct_students_proficient] =~ /\-/ && row[:pct_students_advanced] =~ /\-/
+        row[:value_float] = row[:pct_students_proficient].to_f+row[:pct_students_advanced].to_f
+        row[:value_float] = 100*row[:value_float]
+      elsif row[:pct_students_novice] =~ /\-/ && row[:pct_students_partially_proficient] =~ /\-/
+        row[:value_float] = 1 - row[:pct_students_novice].to_f - row[:pct_students_partially_proficient].to_f
+        row[:value_float] = 100*row[:value_float]
+      end
+      row
+    end
+    .transform("fix special cases for prof and above", WithBlock) do |row|
+          if row[:value_float].nil?
+            row[:value_float]=row[:value_float]
+          elsif row[:value_float] < 0
+            row[:value_float] = 0
+          elsif row[:value_float] > 100
+            row[:value_float] = 100
+          elsif row[:value_float].between?(0,1)
+            row[:value_float]=row[:value_float].round(2)
+          end
+     row
+    end
+    .transform("Skip missing prof and above values", DeleteRows, :value_float, nil)
+    .transform("Creating StateID for schools and state", WithBlock) do |row|
+     if row[:entity_level] == 'school'
+        if row[:school_type] == 'Middle'
+          row[:state_id] = row[:state_id] + 2
+        elsif row[:school_type] == 'Elementary'
+          row[:state_id] = row[:state_id] + 1
+        elsif row[:school_type] == 'Secondary'
+          row[:state_id] = row[:state_id] + 3
+      elsif row[:entity_level] == 'state'
+        row[:state_id] = 'state'
+      end
+     end
+     row
+   end
+   .transform("bye bug", WithBlock) do |row|
+    require 'byebug'
+    byebug
+  end
+end
+
+
+
+  def config_hash
+    {
+        gsdata_source_id: 38,
+        state: 'nd',
+        source_name: 'North Dakota Department of Public Instruction',
+        date_valid: '2015-01-01 00:00:00',
+        url: 'https://insights.nd.gov/Data',
+        file: 'nd/2015/output/nd.2015.1.public.charter.[level].txt',
+        level: nil,
+        school_type: 'public,charter'
+    }
+  end
+end
+
+NDTestProcessor2015NDSA.new(ARGV[0], max: nil).run
