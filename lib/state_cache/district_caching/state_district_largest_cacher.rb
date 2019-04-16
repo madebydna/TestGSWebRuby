bd71e59b863475cc5e0de385bc482952fcfd58bd
Alexina Chong <achong@GSs-MacBook-Pro.local>
2019-04-16 10:20:44 -0700

    JT-8000 Modify State district largest cacher to use district_enrollment_cache method from community_concerns

--- lib/state_cache/district_caching/state_district_largest_cacher.rb
+++ lib/state_cache/district_caching/state_district_largest_cacher.rb
@@ -21,29 +21,29 @@ class StateDistrictLargestCacher < StateCacher
     end
   end
 
-  # AC_TODO: Refactor this to use district_enrollment_cache method to retrieve enrollment values
   def district_ids
     district_ids = District.ids_by_state(state)
     dc_sorted_ids = district_ids.sort do |dc_id1, dc_id2|
-      dc1 = district_characteristics(dc_id1, state)
-      d1 = enrollment_all_students(dc1)
-      dc2 = district_characteristics(dc_id2, state)
-      d2 = enrollment_all_students(dc2)
-      district_enrollment_value(d1) <=> district_enrollment_value(d2)
+      district_enrollment_cache(dc_id1) <=> district_enrollment_cache(dc_id2)
+      # dc1 = district_characteristics(dc_id1, state)
+      # d1 = enrollment_all_students(dc1)
+      # dc2 = district_characteristics(dc_id2, state)
+      # d2 = enrollment_all_students(dc2)
+      # district_enrollment_value(d1) <=> district_enrollment_value(d2)
     end
     dc_sorted_ids.length >= 5 ? dc_sorted_ids[-5..-1].reverse : dc_sorted_ids.reverse 
   end
 
-  def enrollment_all_students(district_characteristics_cache)
-    JSON.parse(district_characteristics_cache).fetch('Enrollment',[{}]).compact.select {|hash| hash['breakdown'] == 'All students' && hash['grade'].nil?} if district_characteristics_cache
-  end
+  # def enrollment_all_students(district_characteristics_cache)
+  #   JSON.parse(district_characteristics_cache).fetch('Enrollment',[{}]).compact.select {|hash| hash['breakdown'] == 'All students' && hash['grade'].nil?} if district_characteristics_cache
+  # end
 
-  def district_characteristics(district_id, state)
-    DistrictCache.for_district('district_characteristics', district_id, state )&.value
-  end
-  # rubocop:disable Lint/SafeNavigationChain
-  def district_enrollment_value(district_characteristics_results)
-    district_characteristics_results&.first.present? ? district_characteristics_results&.first['district_value'].to_i : 0
-  end
-  # rubocop:enable Lint/SafeNavigationChain
+  # def district_characteristics(district_id, state)
+  #   DistrictCache.for_district('district_characteristics', district_id, state )&.value
+  # end
+  # # rubocop:disable Lint/SafeNavigationChain
+  # def district_enrollment_value(district_characteristics_results)
+  #   district_characteristics_results&.first.present? ? district_characteristics_results&.first['district_value'].to_i : 0
+  # end
+  # # rubocop:enable Lint/SafeNavigationChain
 end
