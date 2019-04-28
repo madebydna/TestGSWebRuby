9e5264506f643fffd30f449fe62b3e32bdff08e0
Andy Luo <aluo@greatschools.org>
2019-04-27 18:09:06 -0700

    JT-8196 Allow serialized schools to be data driven rather than hardcoded

--- app/controllers/college_success_award_controller.rb
+++ app/controllers/college_success_award_controller.rb
@@ -11,11 +11,9 @@ class CollegeSuccessAwardController < ApplicationController
 
   layout 'application'
   before_filter :redirect_unless_valid_search_criteria # we need at least a 'q' param or state and city/district
-  before_action :redirect_unless_valid_csa_state
-
-  CSA_STATES = %w(ar co ct fl ga hi id in ky la ma mi mn mo ms mt nc nd ne nj oh ok tx vt wy)
 
   def search
+    redirect_to state_url(state_params(state)) unless serialized_schools.present?
     gon.search = {
       schools: serialized_schools,
     }.tap do |props|
@@ -158,10 +156,6 @@ class CollegeSuccessAwardController < ApplicationController
 
   def redirect_unless_valid_search_criteria
     true
-  end
-
-  def redirect_unless_valid_csa_state
-    redirect_to state_url(States.state_name(state)) unless CSA_STATES.include?(state)
   end
 
   # extra items returned even if not requested (besides school fields etc)
