d612b065226e0085833e2b3a8869e29db9151548
Anthony Roy <aroy@greatschools.org>
2019-05-03 16:31:20 -0700

    JT-8053 Fix spec depending on solr

--- spec/requests/api/schools_spec.rb
+++ spec/requests/api/schools_spec.rb
@@ -5,6 +5,8 @@ describe "Schools API" do
     clean_dbs :gs_schooldb, :ca, :us_geo
   end
 
+  before { stub_request(:post, "#{ENV_GLOBAL['solr.ro.server.url']}select?wt=json").to_return(status: 200, body: "{}", headers: {}) }
+
   before(:all) do
     head '/'
     @csrf_token = request.cookie_jar.fetch(:csrf_token)
