19a407959e30f223aeee174d5c013da12f12386d
Anthony Roy <aroy@greatschools.org>
2019-05-03 16:21:15 -0700

    JT-8053 Fix specs broken by solr upgrade

--- spec/services/search_suggest_school_spec.rb
+++ spec/services/search_suggest_school_spec.rb
@@ -7,18 +7,13 @@ describe SearchSuggestSchool do
       SearchSuggestSchool.new
     end
     let (:sample_result) { {
-        'state' => 'CA',
-        'school_profile_path' => '/path',
-        'school_name' => 'My School',
-        'school_id' => '1',
-        'city' => 'Oakland'} }
-    let (:sample_result_no_path) { {
-        'state' => 'CA',
-        'school_name' => 'My School',
-        'school_id' => '1',
-        'city' => 'Oakland'} }
+        state: 'CA',
+        url: '/1-path',
+        school: 'My School',
+        city: 'Oakland',
+        type: 'school'} }
     let (:sample_result_need_url_encoding) do
-        sample_result['school_profile_path'] = '/####I_need_encoding_!@#$%^&'
+        sample_result[:url] = '/1-####I_need_encoding_!@#$%^&'
         sample_result
     end
 
@@ -28,33 +23,13 @@ describe SearchSuggestSchool do
       expect(result[:school_name]).to eq('My School')
       expect(result[:id]).to eq('1')
       expect(result[:city_name]).to eq('Oakland')
-      expect(result[:url]).to eq('/path')
-    end
-    it 'falls back on fake url if necessary' do
-      result = subject.process_result(sample_result_no_path)
-      expect(result[:state]).to eq('CA')
-      expect(result[:school_name]).to eq('My School')
-      expect(result[:id]).to eq('1')
-      expect(result[:city_name]).to eq('Oakland')
-      expect(result[:url]).to eq('/california/city/1-school')
+      expect(result[:url]).to eq('/1-path')
     end
 
     it 'should encode the school url' do
-      encoding_path = URI.encode(sample_result_need_url_encoding['school_profile_path'])
+      encoding_path = URI.encode(sample_result_need_url_encoding[:url])
       result = subject.process_result(sample_result_need_url_encoding)
       expect(result[:url]).to eq (encoding_path)
-    end
-
-    context 'PK subdomains' do
-      it 'does not prepend for preschools' do
-        sample_prek_result = sample_result.merge('school_grade_level' => %w(p preschool))
-        result = subject.process_result(sample_prek_result)
-        expect(result[:url]).to eq('/path')
-      end
-      it 'does not prepend for non-preschools' do
-        result = subject.process_result(sample_result)
-        expect(result[:url]).to eq('/path')
-      end
     end
   end
 end
