e44fc3b7afe4fa3530a5cc67c5377b92a7e4f251
Andy Luo <aluo@greatschools.org>
2019-05-10 14:48:21 -0700

    JT-8315 removed zombie code

--- client/app/bundles/GSWeb/react_components/search/pagination_buttons.jsx
+++ client/app/bundles/GSWeb/react_components/search/pagination_buttons.jsx
@@ -34,7 +34,6 @@ const PaginationButtons = ({ page, totalPages, onPageChanged, mobileView }) => {
     options.push({
       key: '<',
       value: prev,
-      // label: <span className="icon-caret-down rotate-text-90" />,
       label: <span className="icon-chevron-right rotate-text-180" />,
       preventSelect: !prev
     });
@@ -48,7 +47,6 @@ const PaginationButtons = ({ page, totalPages, onPageChanged, mobileView }) => {
     options.push({
       key: '>',
       value: next,
-      // label: <span className="icon-caret-down rotate-text-270" />,
       label: <span className="icon-chevron-right" />,
       preventSelect: !next
     });
