d3c53bdb7fce056c44ab7bf2c40ac7597efa5c20
Andy Luo <aluo@greatschools.org>
2019-05-30 11:33:17 -0700

    JT-8351 Fix undefined issue that appears in the tool tips

--- client/app/bundles/GSWeb/react_components/search/tooltips.jsx
+++ client/app/bundles/GSWeb/react_components/search/tooltips.jsx
@@ -4,7 +4,14 @@ import { levelCodeLong } from 'util/school';
 import { t } from 'util/i18n';
 
 const renderAssignedTooltip = (lc) => {
-  let school_level = t(levelCodeLong(lc));
+  let school_level = lc.split(',').filter(s => s !== 'p').map(levelcode => t(levelCodeLong(levelcode)))
+
+  if (school_level.length == 2){
+    school_level = school_level.join(` ${t('and')} `)
+  } else if (school_level.length > 2){
+    school_level = school_level.slice(0, school_level.length - 2).join(` ${t('and')} `).concat(school_level[school_level.length - 1])
+  }
+  
   const content = (
     <div
   dangerouslySetInnerHTML={{
