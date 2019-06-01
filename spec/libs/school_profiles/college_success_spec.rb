95d6293d39355d85971da5c07614e4d026fe54cf
Andy Luo <aluo@greatschools.org>
2019-06-01 12:55:36 -0700

    Added specs for college success

--- spec/libs/school_profiles/college_success_spec.rb
+++ spec/libs/school_profiles/college_success_spec.rb
@@ -39,4 +39,62 @@ describe 'CollegeSuccess' do
     end
   end  
 
+  context '#csa_props' do
+    let(:school) { double("school") }
+    let(:csa_winner) do 
+      hash = { 
+        :csa_badge? => true,
+        :csa_awards => [
+          {"breakdowns"=>"non-frl award winning school", "school_value"=>"1", "source_date_valid"=>"20180101 00:00:00", "source_name"=>"GreatSchools"},
+          {"breakdowns"=>"non-frl award winning school", "school_value"=>"1", "source_date_valid"=>"20190101 00:00:00", "source_name"=>"GreatSchools", "grade"=>"All"}
+        ]
+      }
+      double("school_cache_data_reader", hash )
+    end
+    let(:multiple_csa_winner) do 
+      hash = { 
+        :csa_badge? => true,
+        :csa_awards => [
+          {"breakdowns"=>"non-frl award winning school", "school_value"=>"1", "source_date_valid"=>"20180101 00:00:00", "source_name"=>"GreatSchools"},
+          {"breakdowns"=>"non-frl award winning school", "school_value"=>"1", "source_date_valid"=>"20190101 00:00:00", "source_name"=>"GreatSchools", "grade"=>"All"},
+          {"breakdowns"=>"non-frl award winning school", "school_value"=>"1", "source_date_valid"=>"20200101 00:00:00", "source_name"=>"GreatSchools", "grade"=>"All"}
+        ]
+      }
+      double("school_cache_data_reader", hash )
+    end
+    
+    let(:not_a_csa_winner) { double("school_cache_data_reader", :csa_badge? => false) }
+
+    let(:non_winning_college_success) { SchoolProfiles::CollegeSuccess.new(
+        school_cache_data_reader: not_a_csa_winner
+    )}
+    let(:college_success) { SchoolProfiles::CollegeSuccess.new(
+        school_cache_data_reader: csa_winner
+    )}
+    let(:multiple_college_success) { SchoolProfiles::CollegeSuccess.new(
+        school_cache_data_reader: multiple_csa_winner
+    )}
+
+    it 'should return an empty hash when it is not a winner' do
+      expect(non_winning_college_success.csa_props).to eq({})
+    end
+
+    before do
+      allow(csa_winner).to receive(:school).and_return(school)
+      allow(multiple_csa_winner).to receive(:school).and_return(school)
+      allow(school).to receive(:state).and_return('ar')
+    end
+
+    it 'should return the correct csa award winnings years' do
+      expect(college_success.csa_award_winning_years).to eq([2018, 2019])
+      expect(multiple_college_success.csa_award_winning_years).to eq([2018, 2019, 2020])
+    end
+
+    it 'should return html syntax representing the correct years and state page urls' do
+      expect(college_success.csa_props[:csa_badge].include?('2018 and 2019')).to eq(true)
+      expect(multiple_college_success.csa_props[:csa_badge].include?('2018, 2019 and 2020')).to eq(true)
+      expect(multiple_college_success.csa_props[:csa_badge].include?('a href=/arkansas/college-success-award/>See more winners')).to eq(true)
+    end
+  end
+
 end
