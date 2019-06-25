5484347a52f90033634e06a35ade8def71cd0edc
Andy Luo <aluo@greatschools.org>
2019-06-24 17:07:25 -0700

    JT-8332 fix mapView on mobile for params changes

--- client/app/bundles/GSWeb/components/map/map.jsx
+++ client/app/bundles/GSWeb/components/map/map.jsx
@@ -137,7 +137,7 @@ export default class Map extends React.Component {
       this.setState({ markersUpdated: true});
     }
     // the following condition is to help reset the center of map when map object is not viewable but MapMarkers are changed
-    if(this.props.view === 'list' && this.props.view !== prevProps.view){
+    if((this.props.view === 'list' || this.props.view === 'map') && this.props.view !== prevProps.view){
       this.setState({ markersUpdated: true }, () => setTimeout(() => this.map.panBy(1, 1), 50));
     }
   }
