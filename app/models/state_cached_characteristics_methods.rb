d2d47f332f8768b8e7aa6b9aa1b009c5126f45ce
Mitchell Seltzer <mseltzer@greatschools.org>
2019-07-02 14:33:00 -0700

    JT-8495 - fix florida

--- app/models/state_cached_characteristics_methods.rb
+++ app/models/state_cached_characteristics_methods.rb
@@ -11,11 +11,11 @@ module StateCachedCharacteristicsMethods
   end
 
   def with_all_students
-    ->(hash) { hash['breakdown'] == 'All students' }
+    ->(hash) { hash['breakdown'] == 'All students' if hash }
   end
 
   def with_state_value
-    ->(hash) { hash['state_value'] }
+    ->(hash) { hash['state_value']  if hash}
   end
 
 end
