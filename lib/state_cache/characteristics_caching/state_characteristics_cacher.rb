93f5939d307229c3a59b4ac70f8efcfc0e74e27d
Andy Luo <aluo@greatschools.org>
2019-07-17 13:26:24 -0700

    JT-8626 first pass at college readiness and college success panel on State#Show Academics module

--- lib/state_cache/characteristics_caching/state_characteristics_cacher.rb
+++ lib/state_cache/characteristics_caching/state_characteristics_cacher.rb
@@ -3,7 +3,8 @@ class StateCharacteristicsCacher < StateCacher
 
   CACHE_KEY = 'state_characteristics'
   STATE_CHARACTERISTICS_CENSUS_DATA_TYPES = [1, 2, 3, 4, 5, 6, 8, 9, 12, 13, 17, 23, 26, 28, 30, 33, 41, 42,
-    102, 103, 123, 124, 129, 131, 133, 179, 296, 298, 319, 325, 417, 419]
+    102, 103, 123, 124, 129, 131, 133, 179, 296, 298, 319, 325, 417, 419, 443, 444, 445, 446, 447, 448, 449,
+    450, 451, 452, 453, 454, 455, 456, 457, 458, 459, 460, 461]
   # 1 - Percentage of teachers in their first year
   # 2 - Bachelor's degree
   # 3 - Master's degree
@@ -36,6 +37,23 @@ class StateCharacteristicsCacher < StateCacher
   # 325 - ACT percent college ready
   # 417 - Percent of Students Passing AP/IB Exams 
   # 419 - Percent of students who meet UC/CSU entrance requirements 
+  # 443 - Percent enrolled in any public in-state postsecondary institution within 12 months after graduation
+  # 444 - Percent enrolled in any postsecondary institution within 12 months after graduation
+  # 447 - Percent enrolled in any 2 year postsecondary institution within 6 months after graduation
+  # 448 - Percent enrolled in any 2 year postsecondary institution within the immediate fall after graduation
+  # 449 - Percent enrolled in any 2 year public in-state postsecondary institution within the immediate fall after graduation
+  # 450 - Percent enrolled in any 4 year postsecondary institution within 6 months after graduation
+  # 451 - Percent enrolled in any 4 year postsecondary institution within the immediate fall after graduation
+  # 452 - Percent enrolled in any 4 year public in-state postsecondary institution within the immediate fall after graduation
+  # 453 - Percent enrolled in any in-state postsecondary institution within 12 months after graduation
+  # 454 - Percent enrolled in any in-state postsecondary institution within the immediate fall after graduation
+  # 455 - Percent enrolled in any out-of-state postsecondary institution within the immediate fall after graduation
+  # 456 - Percent enrolled in any postsecondary institution within 24 months after graduation
+  # 457 - Percent enrolled in any postsecondary institution within 6 months after graduation
+  # 458 - Percent enrolled in any public in-state postsecondary institution or intended to enroll in any out-of-state institution, or in-state private institution within 18 months after graduation
+  # 459 - Percent enrolled in any public in-state postsecondary institution within the immediate fall after graduation
+  # 460 - Percent Enrolled in a public 4 year college and Returned for a Second Year
+  # 461 - Percent Enrolled in a public 2 year college and Returned for a Second Year
 
   def self.listens_to?(data_type)
     :state_characteristics == data_type
@@ -86,7 +104,6 @@ class StateCharacteristicsCacher < StateCacher
   def data_attributes
     [
         :breakdown,
-        :created,
         :grade,
         :state_average,
         :source,
