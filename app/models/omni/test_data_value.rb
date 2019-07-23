293df9c9890c6c9ce73bc887bd84b3ff7f163462
Osman Afridi <oafridi@greatschools.org>
2019-07-23 16:32:56 -0700

    JT-8674 Add by state query.

--- app/models/omni/test_data_value.rb
+++ app/models/omni/test_data_value.rb
@@ -91,4 +91,44 @@ class TestDataValue < ActiveRecord::Base
     result.map {|row| JSON.parse(row.to_json, object_class: OpenStruct)}
   end
 
+  # test scores gsdata - state
+  def self.by_state(state)
+    #todo validate date format for date valid
+    query = <<-SQL
+        select
+          tdv.value, 
+          ds.state, 
+          tdv.grade,
+          tdv.cohort_count,
+          tdv.proficiency_band_id,
+          b.name as breakdown_names,
+          bt.tag as breakdown_tags,      
+          s.name as academic_names,      
+          ds.data_type_id, 
+          ds.configuration, 
+          ss.name as source, 
+          ss.name as source_name, 
+          ds.date_valid,
+          ds.description,
+          dt.name     
+        from omni.test_data_values tdv 
+        join omni.data_sets ds on tdv.data_set_id = ds.id
+        join omni.data_types dt on dt.id = ds.data_type_id 
+        join omni.data_type_tags dtt on dtt.data_type_id = ds.data_type_id
+        join omni.breakdowns b on tdv.breakdown_id = b.id
+        join omni.breakdown_tags bt on bt.breakdown_id = b.id 
+        join omni.subjects s on tdv.subject_id = s.id
+        join omni.sources ss on ds.source_id = ss.id
+
+        where ds.state = '#{state}' 
+          and entity_type = 'state' 
+        and dtt.tag = 'state_test'
+        and tdv.active = 1
+        AND proficiency_band_id = 1
+    SQL
+
+    result = self.connection.exec_query(query)
+    result.map {|row| JSON.parse(row.to_json, object_class: OpenStruct)}
+  end
+
 end
