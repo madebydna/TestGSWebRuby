6eb1800b35dac72be67acc85a2a02d71cacccff0
Osman Afridi <oafridi@greatschools.org>
2019-07-23 17:37:04 -0700

    JT-8674:  # Please enter the commit message for your changes. Lines starting # with '#' will be ignored, and an empty message aborts the commit. # # On branch JT-8674 # Changes to be committed: # modified: app/models/omni/test_data_value.rb # # Changes not staged for commit: # modified: app/models/gsdata/data_value.rb # modified: app/models/gsdata/load.rb # # ------------------------ >8 ------------------------ # Do not touch the line above. # Everything below will be removed. diff --git a/app/models/omni/test_data_value.rb b/app/models/omni/test_data_value.rb index 820c3871f..394d2f588 100644 --- a/app/models/omni/test_data_value.rb +++ b/app/models/omni/test_data_value.rb @@ -7,8 +7,10 @@ class TestDataValue < ActiveRecord::Base db_magic connection: :omni - # test scores gsdata!!!! proficiency is always 1 - def self.by_school(school) + #todo validate date format for date valid + + # test scores gsdata proficiency is always 1 + def self.by_school(school_state, school_id) query = <<-SQL select tdv.value, @@ -38,9 +40,9 @@ class TestDataValue < ActiveRecord::Base join omni.subject_tags st on st.subject_id = s.id join omni.sources ss on ds.source_id = ss.id - where ds.state = '#{school.state}' + where ds.state = '#{school_state}' and entity_type = 'school' - and gs_id = #{school.id} + and gs_id = #{school_id} and dtt.tag = 'state_test' and tdv.active = 1 and proficiency_band_id = 1 @@ -93,7 +95,6 @@ class TestDataValue < ActiveRecord::Base # test scores gsdata - state def self.by_state(state) - #todo validate date format for date valid query = <<-SQL select tdv.value, @@ -131,4 +132,46 @@ class TestDataValue < ActiveRecord::Base result.map {|row| JSON.parse(row.to_json, object_class: OpenStruct)} end + def self.feeds_by_school(school_state, school_id) + query = <<-SQL + select + tdv.value, + ds.state, + tdv.gs_id as school_id, + tdv.grade, + tdv.cohort_count, + tdv.proficiency_band_id, + b.name as breakdown_names, + bt.tag as breakdown_tags, + s.name as academic_names, + st.tag as academic_tags, + ds.data_type_id, + ds.configuration, + ss.name as source, + ss.name as source_name, + ds.date_valid, + ds.description, + dt.name + from omni.test_data_values tdv + join omni.data_sets ds on tdv.data_set_id = ds.id + join omni.data_types dt on dt.id = ds.data_type_id + join omni.data_type_tags dtt on dtt.data_type_id = ds.data_type_id + join omni.breakdowns b on tdv.breakdown_id = b.id + join omni.breakdown_tags bt on bt.breakdown_id = b.id + join omni.subjects s on tdv.subject_id = s.id + join omni.subject_tags st on st.subject_id = s.id + join omni.sources ss on ds.source_id = ss.id + + where ds.state = '#{school_state}' + and entity_type = 'school' + and gs_id = #{school_id} + and ds.configuration like '%feeds%' + and dtt.tag = 'state_test' + and tdv.active = 1 + SQL + + result = self.connection.exec_query(query) + result.map {|row| JSON.parse(row.to_json, object_class: OpenStruct)} + end + end \ No newline at end of file

--- app/models/omni/test_data_value.rb
+++ app/models/omni/test_data_value.rb
@@ -7,8 +7,10 @@ class TestDataValue < ActiveRecord::Base
 
   db_magic connection: :omni
 
-  # test scores gsdata!!!!  proficiency is always 1
-  def self.by_school(school)
+  #todo validate date format for date valid
+
+  # test scores gsdata  proficiency is always 1
+  def self.by_school(school_state, school_id)
     query = <<-SQL
     select
       tdv.value, 
@@ -38,9 +40,9 @@ class TestDataValue < ActiveRecord::Base
     join omni.subject_tags st on st.subject_id = s.id    
     join omni.sources ss on ds.source_id = ss.id
     
-    where ds.state = '#{school.state}' 
+    where ds.state = '#{school_state}' 
       and entity_type = 'school' 
-      and gs_id = #{school.id} 
+      and gs_id = #{school_id} 
     and dtt.tag = 'state_test'
     and tdv.active = 1
     and proficiency_band_id = 1
@@ -93,7 +95,6 @@ class TestDataValue < ActiveRecord::Base
 
   # test scores gsdata - state
   def self.by_state(state)
-    #todo validate date format for date valid
     query = <<-SQL
         select
           tdv.value, 
@@ -125,6 +126,48 @@ class TestDataValue < ActiveRecord::Base
         and dtt.tag = 'state_test'
         and tdv.active = 1
         AND proficiency_band_id = 1
+    SQL
+
+    result = self.connection.exec_query(query)
+    result.map {|row| JSON.parse(row.to_json, object_class: OpenStruct)}
+  end
+
+  def self.feeds_by_school(school_state, school_id)
+    query = <<-SQL
+    select
+      tdv.value, 
+      ds.state, 
+      tdv.gs_id as school_id,
+      tdv.grade,
+      tdv.cohort_count,
+      tdv.proficiency_band_id,
+      b.name as breakdown_names,
+      bt.tag as breakdown_tags,      
+      s.name as academic_names,      
+      st.tag as academic_tags,      
+      ds.data_type_id, 
+      ds.configuration, 
+      ss.name as source, 
+      ss.name as source_name, 
+      ds.date_valid,
+      ds.description,
+      dt.name       
+    from omni.test_data_values tdv 
+    join omni.data_sets ds on tdv.data_set_id = ds.id
+    join omni.data_types dt on dt.id = ds.data_type_id 
+    join omni.data_type_tags dtt on dtt.data_type_id = ds.data_type_id
+    join omni.breakdowns b on tdv.breakdown_id = b.id
+    join omni.breakdown_tags bt on bt.breakdown_id = b.id 
+    join omni.subjects s on tdv.subject_id = s.id    
+    join omni.subject_tags st on st.subject_id = s.id    
+    join omni.sources ss on ds.source_id = ss.id
+
+    where ds.state = '#{school_state}' 
+      and entity_type = 'school' 
+      and gs_id = #{school_id} 
+      and ds.configuration like '%feeds%' 
+      and dtt.tag = 'state_test'
+      and tdv.active = 1
     SQL
 
     result = self.connection.exec_query(query)
