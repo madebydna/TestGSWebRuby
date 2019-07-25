5630be592d9045f8c6c06447deffe37df84816ff
Andy Luo <aluo@greatschools.org>
2019-07-24 23:44:29 -0700

    JT-8645 updated script_logger_specs

--- spec/models/utils/script_logger_spec.rb
+++ spec/models/utils/script_logger_spec.rb
@@ -5,15 +5,24 @@ describe ScriptLogger do
     do_clean_models(:gs_schooldb, ScriptLogger)
   end
 
-  context "ScriptLogger::record_log_instance" do
+  context ".record_log_instance" do
     it "should save the instance to the database" do
-      ScriptLogger.record_log_instance("NewCachePopulator", {"params" => "some params"})
-      expect(ScriptLogger.where(script_name: "NewCachePopulator").first.arguments).to eq({"params" => "some params"}.to_s)
+      ScriptLogger.record_log_instance({"params" => "some params"})
+      expect(ScriptLogger.where(output: nil).first.arguments).to eq({"params" => "some params"}.to_s)
     end
 
     it "should return the ActiveRecord object after successful initialization" do
-      log = ScriptLogger.record_log_instance("SomeOtherCachePopulator", {"params" => "new params"})
-      expect(ScriptLogger.where(script_name: "SomeOtherCachePopulator").first).to eq(log)
+      hash = {"params" => "new params"}
+      log = ScriptLogger.record_log_instance(hash)
+      expect(ScriptLogger.find_by_arguments(hash.to_s)).to eq(log)
+    end
+  end
+
+  context "#finish_logging_session" do
+    it "update the instance" do
+      log = ScriptLogger.find_by_output(nil)
+      log.finish_logging_session(1,"Great Output!")
+      expect(ScriptLogger.where(output: "Great Output!").first.succeeded).to eq(true)
     end
   end
 end
