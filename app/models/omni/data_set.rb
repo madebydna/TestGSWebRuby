defb1b942383ec427f12e57100cf3993b2008b60
Osman Afridi <oafridi@greatschools.org>
2019-07-25 15:47:20 -0700

    JT-8674 Add model for sources.

--- app/models/omni/data_set.rb
+++ app/models/omni/data_set.rb
@@ -3,11 +3,17 @@
 require 'ruby-prof'
 
 class DataSet < ActiveRecord::Base
-  self.table_name = 'data_sets'
 
   db_magic connection: :omni
+
   has_many :test_data_values
   has_many :data_type_tags
   belongs_to :data_type
+  belongs_to :source, class_name: 'Omni::Source'
 
+  scope :feeds, -> { where(configuration: 'feeds') }
+  scope :web, -> { where(configuration: 'web') }
+
+  scope :by_state, -> (state) { where('state = ?', state) }
+  scope :feeds_by_state, -> (state) { feeds.by_state(state) }
 end
