698b8f096dfde94736c28a036928a05b7081c8a2
Andy Luo <aluo@greatschools.org>
2019-07-26 11:35:26 -0700

    JT-8645 updated spec for script logger

--- spec/models/utils/script_logger_spec.rb
+++ spec/models/utils/script_logger_spec.rb
@@ -6,23 +6,30 @@ describe ScriptLogger do
   end
 
   context ".record_log_instance" do
+    before(:all) do
+      ScriptLogger.record_log_instance([{"params" => "some params"}])
+      ScriptLogger.record_log_instance([{"params" => "new params"}])
+    end
+
     it "should save the instance to the database" do
-      ScriptLogger.record_log_instance({"params" => "some params"})
-      expect(ScriptLogger.where(output: nil).first.arguments).to eq({"params" => "some params"}.to_s)
+      expect(ScriptLogger.where(output: nil).length).to eq(2)
     end
 
     it "should return the ActiveRecord object after successful initialization" do
-      hash = {"params" => "new params"}
-      log = ScriptLogger.record_log_instance(hash)
-      expect(ScriptLogger.find_by_arguments(hash.to_s)).to eq(log)
+      arr = [{"params" => "new params"}].to_s
+      log = ScriptLogger.find_by(arguments: arr)
+      expect(log).to be_truthy
     end
   end
 
   context "#finish_logging_session" do
+    before(:all) do
+      ScriptLogger.record_log_instance("test finishing log")
+    end
     it "update the instance" do
       log = ScriptLogger.find_by_output(nil)
       log.finish_logging_session(1,"Great Output!")
-      expect(ScriptLogger.where(output: "Great Output!").first.succeeded).to eq(true)
+      expect(ScriptLogger.find_by_output("Great Output!").succeeded).to eq(true)
     end
   end
 end
