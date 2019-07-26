9e639b93e4556ac36e13151c766fb90fe698c698
Osman Afridi <oafridi@greatschools.org>
2019-07-26 09:26:26 -0700

    JT-8674 Add initial test.

--- spec/models/omni/test_data_value_spec.rb
+++ spec/models/omni/test_data_value_spec.rb
@@ -1,16 +1,45 @@
 require 'spec_helper'
 
 describe TestDataValue do
-  # after { clean_dbs :gsdata }
 
-  describe 'expired scope' do
-    it 'should return expired rows' do
-      expect(BannedIp.expired).to include expired_banned_ip
+  after { clean_dbs :omni }
+
+  describe '.feeds_by_state' do
+    it 'returns a object that can be used ' do
+      []
+      #<OpenStruct
+      # value="7.0",
+      # state="CA",
+      # grade="8",
+      # cohort_count=407824,
+      # proficiency_band_id=63,
+      # proficiency_band_name="far below basic",
+      # breakdown_names="General-Education students",
+      # breakdown_id_list="30",
+      # academic_names="Science",
+      # data_type_id=298,
+      # configuration="feeds",
+      # source="California Department of Education",
+      # source_name="California Department of Education",
+      # date_valid="20150101 00:00:00",
+      # description="In 2014-2015 California used the California Standards Tests
+      # (CSTs) to test students in science in grades 5, 8 and 10.
+      # The CSTs are standards-based tests, which means they measure
+      # how well students are mastering specific skills defined for each grade
+      # by the state of California. The goal is for all students to score at or
+      # above proficient on the tests.",
+      # name="California Standards Tests">
+
+      tdv  = TestDataValue.create(entity_type: 'state',
+                            gs_id: 1,
+                            data_set_id: 1,
+                            value: 1, proficiency_band_id: 1)
+
+
+
+
     end
 
-    it 'should consider new rows as not expired' do
-      expect(BannedIp.expired).to_not include banned_ip
-    end
   end
 
 end
