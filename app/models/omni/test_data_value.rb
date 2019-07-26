ba58d15af372e3b5dd49b5b08fa7ca849960a76a
Osman Afridi <oafridi@greatschools.org>
2019-07-25 17:16:09 -0700

    JT-8674 Update queries.

--- app/models/omni/test_data_value.rb
+++ app/models/omni/test_data_value.rb
@@ -13,136 +13,73 @@ class TestDataValue < ActiveRecord::Base
 
   has_many :data_sets
 
-  ENTITIES = %w(nation state district school).freeze
-
   scope :state_entity, -> { where(entity_type: 'state') }
   scope :district_entity, -> { where(entity_type: 'district') }
   scope :school_entity, -> { where(entity_type: 'school') }
   scope :active, -> { where(active: 1) }
+  scope :default_proficiency, -> { where(proficiency_band_id: 1) }
 
-  #todo validate date format for date valid
-
-  def self.web_by_school(school_state, school_id)
-    # where ds.state = '#{school_state}'
-    #   and entity_type = 'school'
-    #   and gs_id = #{school_id}
-    # and dtt.tag = 'state_test'
-    # and tdv.active = 1
-    # and proficiency_band_id = 1
-    select(common_cache_keys)
-        .joins(data_set: [:data_type, :source])
-        .joins("join data_type_tags on data_type_tags.data_type_id = data_sets.data_type_id")
-        .with_breakdowns
-        .with_subjects
-        .joins(:proficiency_band)
-        .where(data_type_tags: { tag: 'state_test' })
-        .merge(DataSet.by_state(state))
-        .district_entity
-        .active
-        .where(gs_id: district_id)
+  def self.web_by_school(state, id)
+    select(common_cache_keys).common_web_query(state).school_entity.where(gs_id: id).default_proficiency
   end
 
-  def self.web_by_district(state, district_id)
-    # where ds.state = '#{state}'
-    #   and entity_type = 'district'
-    #   and gs_id = #{district_id}
-    # and dtt.tag = 'state_test'
-    # and tdv.active = 1
-    # AND proficiency_band_id = 1
-    select(common_cache_keys)
-        .joins(data_set: [:data_type, :source])
-        .joins("join data_type_tags on data_type_tags.data_type_id = data_sets.data_type_id")
-        .with_breakdowns
-        .joins("left join subjects on subjects.id = subject_id")
-        .joins("left join subject_tags on subjects.id = subject_tags.subject_id")
-        .joins(:proficiency_band)
-        .where(data_type_tags: { tag: 'state_test' })
-        .merge(DataSet.by_state(state))
-        .district_entity
-        .active
-        .where(gs_id: district_id)
+  def self.web_by_district(state, id)
+    select(common_cache_keys).common_web_query(state).district_entity.where(gs_id: id)
   end
 
   def self.web_by_state(state)
-    # where ds.state = '#{state}'
-    #   and entity_type = 'state'
-    # and dtt.tag = 'state_test'
-    # and tdv.active = 1
-    # AND proficiency_band_id = 1
-
-    select(common_cache_keys)
-        .joins(data_set: [:data_type, :source])
-        .joins("join data_type_tags on data_type_tags.data_type_id = data_sets.data_type_id")
-        .with_breakdowns
-        .joins("left join subjects on subjects.id = subject_id")
-        .joins("left join subject_tags on subjects.id = subject_tags.subject_id")
-        .joins(:proficiency_band)
-        .where(data_type_tags: { tag: 'state_test' })
-        .merge(DataSet.by_state(state))
-        .district_entity
-        .active
-        .where(gs_id: district_id)
+    select(common_cache_keys).common_web_query(state).state_entity
   end
 
-  def self.feeds_by_school(school_state, school_id)
-    # where ds.state = '#{school_state}'
-    #   and entity_type = 'school'
-    #   and gs_id = #{school_id}
-    #   and ds.configuration like '%feeds%'
-    #   and dtt.tag = 'state_test'
-    #   and tdv.active = 1
-    select(common_cache_keys)
-        .joins(data_set: [:data_type, :source])
-        .joins("join data_type_tags on data_type_tags.data_type_id = data_sets.data_type_id")
-        .with_breakdowns
-        .joins("left join subjects on subjects.id = subject_id")
-        .joins("left join subject_tags on subjects.id = subject_tags.subject_id")
-        .joins(:proficiency_band)
-        .where(data_type_tags: { tag: 'state_test' })
-        .merge(DataSet.by_state(state))
-        .district_entity
-        .active
-        .where(gs_id: district_id)
+  def self.feeds_by_school(state, school_id)
+    select(common_cache_keys).common_feeds_query(state).school_entity.where(gs_id: school_id)
   end
 
   def self.feeds_by_district(state, district_id)
-    select(common_cache_keys)
-        .joins(data_set: [:data_type, :source])
-        .joins("join data_type_tags on data_type_tags.data_type_id = data_sets.data_type_id")
-        .with_breakdowns
-        .joins("left join subjects on subjects.id = subject_id")
-        .joins("left join subject_tags on subjects.id = subject_tags.subject_id")
-        .joins(:proficiency_band)
-        .where(data_type_tags: { tag: 'state_test' })
-        .merge(DataSet.by_state(state))
-        .district_entity
-        .active
-        .where(gs_id: district_id)
+    select(common_cache_keys).common_feeds_query(state).district_entity.where(gs_id: district_id)
   end
 
   def self.feeds_by_state(state)
-    select(common_cache_keys)
-        .joins(data_set: [:data_type, :source])
+    select(common_cache_keys).common_feeds_query(state).state_entity
+  end
+
+  def self.common_web_query(state)
+    common_query.merge(DataSet.by_state(state))
+  end
+
+  def self.common_feeds_query(state)
+    common_query.merge(DataSet.feeds_by_state(state))
+  end
+
+  def self.common_query
+    joins(data_set: [:data_type, :source])
         .joins("join data_type_tags on data_type_tags.data_type_id = data_sets.data_type_id")
         .with_breakdowns
+        .with_breakdown_tags
         .with_subjects
+        .with_subject_tags
         .joins(:proficiency_band)
         .where(data_type_tags: { tag: 'state_test' })
-        .merge(DataSet.feeds_by_state(state))
-        .state_entity
         .active
   end
 
   def self.with_breakdowns
     joins("left join breakdowns on breakdown_id = breakdowns.id")
-        .joins("left join breakdown_tags on breakdown_tags.breakdown_id = breakdowns.id")
+  end
+
+  def self.with_breakdown_tags
+    joins("left join breakdown_tags on breakdown_tags.breakdown_id = breakdowns.id")
   end
 
   def self.with_subjects
     joins("left join subjects on subjects.id = subject_id")
-        .joins("left join subject_tags on subjects.id = subject_tags.subject_id")
   end
 
+  def self.with_subject_tags
+    joins("left join subject_tags on subjects.id = subject_tags.subject_id")
+  end
+
+  #todo validate date format for date valid
   def self.common_cache_keys
     [:value,
      :grade,
