63ac7206fd2db6b0d58dd5e4ab5bc24d9279a85b
Osman Afridi <oafridi@greatschools.org>
2019-07-31 16:00:08 -0700

    JT-8875 Override to_hash behavior to accommodate dropping by subject ratings.

--- lib/school_cache/ratings_caching/gsdata_ratings_cacher.rb
+++ lib/school_cache/ratings_caching/gsdata_ratings_cacher.rb
@@ -22,10 +22,6 @@ class RatingsCaching::GsdataRatingsCacher < GsdataCaching::GsdataCacher
   # 187: CSA Badge
 
 
-
-
-
-
   DATA_TYPE_IDS = %w(151 155 156 157 158 159 160 175 176 177 178 179 180 181 182 183 184 185 186 187).freeze
 
   ADVANCED_COURSEWORK_DATA_TYPE_ID = 151
@@ -40,31 +36,29 @@ class RatingsCaching::GsdataRatingsCacher < GsdataCaching::GsdataCacher
 
   def build_hash_for_cache
     @_build_hash_for_cache ||= (
-      school_cache_hash = Hash.new { |h, k| h[k] = [] }
-      r = school_results
+    school_cache_hash = Hash.new { |h, k| h[k] = [] }
 
-      # filter out some data values
-      r = r.group_by(&:data_type_id).reduce([]) do |accum, (data_type_id, data_values)|
-        most_recent_date = data_values.map(&:date_valid).max
-        # remove all data values except those with most recent date except for CSA awards (data_type_id 187)
-         if data_type_id != CSA_AWARD_DATA_TYPE_ID
-          data_values.select! { |dv| dv.date_valid == most_recent_date }
-         end
-        # select correct advanced coursework data values
-        if data_type_id == ADVANCED_COURSEWORK_DATA_TYPE_ID
-          data_values.select! do |dv|
-            advanced_coursework_select_logic?(dv)
-          end
+    # filter out some data values
+    r = school_results.group_by(&:data_type_id).reduce([]) do |accum, (data_type_id, data_values)|
+      most_recent_date = data_values.map(&:date_valid).max
+      # remove all data values except those with most recent date except for CSA awards (data_type_id 187)
+      if data_type_id != CSA_AWARD_DATA_TYPE_ID
+        data_values.select! { |dv| dv.date_valid == most_recent_date }
+      end
+      # select correct advanced coursework data values
+      if data_type_id == ADVANCED_COURSEWORK_DATA_TYPE_ID
+        data_values.select! do |dv|
+          advanced_coursework_select_logic?(dv)
         end
-        accum.concat(data_values)
       end
+      accum.concat(data_values)
+    end
 
-      r.each_with_object(school_cache_hash) do |result, cache_hash|
-        result_hash = result_to_hash(result)
-        result_hash[:description] = result.description if result.description    #source.description
-        next unless validate_result_hash(result_hash, result.data_type_id)
-        cache_hash[result.name] << result_hash
-      end
+    r.each_with_object(school_cache_hash) do |result, cache_hash|
+      result_hash = result_to_hash(result)
+      next unless validate_result_hash(result_hash, result.data_type_id)
+      cache_hash[result.name] << result_hash
+    end
     )
   end
 
@@ -79,22 +73,22 @@ class RatingsCaching::GsdataRatingsCacher < GsdataCaching::GsdataCacher
     breakdown_tags_arr = breakdown_tags.split(',')
     academic_names_arr = (dv['academic_names'] || '').split(',')
     academic_tags_arr = (dv['academic_tags'] || '').split(',')
-     (
-      ( # this selects coursework for all students
-          breakdown_names_arr.include?(ALL_STUDENTS) &&
-          academic_tags_arr.include?(COURSE_SUBJECT_GROUP)
-      ) ||
-      ( # this selects all students overall data
-          breakdown_names == ALL_STUDENTS &&
-          breakdown_tags == 'all_students'
-      ) ||
-      ( # this is to select ethnicity data
-          breakdown_names_arr.length == 1 &&
-          breakdown_tags_arr.include?(BREAKDOWN_TAG_ETHNICITY) &&
-          academic_names_arr.length.zero? &&
-          academic_tags_arr.length.zero?
-      )
-     )
+    (
+    (# this selects coursework for all students
+    breakdown_names_arr.include?(ALL_STUDENTS) &&
+        academic_tags_arr.include?(COURSE_SUBJECT_GROUP)
+    ) ||
+        (# this selects all students overall data
+        breakdown_names == ALL_STUDENTS &&
+            breakdown_tags == 'all_students'
+        ) ||
+        (# this is to select ethnicity data
+        breakdown_names_arr.length == 1 &&
+            breakdown_tags_arr.include?(BREAKDOWN_TAG_ETHNICITY) &&
+            academic_names_arr.length.zero? &&
+            academic_tags_arr.length.zero?
+        )
+    )
   end
 
   def test_score_only?
@@ -105,19 +99,19 @@ class RatingsCaching::GsdataRatingsCacher < GsdataCaching::GsdataCacher
   def gs_rating_data_value
     rating_object = test_score_only? ? build_hash_for_cache['Test Score Rating'] : build_hash_for_cache['Summary Rating']
     (rating_object || [])
-      .map { |h| GsdataCaching::GsDataValue.from_hash(h) }
-      .extend(GsdataCaching::GsDataValue::CollectionMethods)
-      .for_all_students
-      .having_school_value
-      .expect_only_one('Should only find one summary rating', id: school.id, state: school.state)
+        .map { |h| GsdataCaching::GsDataValue.from_hash(h) }
+        .extend(GsdataCaching::GsDataValue::CollectionMethods)
+        .for_all_students
+        .having_school_value
+        .expect_only_one('Should only find one summary rating', id: school.id, state: school.state)
   end
 
   def school_cache
     return @_school_cache if defined?(@_school_cache)
     @_school_cache = SchoolCache.find_or_initialize_by(
-      school_id: school.id,
-      state: school.state,
-      name:self.class::CACHE_KEY
+        school_id: school.id,
+        state: school.state,
+        name: self.class::CACHE_KEY
     )
   end
 
@@ -126,9 +120,9 @@ class RatingsCaching::GsdataRatingsCacher < GsdataCaching::GsdataCacher
 
     if gs_rating_data_value.present?
       replace_rating_into_school_metadata(
-        school.id,
-        school.state.downcase,
-        gs_rating_data_value.school_value
+          school.id,
+          school.state.downcase,
+          gs_rating_data_value.school_value
       )
     else
       delete_rating_row_from_school_metadata(school.id, school.state.downcase)
@@ -151,7 +145,7 @@ class RatingsCaching::GsdataRatingsCacher < GsdataCaching::GsdataCacher
     rescue ActiveRecord::RecordNotUnique
       retry_count += 1
       retry if retry_count < 2
-      GSLogger.error(:school_cache, nil, message: 'Endless loop inserting to school_metadata', vars: {school_id: school_id, state: state, rating: rating})
+      GSLogger.error(:school_cache, nil, message: 'Endless loop inserting to school_metadata', vars: { school_id: school_id, state: state, rating: rating })
       raise
     end
   end
@@ -161,6 +155,20 @@ class RatingsCaching::GsdataRatingsCacher < GsdataCaching::GsdataCacher
     SchoolMetadata.on_db("#{state}_rw") do
       query = "delete from #{table_name_prefix}school_metadata where school_id = #{school_id} and meta_key='overallRating';"
       SchoolMetadata.connection.execute(query)
+    end
+  end
+
+  def result_to_hash(result)
+    breakdowns = result.breakdown_names
+    breakdown_tags = result.breakdown_tags
+
+    {}.tap do |h|
+      h[:breakdowns] = breakdowns if breakdowns
+      h[:breakdown_tags] = breakdown_tags if breakdown_tags
+      h[:school_value] = result.value
+      h[:source_date_valid] = result.date_valid
+      h[:source_name] = result.source
+      h[:description] = result.description if result.description #source.description
     end
   end
 end
