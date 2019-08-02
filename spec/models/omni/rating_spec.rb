948bb76e3ab0111c003497be45e0896852704524
Osman Afridi <oafridi@greatschools.org>
2019-08-02 10:01:55 -0700

    JT-8875 Update specs with factories.

--- spec/models/omni/rating_spec.rb
+++ spec/models/omni/rating_spec.rb
@@ -6,19 +6,12 @@ describe Omni::Rating do
   before { clean_dbs :omni, :ca }
 
   describe ".by_school(state, id)" do
-    let(:data_set) do
-      Omni::DataSet.create(data_type_id: data_type.id,
-                           source_id: source.id,
-                           notes: "test",
-                           state: school.state)
-    end
-
-    let(:source) { Omni::Source.create(name: 'Foo') }
+    let(:data_set) { create(:data_set, state: school.state, data_type: data_type) }
     let(:data_type) { Omni::DataType.create(name: "test") }
     let(:school) { create(:school) }
     let!(:data_type_tag) { Omni::DataTypeTag.create(data_type_id: data_set.data_type_id, tag: 'rating') }
 
-    it 'the return object has the required keys' do
+    it 'returns an object that has the required keys' do
       Omni::Rating.create(entity_type: Omni::Rating::SCHOOL_ENTITY,
                           gs_id: school.id,
                           data_set_id: data_set.id,
