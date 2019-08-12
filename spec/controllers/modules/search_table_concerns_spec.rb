c45277ee0f9b0615ab262ae4888dedfb665acee2
Andy Luo <aluo@greatschools.org>
2019-08-12 13:09:22 -0700

    JT-8786 updated specs for search table concerns

--- spec/controllers/modules/search_table_concerns_spec.rb
+++ spec/controllers/modules/search_table_concerns_spec.rb
@@ -6,10 +6,8 @@ describe SearchTableConcerns do
   end
 
   let(:dummy_controller) { DummyController.new }
-  let(:dummy_controller2) { DummyController.new }
-  let(:dummy_controller3) { DummyController.new }
 
-  let(:array_of_schools) {
+  let(:array_of_growth_data_state_and_all_subjects_remediation_schools) {
     [
       {
         :name => 'Growth Data State Rating School',
@@ -22,7 +20,7 @@ describe SearchTableConcerns do
         }]
       },
       {
-        :name => 'Growth Proxy State Rating School',
+        :name => 'Growth Proxy State Rating School That Shouldn\'t Be here',
         :subratings => {
           'Academic Progress Rating' => 10
         },
@@ -32,7 +30,7 @@ describe SearchTableConcerns do
         }]
       },
       {
-        :name => 'Cowabunga',
+        :name => 'Another Growth Data State School with Outlier Remediation Data',
         :subratings => {
           'Student Progress Rating' => 6
         },
@@ -43,10 +41,10 @@ describe SearchTableConcerns do
       }
     ]
   }
-  let(:array_of_schools2) {
+  let(:array_of_growth_proxy_state_and_specific_subjects_remediation_schools) {
     [
       {
-        :name => 'New Proxy Rating School',
+        :name => 'Proxy Rating School',
         :subratings => {
           'Academic Progress Rating' => 7
         },
@@ -63,7 +61,7 @@ describe SearchTableConcerns do
         ]
       },
       {
-        :name => 'Proxy Rating School',
+        :name => 'New Proxy Rating School',
         :subratings => {
           'Academic Progress Rating' => 10
         },
@@ -93,62 +91,59 @@ describe SearchTableConcerns do
   }
   let(:empty_array) {[]}
 
-  before(:each) do
-    allow(dummy_controller).to receive(:serialized_schools).and_return(array_of_schools)
-    allow(dummy_controller2).to receive(:serialized_schools).and_return(array_of_schools2)
-  end
-
-  describe '#growth_progress_rating_header' do
-    it 'returns the right header if the state has growth data' do
+  describe 'Cache Data for a Growth Data State with All Subjects Data for Remediation' do
+    before {allow(dummy_controller).to receive(:serialized_schools).and_return(array_of_growth_data_state_and_all_subjects_remediation_schools)}
+    it 'returns the right header for #growth_progress_rating_header' do
       expect(dummy_controller.growth_progress_rating_header).to eq('Student Progress Rating')
     end
 
-    it 'returns the right header if the state has growth proxy data' do
-      expect(dummy_controller2.growth_progress_rating_header).to eq('Academic Progress Rating')
-    end
-  end
-
-  describe '#academic_header_names' do
-    it 'returns the right academic headers for tableview if the state has growth data' do
+    it 'returns the right header for #academic_header_names' do
       expect(dummy_controller.academic_header_names).to eq(['Test Scores Rating', 'Student Progress Rating', 'College Readiness Rating', 'Advanced Courses Rating', 'Equity Overview Rating'])
     end
-
-    it 'returns the right academic headers for tableview if the state has growth proxy data' do
-      expect(dummy_controller2.academic_header_names).to eq(['Test Scores Rating', 'Academic Progress Rating', 'College Readiness Rating', 'Advanced Courses Rating', 'Equity Overview Rating'])
-    end
-  end
-
-  describe '#generate_remediation_headers' do
-    before { allow(dummy_controller3).to receive(:serialized_schools).and_return(empty_array) }
 
     it 'returns the overall remediation headers for tableview if overall data is present' do
       remediation_header = dummy_controller.generate_remediation_headers
       expect(remediation_header.fetch(:key, nil)).to eq('percentCollegeRemediation')
     end
+  end
+
+  describe 'Cache Data for a Growth Proxy Data State with Math/Reading Data for Remediation' do
+    before {allow(dummy_controller).to receive(:serialized_schools).and_return(array_of_growth_proxy_state_and_specific_subjects_remediation_schools)}
+    it 'returns the right header for #growth_progress_rating_header' do
+      expect(dummy_controller.growth_progress_rating_header).to eq('Academic Progress Rating')
+    end
+
+    it 'returns the right header for #academic_header_names' do
+      expect(dummy_controller.academic_header_names).to eq(['Test Scores Rating', 'Academic Progress Rating', 'College Readiness Rating', 'Advanced Courses Rating', 'Equity Overview Rating'])
+    end
 
     it 'returns the english/math remediation headers for tableview if English/Math is present and not overall data' do
-      remediation_header = dummy_controller2.generate_remediation_headers
+      remediation_header = dummy_controller.generate_remediation_headers
       expect(remediation_header.length).to eq(2)
       expect(remediation_header.map {|x| x.fetch(:key, nil)}).to eq(['percentCollegeRemediationEnglish','percentCollegeRemediationMath'])
     end
+  end
+
+  describe 'handles the null case' do
+    before { allow(dummy_controller).to receive(:serialized_schools).and_return(empty_array) }
 
     it 'return nil if there isn\'t instance of overall, math, or english as a remediation subject' do
-      remediation_header = dummy_controller3.generate_remediation_headers
-      expect(remediation_header)&.to eq(nil)
+      remediation_header = dummy_controller.generate_remediation_headers
+      expect(remediation_header).to be_nil
     end
   end
 
   describe '#mode' do
-    context 'picks the most frequent repeated value' do
-      it "picks the most frequent number" do
-        array_of_integers = [77, 77, 89, 77]
-        expect(dummy_controller.mode(array_of_integers)).to eq(77)
-      end
+    subject {dummy_controller.mode(input_array)}
 
-      it "picks the most frequent word" do
-        array_of_words = ['cat', 'dog', 'cat', 'mouse']
-        expect(dummy_controller.mode(array_of_words)).to eq('cat')
-      end
+    context 'with an array of numbers' do
+      let(:input_array) {[77, 77, 89, 77]}
+      it { is_expected.to eq(77) }
+    end
+
+    context 'with an array of words' do
+      let(:input_array) {%w(cat dog cat mouse)}
+      it { is_expected.to eq('cat')}
     end
   end
 end
