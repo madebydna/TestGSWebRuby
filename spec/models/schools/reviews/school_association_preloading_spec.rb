402f556dc3e6d448c6876e8b2e994838bcd5bae2
Andrea Singh <asingh@greatschools.org>
2019-08-13 11:09:38 -0700

    Removed OpenSans-CondBold

--- spec/models/schools/reviews/school_association_preloading_spec.rb
+++ spec/models/schools/reviews/school_association_preloading_spec.rb
@@ -2,38 +2,38 @@ require 'spec_helper'
 
 describe SchoolAssociationPreloading do
     
-    describe '.extend' do
-        it 'works for ActiveRecord relations that have a state and school_id method' do
-            expect { Review.has_comment.extend(SchoolAssociationPreloading) }.not_to raise_error
-        end
+  describe '.extend' do
+    class Foo
+      include Enumerable
 
-        it 'works for Enumerables whose objects have a state and school_id method' do
-            expect { [Review.new, Review.new].extend(SchoolAssociationPreloading) }.not_to raise_error
-        end
+      def state
+        "state"
+      end
 
-        it 'does not work for non ActiveRecord relations or Enumerables' do 
-            expect { OpenStruct.new(school_id: 123, state: "CA").extend(SchoolAssociationPreloading) }.to raise_error(/must be mixed into an ActiveRecord relation or an Enumerable object/)
-        end
-
-        it 'does not work for ActiveRecord relations without the state method' do
-            expect { Country.unscoped.extend(SchoolAssociationPreloading) }.to raise_error(/Country does not respond to \.state/)
-        end
-
-        it 'does not work for ActiveRecord relations without the school_id method' do
-            expect { City.unscoped.extend(SchoolAssociationPreloading) }.to raise_error(/City does not respond to \.school_id/)
-        end
-
-        it 'does not work for Enumerable objects whose members don\'t have the state method' do
-            expect { [ OpenStruct.new(school_id: 123) ].extend(SchoolAssociationPreloading) }.to raise_error(/OpenStruct does not respond to \.state/)
-        end
-
-        it 'does not work for Enumerable objects whose members don\'t have the school_id method' do
-            expect { [ OpenStruct.new(state: "CA") ].extend(SchoolAssociationPreloading) }.to raise_error(/OpenStruct does not respond to \.school_id/)
-        end
-
-        it 'works for empty Enumerable objects' do
-            expect { [].extend(SchoolAssociationPreloading) }.not_to raise_error
-        end
+      def school_id
+        "school_id"
+      end
     end
+
+    it 'does not work for ActiveRecord relations without the state method' do
+        expect { Country.unscoped.extend(SchoolAssociationPreloading) }.to raise_error(/Country does not respond to \.state/)
+    end
+
+    it 'works for ActiveRecord relations that have a state and school_id method' do
+      expect { Review.has_comment.extend(SchoolAssociationPreloading) }.not_to raise_error(ArgumentError)
+    end
+
+    it 'does not work for non ActiveRecord relations' do 
+      expect { Foo.extend(SchoolAssociationPreloading) }.to raise_error(ArgumentError, /must be mixed into an ActiveRecord relation/)
+    end
+
+    it 'does not work for ActiveRecord relations without the state method' do
+      expect { Country.unscoped.extend(SchoolAssociationPreloading) }.to raise_error(ArgumentError, /Country does not respond to \.state/)
+    end
+
+    it 'does not work for ActiveRecord relations without the school_id method' do
+      expect { City.unscoped.extend(SchoolAssociationPreloading) }.to raise_error(ArgumentError, /City does not respond to \.school_id/)
+    end
+  end
 
 end
