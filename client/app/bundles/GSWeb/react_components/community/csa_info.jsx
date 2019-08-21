24946aa50cdaa1a9ed4065926c0bc3df766f4756
Alexina Chong <achong@greatschools.org>
2019-08-21 14:23:28 -0700

    JT-8903 Add Owl icon for CA CSA module

--- client/app/bundles/GSWeb/react_components/community/csa_info.jsx
+++ client/app/bundles/GSWeb/react_components/community/csa_info.jsx
@@ -2,13 +2,16 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { t } from 'util/i18n';
 import csaBadgeGenLg from 'school_profiles/csa_generic_badge_lg_icon.png';
+import owlPng from 'school_profiles/owl.png';
 
 const CsaInfo = ({ locality, caAdvocacy }) => {
+  let moduleIcon = csaBadgeGenLg;
   let headerText = `${t('award_winners')}`;
   let blurbText = `${t("csa_district_schools_info_html")}`;
   let buttonText = `${t('see_winning_schools_in')} ${locality.stateLong}`;
 
   if (caAdvocacy) {
+    moduleIcon = owlPng;
     headerText = `CA header text`;
     blurbText = `CA blurb text`;
     buttonText = `CA button text`;
@@ -19,7 +22,7 @@ const CsaInfo = ({ locality, caAdvocacy }) => {
       <h3>{headerText}</h3>
       <div className="csa-state-blurb">
         <img 
-          src={csaBadgeGenLg}
+          src={moduleIcon}
           className="csa-badge-gen-lg"
           alt="csa-badge-icon"
         />
