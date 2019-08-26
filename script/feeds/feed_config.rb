5523c54d0ca855c2224a0827d7ad8609893f065a
Andrea Singh <asingh@greatschools.org>
2019-08-26 09:50:00 -0700

    JT-9001 Update feed_config to include new file types

--- script/feeds/feed_config.rb
+++ script/feeds/feed_config.rb
@@ -28,9 +28,9 @@ class FeedConfig
     @state_abbreviations.map { |state| "local-greatschools-feed-#{state}.xml" }
   end
 
-  # def city_feed_files
-  #   @state_abbreviations.map { |state| "local-greatschools-city-feed-#{state}.xml" }
-  # end
+  def local_new_test_feed_files
+    @state_abbreviations.map { |state| "local-gs-new-test-feed-#{state}.xml" }
+  end
 
   def official_overall_rating_feed_files
     @state_abbreviations.map { |state| "local-gs-official-overall-rating-feed-#{state}.xml" }
@@ -40,20 +40,12 @@ class FeedConfig
     @state_abbreviations.map { |state| "local-gs-parent-review-feed-#{state}.xml" }
   end
 
-  # def test_feed_files
-  #   @state_abbreviations.map { |state| "local-gs-test-feed-#{state}.xml" }
-  # end
-
-  # def test_rating_feed_files
-  #   @state_abbreviations.map { |state| "local-gs-test-rating-feed-#{state}.xml" }
-  # end
-
-  # def test_subgroup_feed_files
-  #   @state_abbreviations.map { |state| "local-gs-test-subgroup-feed-#{state}.xml" }
-  # end
+  def local_gs_new_test_subgroup_feed_files
+    @state_abbreviations.map { |state| "local-gs-new-test-subgroup-feed-#{state}.xml" }
+  end
 
   def all_feed_files
-    local_feed_files + 
+    local_feed_files + local_new_test_feed_files + local_gs_new_test_subgroup_feed_files + 
     official_overall_rating_feed_files + parent_review_feed_files
   end
 
