1b5bd5feacee8d0f92ae2547de388d5d1398050b
Andrea Singh <asingh@greatschools.org>
2019-08-28 14:01:57 -0700

    JT-8997 : WIP :: Remove hash argument as it is also an instance method

--- app/services/act_sat_handler.rb
+++ app/services/act_sat_handler.rb
@@ -10,32 +10,32 @@ class ActSatHandler
 
   def handle_ACT_SAT_to_display!
     # returns max_year if we have at least one ACT data type to display, else: nil
-    act_content = enforce_latest_year_school_value_for_data_types!(hash, ACT_SCORE, ACT_PARTICIPATION, ACT_PERCENT_COLLEGE_READY)
+    act_content = enforce_latest_year_school_value_for_data_types!(ACT_SCORE, ACT_PARTICIPATION, ACT_PERCENT_COLLEGE_READY)
     # returns max_year if we have at least one SAT data type to display, else: nil
-    sat_content = enforce_latest_year_school_value_for_data_types!(hash, SAT_SCORE, SAT_PARTICIPATION, SAT_PERCENT_COLLEGE_READY)
+    sat_content = enforce_latest_year_school_value_for_data_types!(SAT_SCORE, SAT_PARTICIPATION, SAT_PERCENT_COLLEGE_READY)
 
-    remove_crdc_for_unfresh_data(act_content, sat_content, hash)
+    remove_crdc_for_unfresh_data(act_content, sat_content)
 
     if act_content || sat_content
-      remove_crdc_breakdown!(hash, ACT_SAT_PARTICIPATION, ACT_SAT_PARTICIPATION_9_12)
+      remove_crdc_breakdown!(ACT_SAT_PARTICIPATION, ACT_SAT_PARTICIPATION_9_12)
     else
-      enforce_latest_year_gsdata!(hash, ACT_SAT_PARTICIPATION, ACT_SAT_PARTICIPATION_9_12)
+      enforce_latest_year_gsdata!(ACT_SAT_PARTICIPATION, ACT_SAT_PARTICIPATION_9_12)
       part912 = hash.slice(ACT_SAT_PARTICIPATION_9_12).values.flatten.select(&:all_students?).flatten
-      remove_crdc_breakdown!(hash, ACT_SAT_PARTICIPATION) if part912.present?
+      remove_crdc_breakdown!(ACT_SAT_PARTICIPATION) if part912.present?
     end
   end
 
   # JT-8787: Displayed ACT & SAT data must be within 2 years of one another, otherwise hide the older data type
-  def remove_crdc_for_unfresh_data(act_content, sat_content, hash)
+  def remove_crdc_for_unfresh_data(act_content, sat_content)
     return unless act_content && sat_content
     return unless ((act_content - sat_content).abs > 2)
     if act_content > sat_content
-      return remove_crdc_breakdown!(hash, SAT_SCORE, SAT_PARTICIPATION, SAT_PERCENT_COLLEGE_READY)
+      return remove_crdc_breakdown!(SAT_SCORE, SAT_PARTICIPATION, SAT_PERCENT_COLLEGE_READY)
     end
-    remove_crdc_breakdown!(hash, ACT_SCORE, ACT_PARTICIPATION, ACT_PERCENT_COLLEGE_READY)
+    remove_crdc_breakdown!(ACT_SCORE, ACT_PARTICIPATION, ACT_PERCENT_COLLEGE_READY)
   end
 
-  def enforce_latest_year_gsdata!(hash, *data_types)
+  def enforce_latest_year_gsdata!(*data_types)
     data_type_hashes = hash.slice(*data_types).values.flatten.select(&:all_students?).flatten.extend(GsdataCaching::GsDataValue::CollectionMethods)
     max_year = data_type_hashes.year_of_most_recent
     data_type_hashes.each do |v|
@@ -45,7 +45,7 @@ class ActSatHandler
     end
   end
 
-  def remove_crdc_breakdown!(hash, *data_types)
+  def remove_crdc_breakdown!(*data_types)
     data_type_hashes = hash.slice(*data_types).values.flatten.select do |tds|
       tds.all_students?
     end.flatten
@@ -58,13 +58,13 @@ class ActSatHandler
   # Assuming we have >= 1 year(s)' worth of school_values for a given data type,
   # this will return the most recent year (i.e., "max year") for which we have data
   # and set all previous years' school_values to nil
-  def enforce_latest_year_school_value_for_data_types!(hash, *data_types)
-    data_type_hashes = build_data_type_hashes(data_types, hash)
+  def enforce_latest_year_school_value_for_data_types!(*data_types)
+    data_type_hashes = build_data_type_hashes(data_types)
     max_year = get_max_year(data_type_hashes)
     check_school_value_max(data_type_hashes, max_year)
   end
 
-  def build_data_type_hashes(data_types, hash)
+  def build_data_type_hashes(data_types)
     hash.slice(*data_types).values.flatten.select do |tds|
       tds.all_subjects_and_students?
     end.flatten
