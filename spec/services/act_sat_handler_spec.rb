3bfdb139fed6d786e5370294e3d0657b49e46288
Andrea Singh <asingh@greatschools.org>
2019-09-10 14:01:26 -0700

    JT-8997 WIP better specs

--- spec/services/act_sat_handler_spec.rb
+++ spec/services/act_sat_handler_spec.rb
@@ -4,34 +4,28 @@ include SchoolProfiles::CollegeReadinessConfig
 describe SchoolProfiles::CollegeReadiness do
   describe "#handle_ACT_SAT_to_display!" do
 
-    let(:cv1) { SchoolProfiles::CollegeReadinessComponent::CharacteristicsValue.new }
-    let(:cv2) { SchoolProfiles::CollegeReadinessComponent::CharacteristicsValue.new }
-    let(:cv3) { SchoolProfiles::CollegeReadinessComponent::CharacteristicsValue.new }
     let(:act_score_dt) { SchoolProfiles::CollegeReadinessConfig::ACT_SCORE }
     let(:act_participation_dt) { SchoolProfiles::CollegeReadinessConfig::ACT_PARTICIPATION }
     let(:act_sat_912_participation_dt) { SchoolProfiles::CollegeReadinessConfig::ACT_SAT_PARTICIPATION_9_12 }
     let(:act_sat_participation_dt) { SchoolProfiles::CollegeReadinessConfig::ACT_SAT_PARTICIPATION }
 
     context 'given hash contains only ACT content' do
+      let(:cv1) do 
+        FactoryBot.build(:school_characteristics_value, school_value: 123, year: 2018, school_value_2018: 123, school_value_2014: 200)
+      end
 
-      it 'sets school value to nil for all students for ACT_SAT data types' do
-        cv1.subject              = 'All subjects'
-        cv1.breakdown            = 'All students'
-        cv1.year                 = "2018"
-        cv1["school_value_2018"] = 100
-        cv1["school_value_2014"] = 200
-        cv1.school_value         = 123
+      let(:cv2) do 
+        FactoryBot.build(:school_characteristics_value, school_value: 234, year: 2018, school_value_2018: 234, school_value_2014: 200)
+      end
 
-        cv2.subject              = 'All subjects'
-        cv2.breakdown            = 'All students'
-        cv2.year                 = "2018"
-        cv2["school_value_2018"] = 100
-        cv2["school_value_2014"] = 200
-        cv2.school_value         = 234
+      let(:cv3) do 
+        FactoryBot.build(:school_characteristics_value, school_value: 444, year: 2014, school_value_2014: 444)
+      end
 
+      it 'sets school value to nil for all students for ACT_SAT combined participation data types' do
         hash = {
-          act_sat_participation_dt     => [cv1],
-          act_score_dt         => [cv2],
+          act_sat_participation_dt => [cv1],
+          act_score_dt => [cv2],
           act_sat_912_participation_dt => []
         }
 
@@ -42,64 +36,80 @@ describe SchoolProfiles::CollegeReadiness do
         expect(handler.hash[act_sat_participation_dt].first.school_value).to be nil
       end
 
-      context 'data type is for all students and subjects' do
+      it 'enforces latest year for ACT data type records' do
+        hash = {
+          act_sat_participation_dt => [cv1],
+          act_score_dt => [cv2, cv3],
+          act_sat_912_participation_dt => []
+        }
 
-        it 'sets school value to nil for ACT data types where no max year data' do
-          cv1.subject              = 'All subjects'
-          cv1.breakdown            = 'All students'
-          cv1.year                 = "2018"
-          cv1["school_value_2018"] = 100
-          cv1["school_value_2014"] = 300
-          cv1.school_value         = 123
-
-          cv2.subject      = 'All subjects'
-          cv2.breakdown    = 'All students'
-          cv2.year         = "2014"
-          cv2.school_value = 123
-
-          hash = {
-            act_participation_dt => [cv1],
-            act_score_dt   => [cv2]
-          }
-
-          handler = ActSatHandler.new(hash)
-
-          expect(handler.hash[act_participation_dt].first.school_value).to_not be nil
-          expect(handler.hash[act_score_dt].first.school_value).to_not be nil
-          handler.handle_ACT_SAT_to_display!
-          expect(handler.hash[act_participation_dt].first.school_value).to_not be nil
-          expect(handler.hash[act_score_dt].first.school_value).to be nil
-        end
+        handler = ActSatHandler.new(hash)
+        expect(handler.hash[act_score_dt].last.school_value).to_not be nil
+        handler.handle_ACT_SAT_to_display!
+        expect(handler.hash[act_score_dt].last.school_value).to be nil
       end
 
-      context 'data type is not for all students and subjects' do
-        it 'does not modify the school value for ACT data types' do
-          cv1.subject              = 'Test Subject'
-          cv1.breakdown            = 'Test students'
-          cv1.year                 = "2018"
-          cv1["school_value_2018"] = 345
-          cv1["school_value_2014"] = 456
-          cv1.school_value         = 123
+      it "ignores records for other subjects"
+      it "ignores records for other CRDC breakdowns"
 
-          cv2.subject      = 'Test subject'
-          cv2.breakdown    = 'Test students'
-          cv2.year         = "2014"
-          cv2.school_value = 123
+      # context 'data type is for all students and subjects' do
 
-          hash            = {
-            act_participation_dt => [cv1],
-            act_score_dt   => [cv2]
-          }
+      #   it 'sets school value to nil for ACT data types where no max year data' do
+      #     cv1.subject              = 'All subjects'
+      #     cv1.breakdown            = 'All students'
+      #     cv1.year                 = "2018"
+      #     cv1["school_value_2018"] = 100
+      #     cv1["school_value_2014"] = 300
+      #     cv1.school_value         = 123
 
-          handler = ActSatHandler.new(hash)
+      #     cv2.subject      = 'All subjects'
+      #     cv2.breakdown    = 'All students'
+      #     cv2.year         = "2014"
+      #     cv2.school_value = 123
 
-          expect(handler.hash[act_participation_dt].first.school_value).to_not be nil
-          expect(handler.hash[act_score_dt].first.school_value).to_not be nil
-          handler.handle_ACT_SAT_to_display!
-          expect(handler.hash[act_participation_dt].first.school_value).to_not be nil
-          expect(handler.hash[act_score_dt].first.school_value).to_not be nil
-        end
-      end
+      #     hash = {
+      #       act_participation_dt => [cv1],
+      #       act_score_dt   => [cv2]
+      #     }
+
+      #     handler = ActSatHandler.new(hash)
+
+      #     expect(handler.hash[act_participation_dt].first.school_value).to_not be nil
+      #     expect(handler.hash[act_score_dt].first.school_value).to_not be nil
+      #     handler.handle_ACT_SAT_to_display!
+      #     expect(handler.hash[act_participation_dt].first.school_value).to_not be nil
+      #     expect(handler.hash[act_score_dt].first.school_value).to be nil
+      #   end
+      # end
+
+      # context 'data type is not for all students and subjects' do
+      #   it 'does not modify the school value for ACT data types' do
+      #     cv1.subject              = 'Test Subject'
+      #     cv1.breakdown            = 'Test students'
+      #     cv1.year                 = "2018"
+      #     cv1["school_value_2018"] = 345
+      #     cv1["school_value_2014"] = 456
+      #     cv1.school_value         = 123
+
+      #     cv2.subject      = 'Test subject'
+      #     cv2.breakdown    = 'Test students'
+      #     cv2.year         = "2014"
+      #     cv2.school_value = 123
+
+      #     hash            = {
+      #       act_participation_dt => [cv1],
+      #       act_score_dt   => [cv2]
+      #     }
+
+      #     handler = ActSatHandler.new(hash)
+
+      #     expect(handler.hash[act_participation_dt].first.school_value).to_not be nil
+      #     expect(handler.hash[act_score_dt].first.school_value).to_not be nil
+      #     handler.handle_ACT_SAT_to_display!
+      #     expect(handler.hash[act_participation_dt].first.school_value).to_not be nil
+      #     expect(handler.hash[act_score_dt].first.school_value).to_not be nil
+      #   end
+      # end
     end
 
     context 'given hash contains only SAT content' do
@@ -143,9 +153,5 @@ describe SchoolProfiles::CollegeReadiness do
     context 'with both SAT and ACT content' do
 
     end 
-  end
-
-  def create_characteristics_value()
-
   end
 end
