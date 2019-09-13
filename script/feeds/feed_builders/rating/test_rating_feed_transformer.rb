9d0a5d94a9adb46cb4416dfa38a3fe11ec364479
Osman Afridi <oafridi@greatschools.org>
2019-09-13 08:27:18 -0700

    JT-9163: Remove redundant code that has reference to advanced courses.

--- script/feeds/feed_builders/rating/test_rating_feed_transformer.rb
+++ script/feeds/feed_builders/rating/test_rating_feed_transformer.rb
@@ -51,22 +51,6 @@ module Feeds
          state_url(state_params(state))
       end
     end
-    def transpose_ratings_description(data_type_id,state)
-      state_name= States.state_name(state).titleize
-      # How we calculate test_description  can change based on decision from Product team
-      if data_type_id == RATINGS_ID_RATING_FEED_MAPPING['official_overall']
-        "The GreatSchools Rating helps parents compare schools within a state based on a variety of school quality indicators and provides a helpful picture of how effectively each school serves all of its students. Ratings are on a scale of 1 (below average) to 10 (above average) and can include test scores, college readiness, academic progress, advanced courses, equity, discipline and attendance data. We also advise parents to visit schools, consider other information on school performance and programs, and consider family needs as part of the school selection process."
-      elsif data_type_id == RATINGS_ID_RATING_FEED_MAPPING['test_rating']
-
-        "GreatSchools compared the test results for each grade and subject across all #{state_name} schools and divided them into 1 through 10 ratings (10 is the best).\
-Please note, private schools are not required to release test results, so ratings are available \
-only for public schools. GreatSchools Ratings cannot be compared across states,\
-because of differences in the states' standardized testing programs.\
-Keep in mind that when comparing schools using GreatSchools Ratings it's important to factor in \
-other information, including the quality of each school's teachers, the school culture, special programs, etc."
-
-      end
-    end
 
   end
 end
