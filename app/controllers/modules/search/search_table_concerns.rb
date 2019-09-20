0a5258bd40ca04c1d73c2a3da8d2581b2c6aed4d
Andy Luo <aluo@greatschools.org>
2019-09-20 11:00:41 -0700

    JT-9103 Updated growth_data_proxy_state? to return correct boolean

--- app/controllers/modules/search/search_table_concerns.rb
+++ app/controllers/modules/search/search_table_concerns.rb
@@ -33,8 +33,8 @@ module SearchTableConcerns
   def growth_data_proxy_state?
     # finds most frequent state if a la carte search is used and no state in query params
     most_frequent_state = mode(serialized_schools.map {|school| school.dig(:state) }) if state.nil?
-    cache_data = StateCache.for_state('state_attributes', (state || most_frequent_state)).cache_data
-    return false if cache_data.empty?
+    cache_data = StateCache.for_state('state_attributes', (state || most_frequent_state))&.cache_data
+    return false if cache_data.nil?
     cache_data['growth_type'] == ACADEMIC_PROGRESS_RATING
   end
 
