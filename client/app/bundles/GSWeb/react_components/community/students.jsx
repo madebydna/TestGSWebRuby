470578db15d06a8f9aad2c280812d1d03476e3ee
Andy Luo <aluo@greatschools.org>
2019-09-24 15:39:35 -0700

    updated students.jsx per eslint rules

--- client/app/bundles/GSWeb/react_components/community/students.jsx
+++ client/app/bundles/GSWeb/react_components/community/students.jsx
@@ -1,9 +1,9 @@
 import React, { useEffect } from "react";
 import PropTypes from "prop-types";
+import { t } from "util/i18n";
 import { generateEthnicityChart } from "../../components/ethnicity_pie_chart";
 import { generateSubgroupPieCharts } from "../../components/subgroup_charts";
 import InfoBox from '../school_profiles/info_box';
-import { t } from "util/i18n";
 
 const generateLegend = (ethnicityData, pageType) => {
   const sortedEthnicityData = ethnicityData.sort((a, b) => {
@@ -16,8 +16,8 @@ const generateLegend = (ethnicityData, pageType) => {
         const displayedValue = value > 0 ? value : '<1';
         const ethnicityColors = ["#0f69c4", "#2bdc99", "#f1830f", "#f1e634", "#6f2eb4", "#ef60d0", "#ca3154", "#999EFF"]
         return (
-          <div className="legend-separator js-highlightPieChart clearfix" data-slice-id={idx} key={`${bd.breakdown}-${idx}`}>
-            <div className="legend-square" style={{ float: "left", backgroundColor: ethnicityColors[idx] }}></div>
+          <div className="legend-separator js-highlightPieChart clearfix" data-slice-id={idx} key={bd.breakdown}>
+            <div className="legend-square" style={{ float: "left", backgroundColor: ethnicityColors[idx] }} />
             <div className="legend-title" style={{ float: 'left' }}>{bd.breakdown}</div>
             <div className="legend-title" style={{ float: 'right' }}>{displayedValue}%</div>
           </div>
@@ -37,7 +37,7 @@ const Students = ({ ethnicityData, subgroupsData, genderData, translations, sour
     <React.Fragment>
       <div className="module-header">
         <div className="circle-rating--equity-blue">
-          <span className="icon-users"></span>
+          <span className="icon-users"/>
         </div>
         <div className="title-container">
           <h2 className="modules-title"
@@ -49,15 +49,14 @@ const Students = ({ ethnicityData, subgroupsData, genderData, translations, sour
       <section className="students-module">
         <div className='students-container'>
           <div className='students-demographic-chart'>
-            <div id="ethnicity-graph"></div>
+            <div id="ethnicity-graph" />
           </div>
           {generateLegend(ethnicityData, pageType)}
         </div>
         <div className="subgroups">
-          <div className="row">
-          </div>
+          <div className="row" />
         </div>
-        <div className="gender"> </div>
+        <div className="gender" />
       </section>
       <InfoBox content={sources} element_type="sources" pageType={pageType}>{t('See notes')}</InfoBox>
     </React.Fragment>
