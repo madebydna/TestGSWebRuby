69f2f94b1331f7904d328ae3b62cf2382f3bc6c2
Nina Magnuson <nmagnuson@Admins-MacBook-Pro-2.local>
2019-09-26 12:08:36 -0700

    changes to sc test processor

--- lib/etl/test_processors/sc_test_processor_2017_READY_PASS_EOCEP.rb
+++ lib/etl/test_processors/sc_test_processor_2017_READY_PASS_EOCEP.rb
@@ -1,75 +1,13 @@
 require_relative "../test_processor"
-GS::ETL::Logging.disable
 
-class SCTestProcessor2018_ACTAspire_PASS_EOCEP < GS::ETL::TestProcessor
+class SCTestProcessor2018READYPASSEOCEP < GS::ETL::TestProcessor
 
 	def initialize(*args)
 		super
 		@year = 2018
 	end
 
-
-	source("2018SCPASSv2_state.txt",[],col_sep:"\t") do |s|
-		s.transform("Fill entity level", Fill,{
-			entity_type: 'state',
-			test_data_type: 'sc pass',
-			test_data_type_id: '146',
-			})
-	end
-	source("2018SCPASSv2_district.txt",[],col_sep:"\t") do |s|
-		s.transform("Fill entity level", Fill,{
-			entity_type: 'district',
-			test_data_type: 'sc pass',
-			test_data_type_id: '146',
-			})
-	end
-	source("2018SCPASSv2_school.txt",[],col_sep:"\t") do |s|
-		s.transform("Fill entity level", Fill,{
-			entity_type: 'school',
-			test_data_type: 'sc pass',
-			test_data_type_id: '146',
-			})
-	end
-	source("EOCEP2018_state.txt",[],col_sep:"\t") do |s|
-		s.transform("Fill entity level", Fill,{
-			entity_type: 'state',
-			test_data_type: 'eocep',
-			test_data_type_id: '191',
-			grade: 'All',
-			})
-			.transform("delete rows where testid is 17 or greater",DeleteRows, :demoid, '17','18','19','20','21')
-			.transform("delete rows with no data because number tested < 10",DeleteRows, :numbertested, nil,'0','1','2','3','4','5','6','7','8','9')
-	end
-	source("EOCEP2018_district.txt",[],col_sep:"\t") do |s|
-		s.transform("Fill entity level", Fill,{
-			entity_type: 'district',
-			test_data_type: 'eocep',
-			test_data_type_id: '191',
-			grade: 'All',
-			})
-			.transform("delete rows where testid is 17 or greater",DeleteRows, :demoid, '17','18','19','20','21')
-			.transform("delete rows with no data because number tested < 10",DeleteRows, :numbertested, nil,'0','1','2','3','4','5','6','7','8','9')
-	end
-
-	source("EOCEP2018_school.txt",[],col_sep:"\t") do |s|
-		s.transform("Fill entity level", Fill,{
-			entity_type: 'school',
-			test_data_type: 'eocep',
-			test_data_type_id: '191',
-			grade: 'All',
-			})
-			.transform("delete rows where testid is 17 or greater",DeleteRows, :demoid, '17','18','19','20','21')
-			.transform("delete rows with no data because number tested < 10",DeleteRows, :numbertested, nil,'0','1','2','3','4','5','6','7','8','9')
-	end
-	# map_prof_band_aspire = {
-	# '1' => 34,
-	# '2' => 35,
-	# '3' => 36,
-	# '4' => 37,
-	# '34' => 'null'
-	# }
-
-	map_prof_band_pass = {
+	map_prof_band = {
 	'sci_pct1' => 5,
 	'sci_pct2' => 6,
 	'sci_pct3' => 7,
@@ -77,18 +15,28 @@ class SCTestProcessor2018_ACTAspire_PASS_EOCEP < GS::ETL::TestProcessor
 	'soc_pct1' => 2,
 	'soc_pct2' => 3,
 	'soc_pct3' => 4,
-	'sci_prof_and_above' => 1
-	'soc_prof_and_above' => 1
+	'sci_prof_and_above' => 1,
+	'soc_prof_and_above' => 1,
+	'ela_pct1' => 5,
+	'ela_pct2' => 6,
+	'ela_pct3' => 7,
+	'ela_pct4' => 8,
+	'math_pct1' => 5,
+	'math_pct2' => 6,
+	'math_pct3' => 7,
+	'math_pct4' => 8,
+	'ela_prof_and_above' => 1,
+	'math_prof_and_above' => 1
 	}
 
-	map_prof_band_eoc = {
-	'f' => 195,
-	'd' => 196,
-	'c' => 197,
-	'b' => 198,
-	'a' => 199,
-	'null' => 'null'
-	}
+	# map_prof_band_eoc = {
+	# 'f' => 195,
+	# 'd' => 196,
+	# 'c' => 197,
+	# 'b' => 198,
+	# 'a' => 199,
+	# 'null' => 'null'
+	# }
 
 	map_subject = {
 	'ela' => 4,
@@ -96,141 +44,116 @@ class SCTestProcessor2018_ACTAspire_PASS_EOCEP < GS::ETL::TestProcessor
 	'sci' => 19,
 	'soc' => 18,
 	'1' => 6,#alg1
-	'2' => 22, #bio 1
-	'3' => 19, #english 1
-	'4' => 31, #physical science
-	'5' => 66, #US history and constitution
+	'2' => 22 #bio 1
+	# '3' => 19, #english 1
+	# '4' => 31, #physical science
+	# '5' => 66, #US history and constitution
 	}
 
 	map_breakdown = {
 	'01ALL' => 1,
-	'02M' => 12,
-	'03F' => 11,
-	'04H' => 6,
-	'05I' => 4,
-	'06A' => 2,
-	'07B' => 3,
-	'08P' => 112,
-	'09W' => 8,
-	'10M' => 21,
-	'11SWD' => 13,#disabilities
-	'12NSWD' => 14,#no disabilities
-	'13MIG' => 19,#migrant
-	'14NMIG' => 28,#not migrant
-	'15LEP' => 15,#lep
-	'16NLEP' => 16,#not lep
-	'17SIP' => 9,#econ dis
-	'18NSIP' => 10,#not econ dis
+	'02M' => 25,
+	'03F' => 26,
+	'04H' => 19,
+	'05I' => 18,
+	'06A' => 16,
+	'07B' => 17,
+	'08P' => 20,
+	'09W' => 21,
+	'10M' => 22,
+	'11SWD' => 27,#disabilities
+	'12NSWD' => 30,#no disabilities
+	'13MIG' => 19,#migrant --skip
+	'14NMIG' => 28,#not migrant --skip
+	'15LEP' => 32,#lep 
+	'16NLEP' => 33,#not lep
+	'17SIP' => 23,#econ dis
+	'18NSIP' => 24,#not econ dis
 	'1' => 1,#all
-	'2' => 12,#male
-	'3' => 11,#female
-	'4' => 6,#hispanic/latino
-	'5' => 4,#am-in/alaska native
-	'6' => 2,#asian
-	'7' => 3,#african american /black
-	'8' => 112,#native hawaiian other pacific islander
-	'9' => 8,#white
-	'10' => 21,#two more races
-	'11' => 13,#disabled
-	'12' => 14,#not disables
-	'13' => 15,#lep
-	'14' => 16,#not lep
-	'15' => 9,#econ dis
-	'16' => 10,#not econ dis
+	'2' => 25,#male
+	'3' => 26,#female
+	'4' => 19,#hispanic/latino
+	'5' => 18,#am-in/alaska native
+	'6' => 16,#asian
+	'7' => 17,#african american /black
+	'8' => 20,#native hawaiian other pacific islander
+	'9' => 21,#white
+	'10' => 22,#two more races
+	'11' => 27,#disabled
+	'12' => 30,#not disables
+	'13' => 32,#lep
+	'14' => 33,#not lep
+	'15' => 23,#econ dis
+	'16' => 24#not econ dis
 	}
 
+	# source("SC_PASS_2017.txt",[],col_sep:"\t") do |s|
+	# 	s.transform("Fill values", Fill,{
+	# 		test_data_type: 'pass',
+	# 		test_data_type_id: '276',
+	# 		description: 'In 2016-2017, students took The South Carolina Palmetto Assessment of State Standards (SCPASS). SCPASS is a statewide assessment administered to students in grades four through eight. All students in these grade levels are required to take the SCPASS except those who qualify for the South Carolina Alternate Assessment (SC-Alt). SCPASS includes tests in two subjects: science and social studies.'
+	# 		})
+	# end
+
+	# source("SC_PASS_2018.txt",[],col_sep:"\t") do |s|
+	# 	s.transform("Fill values", Fill,{
+	# 		test_data_type: 'pass',
+	# 		test_data_type_id: '276',
+	# 		description: 'In 2017-2018, students took The South Carolina Palmetto Assessment of State Standards (SCPASS). SCPASS is a statewide assessment administered to students in science in the 4th, 6th and 8th grade and in social studies in the 5th and 7th grade. All students in these grade levels are required to take the SCPASS except those who qualify for the South Carolina Alternate Assessment (SC-Alt). SCPASS includes tests in two subjects: science and social studies.'
+	# 		})
+	# end
+
+
+	source("SC_READY_2017.txt",[],col_sep:"\t") do |s|
+		s.transform("Fill values", Fill,{
+			test_data_type: 'sc ready',
+			test_data_type_id: '279',
+			description: 'In 2016-2017, students took the South Carolina College- and Career-Ready Assessments (SC READY). The SC Ready is a statewide assessment that includes tests in English Language Arts (ELA) and mathematics administered to students in grades 3-8. All students in grades 3-8 are required to participate in the SC READY, except those who qualify for the South Carolina National Center and State Collaborative (SC-NCSC) alternate assessment. The initial administration of the SC READY was in spring 2016, and the SC READY test results will be used for state and federal accountability purposes.'
+			})
+	end
+
+	source("SC_READY_2018.txt",[],col_sep:"\t") do |s|
+		s.transform("Fill values", Fill,{
+			test_data_type: 'sc ready',
+			test_data_type_id: '279',
+			description: 'In 2017-2018, students took the South Carolina College- and Career-Ready Assessments (SC READY). The SC Ready is a statewide assessment that includes tests in English Language Arts (ELA) and mathematics administered to students in grades 3-8. All students in grades 3-8 are required to participate in the SC READY, except those who qualify for the South Carolina National Center and State Collaborative (SC-NCSC) alternate assessment. The initial administration of the SC READY was in spring 2016, and the SC READY test results will be used for state and federal accountability purposes.'
+			})
+	end
+
+
 	shared do |s|
-		s.transform("Rename columns", MultiFieldRenamer,{
-		testgrade: :grade,
-		demoid: :breakdown,
-		districtname: :district_name,
-		distcode: :district_id,
-		schoolname: :school_name,
-		schoolid: :school_id,
-		numbertested: :number_tested,
-		testid: :subject,
-		chbedscode: :school_id
-		})
-		.transform("null prof band for pass",WithBlock) do |row|
-			if row[:test_data_type_id] == '146'
-				row[:scipctnull] = (row[:scipct2].to_f + row[:scipct3].to_f).round(2) 
-			        row[:socpctnull] = (row[:socpct2].to_f + row[:socpct3].to_f).round(2) 
-			elsif row[:test_data_type_id] == '191'
-				row[:null]= (row[:pcta].to_f + row[:pctb].to_f+row[:pctc].to_f+row[:pctd].to_f).round(2) 
-			end
-			row
-		end
-		.transform("Transpose subjects prof band columns",Transposer,:subjectprof, :value_float, :engpct1, :engpct2, :engpct3, :engpct4, :engpct34,:mathpct1,:mathpct2,:mathpct3,:mathpct4,:mathpct34,:readpct1, :readpct2,:readpct3,:readpct4,:readpct34,:writpct1,:writpct2,:writpct3,:writpct4,:writpct34,:scipct1,:scipct2,:scipct3,:scipctnull,:socpct1,:socpct2,:socpct3,:socpctnull,:pcta,:pctb,:pctc,:pctd,:pctf,:null)
-		.transform("set prof band and subject",WithBlock) do |row|
-			if row[:test_data_type_id]  != '191'
-				row[:subject] = (row[:subjectprof].to_s).split(/pct/).first
-			end
-			row[:proficiency_band] = (row[:subjectprof].to_s).split(/pct/).last
-			row
-		end
-		.transform("number tested", WithBlock) do |row|
-			if row[:subject] ==  'ela'
-				row[:number_tested] = row[:engn]
-			elsif row[:subject] == 'math'
-				row[:number_tested] = row[:mathn]
-			elsif row[:subject] ==  'soc'
-				row[:number_tested] = row[:socn]
-			elsif row[:subject] == 'sci'
-				row[:number_tested] = row[:scin]
-			end
-			row
-		end
-		.transform("delete rows with no data because number tested < 10",DeleteRows, :number_tested, nil,'0','1','2','3','4','5','6','7','8','9')
-		.transform("Map subject id", HashLookup, :subject, map_subject, to: :subject_id)
-		.transform("Map breakdown id", HashLookup, :breakdown, map_breakdown, to: :breakdown_id)
+		s.transform("Map subject id", HashLookup, :subject, map_subject, to: :subject_id)
+		.transform("Map breakdown id", HashLookup, :demoid, map_breakdown, to: :breakdown_id)
 		.transform("Map prof band id", WithBlock) do |row|
-			if row[:test_data_type_id] == '312'
-				row[:proficiency_band_id] = map_prof_band_aspire[row[:proficiency_band]]
-			elsif row[:test_data_type_id] == '146'
-				row[:proficiency_band_id] = map_prof_band_pass[row[:proficiency_band]]
-			elsif row[:test_data_type_id] == '191'
-				row[:proficiency_band_id] = map_prof_band_eoc[row[:proficiency_band]]
+			if row[:test_data_type_id] == '276' or row[:test_data_type_id] == '279'
+				row[:proficiency_band_id] = map_prof_band[row[:name]]
+			# elsif row[:test_data_type_id] == '191'
+			# 	row[:proficiency_band_id] = map_prof_band_eoc[row[:proficiency_band]]
 			end
 			row
 		end
-		.transform("state_id",WithBlock) do |row|
-			if row[:entity_type] == 'school'
-				row[:state_id] = "%07i" %(row[:school_id].to_i)
-			elsif row[:entity_type] == 'district'
-				row[:state_id] = "%04i" %(row[:district_id].to_i)
-			elsif row[:entity_type] == 'state'
-				row[:state_id] = 'state'
-			end
-			row
-		end
-		.transform("remove leading zeros from grade", WithBlock) do |row|
-			if row[:grade] != 'All'
-				row[:grade] = row[:grade].to_i
-			end
-			row
-		end
-		.transform("check for blank value_float that should be zero", WithBlock) do |row|
-			if row[:value_float].nil?
-			#if row[:value_float] =~ /\s/
-			#if row[:value_float] !~ /\d/
-			#if row[:value_float] == ''
-				row[:value_float] = 0
-			end
-			row
-		end
+		.transform("Fix inequalities in value and fix grade padding", WithBlock,) do |row|
+	     if row[:value] < 0 
+	      row[:value] = 0
+	     end
+         if row[:value] > 100
+	      row[:value] = 100
+	      end
+	      row[:grade].sub!(/^0/, '')
+	      row
+        end
 		.transform("Fill other columns", Fill,{
-		year: 2018
 		notes: 'DXT-3136 SC READY, PASS, EOCEP',
-		date_valid: '2018-01-01 00:00:00',
+		date_valid: '2018-01-01 00:00:00'
 		})
 	end
 
 	def config_hash
 		{
-			source_id: 8,
+			source_id: 45,
 			state: 'sc'
 		}
 	end
 end
 
-SCTestProcessor2018_ACTAspire_PASS_EOCEP.new(ARGV[0],max:nil).run
+SCTestProcessor2018READYPASSEOCEP.new(ARGV[0],max:nil).run
