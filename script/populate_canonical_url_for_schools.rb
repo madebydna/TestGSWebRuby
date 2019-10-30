50b9d4b7021a17cf3b6c9d82690734e7565b4a3c
Andy Luo <aluo@greatschools.org>
2019-10-30 10:53:27 -0700

    JT-8983 added logging capabilities

--- script/populate_canonical_url_for_schools.rb
+++ script/populate_canonical_url_for_schools.rb
@@ -25,21 +25,44 @@ raise ArgumentError.new("Can't refresh and update the database at the same time!
 raise ArgumentError.new("Need to give command to either update the database or refresh the database!") unless should_refresh || should_update
 raise ArgumentError.new("Need to supply valid states!") unless states.all? {|s| States.state_hash.values.include?(s)}
 
-states.each do |state|
-  School.on_db(state) do
-    if should_update
-      School.active.where(canonical_url: nil).each do |school|
-        # done this way since ActiveRecord#update and ActiveRecord#update_attributes seems bugged
-        school.canonical_url  = school_path(school, trailing_slash: true)
-        school.save
-      end
-    elsif should_refresh
-      School.active.each do |school|
-        # done this way since ActiveRecord#update and ActiveRecord#update_attributes seems bugged
-        # send a param to school_path to not use the canonical_url found in the db
-        school.canonical_url  = school_path(school, trailing_slash: true, refresh_canonical_link: should_refresh)
-        school.save
+counter = 0
+
+begin log = ScriptLogger.record_log_instance(script_args); rescue;end
+
+begin
+  states.each do |state|
+    School.on_db(state) do
+      if should_update
+        School.active.where(canonical_url: nil).each do |school|
+          puts "Working on..."
+          puts "......#{state}"
+          # done this way since ActiveRecord#update and ActiveRecord#update_attributes seems bugged
+          school.canonical_url  = school_path(school, trailing_slash: true)
+          school.save
+          counter += 1
+        end
+      elsif should_refresh
+        School.active.each do |school|
+          puts "Working on..."
+          puts "......#{state}"
+          # done this way since ActiveRecord#update and ActiveRecord#update_attributes seems bugged
+          # send a param to school_path to not use the canonical_url found in the db
+          school.canonical_url  = school_path(school, trailing_slash: true, refresh_canonical_link: should_refresh)
+          school.save
+          counter += 1
+        end
       end
     end
   end
+
+  begin log.finish_logging_session(1, "Updated #{counter} canonical urls"); rescue;end
+
+rescue => e
+  begin log.finish_logging_session(0, e); rescue;end
+  raise
+rescue SignalException
+  begin log = log.finish_logging_session(0, "Process ended early. User manually cancelled process."); rescue;end
+  abort
 end
+
+puts "Updated #{counter} canonical urls"
