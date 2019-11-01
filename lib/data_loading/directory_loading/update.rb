be4c4b237727f19ff9ad2c5d4ba0c30efc96d988
Anthony Roy <aroy@greatschools.org>
2019-11-01 09:07:38 -0700

    Clean up class a little

--- lib/data_loading/directory_loading/update.rb
+++ lib/data_loading/directory_loading/update.rb
@@ -12,18 +12,15 @@ class DirectoryLoading::Update
       instance_variable_set("@#{key}", value)
     end
 
-    parse_attributes!
-  end
-
-  def parse_attributes!
     @shard = entity_state.to_s.downcase.to_sym
-    self.entity_type = self.entity_type.to_s.downcase.to_sym
+    @entity_type = entity_type.to_s.downcase.to_sym
+    @model = entity_type.to_s.titleize.constantize unless @entity_type == :state
   end
 
   def entity
     return @_entity if defined?(@_entity)
     @_entity ||= begin
-      entity_type == :state ? nil : entity_type.to_s.titleize.constantize.on_db(shard).find(entity_id)
+      @model&.on_db(shard)&.find(entity_id)
     end
   end
 end
