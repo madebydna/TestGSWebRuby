e518d01aa40b60b8c1edc4423de70850f9e37edf
Alexina Chong <achong@greatschools.org>
2019-11-12 15:42:20 -0800

    JT-9364 Register individual React modules and add TOC scrollspy script

--- client/app/bundles/GSWeb/community.js
+++ client/app/bundles/GSWeb/community.js
@@ -5,11 +5,18 @@ import State from "./react_components/community/state";
 import SearchBox from 'react_components/search_box';
 import withViewportSize from 'react_components/with_viewport_size';
 import TopSchoolsStateful from 'react_components/community/top_schools_stateful';
+import CsaTopSchools from 'react_components/community/csa_top_schools';
 import SchoolBrowseLinks from 'react_components/community/school_browse_links';
+import AcademicsDataModule from 'react_components/community/academics_data_module';
+import Students from 'react_components/community/students';
+import CityBrowseLinks from 'react_components/community/city_browse_links';
+import DistrictsInState from 'react_components/community/districts_in_state';
+import RecentReviews from 'react_components/community/recent_reviews';
 import Ad from 'react_components/ad';
 import commonPageInit from './common';
 import { scrollToElement } from 'util/scrolling';
-import { keepInViewport } from 'util/viewport';
+import { keepInViewport, isScrolledInViewport } from 'util/viewport';
+import { throttle } from 'lodash';
 
 const SearchBoxWrapper = withViewportSize({ propName: 'size' })(SearchBox);
 
@@ -19,7 +26,13 @@ ReactOnRails.register({
   State,
   SearchBoxWrapper,
   TopSchoolsStateful,
+  CsaTopSchools,
   SchoolBrowseLinks,
+  AcademicsDataModule,
+  Students,
+  CityBrowseLinks,
+  DistrictsInState,
+  RecentReviews,
   Ad
 });
 
@@ -63,4 +76,20 @@ $(() => {
       }
     }
   );
+
+  $(window).on('scroll', throttle(function() {
+    const tocElements = [...document.querySelectorAll('.module-section')].filter(ele => isScrolledInViewport(ele));
+    const selectedToc = tocElements.length > 0 ? tocElements[0].id : [];
+
+    window.document.querySelectorAll('.toc li').forEach(element => {
+      if (element.getAttribute('anchor') === `#${selectedToc}`) {
+        element.classList.add('selected');
+      } else {
+        element.classList.remove('selected');
+      }
+    });
+  }, 100));
+
+
+  
 });
