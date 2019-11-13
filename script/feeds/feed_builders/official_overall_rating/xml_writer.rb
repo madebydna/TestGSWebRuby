dd95abc416a88f7cd8d4071b936b582192b361a6
Mitchell Seltzer <mseltzer@greatschools.org>
2019-11-13 09:52:17 -0800

    JT-8990 - This will verify that the newly created table is identical to the one it was created from

--- script/feeds/feed_builders/official_overall_rating/xml_writer.rb
+++ script/feeds/feed_builders/official_overall_rating/xml_writer.rb
@@ -17,7 +17,6 @@ module Feeds
         within_root_node do
           write_state_info
           write_schools_info
-          # @data_reader.each_result { |hash| write_school_info(hash)}
         end
         close_file
       end
@@ -26,7 +25,6 @@ module Feeds
 
       def write_state_info
         state_results = @data_reader.state_results
-        # require 'pry'; binding.pry;
         within_tag('test-rating') do
           xml_builder.tag!('id', state_results['id'])
           xml_builder.tag!('year', state_results['year'])
@@ -35,40 +33,16 @@ module Feeds
       end
 
       def write_schools_info
-        # within_tag('test-rating-valuexxx') do
-        # xml_builder.tag!('id', 59055)
-        # require 'pry'; binding.pry;
-        @data_reader.each_result do |school_hash|
-            # write_xml_tag(data, tag_name)
-            # if hash[:rating].present?
-            # within_tag('test-rating-value') do
-
-              within_tag('test-rating-value') do
-                xml_builder.tag!('universal-id', school_hash[:id])
-                xml_builder.tag!('test-rating-id', school_hash[:test_rating_id])
-                xml_builder.tag!('rating', school_hash[:rating])
-                xml_builder.tag!('url', school_hash[:url])
-              end
-              # school_hash = @data_reader.each_result.each {|x| nil }
-            # end
-
-          # end
-        end
-        # end
+        @data_reader.each_result { |hash| write_school_info(hash) if hash[:rating].present? }
       end
+
       def write_school_info(school_hash)
-        # within_tag('test-rating-valuexxx') d_obj
-        #   xml_builder.tag!('id', school_hash[:id])
-        # end
-        # if school_hash[:rating].present?
-          # require 'pry'; binding.pry;
           within_tag('test-rating-value') do
-            xml_builder.tag!('universal-id', school_hash[:id])
+            xml_builder.tag!('universal-id', school_hash[:id].to_s)
             xml_builder.tag!('test-rating-id', school_hash[:test_rating_id])
-            xml_builder.tag!('rating', school_hash[:rating])
+            xml_builder.tag!('rating', school_hash[:rating].to_s)
             xml_builder.tag!('url', school_hash[:url])
           end
-        # end
       end
 
       def file
