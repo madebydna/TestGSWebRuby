ea99d666d1cbeb1435cab6ee24ad7bdf79779c2c
Andrea Singh <asingh@greatschools.org>
2020-01-17 15:05:27 -0800

    JT-9747 Fix SFTP upload and add to list signups

--- script/exacttarget/exacttarget_builders/subscription_list_data_extension/csv_writer.rb
+++ script/exacttarget/exacttarget_builders/subscription_list_data_extension/csv_writer.rb
@@ -1,21 +1,35 @@
 require 'csv'
-
+require 'zip'
 module Exacttarget
   module SubscriptionListDataExtension
     class CsvWriter
       HEADERS = %w(id member_id list language)
+      LOCAL_PATH = "/tmp/et_list_signups.csv"
 
       def initialize
         @data_reader = DataReader.new
       end
 
       def write_file
-        CSV.open("/tmp/et_list_signups.csv", 'w') do |csv|
+        CSV.open(LOCAL_PATH, 'w') do |csv|
           csv << HEADERS
           @data_reader.list_signups do |signup|
             csv << signup
           end
         end
+        zip_file
+        upload_file
+      end
+
+      def zip_file
+        file = File.new("#{LOCAL_PATH}.zip", "w")
+        Zip::File.open(file.path, Zip::File::CREATE) do |zipfile|
+          zipfile.add(File.basename(LOCAL_PATH), LOCAL_PATH)
+        end
+      end
+
+      def upload_file
+        ExacttargetSFTP.new.upload("#{LOCAL_PATH}.zip")
       end
     end
   end
--- script/exacttarget/exacttarget_helpers/exacttarget_sftp.rb
+++ script/exacttarget/exacttarget_helpers/exacttarget_sftp.rb
@@ -1,12 +1,12 @@
 require 'net/sftp'
 
-module ExactTarget
-  class ExactTargetSFTP
+module Exacttarget
+  class ExacttargetSFTP
     def upload(file, remote_location=nil)
       remote_location ||= "Import/"
       Net::SFTP.start(ENV_GLOBAL['exacttarget_host'], ENV_GLOBAL['exacttarget_username'], :password => ENV_GLOBAL['exacttarget_password']) do |sftp|
         # upload a file or directory to the remote host
-        sftp.upload!(file, remote_location) if file
+        sftp.upload!(file, File.join(remote_location, File.basename(file))) if file
       end
     end
     def download(remote_file, local_location=nil)
