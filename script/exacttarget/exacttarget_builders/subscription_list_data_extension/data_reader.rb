4909fdae1f23bd643ded882b22b1f66cf2ee2fa5
Andrea Singh <asingh@greatschools.org>
2020-01-17 16:30:12 -0800

    JT-9747 Simplify + correct subscription list builder

--- script/exacttarget/exacttarget_builders/subscription_list_data_extension/data_reader.rb
+++ script/exacttarget/exacttarget_builders/subscription_list_data_extension/data_reader.rb
@@ -5,25 +5,9 @@ module Exacttarget
       LIST_TYPES = %w(greatnews osp sponsored)
 
       def list_signups
-        States::STATE_HASH.values.each do |state|
-          Subscription.connection.select_all(generate_sql(state).squish).each do |signup|
-            yield CsvWriter::HEADERS.map {|header| signup[header] }
-          end
+        Subscription.where(list: LIST_TYPES).find_each do |signup|
+          yield CsvWriter::HEADERS.map {|header| signup[header] }
         end
-      end
-
-
-      private
-
-      def generate_sql(state)
-        <<~SQL
-          Select id, member_id, list, 'en' as language from gs_schooldb.list_active
-          where state = '#{state}' and list in (#{quoted_list_types});
-        SQL
-      end
-
-      def quoted_list_types
-        LIST_TYPES.map {|list| "'#{list}'" }.join(",")
       end
 
     end
