e4615c661e37d35f9d17d58e1b03d9d3a1360511
Andy Luo <aluo@greatschools.org>
2020-01-25 00:19:14 -0800

    JT-9773 updated flat feeds check

--- script/feeds/feed_config.rb
+++ script/feeds/feed_config.rb
@@ -69,8 +69,38 @@ class FeedConfig
     @state_abbreviations.map { |state| "local-gs-parent-ratings-feed-flat-#{state}.txt" }
   end
 
+  def local_new_test_flat_feed_files
+    @state_abbreviations.map { |state| "local-gs-new-test-feed-#{state}.txt" }
+  end
+
+  def new_test_result_flat_feeds_files
+    @state_abbreviations.map { |state| "local-gs-new-test-result-feed-#{state}.txt" }
+  end
+
+  def local_gs_new_test_subgroup_flat_feed_files
+    @state_abbreviations.map { |state| "local-gs-new-test-subgroup-feed-#{state}.txt" }
+  end
+
+  def subrating_description_flat_feed_files
+    @state_abbreviations.map { |state| "local-gs-subrating-description-feed-#{state}.txt" }
+  end
+
+  def subrating_flat_feed_files
+    @state_abbreviations.map { |state| "local-gs-subrating-feed-#{state}.txt" }
+  end
+
+  def official_overall_rating_flat_feed_files
+    @state_abbreviations.map { |state| "local-gs-official-overall-rating-feed-flat-#{state}.txt" }
+  end
+
+  def official_overall_rating_result_flat_feed_files
+    @state_abbreviations.map { |state| "local-gs-official-overall-rating-result-feed-flat-#{state}.txt" }
+  end
+
   def all_flat_feed_files
-    parent_reviews_flat_feed_files + parent_ratings_flat_feed_files
+    parent_reviews_flat_feed_files + parent_ratings_flat_feed_files + local_new_test_flat_feed_files +
+      new_test_result_flat_feeds_files + local_gs_new_test_subgroup_flat_feed_files + subrating_description_flat_feed_files +
+      subrating_flat_feed_files + official_overall_rating_flat_feed_files + official_overall_rating_result_flat_feed_files
   end
 
   def each_pair_old_and_new_flat_feed_files
