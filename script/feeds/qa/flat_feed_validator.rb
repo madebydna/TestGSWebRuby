bece023b5d1c2412aeb4420da642c7c73a5c1204
Andy Luo <aluo@greatschools.org>
2020-01-27 11:42:42 -0800

    JT-9773 change how the percentage was calculated

--- script/feeds/qa/flat_feed_validator.rb
+++ script/feeds/qa/flat_feed_validator.rb
@@ -9,12 +9,12 @@ class FlatFeedValidator
   def verify
     all_entries_over_threshold = []
     feed_config.each_pair_old_and_new_flat_feed_files do |old_file, new_file|
-      csv_old_file = CSV.read(old_file, {:col_sep => "\t", encoding: "bom|utf-8", quote_char: "|", liberal_parsing: true})
-      csv_new_file = CSV.read(new_file, {:col_sep => "\t", encoding: "bom|utf-8", quote_char: "|", liberal_parsing: true})
-      exact_matches = (csv_old_file & csv_new_file).length
+      old_file_count = system(`wc -l "#{old_file}"`).strip.split(' ').first.to_i
+      new_file_count = system(`wc -l "#{new_file}"`).strip.split(' ').first.to_i
+      element_diff_count = (old_file_count - new_file_count).abs
       max_lines = [csv_old_file.length, csv_new_file.length].max
-      difference_pct = (1 - exact_matches.to_f / max_lines)
-      all_entries_over_threshold << "#{File.basename(new_file)}\t#{(difference_pct * 100).round(2)}%\tOld:#{csv_old_file.length}/New:#{csv_new_file.length} lines" if difference_pct > 0.10
+      difference_pct = (element_diff_count.to_f / max_lines)
+      all_entries_over_threshold << "#{File.basename(new_file)}\t#{(difference_pct * 100).round(2)}%\tOld:#{old_file_count}/New:#{new_file_count} lines" if difference_pct > 0.10
     end
 
     all_entries_over_threshold
