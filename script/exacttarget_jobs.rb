76b123d55270792dae47c0d2d97fcdd1f6bf921c
Alexina Chong <achong@greatschools.org>
2020-02-04 17:00:53 -0800

    JT-9862 Add class for ExacttargetJobs run script

--- script/exacttarget_jobs.rb
+++ script/exacttarget_jobs.rb
@@ -1,41 +1,49 @@
 process_to_run = ARGV.first
 
-if VALID_ET_PARAMETERS.include?(process_to_run)
-  run
+class ExacttargetJobs
+  include Exacttarget::Config::Constants
+
+  def initialize(process_to_run)
+    @process_to_run = process_to_run
+  end
+
+  def run
+    ptr = @process_to_run.to_sym
+    map_class = MAPPING_CLASSES[ptr]
+    if ptr == :all
+      MAPPING_CLASSES.each {| key, _ | write_to_file(key)}
+      unsubscribe_run
+    elsif map_class
+      write_to_file(ptr)
+    else
+      unsubscribe_run
+    end
+  end
+
+  def unsubscribe_run
+    processor = Exacttarget::Unsubscribes::Processor.new
+    processor.download_file
+    processor.run
+  end
+
+  def write_to_file(key)
+    writer_string = "Exacttarget::Builders::#{MAPPING_CLASSES[key]}::CsvWriterComponent"
+    writer = writer_string.constantize.new
+    writer.write_file
+    validator = writer.validate_file
+    if validator.valid?
+      writer.zip_file
+      writer.upload_file
+    else
+      # validator.errors
+      puts 'ERROR: Invalid file.'
+      puts validator.errors
+    end
+  end
+end
+
+if Exacttarget::Config::Constants::VALID_ET_PARAMETERS.include?(process_to_run)
+  ExacttargetJobs.new(process_to_run).run
 else
   puts "'#{process_to_run}' is not a valid ExactTarget parameter."
-end
-
-# TODO: Account for 'unsubscribes' which only has a processor file and 'all'
-def run
-  ptr = process_to_run.to_sym
-  map_class = MAPPING_CLASSES[ptr]
-  if ptr == :all
-    MAPPING_CLASSES.each {| key, _ | write_to_file(key)}
-    unsubscribe_run
-  elsif map_class
-   write(map_class)
-  else
-    unsubscribe_run
-  end
-end
-
-def unsubscribe_run
-  processor = Exacttarget::Unsubscribes::Processor.new
-  processor.download_file
-  processor.run
-end
-
-def write_to_file(key)
-  writer = Exacttarget::Builders::MAPPING_CLASSES[key]::CsvWriterComponent.new
-  writer.write_file
-  validator = writer.validate_file
-  if validator.valid?
-    writer.zip_file
-    writer.upload_file
-  else
-    # validator.errors
-    puts 'ERROR: Invalid file.'
-    puts validator.errors
-  end
 end
