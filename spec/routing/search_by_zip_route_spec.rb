2d1255035d1952c984924ac177ac2250b9c6e90c
Andrea Singh <asingh@greatschools.org>
2020-03-05 10:56:01 -0800

    JT-9764 Remove unneeded spec file

--- spec/routing/search_by_zip_route_spec.rb
+++
@@ -1,7 +0,0 @@
-require 'spec_helper'
-
-describe 'search by zip routing' do
-  it 'should route /search/nearbySearch.page' do
-    expect( get '/search/nearbySearch.page' ).to route_to('search#by_zip')
-  end
-end
