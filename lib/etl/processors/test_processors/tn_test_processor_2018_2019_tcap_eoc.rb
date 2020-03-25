5de7dc7152a397aa6f6d0e7f2516e8d138cf8839
Nina Magnuson <nmagnuson@Ninas-Work-Computer.local>
2020-03-25 15:34:34 -0700

    DXT-3433 tn test processor update

--- lib/etl/processors/test_processors/tn_test_processor_2018_2019_tcap_eoc.rb
+++ lib/etl/processors/test_processors/tn_test_processor_2018_2019_tcap_eoc.rb
@@ -45,7 +45,7 @@ class TNTestProcessor20182019TCAPEOC < GS::ETL::TestProcessor
     'US History' => 23
    }
 
-  map_gsdata_prof_band_id = {
+  map_prof_band = {
     'pct_below' => 79,
     'pct_approaching' => 80,
     'pct_on_track' => 81,
@@ -63,24 +63,30 @@ class TNTestProcessor20182019TCAPEOC < GS::ETL::TestProcessor
           row[:data_type_id] = 257
           row[:notes] = 'DXT-3433: TN TCAP'
             if row[:year] == '2018'
+              row[:date_valid] ='2018-01-01 00:00:00'
               row[:description] = 'In 2017-2018, Tennessee used the Tennessee Comprehensive Assessment Program (TCAP) Achievement Test to test students in grades 3 through 8 in reading/language arts, math, and social studies, and grades 5 through 8 in science. The TCAP is a standards-based test that measures specific skills defined for each grade by the state of Tennessee. The goal is for all students to score at or above the proficient level.'   
             elsif row[:year] == '2019'
-              row[:description] = 'In 2018-2019, Tennessee used the Tennessee Comprehensive Assessment Program (TCAP) Achievement Test to test students in grades 3 through 8 in reading/language arts and math, and grades 6 through 8 in social studies. The TCAP is a standards-based test that measures specific skills defined for each grade by the state of Tennessee. The goal is for all students to score at or above the proficient level.'       
+              row[:date_valid] ='2019-01-01 00:00:00'
+              row[:description] = 'In 2018-2019, Tennessee used the Tennessee Comprehensive Assessment Program (TCAP) Achievement Test to test students in grades 3 through 8 in reading/language arts and math, and grades 6 through 8 in social studies. The TCAP is a standards-based test that measures specific skills defined for each grade by the state of Tennessee. The goal is for all students to score at or above the proficient level.'
+            end       
         elsif row[:data_type] == 'GATEWAY'
           row[:data_type_id] = 258
           row[:notes] = 'DXT-3433: TN GATEWAY'
             if row[:year] == '2018'
+              row[:date_valid] ='2018-01-01 00:00:00'
               row[:description] = 'In 2017-2018, Tennessee used the Gateway/EOC (EOC) exams to test high school students in language arts, math, science, and social studies upon completion of relevant courses. The Gateway/EOC exams are standards-based tests that measure how well students are mastering specific skills defined by the state of Tennessee. The goal is for all students to score at or above the proficient level.'    
             elsif row[:year] == '2019'
+              row[:date_valid] ='2019-01-01 00:00:00'
               row[:description] = 'In 2018-2019, Tennessee used the Gateway/End-of-Course (EOC) exams to test high school students in language arts, math, science, and social studies upon completion of relevant courses. The Gateway/EOC exams are standards-based tests that measure how well students are mastering specific skills defined by the state of Tennessee. The goal is for all students to score at or above the proficient level.'
+            end
         end
         row
       end 
       .transform("map subject ids", HashLookup, :subject, map_subject, to: :subject_id)
       .transform("map breakdown id",HashLookup, :breakdown, map_breakdown, to: :breakdown_id)
-      .transform("map prof band id",HashLookup, :proficiency_band, map_breakdown, to: :proficiency_band_id)
+      .transform("map prof band id",HashLookup, :proficiency_band, map_prof_band, to: :proficiency_band_id)
    end
-end
+
 
   def config_hash
     {
