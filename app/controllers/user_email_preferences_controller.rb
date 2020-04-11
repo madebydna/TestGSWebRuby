5bf10261529352de97ca3ab430602497553e72a7
Alexina Chong <achong@greatschools.org>
2020-04-10 20:44:22 -0700

    JT-10113 WIP create districts hash

--- app/controllers/user_email_preferences_controller.rb
+++ app/controllers/user_email_preferences_controller.rb
@@ -17,11 +17,8 @@ class UserEmailPreferencesController < ApplicationController
     @current_preferences_en = UserSubscriptions.new(@current_user).get.select { |sub| sub[:language] == 'en' }.map(&:list)
     @current_preferences_es = UserSubscriptions.new(@current_user).get.select { |sub| sub[:language] == 'es' }.map(&:list)
     account_meta_tags('My email preferences')
-    @current_grades = current_district_grades
-    @current_grades_en = current_district_grades.select { |el| el[:language] == 'en' }
-    @current_grades_es = current_district_grades.select { |el| el[:language] == 'es' }
-    @available_grades = available_grades
-    @available_grades_spanish = available_grades_spanish
+    # @available_grades = available_grades
+    # @available_grades_spanish = available_grades_spanish
     @grades_hashes = create_grades
     @mss_subscriptions = current_user
       .subscriptions_matching_lists([:mystat, :mystat_private, :mystat_unverified])
@@ -92,18 +89,92 @@ class UserEmailPreferencesController < ApplicationController
     params['subscription_ids_to_remove_es']
   end
 
-  def current_district_grades
-    records = @current_user.student_grade_levels
-    records.map do |record|
-      {
-        :grade => record.grade,
-        :district_state => record.district_state,
-        :district_id => record.district_id,
-        :language => record.language,
-        :district_state_and_id => "#{record.district_state}-#{record.district_id}",
-        :district_name => DistrictRecord.find_by(state: record.district_state, district_id: record.district_id)&.name
-      }
+  def create_grades
+    grades = @current_user.student_grade_levels
+    overall_en = grades.select { |record| record[:language] == 'en' && record[:district_id].blank? && record[:district_state].blank? }
+    overall_es = grades.select { |record| record[:language] == 'es' && record[:district_id].blank? && record[:district_state].blank? }
+    district_grades_en = grades.select { |record| record[:language] == 'en' && record[:district_id].present? && record[:district_state].present? }
+    district_grades_es = grades.select { |record| record[:language] == 'es' && record[:district_id].present? && record[:district_state].present? }
+
+    districts_en, districts_es = create_districts(district_grades_en, district_grades_es)
+
+    {
+      :en => {
+              :overall => create_hash(overall_en, 'en'),
+              :districts => districts_en
+             },
+      :es => {
+              :overall => create_hash(overall_es, 'es'),
+              :districts => districts_es
+             }
+    }
+  end
+
+  def create_districts(district_en, district_es)
+    district_en_grades = district_en.group_by { |r| [r[:district_state], r[:district_id]] }
+    district_es_grades = district_es.group_by { |r| [r[:district_state], r[:district_id]] }
+    districts_en = {}
+    districts_es = {}
+
+    district_en_grades.each do |k, grade_info|
+      key = k.join("-")
+      districts_en[key] = create_district(grade_info)
+
+      unless districts_es[key]
+        district_state = k[0]
+        district_id = k[1]
+        district_name = DistrictRecord.find_by(state: district_state, district_id: district_id)&.name
+
+        districts_es[key] = create_hash([], 'es', district_state, district_id, district_name)
+      end
     end
+
+    district_es_grades.each do |k, grade_info|
+      key = k.join("-")
+      districts_en[key] = create_district(grade_info)
+
+      unless districts_en[key]
+        district_state = k[0]
+        district_id = k[1]
+        district_name = DistrictRecord.find_by(state: district_state, district_id: district_id)&.name
+
+        districts_en[key] = create_hash([], 'en', district_state, district_id, district_name)
+      end
+    end
+    [districts_en, districts_es]
+  end
+
+  def create_district(grade_info)
+    district_data = grade_info.first
+    district_name = DistrictRecord.find_by(state: district_data[:district_state], district_id: district_data[:district_id])&.name
+
+    create_hash(grade_info, district_data[:language], district_data[:district_state], district_data[:district_id], district_name)
+  end
+
+  def create_hash(grade_info = [], language = 'en', district_state = '', district_id = '', district_name = '')
+    if language == 'es' && district_name.present?
+      title = "Grado por grado en #{district_name}"
+    elsif language == 'es'
+      title = "Grado por grado"
+    elsif language == 'en' && district_name.present?
+      title = "Grade by Grade at #{district_name}"
+    else
+      title = 'Grade by Grade'
+    end
+
+    grades_labels = language == 'es' ? available_grades_spanish : available_grades
+
+    {
+      :active_grades => grade_info.map { |record| record[:grade] },
+      :district_state => district_state,
+      :district_id => district_id,
+      :language => language,
+      :district_name => district_name,
+      :title => title,
+      :subtitle => "greatkidsnews_subtitle",
+      :label => "select_grades",
+      :available_grades => grades_labels
+    }
   end
 
   private
