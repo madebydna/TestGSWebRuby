895cf1ca6d313143e44a0f5f92e865d8a985928e
Andy Luo <aluo@greatschools.org>
2020-04-13 14:40:31 -0700

    JT-10198 specs for #page_name_analytics

--- spec/controllers/search_controller_spec.rb
+++ spec/controllers/search_controller_spec.rb
@@ -4,11 +4,10 @@ require 'spec_helper'
 
 describe SearchController do
   before { stub_request(:post, /\/solr\/main\/select/).to_return(status: 200, body: "{}", headers: {}) }
+  before { allow(controller).to receive(:params).and_return(params) }
 
   describe '#choose_meta_tag_implementation' do
     subject { controller.send(:choose_meta_tag_implementation) }
-
-    before { allow(controller).to receive(:params).and_return(params) }
 
     context 'with state and district params' do
       let(:params) { {state:'ca', district: 'Alameda Unified School District'} }
@@ -52,6 +51,54 @@ describe SearchController do
       let(:params) { {} }
 
       it { is_expected.to eq(MetaTag::OtherMetaTags) }
+    end
+  end
+
+  describe '#page_name_analytics' do
+    subject { controller.send(:page_name_analytics) }
+
+    context 'with state and district params' do
+      let(:params) { {state:'ca', district: 'Alameda Unified School District'} }
+
+      it { is_expected.to eq('GS:SchoolSearchBrowse') }
+    end
+
+    context 'with state and city params' do
+      let(:params) { {state:'ca', city: 'Alameda'} }
+
+      context 'which matches a valid city' do
+        before { expect(controller).to receive(:city_record).and_return(double) }
+
+        it { is_expected.to eq('GS:SchoolSearchBrowse') }
+      end
+
+      context 'which does not match a valid city' do
+        it { is_expected.to eq('GS:SchoolSearchResults') }
+      end
+    end
+
+    context 'with locationType=zip param' do
+      let(:params) { {locationType: 'zip'}.with_indifferent_access }
+
+      it { is_expected.to eq('GS:SchoolSearchResults') }
+    end
+
+    context 'with locationType=street_address param' do
+      let(:params) { {locationType: 'street_address'}.with_indifferent_access }
+
+      it { is_expected.to eq('GS:SchoolSearchResults') }
+    end
+
+    context 'with state param only' do
+      let(:params) { {state: 'ca'} }
+
+      it { is_expected.to eq('GS:Search:State') }
+    end
+
+    context 'with no matching parameters' do
+      let(:params) { {} }
+
+      it { is_expected.to eq('GS:SchoolSearchResults') }
     end
   end
 end
