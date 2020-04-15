fc17d2e0501f95b9bdebe88dd38ed21f3aeb8d92
Alexina Chong <achong@greatschools.org>
2020-04-15 16:10:30 -0700

    JT-10113 Revert changes

--- app/models/accounts/user_subscription_manager.rb
+++ app/models/accounts/user_subscription_manager.rb
@@ -5,16 +5,10 @@ class UserSubscriptionManager
   end
 
   def update(new_subscriptions)
-    # require 'pry'
-    # binding.pry
-    delete_subs_en = subscriptions_to_delete(new_subscriptions['en'], get_subscriptions['en'])
-    delete_subs_es = subscriptions_to_delete(new_subscriptions['es'], get_subscriptions['es'])
-    delete_subscriptions(delete_subs_en, 'en')
-    delete_subscriptions(delete_subs_es, 'es')
-    add_subs_en = subscriptions_to_add(new_subscriptions['en'], get_subscriptions['en'])
-    add_subs_es = subscriptions_to_add(new_subscriptions['es'], get_subscriptions['es'])
-    save_subscriptions(add_subs_en, 'en')
-    save_subscriptions(add_subs_es, 'es')
+    delete_subs = subscriptions_to_delete(new_subscriptions, get_subscriptions)
+    delete_subscriptions(delete_subs)
+    add_subs = subscriptions_to_add(new_subscriptions, get_subscriptions)
+    save_subscriptions(add_subs)
   end
 
   def unsubscribe
@@ -30,27 +24,24 @@ class UserSubscriptionManager
 
   private
 
-  def save_subscriptions(subs_to_add, language)
+  def save_subscriptions(subs_to_add)
     subs_to_add.each do |list|
       s = Subscription.new
       s.list = list
       s.member_id = @user.id
-      s.language = language
       unless s.save!
         GSLogger.error(:preferences, nil, message: 'User subscriptions failed to save', vars: {
             member_id: member_id,
-            list: list,
-            language: language
+            list: list
         })
 
       end
     end
   end
 
-  def delete_subscriptions(subs_to_delete, language)
-    # TODO: FIX THIS
+  def delete_subscriptions(subs_to_delete)
     begin
-      subscriptions = @user.subscriptions_matching_lists(subs_to_delete, language)
+      subscriptions = @user.subscriptions_matching_lists(subs_to_delete)
       subscriptions.each { |s| SubscriptionHistory.archive_subscription(s) }
       subscriptions.destroy_all
     rescue
@@ -65,12 +56,7 @@ class UserSubscriptionManager
   end
 
   def get_subscriptions
-    # UserSubscriptions.new(@user).get.map(&:to_s)
-    subs = UserSubscriptions.new(@user).get
-    {
-      'en' => subs.select { |sub| sub[:language] == 'en' }.map(&:list),
-      'es' => subs.select { |sub| sub[:language] == 'es' }.map(&:list)
-    }
+    UserSubscriptions.new(@user).get.map(&:to_s)
   end
 
   def subscriptions_to_add(desired_subscriptions, current_subscriptions)
