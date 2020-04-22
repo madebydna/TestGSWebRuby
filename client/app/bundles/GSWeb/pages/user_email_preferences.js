10917875151c71b2a881a052173046f509c2c465
Mitchell Seltzer <mseltzer@greatschools.org>
2020-04-22 15:38:19 -0700

    JT-10112 - adding js from deprecated to application

--- client/app/bundles/GSWeb/pages/user_email_preferences.js
+++ client/app/bundles/GSWeb/pages/user_email_preferences.js
@@ -1,3 +1,17 @@
+import ReactOnRails from 'react-on-rails';
+import SearchBox from 'react_components/search_box';
+import withViewportSize from 'react_components/with_viewport_size';
+import commonPageInit from './common';
+import {indexOf} from 'lodash';
+
+const SearchBoxWrapper = withViewportSize({propName: 'size'})(SearchBox);
+ReactOnRails.register({
+  SearchBoxWrapper,
+});
+
+$(commonPageInit);
+
+
 $(function () {
   if (gon.pagename == "User Email Preferences") {
     var $formContainer = $('.js-user-preferences-form-container');
@@ -8,55 +22,74 @@ $(function () {
           .toggleClass('i-16-blue-check-box i-grey-unchecked-box');
     });
 
-    $('#tab-news-en').click(function () {
-      $("#news-en").addClass("active");
+    $('.tab-news-en').click(function () {
+      $(".news-en").addClass("active");
       $(this).addClass("tab-selected");
-      $("#news-es").removeClass("active");
-      $("#tab-news-es").removeClass("tab-selected");
+      $(".news-es").removeClass("active");
+      $(".tab-news-es").removeClass("tab-selected");
     });
 
-    $('#tab-news-es').click(function () {
-      $("#news-es").addClass("active");
+    $('.tab-news-es').click(function () {
+      $(".news-es").addClass("active");
       $(this).addClass("tab-selected");
-      $("#news-en").removeClass("active");
-      $("#tab-news-en").removeClass("tab-selected");
+      $(".news-en").removeClass("active");
+      $(".tab-news-en").removeClass("tab-selected");
     });
 
+    $formContainer.on('click', '.js-greatkidsnewsCheckbox', function () {
+      if (!$(this).hasClass('active')) {
+        $(this).parent().siblings().find('.js-gradeCheckbox').each(function () {
+          $(this).removeClass('active');
+        })
+      }
+    });
+
+    function changeGbyGState(overallGbyGCheckbox, changeToActive) {
+      if (changeToActive) {
+        overallGbyGCheckbox.removeClass('i-grey-unchecked-box');
+        overallGbyGCheckbox.addClass('active');
+        overallGbyGCheckbox.addClass('i-16-blue-check-box');
+      } else {
+        overallGbyGCheckbox.removeClass('active');
+        overallGbyGCheckbox.removeClass('i-16-blue-check-box');
+        overallGbyGCheckbox.addClass('i-grey-unchecked-box');
+      }
+    }
+
+    $formContainer.on('click', '.js-gradeCheckbox', function () {
+      var activeGbyG = $(this).parent().parent().find('.active');
+      var overallCheckboxParent = $(this).parent().parent().parent().siblings();
+      var overallCheckbox = overallCheckboxParent.find('.js-greatkidsnewsCheckbox');
+      if (activeGbyG.length == 1) {
+        if ($(this).hasClass('active')) {
+          changeGbyGState(overallCheckbox, false)
+        }
+      }
+      if (activeGbyG.length == 0) {
+        changeGbyGState(overallCheckbox, true)
+      }
+    });
 
     $formContainer.find('form').on('submit', function () {
-      let $form = $(this);
-      let grades = [];
-      let subscriptions = [];
-      console.log("form");
-
-
+      var $form = $(this);
+      var grades = [];
+      var subscriptions = [];
+      var schools = [];
 
       $form.find('.js-gradeCheckbox.active').each(function () {
-        // console.log("loop");
-        // console.log($(this).data('districtState'));
-        // console.log($(this).data('districtId'));
         grades.push([$(this).data('grade'), $(this).data('language'), $(this).data('districtId'), $(this).data('districtState')]);
       });
-      console.log("grades:" + JSON.stringify(grades));
-
-      let grades_value = $('.js-gradeSubmitValue');
-      grades_value.val(JSON.stringify(grades));
-      console.log(grades_value.val());
-
-
+      $('.js-gradeSubmitValue').val(JSON.stringify(grades));
 
       $form.find('.js-subscriptionCheckbox.active').each(function () {
-        console.log("loop");
-        console.log($(this).data('list'));
-        console.log($(this).data('language'));
         subscriptions.push([$(this).data('list'), $(this).data('language')]);
       });
-      console.log("subscriptions:" + JSON.stringify(subscriptions));
+      $('.js-subscriptionSubmitValue').val(JSON.stringify(subscriptions));
 
-      let subscriptions_value = $('.js-subscriptionSubmitValue');
-      subscriptions_value.val(JSON.stringify(subscriptions));
-      console.log(subscriptions_value.val());
-
+      $form.find('.js-mssSubscriptionCheckbox.active').each(function () {
+        schools.push([$(this).data('list'), $(this).data('language'), $(this).data('state'), $(this).data('schoolId')]);
+      });
+      $('.js-schoolSubmitValue').val(JSON.stringify(schools));
 
       $form.find('.js-inverted-checkbox').each(function () {
         $(this).toggleClass('active');
@@ -66,22 +99,6 @@ $(function () {
         $(this).prop('disabled', false);
       });
     });
-
-    $formContainer.on('click', '.js-greatkidsnews-grades-checkbox:not(.active)', function () {
-      var $greatkidsnewsCheckbox = $('.js-greatkidsnews-checkbox');
-      if (!($greatkidsnewsCheckbox.hasClass('active'))) {
-        $greatkidsnewsCheckbox.trigger('click');
-      }
-    });
-
-    $formContainer.on('click', '.js-greatkidsnews-grades-checkbox.active', function () {
-      var $greatkidsnewsCheckbox = $('.js-greatkidsnews-checkbox');
-      var activeItems = $('.js-greatkidsnews-grades-checkbox.active');
-      if (activeItems.length < 2 && $greatkidsnewsCheckbox.hasClass('active')) {
-        $greatkidsnewsCheckbox.trigger('click');
-      }
-    });
-
 
   }
 });
