bd5afdca87127a09b8ac928c500f0d17134163fc
Mitchell Seltzer <mseltzer@greatschools.org>
2020-04-28 18:01:27 -0700

    JT-10276 - Adding download and import of spanish subscription sign ups and unsubscribe data from ET. Bunch of refactoring that will need thorough testing.

--- app/models/accounts/user_email_grade_manager.rb
+++ app/models/accounts/user_email_grade_manager.rb
@@ -11,6 +11,11 @@ class UserEmailGradeManager
     save_grades(add_grades)
   end
 
+  def add_no_duplicates(new_grades)
+    add_grades = grades_to_add(new_grades, get_grades)
+    save_grades(add_grades)
+  end
+
   def save_grades(grades_to_add)
     grades_to_add.each do |subscription|
       s = StudentGradeLevel.new
@@ -32,22 +37,35 @@ class UserEmailGradeManager
     end
   end
 
-  def delete_grades(grades_to_delete = :all)
+  def get_grades_by_array(grades)
+    grades.map do |subscription|
+      grade = subscription[0]
+      language = subscription[1]
+      district_id = subscription[2].blank? ? nil : subscription[2]
+      district_state = subscription[3].blank? ? nil : subscription[3]
+      @user.student_grade_levels.where(grade: grade, language: language, district_id: district_id, district_state: district_state)
+    end
+  end
+
+  def delete_grades_by_language(language)
+    grade_objects = @user.student_grade_levels.where(language: language)
+    do_delete_grades(grade_objects)
+  end
+
+  def delete_all_grades
+    user_grades = @user.student_grade_levels
+    do_delete_grades(user_grades)
+  end
+
+  def delete_grades(grades_to_delete)
+    user_grades = get_grades_by_array(grades_to_delete)
+    do_delete_grades(user_grades)
+  end
+
+  def do_delete_grades(grade_objects)
     begin
-      if grades_to_delete == :all
-        user_grades = @user.student_grade_levels
-      else
-        user_grades = []
-        grades_to_delete.each do |subscription|
-          grade = subscription[0]
-          language = subscription[1]
-          district_id = subscription[2].blank? ? nil : subscription[2]
-          district_state = subscription[3].blank? ? nil : subscription[3]
-          user_grades += @user.student_grade_levels.where(grade: grade, language: language, district_id: district_id, district_state: district_state)
-        end
-      end
-      user_grades.each { |g| StudentGradeLevelHistory.archive_student_grade_level(g) }
-      user_grades.each(&:destroy)
+      grade_objects.each { |g| StudentGradeLevelHistory.archive_student_grade_level(g) }
+      grade_objects.each(&:destroy)
     rescue
       GSLogger.error(:unsubscribe, nil, message: 'User delete grades failed', vars: {
           member_id: @user.id
@@ -57,7 +75,6 @@ class UserEmailGradeManager
 
   def get_grades
     current_grades = @user.student_grade_levels
-
     current_grades.map { |r| [r[:grade], r[:language], convert_nil_to_string(r[:district_id]), r[:district_state].to_s] }
   end
 
