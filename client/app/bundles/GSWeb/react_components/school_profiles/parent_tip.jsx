296de296aec869703ad74d784baae450cb293d90
Andy Luo <aluo@greatschools.org>
2020-04-29 11:45:50 -0700

    JT-10250 updated owl art asset. change to alt text null

--- client/app/bundles/GSWeb/react_components/school_profiles/parent_tip.jsx
+++ client/app/bundles/GSWeb/react_components/school_profiles/parent_tip.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { t } from '../../util/i18n';
-import owlPng from 'school_profiles/owl.png';
+import owlPng from 'school_profiles/brown-owl.svg';
 
 const ParentTip = ({children}) => {
   return (
     <div>
       <p className="parent-tip">
-        <img src={owlPng} />
+        <img src={owlPng} alt="" />
         <span className="speech-bubble left">{t('Parent tip')}</span>
-      </p> 
+      </p>
       <p className="footnote">
         {children}
       </p>
