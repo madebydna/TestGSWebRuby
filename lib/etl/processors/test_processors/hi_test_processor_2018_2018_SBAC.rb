b99aee9c98a3161c0c5a72d36d87d5ac7e01e5ba
Nina Magnuson <nmagnuson@Ninas-Work-Computer.local>
2020-04-29 16:27:30 -0700

    DXT-3447 HI test processor update

--- lib/etl/processors/test_processors/hi_test_processor_2018_2018_SBAC.rb
+++ lib/etl/processors/test_processors/hi_test_processor_2018_2018_SBAC.rb
@@ -28,25 +28,24 @@ class HITestProcessor20182019SBAC < GS::ETL::TestProcessor
       data_type_id: 189,
       notes: 'DXT-3447: HI HI SBAC'
     })
-    .transform("Filling in description and date valid", WithBlock) do |row|
-      if row[:year] == '2018'
-            row[:date_valid] = '2018-01-01 00:00:00'
-            row[:description] = 'In 2017-2018, students in Hawaii took the Smarter Balanced Assessments (SBA) in Mathematics and English Language Arts/Literacy (ELA). The SBA is aligned to the Hawaii Common Core Standards, and designed to measure whether students are on track for readiness in college and/or career. SBA replaced the Hawaii State Assessment in math and reading. These are mandatory assessments given to students in grades 3-8 and 11.'
-
-      elsif row[:year] == '2019'
-            row[:date_valid] = '2019-01-01 00:00:00'
-            row[:description] = 'In 2018-2019, students in Hawaii took the Smarter Balanced Assessments (SBA) in Mathematics and English Language Arts/Literacy (ELA). The SBA is aligned to the Hawaii Common Core Standards, and designed to measure whether students are on track for readiness in college and/or career. SBA replaced the Hawaii State Assessment in math and reading. These are mandatory assessments given to students in grades 3-8 and 11.'
-      end
-     row
-    end
     .transform("Adding column breakdown_id from breadown", HashLookup, :breakdown, map_breakdown, to: :breakdown_id)
     .transform("Adding column subject_id from subject", HashLookup, :subject, map_subject, to: :subject_id)
     .transform("Adding column proficiency_band_id from proficiency band", HashLookup, :proficiency_band, map_prof_band_id, to: :proficiency_band_id)
+    .transform("Filling in description and date valid", WithBlock) do |row|
+      if row[:year] == '2018'
+        row[:date_valid] = '2018-01-01 00:00:00'
+        row[:description] = 'In 2017-2018, students in Hawaii took the Smarter Balanced Assessments (SBA) in Mathematics and English Language Arts/Literacy (ELA). The SBA is aligned to the Hawaii Common Core Standards, and designed to measure whether students are on track for readiness in college and/or career. SBA replaced the Hawaii State Assessment in math and reading. These are mandatory assessments given to students in grades 3-8 and 11.'
+      elsif row[:year] == '2019'
+        row[:date_valid] = '2019-01-01 00:00:00'
+        row[:description] = 'In 2018-2019, students in Hawaii took the Smarter Balanced Assessments (SBA) in Mathematics and English Language Arts/Literacy (ELA). The SBA is aligned to the Hawaii Common Core Standards, and designed to measure whether students are on track for readiness in college and/or career. SBA replaced the Hawaii State Assessment in math and reading. These are mandatory assessments given to students in grades 3-8 and 11.'
+      end
+     row
+    end
   end
 
   def config_hash
     {
-        gsdata_source_id: 15,
+        source_id: 15,
         state: 'hi'
     }
   end
