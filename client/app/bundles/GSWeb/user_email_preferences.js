ffce7fd384799cc11800f89ee058c7eb649186b3
Mitchell Seltzer <mseltzer@greatschools.org>
2020-04-30 16:09:38 -0700

    JT-10112 - resetting form if email valid fails. Also some refactoring to get js for user sign up to work correctly

--- client/app/bundles/GSWeb/user_email_preferences.js
+++ client/app/bundles/GSWeb/user_email_preferences.js
@@ -3,5 +3,9 @@ import SearchBoxWrapper from "./react_components/search_box_wrapper";
 import commonPageInit from "./common";
 import preferencePageInit from './pages/user_email_preferences';
 
+ReactOnRails.register({
+  SearchBoxWrapper,
+});
+
 $(commonPageInit());
 $(preferencePageInit());
