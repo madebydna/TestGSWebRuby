764e49910fc9a416abf337769f8b8e3e68c2a70b
Andy Luo <aluo@greatschools.org>
2020-05-08 17:51:28 -0700

    JT-10326 remote learning CTA from school profiles

--- app/controllers/school_profiles_controller.rb
+++ app/controllers/school_profiles_controller.rb
@@ -102,6 +102,7 @@ class SchoolProfilesController < ApplicationController
         sp.stem_courses = stem_courses
         sp.academic_progress = academic_progress
         sp.reviews_on_demand = reviews_on_demand?
+        sp.distance_learning = distance_learning.data_values
       end
     )
   end
@@ -150,6 +151,14 @@ class SchoolProfilesController < ApplicationController
   def school_cache_data_reader
     @_school_cache_data_reader ||=
       SchoolProfiles::SchoolCacheDataReader.new(school)
+  end
+
+  def district_cache_data_reader
+    @_district_cache_data_reader ||= DistrictCacheDataReader.new(school.district, district_cache_keys: Array.wrap('crpe'))
+  end
+
+  def distance_learning
+    @_distance_learning ||= SchoolProfiles::DistanceLearning.new(school, district_cache_data_reader: district_cache_data_reader)
   end
 
   def hero
