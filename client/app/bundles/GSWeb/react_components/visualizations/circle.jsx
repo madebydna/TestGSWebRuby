6f4d38bbc1444d8dd10f4fad28a4d5babbbf68d6
Alexina Chong <achong@greatschools.org>
2020-05-14 02:51:19 -0700

    JT-10319 Add Circle data viz row type

---
+++ client/app/bundles/GSWeb/react_components/visualizations/circle.jsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { t } from '../../util/i18n';
+import CircleCheck from '../icons/circle_check';
+import CircleDash from '../icons/circle_dash';
+import CircleX from '../icons/circle_x';
+
+export default class Circle extends React.Component {
+  static propTypes = {
+    value: PropTypes.string.isRequired
+    // breakdown: PropTypes.string.isRequired,
+    // score: PropTypes.number.isRequired,
+    // label: PropTypes.string.isRequired,
+    // percentage: PropTypes.string,
+    // display_percentages: PropTypes.bool,
+    // number_students_tested: PropTypes.string,
+    // state_average: PropTypes.number,
+    // state_average_label: PropTypes.string,
+    // invertedRatings:  PropTypes.bool,
+    // use_gray: PropTypes.bool
+  };
+
+  constructor(props) {
+    super(props);
+    this.renderCircle = this.renderCircle.bind(this);
+    this.renderKey = this.renderKey.bind(this);
+  }
+
+  renderCircle() {
+    if (this.props.value === 'All') {
+      return <CircleCheck key={this.renderKey()} />;
+    } else if (this.props.value === 'Partial') {
+      return <CircleDash key={this.renderKey()} />;
+    } else {
+      return <CircleX key={this.renderKey()} />;
+    }
+  }
+
+
+
+  renderKey() {
+    return this.props.breakdown + Math.random();
+  }
+
+  render() {
+    return (
+      <div className="circle-container">
+        {this.renderCircle()}
+      </div>
+    );
+  }
+}
