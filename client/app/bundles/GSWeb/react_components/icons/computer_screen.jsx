8aea5ef8ccc01f551a36f42b5dbdc0e0c213007b
Andy Luo <aluo@greatschools.org>
2020-05-27 11:22:22 -0700

    JT-10423 change computer screen to be reuseable component

--- client/app/bundles/GSWeb/react_components/icons/computer_screen.jsx
+++ client/app/bundles/GSWeb/react_components/icons/computer_screen.jsx
@@ -1,16 +1,38 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { t } from '../../util/i18n';
 
-const ComputerScreen = (props) => {
-  return (
-    <svg width="51" height="50" viewBox="0 0 51 50" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <ellipse cx="25.5" cy="25" rx="25.5" ry="25" fill="#22A4DD"/>
-      <rect x="9.5" y="13" width="32" height="19" rx="1" fill="#CAE3F3"/>
-      <rect x="23" y="32.85" width="5" height="2" fill="#CAE3F3"/>
-      <path fillRule="evenodd" clipRule="evenodd" d="M15.0017 37.8416H37.7783C37.5616 36.6242 36.4978 35.7 35.2181 35.7H17.5619C16.2822 35.7 15.2185 36.6242 15.0017 37.8416Z" fill="#CAE3F3"/>
-    </svg>
-  );
-}
+const ComputerScreen = (props) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={props.width}
+    height={props.height}
+    fill="none"
+    viewBox={props.viewBox}
+  >
+    <ellipse cx="25.5" cy="25" fill={props.background} rx="25.5" ry="25"></ellipse>
+    <rect width="32" height="19" x="9.5" y="13" fill={props.color} rx="1"></rect>
+    <path fill={props.color} d="M23 32.85H28V34.85H23z"></path>
+    <path
+      fill={props.color}
+      d="M15.002 37.842h22.776a2.601 2.601 0 00-2.56-2.142H17.562c-1.28 0-2.344.924-2.56 2.142z"
+    ></path>
+  </svg>
+);
+
+ComputerScreen.propTypes = {
+  width: PropTypes.string.isRequired,
+  height: PropTypes.string.isRequired,
+  viewBox: PropTypes.string.isRequired,
+  color: PropTypes.string,
+  background: PropTypes.string
+};
+
+ComputerScreen.defaultProps = {
+  width: "51",
+  height: "50",
+  viewBox: "0 0 51 50",
+  color: "#CAE3F3",
+  background: "#22A4DD"
+};
 
 export default ComputerScreen;
