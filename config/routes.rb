dd53bd0aa74483d8f29b3441387f5370e53375ff
Alexina Chong <achong@greatschools.org>
2020-05-29 14:33:57 -0700

    JT-10408 WIP Develop frontend form

--- config/routes.rb
+++ config/routes.rb
@@ -238,6 +238,10 @@ LocalizedProfiles::Application.routes.draw do
   get '/api/request-api-key/success/', to: 'admin/api_accounts#success', as: :request_api_key_success
   post '/api/request-api-key/', to: 'admin/api_accounts#create_api_account', as: :post_request_api_key
 
+  get '/api/request-api-key-new/', to: 'admin/api_users#register', as: :request_api_key_new
+  get '/api/request-api-key-new/success/', to: 'admin/api_users#success', as: :request_api_key_success_new
+  post '/api/request-api-key-new/', to: 'admin/api_users#create_api_account', as: :post_request_api_key_new
+
 
   namespace :api, controller: 'api', path:'/gsr/api' do
     resource :session
