94357d601aeb0b001872b014077eb2666f99205b
Alexina Chong <achong@greatschools.org>
2020-06-10 21:52:24 -0700

    JT-10402 Hide hr if no graph in module

--- client/app/bundles/GSWeb/react_components/equity/equity_content_pane.jsx
+++ client/app/bundles/GSWeb/react_components/equity/equity_content_pane.jsx
@@ -36,6 +36,8 @@ export default class EquityContentPane extends React.Component {
         'Learning',
         'Planning',
         'Teaching',
+        'K-8',
+        'High School',
         'main',
         undefined
     ].indexOf(this.props.anchor) === -1;
@@ -52,7 +54,7 @@ export default class EquityContentPane extends React.Component {
     return(
       <div className={'tabs-panel tabs-panel_selected'}>
         <div className="row">
-          <div className="top-content">{this.get_narrative()}<hr  /></div>
+    <div className="top-content">{this.get_narrative()}{this.props.graph && <hr  />}</div>
           {this.getColumnHeader()}
           <div>{this.props.graph}</div>
         </div>
