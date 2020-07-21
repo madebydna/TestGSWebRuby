fa67705669f0a9a1cbe32e5e977f20e7c836a138
Andy Luo <aluo@greatschools.org>
2020-07-21 09:05:01 -0700

    JT-10712 updated specs to match new intent

--- spec/libs/school_profiles/test_scores_spec.rb
+++ spec/libs/school_profiles/test_scores_spec.rb
@@ -29,32 +29,29 @@ describe SchoolProfiles::TestScores do
     state_without_standrized_testing = SchoolProfiles::TestScores::STATES_WITHOUT_HS_STANDARDIZED_TESTS.sample
     state_with_testing = (States.abbreviations - SchoolProfiles::TestScores::STATES_WITHOUT_HS_STANDARDIZED_TESTS).sample
 
-    it 'should return false if test scores is not present' do
-      allow(subject).to receive(:subject_scores).and_return([])
+    it 'should return false if the module should be suppressed' do
+      allow(subject).to receive(:suppress_module?).and_return(true)
       expect(subject.visible?).to be_falsey
     end
 
     it 'should have a list of states without standarized testing' do
       expect {  SchoolProfiles::TestScores::STATES_WITHOUT_HS_STANDARDIZED_TESTS }.to_not raise_error
       expect(SchoolProfiles::TestScores::STATES_WITHOUT_HS_STANDARDIZED_TESTS).to eq(%w(al ct de il mt nh pa ri wv))
+      expect(SchoolProfiles::TestScores::STATES_WITHOUT_HS_STANDARDIZED_TESTS).to eq(TestScoresCaching::TestScoresCacherGsdata::ALT_NULL_STATE_FILTER)
     end
 
     it 'should return true if test scores are present' do
-      allow(subject).to receive(:subject_scores).and_return(test_scores)
+      allow(subject).to receive(:suppress_module?).and_return(false)
       expect(subject.visible?).to be_truthy
     end
 
     [
-      [state_without_standrized_testing, false, true, true],
-      [state_without_standrized_testing, false, false, false],
-      [state_without_standrized_testing, true, true, false],
-      [state_without_standrized_testing, true, false, false],
-      [state_with_testing, false, false, false],
-      [state_with_testing, false, true, true],
-      [state_with_testing, true, false, false],
-      [state_with_testing, true, true, true],
-    ].each do |(state, is_high_school, test_scores, result)|
-      context "State:#{state}, high_school?: #{is_high_school}, Test_scores?: #{test_scores}, Expect_Result: #{result}" do
+      [state_without_standrized_testing, false, true],
+      [state_without_standrized_testing, true, false],
+      [state_with_testing, false, true],
+      [state_with_testing, false, true],
+    ].each do |(state, is_high_school, result)|
+      context "State:#{state}, high_school?: #{is_high_school}, Expect_Result: #{result}" do
         let(:school) { double("school", state: state) }
         let(:school_cache_data_reader) { double("school_cache_data_reader") }
         subject do
@@ -65,7 +62,6 @@ describe SchoolProfiles::TestScores do
         end
 
         it do
-          allow(subject).to receive(:subject_scores).and_return(test_scores ? [double("test_scores")] : [])
           allow(school).to receive(:high_school?).and_return(is_high_school)
 
           expect(subject.visible?).to eq(result)
