5cf9a62547b853bdd72af0c552516246c8d53d52
Andy Luo <aluo@greatschools.org>
2020-08-12 13:18:00 -0700

    add fullhost change

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2, ENV_GLOBAL['GOOGLE_OAUTH2_CLIENT_ID'], ENV_GLOBAL['GOOGLE_OAUTH2_CLIENT_SECRET'], access_type: 'online', prompt: ''
 end
+
+OmniAuth.config.full_host = Rails.env.production? ? 'https://www.greatschools.org' : 'https://qa.greatschools.org'
