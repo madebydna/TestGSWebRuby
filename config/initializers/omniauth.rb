59493f37058b23a1dd8f01ecc6ce1d3a1b9e0033
Andy Luo <aluo@greatschools.org>
2020-08-12 15:06:41 -0700

    change full host name

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,12 +2,11 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2, ENV_GLOBAL['GOOGLE_OAUTH2_CLIENT_ID'], ENV_GLOBAL['GOOGLE_OAUTH2_CLIENT_SECRET'], access_type: 'online', prompt: ''
 end
 
-OmniAuth.config.full_host do
+OmniAuth.config.full_host = 
   if ENV_GLOBAL['app_host'] =~ /qa-web/
     'https://qa.greatschools.org'
   elsif ENV_GLOBAL['app_host'] =~ /prod-web/
     'https://www.greatschools.org'
   else
-    ENV_GLOBAL['app_host']
+    "http://#{ENV_GLOBAL['app_host']}:#{ENV_GLOBAL['app_port']}"
   end
-end
