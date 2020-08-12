9940211fb8c98c97bbb73eb0bc962f35a9f70d02
Andy Luo <aluo@greatschools.org>
2020-08-12 14:07:38 -0700

    updated hostname

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,4 +2,9 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2, ENV_GLOBAL['GOOGLE_OAUTH2_CLIENT_ID'], ENV_GLOBAL['GOOGLE_OAUTH2_CLIENT_SECRET'], access_type: 'online', prompt: ''
 end
 
-OmniAuth.config.full_host = Rails.env.production? ? 'https://qa.greatschools.org' : 'https://qa.greatschools.org'
+OmniAuth.config.full_host do
+  return 'https://qa.greatschools.org' if ENV_GLOBAL['app_host'] =~ /qa-web/
+  return 'https://www.greatschools.org' if ENV_GLOBAL['app_host'] =~ /prod-web/
+
+  ENV_GLOBAL['app_host']
+end
