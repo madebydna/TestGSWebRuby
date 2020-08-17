9a65943c241d50c49e0718e62015dcbbac70bd24
Andy Luo <aluo@greatschools.org>
2020-08-17 11:46:22 -0700

    JT-10763 added specs

--- spec/models/api/user_spec.rb
+++ spec/models/api/user_spec.rb
@@ -3,6 +3,8 @@
 require 'spec_helper'
 
 describe Api::User do
+  let(:user) { create(:api_user, first_name: 'Andy', last_name: 'Luo', city: 'Oakland', state: 'ca') }
+
   after do
     clean_models Api::User
   end
@@ -12,5 +14,13 @@ describe Api::User do
     user2 = Api::User.new(email: 'test@test.com')
     expect(user2.valid?).to be_falsey
     expect(user2.errors.messages[:email]).to eq ['has already been taken']
+  end
+
+  it '#full_name' do
+    expect(user.full_name).to eq('Andy Luo')
+  end
+
+  it '#locality' do
+    expect(user.locality).to eq('Oakland, CA')
   end
 end
