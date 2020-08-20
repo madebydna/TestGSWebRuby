29f15f553f673d0f30bcfacaab829de1688bba62
Andrea Singh <asingh@greatschools.org>
2020-08-20 13:08:39 -0700

    JT-10852 Change db's to pull structure from

--- script/ci/build_env.sh
+++ script/ci/build_env.sh
@@ -10,11 +10,11 @@ sudo /sbin/route add -net 192.168.121.0 netmask 255.255.255.0  gw 192.168.111.18
 # bundle install --deployment --without development
 
 echo 'Setting up databases'
-mysqldump -hqa-db1.greatschools.org -d -u${DB_USER} -p${DB_PASS} --databases gs_schooldb localized_profiles us_geo community api _ak _al _ar _az _ca _co \
+mysqldump -hstaging.greatschools.org -d -u${DB_USER} -p${DB_PASS} --databases gs_schooldb localized_profiles us_geo community api _ak _al _ar _az _ca _co \
 _ct _dc _de _fl _ga _hi _ia _id _il _in _ks _ky _la _ma _md _me _mi _mn _mo _ms _mt _nc _nd _ne _nh _nj _nm _nv _ny _oh _ok _or _pa _ri _sc \
 _sd _tn _tx _ut _va _vt _wa _wi _wv _wy | sed -e 's/\(.*DATABASE.*\)`\(.*\)`/\1`\2_test`/;s/\(.*USE \)`\(.*\)`/\1`\2_test`/' | mysql -f -uroot
 
-mysqldump -hqa-warehouse.greatschools.org -d -u${DB_USER} -p${DB_PASS} --databases omni gsdata | sed -e 's/\(.*DATABASE.*\)`\(.*\)`/\1`\2_test`/;s/\(.*USE \)`\(.*\)`/\1`\2_test`/' | mysql -f -uroot
+mysqldump -hdev-gsdata.greatschools.org -d -u${DB_USER} -p${DB_PASS} --databases omni gsdata | sed -e 's/\(.*DATABASE.*\)`\(.*\)`/\1`\2_test`/;s/\(.*USE \)`\(.*\)`/\1`\2_test`/' | mysql -f -uroot
 
 # echo 'Webpack and npm install'
 # mkdir -p app/assets/webpack
