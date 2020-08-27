e90cc95f3740e1cd9f1849dc97d587437ee0b706
Anthony Roy <aroy@greatschools.org>
2020-08-27 11:57:39 -0700

    JT-10976 Add LI and ethnicity underserved indicator to data layer

--- spec/controllers/school_profiles_controller_spec.rb
+++ spec/controllers/school_profiles_controller_spec.rb
@@ -1,5 +1,158 @@
 require 'spec_helper'
 describe SchoolProfilesController do
+  describe '#student_li_indicator' do
+    subject { controller.send(:student_li_indicator) }
+
+    context 'with no data' do
+      before do
+        subgroups_data = {}
+        students = double(subgroups_data: subgroups_data)
+        allow(controller).to receive(:students).and_return(students)
+      end
+
+      it { is_expected.to be_falsey }
+    end
+
+    context 'with no state data' do
+      before do
+        values = [MetricsCaching::Value.from_hash({breakdown: 'All students', school_value: 15})].extend(MetricsCaching::Value::CollectionMethods)
+
+        students = double(subgroups_data: {'Students participating in free or reduced-price lunch program' => values})
+        allow(controller).to receive(:students).and_return(students)
+      end
+
+      it { is_expected.to be_falsey }
+    end
+
+    context 'with LI % less than state average' do
+      before do
+        values = [MetricsCaching::Value.from_hash({breakdown: 'All students', school_value: 15, state_average: 35})].extend(MetricsCaching::Value::CollectionMethods)
+
+        students = double(subgroups_data: {'Students participating in free or reduced-price lunch program' => values})
+        allow(controller).to receive(:students).and_return(students)
+      end
+
+      it { is_expected.to be_falsey }
+    end
+
+    context 'with LI % greater than state average' do
+      before do
+        values = [MetricsCaching::Value.from_hash({breakdown: 'All students', school_value: 15, state_average: 5})].extend(MetricsCaching::Value::CollectionMethods)
+
+        students = double(subgroups_data: {'Students participating in free or reduced-price lunch program' => values})
+        allow(controller).to receive(:students).and_return(students)
+      end
+
+      it { is_expected.to be_truthy }
+    end
+  end
+
+  describe '#student_ethnicity_indicator' do
+    subject { controller.send(:student_ethnicity_indicator) }
+
+    context 'with no data' do
+      before do
+        ethnicity_data = {}
+        students = double(ethnicity_data: ethnicity_data)
+        allow(controller).to receive(:students).and_return(students)
+      end
+
+      it { is_expected.to be_falsey }
+    end
+
+    context 'with no state data' do
+      before do
+        ethnicity_data = [{breakdown: 'Black', school_value: 15},
+                          {breakdown: 'Hispanic', school_value: 5},
+                          {breakdown: 'Native American', school_value: 3}]
+        students = double(ethnicity_data: ethnicity_data)
+        allow(controller).to receive(:students).and_return(students)
+        allow(controller).to receive(:school).and_return(double(state: 'ca'))
+        allow(StateCacheDataReader).to receive(:new).and_return(double(ethnicity_data: {}))
+      end
+
+      it { is_expected.to be_falsey }
+    end
+
+    context 'with ethnicities summing less than state average' do
+      before do
+        ethnicity_data = [{breakdown: 'Black', school_value: 15},
+                          {breakdown: 'Hispanic', school_value: 5},
+                          {breakdown: 'Native American', school_value: 3}]
+        students = double(ethnicity_data: ethnicity_data)
+        allow(controller).to receive(:students).and_return(students)
+        allow(controller).to receive(:school).and_return(double(state: 'ca'))
+        state_data = [{'breakdown' => 'Black', 'state_value' => 18},
+                      {'breakdown' => 'Hispanic', 'state_value' => 4},
+                      {'breakdown' => 'Native American', 'state_value' => 3}]
+        allow(StateCacheDataReader).to receive(:new).and_return(double(ethnicity_data: state_data))
+      end
+
+      it { is_expected.to be_falsey }
+    end
+
+    context 'with ethnicities summing greater than state average' do
+      before do
+        ethnicity_data = [{breakdown: 'Black', school_value: 15},
+                          {breakdown: 'Hispanic', school_value: 5},
+                          {breakdown: 'Native American', school_value: 3}]
+        students = double(ethnicity_data: ethnicity_data)
+        allow(controller).to receive(:students).and_return(students)
+        allow(controller).to receive(:school).and_return(double(state: 'ca'))
+        state_data = [{'breakdown' => 'Black', 'state_value' => 12},
+                      {'breakdown' => 'Hispanic', 'state_value' => 6},
+                      {'breakdown' => 'Native American', 'state_value' => 3}]
+        allow(StateCacheDataReader).to receive(:new).and_return(double(ethnicity_data: state_data))
+      end
+
+      it { is_expected.to be_truthy }
+    end
+  end
+
+  describe '#data_layer_through_gon' do
+    subject { controller.send(:data_layer_through_gon) }
+    before do
+      allow(controller).to receive(:data_layer_gon_hash).and_return({})
+      allow(controller).to receive(:page_view_metadata).and_return({})
+    end
+
+    context 'with both indicators false' do
+      before do
+        allow(controller).to receive(:student_li_indicator).and_return(false)
+        allow(controller).to receive(:student_ethnicity_indicator).and_return(false)
+      end
+
+      it { is_expected.to eq({'uInd' => '' }) }
+    end
+
+    context 'with low income indicator true' do
+      before do
+        allow(controller).to receive(:student_li_indicator).and_return(true)
+        allow(controller).to receive(:student_ethnicity_indicator).and_return(false)
+      end
+
+      it { is_expected.to eq({'uInd' => 'lInd' }) }
+    end
+
+    context 'with ethnicity indicator true' do
+      before do
+        allow(controller).to receive(:student_li_indicator).and_return(false)
+        allow(controller).to receive(:student_ethnicity_indicator).and_return(true)
+      end
+
+      it { is_expected.to eq({'uInd' => 'eInd' }) }
+    end
+
+    context 'with both indicators true' do
+      before do
+        allow(controller).to receive(:student_li_indicator).and_return(true)
+        allow(controller).to receive(:student_ethnicity_indicator).and_return(true)
+      end
+
+      it { is_expected.to eq({'uInd' => 'lInd,eInd' }) }
+    end
+  end
+
   describe '#require_school' do
     subject { controller.send(:require_school) }
 
