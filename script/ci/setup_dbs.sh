6598e9354dff37f12357d6fe9dc8c1346c5a59b6
Andrea Singh <asingh@greatschools.org>
2020-09-02 15:08:25 -0700

    JT-11017 Changed setup_db script

--- script/ci/setup_dbs.sh
+++ script/ci/setup_dbs.sh
@@ -1,9 +1,24 @@
 #!/bin/bash
 
 echo 'Setting up databases'
-echo 'Setting up gs_schooldb and sharded dbs'
-mysqldump --no-data -hstaging.greatschools.org -d -u${DB_USER} -p${DB_PASS} --databases gs_schooldb localized_profiles us_geo community api _ak _al _ar _az _ca _co \
-_ct _dc _de _fl _ga _hi _ia _id _il _in _ks _ky _la _ma _md _me _mi _mn _mo _ms _mt _nc _nd _ne _nh _nj _nm _nv _ny _oh _ok _or _pa _ri _sc \
-_sd _tn _tx _ut _va _vt _wa _wi _wv _wy | sed -e 's/\(.*DATABASE.*\)`\(.*\)`/\1`\2_test`/;s/\(.*USE \)`\(.*\)`/\1`\2_test`/' | mysql -f -uroot
-echo 'Setting up warehouse dbs'
-mysqldump --no-data -hdev-gsdata.greatschools.org -d -u${DB_USER} -p${DB_PASS} --databases omni gsdata | sed -e 's/\(.*DATABASE.*\)`\(.*\)`/\1`\2_test`/;s/\(.*USE \)`\(.*\)`/\1`\2_test`/' | mysql -f -uroot
+echo 'SCP-ing gs_schooldb and sharded dbs from staging.greatschools.org'
+scp syncer@staging.greatschools.org:jenkins_mysqldump_db /home/admin/
+
+if test "`find /home/admin/jenkins_mysqldump_db -mmin -300`"; then
+  echo 'jenkins_mysqldump_db is current'
+  mysql -f -uroot < /home/admin/jenkins_mysqldump_db
+else
+  echo 'jenkins_mysqldump_db is older than 5h --aborting'
+  exit 1
+fi
+
+echo 'SCP-ing warehouse dbs from dev-gsdata.greatschools.org'
+scp syncer@dev-gsdata.greatschools.org:jenkins_mysqldump_gsdata /home/admin/
+
+if test "`find /home/admin/jenkins_mysqldump_gsdata -mmin -300`"; then
+  echo 'jenkins_mysqldump_gsdata is current'
+  mysql -f -uroot < /home/admin/jenkins_mysqldump_gsdata
+else
+  echo 'jenkins_mysqldump_gsdata is older than 5h --aborting'
+  exit 1
+fi
