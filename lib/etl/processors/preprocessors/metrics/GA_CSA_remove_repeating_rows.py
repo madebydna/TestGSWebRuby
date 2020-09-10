da9e928a790f189ef2d55fd09581473ba5b468a1
Elle Pujals <epujals@greatschools.org>
2020-09-09 17:26:49 -0700

    Tidying up python files (#114)
    * Apply standardized formatting to python files
    
    * Move python files into subfolders with the others
    
    * Add pre-processing template to helpful_code
    
    * Move OK metrics processor into metrics_processors

---
+++ lib/etl/processors/preprocessors/metrics/GA_CSA_remove_repeating_rows.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python
+
+import csv
+
+open_act = open("ACT_HIGHEST_2019_FEB_24_2020.csv")
+act_file = list(csv.reader(open_act))
+
+open_sat = open("SAT_NEW_HIGHEST_2019_MAR_27_2020.csv")
+sat_file = list(csv.reader(open_sat))
+
+act_headers = act_file[0]
+act_rows = act_file[1:]
+
+sat_headers = sat_file[0]
+sat_rows = sat_file[1:]
+
+act_school_file = []
+act_district_file = []
+act_state_file = []
+
+sat_school_file = []
+sat_district_file = []
+sat_state_file = []
+
+act_state_unique_row = {}
+act_district_unique_row = {}
+
+sat_state_unique_row = {}
+sat_district_unique_row = {}
+
+for row in act_rows:
+    district = row[1]
+    subject = row[6]
+    unique_district = row[1] + row[6]
+    unique_state = subject
+    act_school_file.append(row)
+    if not unique_district in act_district_unique_row:
+        act_district_file.append(row)
+        act_district_unique_row[unique_district] = "written"
+    if not subject in act_state_unique_row:
+        act_state_file.append(row)
+        act_state_unique_row[subject] = "written"
+
+for row in sat_rows:
+    district = row[1]
+    subject = row[6]
+    unique_district = row[1] + row[6]
+    unique_state = subject
+    sat_school_file.append(row)
+    if not unique_district in sat_district_unique_row:
+        sat_district_file.append(row)
+        sat_district_unique_row[unique_district] = "written"
+    if not subject in sat_state_unique_row:
+        sat_state_file.append(row)
+        sat_state_unique_row[subject] = "written"
+
+
+with open("act_school_file.csv", "w") as school_file:
+    wr = csv.writer(school_file, dialect="excel")
+    wr.writerow(act_headers)
+    wr.writerows(act_school_file)
+with open("act_district_file.csv", "w") as district_file:
+    wr = csv.writer(district_file, dialect="excel")
+    wr.writerow(act_headers)
+    wr.writerows(act_district_file)
+with open("act_state_file.csv", "w") as state_file:
+    wr = csv.writer(state_file, dialect="excel")
+    wr.writerow(act_headers)
+    wr.writerows(act_state_file)
+
+with open("sat_school_file.csv", "w") as school_file:
+    wr = csv.writer(school_file, dialect="excel")
+    wr.writerow(sat_headers)
+    wr.writerows(sat_school_file)
+with open("sat_district_file.csv", "w") as district_file:
+    wr = csv.writer(district_file, dialect="excel")
+    wr.writerow(sat_headers)
+    wr.writerows(sat_district_file)
+with open("sat_state_file.csv", "w") as state_file:
+    wr = csv.writer(state_file, dialect="excel")
+    wr.writerow(sat_headers)
+    wr.writerows(sat_state_file)
+
+open_act.close()
+open_sat.close()
---
+++ lib/etl/processors/preprocessors/metrics/HI_CSA_read_in_excel_and_crosswalk_files.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+
+import pandas as pd
+
+# using read_excel to use the excel files directly
+raw_CCRI_Data = pd.read_excel(
+    "CCRI_Data_2016-2017-2018-Public.xlsx", sheet_name="CCRI-2016-2017-2018"
+)
+raw_MasterDataFile = pd.read_excel(
+    "2018-19MasterDataFile20191104Rev.xls", sheet_name="School Data 2019"
+)
+
+
+# Crosswalking to get proper state_ids into the files
+crosswalk_file = pd.read_csv("hi_state_id_map.txt", delimiter="\t")
+
+merged_CCRI_Data = raw_CCRI_Data.merge(
+    right=crosswalk_file, how="left", left_on="SchCode", right_on="bb_state_id"
+)
+merged_MasterDataFile = raw_MasterDataFile.merge(
+    right=crosswalk_file, how="left", left_on="School ID", right_on="bb_state_id"
+)
+
+
+# Transposing and selecting the columns needed
+transposed_CCRI_Data = pd.melt(
+    merged_CCRI_Data,
+    id_vars=[
+        "GradYr",
+        "SchCode",
+        "state_id",
+        "name",
+        "Completers",
+        "NSC_Fall_Count",
+        "UH_Fall",
+    ],
+    value_vars=[
+        "ACT_Taken_Pct",
+        "NSC_Persist_Pct",
+        "UH_Math_RemDev_Pct",
+        "UH_Eng_RemDev_Pct",
+    ],
+)
+
+transposed_MasterDataFile = pd.melt(
+    merged_MasterDataFile,
+    id_vars=[
+        "Year",
+        "state_id",
+        "name",
+        "School Type for Strive HI",
+        "Subgroup Description",
+    ],
+    value_vars=["Graduation Rate (%)", "College Enrollment Rate (%)"],
+)
+
+
+transposed_CCRI_Data.to_csv("transposed_CCRI_Data.txt", index=None, sep="\t")
+transposed_MasterDataFile.to_csv("transposed_MasterDataFile.txt", index=None, sep="\t")
