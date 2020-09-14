34c988acaca7dad2cb27311d9d881f39ad846e84
Andy Luo <aluo@greatschools.org>
2020-09-14 10:52:01 -0700

    JT-11085 first pass at carousel module

---
+++ client/app/bundles/GSWeb/react_components/home/carousel.jsx
@@ -0,0 +1,91 @@
+import React, { useEffect, useState, useCallback } from "react";
+import PropTypes from "prop-types";
+import CarouselDisplay from "react_components/home/carousel_display";
+import ArrowLeft from "react_components/icons/arrow_left";
+import ArrowRight from "react_components/icons/arrow_right";
+import { SM, XS, validSizes, viewport } from "util/viewport";
+import slideshow1 from "home/slideshow_2/01_CSA_Insights_2020_Success_without_Selection.jpg";
+import slideshow2 from "home/slideshow_2/02_GreatSchools_College_Sucess_Awards_Report_Final-1.jpg";
+import slideshow3 from "home/slideshow_2/03_GreatSchools_Transparency_Report_Web.jpg";
+import slideshow4 from "home/slideshow_2/04_Parent_Insights_Supporting_learning_at_home_during_COVID-19.png";
+import slideshow5 from "home/slideshow_2/05_searching_for_opportunity.jpg";
+
+
+const Carousel = ({size}) => {
+
+  const [index, setIndex] = useState(0);
+  const [ direction, setDirection] = useState('right')
+  const translateRight = useCallback(() => {
+    setDirection('right')
+    setIndex(((index - 1) + slideshowUrls.length) % slideshowUrls.length)
+  })
+
+  const translateLeft = useCallback(() => {
+    setDirection("left");
+    setIndex(((index + 1) + slideshowUrls.length) % slideshowUrls.length)
+  })
+
+  const slideshowUrls = [
+    slideshow1,
+    slideshow2,
+    slideshow3,
+    slideshow4,
+    slideshow5,
+  ];
+
+  return (
+    <React.Fragment>
+      <h2>What we've been working on</h2>
+      <p>Greatschools is committed to bringing forth equitable opportunities</p>
+      <div className="module-row">
+        <div>
+          <h3>Finding the Data</h3>
+          <p>A state-by-state assessment of education data transparency
+            <br/>
+            <br/>
+            <a href="/" className="primary-button">Learn More</a>
+          </p>
+        </div>
+        <div className="carousel-container">
+          {size > XS && <CarouselDisplay
+              type="large"
+              size={size}
+              index={index}
+              direction={direction}
+              slideshowUrls={slideshowUrls}
+            />
+          }
+          <div>
+            <div style={{ display: "flex" }}>
+              <CarouselDisplay
+                type="small-left"
+                size={size}
+                index={
+                  (index + 1 + slideshowUrls.length) % slideshowUrls.length
+                }
+                direction={direction}
+                slideshowUrls={slideshowUrls}
+              />
+              <CarouselDisplay
+                type="small-right"
+                size={size}
+                index={
+                  (index + 2 + slideshowUrls.length) % slideshowUrls.length
+                }
+                direction={direction}
+                slideshowUrls={slideshowUrls}
+              />
+            </div>
+            <div className="carousel-buttons">
+              <ArrowLeft onClick={translateLeft} className={'arrow'} />
+              <ArrowRight onClick={translateRight} className={'arrow'} />
+            </div>
+          </div>
+        </div>
+      </div>
+    </React.Fragment>
+  );
+}
+
+
+export default props => <Carousel {...props}/>;
---
+++ client/app/bundles/GSWeb/react_components/home/carousel_display.jsx
@@ -0,0 +1,77 @@
+import React, { useEffect } from "react";
+import PropTypes from "prop-types";
+import { useTransition, animated, config } from "react-spring";
+
+const CarouselDisplay = ({ type, size, index, direction, slideshowUrls }) => {
+
+  const formattedUrls = slideshowUrls.map((url, idx) => ({id: idx, url: url }))
+  
+  const transitionGoingLeft = useTransition(
+    formattedUrls[index],
+    (item) => item.id,
+    {
+      from: { transform: "translate3d(100%,0,0)" },
+      enter: { transform: "translate3d(0%,0,0)" },
+      leave: { transform: "translate3d(-100%,0,0)" },
+      config: config.stiff,
+    }
+  );
+  const transitionGoingRight = useTransition(
+    formattedUrls[index],
+    (item) => item.id,
+    {
+      from: { transform: "translate3d(-100%,0,0)" },
+      enter: { transform: "translate3d(0%,0,0)" },
+      leave: { transform: "translate3d(100%,0,0)" },
+      config: config.stiff,
+    }
+  );
+
+  if (direction == "right") {
+    return (
+      <span className={`carousel-display-${type}`}>
+        {transitionGoingRight.map(({ item, props, key }) => {
+          return (
+            <animated.div
+              key={key}
+              style={{
+                ...props,
+                position: "absolute",
+                backgroundImage: `url(${item.url})`,
+                width: "100%",
+                height: "100%",
+                backgroundRepeat:"no-repeat",
+                backgroundSize:'contain'
+              }}
+            >
+            </animated.div>
+          );
+        })}
+      </span>
+    );
+  } else {
+    return (
+      <span className={`carousel-display-${type}`}>
+        {transitionGoingLeft.map(({ item, props, key }) => {
+          return (
+            <animated.div
+              key={key}
+              style={{
+                ...props,
+                position: "absolute",
+                backgroundImage: `url(${item.url})`,
+                width: "100%",
+                height: "100%",
+                backgroundRepeat: "no-repeat",
+                backgroundSize: "contain",
+              }}
+            >
+            </animated.div>
+          );
+        })}
+      </span>
+    );
+  }
+};
+
+export default CarouselDisplay;
