4ab4d8f73c24a06876441b00c45183424236cf4d
Andy Luo <aluo@greatschools.org>
2020-09-14 12:32:54 -0700

    JT-11085 updated arrows

--- client/app/bundles/GSWeb/react_components/icons/arrow_right.jsx
+++ client/app/bundles/GSWeb/react_components/icons/arrow_right.jsx
@@ -12,7 +12,7 @@ const ArrowRight = (props) => (
     className={props.className}
   >
     <path
-      fill="currentColor"
+      fill={props.color}
       d="M190.5 66.9l22.2-22.2c9.4-9.4 24.6-9.4 33.9 0L441 239c9.4 9.4 9.4 24.6 0 33.9L246.6 467.3c-9.4 9.4-24.6 9.4-33.9 0l-22.2-22.2c-9.5-9.5-9.3-25 .4-34.3L311.4 296H24c-13.3 0-24-10.7-24-24v-32c0-13.3 10.7-24 24-24h287.4L190.9 101.2c-9.8-9.3-10-24.8-.4-34.3z"
     ></path>
   </svg>
@@ -23,7 +23,7 @@ ArrowRight.propTypes = {
   height: PropTypes.string,
   viewBox: PropTypes.string,
   className: PropTypes.string,
-  // color: PropTypes.string,
+  color: PropTypes.string,
 };
 
 ArrowRight.defaultProps = {
@@ -31,7 +31,7 @@ ArrowRight.defaultProps = {
   height: "24",
   viewBox: "0 0 448 512",
   className: '',
-  // color: "#777",
+  color: "#000",
 };
 
 export default ArrowRight;
