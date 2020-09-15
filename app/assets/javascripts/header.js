67598f20a03b559fa1dc1573bd589ecc67749b18
Andy Luo <aluo@greatschools.org>
2020-09-14 17:58:30 -0700

    JT-11085 fix small typo

--- app/assets/javascripts/header.js
+++ app/assets/javascripts/header.js
@@ -1,6 +1,6 @@
 //= require ./header/utils
 //= require ./header/query_param_utils
-// = require ./header/menu
+//= require ./header/menu
 //= require ./header/search_bar
 //= require ./header/language_links
 //= require ./header/header_init
