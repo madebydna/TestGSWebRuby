8b92356f2879eb026297590d2420a7b1083162c7
Osman Afridi <oafridi@greatschools.org>
2020-09-17 10:30:06 -0700

    JT-11019 Add specs, add buttons for filters

--- app/controllers/admin/api/subscriptions_controller.rb
+++ app/controllers/admin/api/subscriptions_controller.rb
@@ -1,23 +1,23 @@
 class Admin::Api::SubscriptionsController < ApplicationController
 
   def index
-    @subscriptions = Api::Subscription.includes(:user)
+    @subscriptions = Api::Subscription.all
   end
 
   def pending_approval
-    @subscriptions = Api::Subscription.pending_approval.includes(:user)
+    @subscriptions = Api::Subscription.pending_approval
   end
 
   def show
-    @subscription = Api::Subscription.where(params[:id]).first
+    @subscription = Api::Subscription.find(params[:id])
   end
 
   def edit
-    @subscription = Api::Subscription.where(params[:id]).first
+    @subscription = Api::Subscription.find(params[:id])
   end
 
   def update
-    subscription = Api::Subscription.where(params[:id]).first
+    subscription = Api::Subscription.find(params[:id])
     subscription.update!(subscription_params)
   end
 
---
+++ spec/services/api/account_approver_spec.rb
@@ -0,0 +1,9 @@
+require "spec_helper"
+
+describe Api::AccountApprover do
+
+  describe '#approve' do
+
+  end
+
+end
