c1a43e78c93c8d386214e8bb044e1ef2c734726c
Alexina Chong <achong@greatschools.org>
2020-09-24 16:09:12 -0700

    JT-11129 Remove state exception conditionals

--- app/controllers/concerns/rating_methodology_selector.rb
+++ app/controllers/concerns/rating_methodology_selector.rb
@@ -1,20 +1,16 @@
 module RatingMethodologySelector
   extend ActiveSupport::Concern
 
-  STATE_EXCEPTIONS = %w(in nd)
-
   included do
     helper_method :ratings_link, :path_to_yaml
   end
 
-  # JT-11010: We want to use the new copy ("*_alt") with the old link (ratings_path) for most states.
-  # The exception states (IN & ND) should use the old copy with the new link (ratings_alt_path).
   def ratings_link_english
-    STATE_EXCEPTIONS.exclude?(state.downcase) ? ratings_path : ratings_alt_path
+    ratings_path
   end
 
   def ratings_link_spanish
-    STATE_EXCEPTIONS.exclude?(state.downcase) ? ratings_spanish_path : ratings_alt_path
+    ratings_spanish_path
   end
 
   def ratings_link
@@ -26,6 +22,6 @@ module RatingMethodologySelector
   end
 
   def path_to_yaml(context)
-    STATE_EXCEPTIONS.exclude?(state.downcase) ? "#{context}_alt" : context
+    context
   end
 end
