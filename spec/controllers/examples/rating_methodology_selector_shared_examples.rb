1084990672c9d33220e7d7e8c270f17211b693c6
Alexina Chong <achong@greatschools.org>
2020-09-24 16:45:36 -0700

    JT-11129 Remove unnecessary specs

--- spec/controllers/examples/rating_methodology_selector_shared_examples.rb
+++ spec/controllers/examples/rating_methodology_selector_shared_examples.rb
@@ -30,25 +30,5 @@ RSpec.shared_examples "#ratings_link" do |extra_params|
         expect(subject.ratings_link).to eq('/gk/como-clasificamos/?lang=es')
       end
     end
-
-    context 'with traditional ratings state and English' do
-      before do
-        get :show, { state: 'indiana' }.merge(extra_params)
-      end
-
-      it 'should have the old English ratings link' do
-        expect(subject.ratings_link).to eq('/gk/ratings-in-nd/')
-      end
-    end
-
-    context 'with traditional ratings state and Spanish' do
-      before do
-        get :show, { state: 'indiana', lang: 'es' }.merge(extra_params)
-      end
-
-      it 'should have the old Spanish ratings link' do
-        expect(subject.ratings_link).to eq('/gk/ratings-in-nd/?lang=es')
-      end
-    end
   end
 end
